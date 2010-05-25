4974d78932bcc3cefdea50b04fba1adfe8792132
nitram <nitram@illarion.org>
2010-05-25 17:04:15 +0000

    Cleanup

--- I_2608_buch.lua
+++
@@ -1,9 +0,0 @@
---Buch 2608
-function UseItem(User, SourceItem, TargetItem, Counter, Param)
-  User:inform("Hallo Welt")
-end  
-
-
-
-                                                               
-                                                               
--- I_264_painting.lua
+++
@@ -1,4 +0,0 @@
-dofile("base_painting_lookat.lua");
-function LookAtItem(User,Item)
-    LookAtPaintingItem(User,Item);
-end
