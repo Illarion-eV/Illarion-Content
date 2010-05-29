b2fa68c30b7cd793f54544f339ea73c640d60bdb
martin <martin@illarion.org>
2010-05-29 10:04:09 +0000

    used the better bridge-graphics as drawbridges.

--- handler/deletebridge.lua
+++ handler/deletebridge.lua
@@ -2,14 +2,14 @@ require("base.class")
 
 module("handler.deletebridge", package.seeall);
 
-deleteBridge = base.class.class(function(delbrg, posi)
+deleteBridge = class(function(delbrg, posi)
     delbrg.pos=posi;
 end);
 
 function deleteBridge:execute()
     if (world:isItemOnField(self.pos)==true) then
         item=world:getItemOnField(self.pos);
-        if(item.id==601 or item.id==606 or item.id==607 or item.id==612) then
+        if(item.id==618 or item.id==621 or item.id==614 or item.id==617) then
             t=0;
             if item.data==1 then
                 keepGoing=1;
@@ -17,7 +17,7 @@ function deleteBridge:execute()
                     delPos=position(self.pos.x,self.pos.y-t,self.pos.z)
                     if (world:isItemOnField(delPos)==true) then
                         item=world:getItemOnField(delPos);
-                        if (item.data==1 and (item.id==612 or item.id==609 or item.id==607)) then
+                        if (item.data==1 and (item.id==617 or item.id==615 or item.id==614)) then
                             world:erase(item,1);
                             t=t+1;
                         else
@@ -33,7 +33,7 @@ function deleteBridge:execute()
                     delPos=position(self.pos.x+t,self.pos.y,self.pos.z)
                     if (world:isItemOnField(delPos)==true) then
                         item=world:getItemOnField(delPos);
-                        if (item.data==2 and (item.id==606 or item.id==603 or item.id==601)) then
+                        if (item.data==2 and (item.id==621 or item.id==619 or item.id==618)) then
                             world:erase(item,1);
                             t=t+1;
                         else
@@ -49,7 +49,7 @@ function deleteBridge:execute()
                     delPos=position(self.pos.x,self.pos.y+t,self.pos.z)
                     if (world:isItemOnField(delPos)==true) then
                         item=world:getItemOnField(delPos);
-                        if (item.data==3 and (item.id==612 or item.id==609 or item.id==607)) then
+                        if (item.data==3 and (item.id==617 or item.id==615 or item.id==614)) then
                             world:erase(item,1);
                             t=t+1;
                         else
@@ -65,7 +65,7 @@ function deleteBridge:execute()
                     delPos=position(self.pos.x-t,self.pos.y,self.pos.z)
                     if (world:isItemOnField(delPos)==true) then
                         item=world:getItemOnField(delPos);
-                        if (item.data==4 and (item.id==606 or item.id==603 or item.id==601)) then
+                        if (item.data==4 and (item.id==621 or item.id==619 or item.id==618)) then
                             world:erase(item,1);
                             t=t+1;
                         else
