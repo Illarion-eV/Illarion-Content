49919c4b18fe8a5e698433c6247113eff9b5b432
vilarion <vilarion@illarion.org>
2010-06-04 14:00:58 +0000

    testing globals

--- item/id_2_flour.lua
+++ item/id_2_flour.lua
@@ -6,8 +6,13 @@ require "bla.blubb"
 
 module("item.id_2_flour", package.seeall)
 
+globtest1 = 7;
+
 function UseItem( User, SourceItem, TargetItem, counter, Param, ltstate )
-    User:inform("package.cpath: "..package.cpath);
-    User:inform("package.path: "..package.path);
+    globtest2 = 9;
+    User:inform("globtest1a: "..item.id_2_flour.globtest1);
+    User:inform("globtest2a: "..item.id_2_flour.globtest2);
+    User:inform("globtest1b: ".._G.globtest1);
+    User:inform("globtest2b: ".._G.globtest2);
     bla.blubb.Success(User);
 end
