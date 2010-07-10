9a555292a2936305b61ea403a1ec8105963a2792
estralis <estralis@illarion.org>
2010-07-10 08:18:44 +0000

    Some new/finished quest definitions.

--- monster/base/quests.lua
+++ monster/base/quests.lua
@@ -59,12 +59,40 @@ function iniQuests()
     questList[84]={}; --wolves
     questList[85]={}; --wolves
     questList[35]={}; --elven hunter
+    questList[4]={}; --human necromancer
+    questList[173]={}; --demon skeleton warrior
+    questList[264]={}; --ancient fire dragon
+    questList[131]={}; --flies
+    questList[132]={}; --flies
+    questList[133]={}; --flies
+    questList[134]={}; --flies
+    questList[135]={}; --flies
+    questList[23]={}; --halfling thief
 
     --Quest 1: Slay 10 mummies (101); Reserved for Galmair Sewers I (77)
     
     local id=1;
     germanTitle[id]="Die Kanalisation von Galmair I"; 
     englishTitle[id]="Galmair Sewers I";
+    NPCName[id]="Miggs"; 
+    statusId[id]=77;
+    germanRace[id]="Schmeiﬂfliegen"; 
+    englishRace[id]="dirty flies";
+    table.insert(questList[131],id); 
+    table.insert(questList[132],id); 
+    table.insert(questList[133],id); 
+    table.insert(questList[134],id); 
+    table.insert(questList[135],id); 
+    minimumQueststatus[id]=1; 
+    maximumQueststatus[id]=11; 
+    questLocation[id]=position(0,0,0); 
+    radius[id]=50; 
+
+    --Quest 2: Reserved for Galmair Sewers II (77)
+
+    local id=1;
+    germanTitle[id]="Die Kanalisation von Galmair II"; 
+    englishTitle[id]="Galmair Sewers II";
     NPCName[id]="Miggs"; 
     statusId[id]=77;
     germanRace[id]="stinkige Gullimumien"; 
@@ -76,13 +104,25 @@ function iniQuests()
     table.insert(questList[105],id); 
     table.insert(questList[106],id);    
     table.insert(questList[107],id); 
-    minimumQueststatus[id]=1; 
-    maximumQueststatus[id]=11; 
+    minimumQueststatus[id]=13; 
+    maximumQueststatus[id]=18; 
     questLocation[id]=position(0,0,0); 
     radius[id]=50; 
 
-    --Quest 2: Reserved for Galmair Sewers II (77)
     --Quest 3: Reserved for Galmair Sewers III (77)
+
+    local id=1;
+    germanTitle[id]="Die Kanalisation von Galmair III"; 
+    englishTitle[id]="Galmair Sewers III";
+    NPCName[id]="Miggs"; 
+    statusId[id]=77;
+    germanRace[id]="Halblingdiebe"; 
+    englishRace[id]="halfling thieves";
+    table.insert(questList[23],id); 
+    minimumQueststatus[id]=20; 
+    maximumQueststatus[id]=21; 
+    questLocation[id]=position(0,0,0); 
+    radius[id]=50; 
 
     --Quest 4: Reserved for Sir Reginald's Tomb I (78)
     --Quest 5: Reserved for Sir Reginald's Tomb II (78)
@@ -104,8 +144,8 @@ function iniQuests()
     table.insert(questList[295],id); 
     minimumQueststatus[id]=1; 
     maximumQueststatus[id]=7; 
-    questLocation[id]=position(0,0,0); 
-    radius[id]=50; 
+    questLocation[id]=position(775,475,0); 
+    radius[id]=75; 
 
     --Quest 11: The Poacher II (4)
 
@@ -119,8 +159,8 @@ function iniQuests()
     table.insert(questList[294],id); 
     minimumQueststatus[id]=9; 
     maximumQueststatus[id]=21; 
-    questLocation[id]=position(0,0,0); 
-    radius[id]=50; 
+    questLocation[id]=position(775,475,0); 
+    radius[id]=75; 
 
     --Quest 12: The Poacher III (4)
 
@@ -138,8 +178,8 @@ function iniQuests()
     table.insert(questList[85],id); 
     minimumQueststatus[id]=23; 
     maximumQueststatus[id]=29; 
-    questLocation[id]=position(0,0,0); 
-    radius[id]=50; 
+    questLocation[id]=position(775,475,0); 
+    radius[id]=75; 
 
     --Quest 13: The Poacher IV (4)
 
@@ -153,8 +193,8 @@ function iniQuests()
     table.insert(questList[35],id); 
     minimumQueststatus[id]=30; 
     maximumQueststatus[id]=31; 
-    questLocation[id]=position(0,0,0); 
-    radius[id]=50; 
+    questLocation[id]=position(775,475,0); 
+    radius[id]=75; 
 
     --Quest 14: The Craven Orc II (3)
 
@@ -174,8 +214,8 @@ function iniQuests()
     table.insert(questList[107],id); 
     minimumQueststatus[id]=3; 
     maximumQueststatus[id]=8; 
-    questLocation[id]=position(0,0,0); 
-    radius[id]=50; 
+    questLocation[id]=position(665,415,0); 
+    radius[id]=35; 
 
     --Quest 15: The Craven Orc III (3)
 
@@ -193,8 +233,93 @@ function iniQuests()
     table.insert(questList[115],id); 
     minimumQueststatus[id]=10; 
     maximumQueststatus[id]=20; 
-    questLocation[id]=position(0,0,0); 
-    radius[id]=50; 
+    questLocation[id]=position(665,415,0); 
+    radius[id]=35; 
+
+    --Quest 16: The Errant Crusade II
+
+    local id=16;
+    germanTitle[id]="The Errant Crusade II"; 
+    englishTitle[id]="Der fehlgeleitete Kreuzzug II";
+    NPCName[id]="Arenius Batavius"; 
+    statusId[id]=104;
+    germanRace[id]="mummies"; 
+    englishRace[id]="Mumien";
+    table.insert(questList[101],id); 
+    table.insert(questList[102],id); 
+    table.insert(questList[103],id); 
+    table.insert(questList[104],id); 
+    table.insert(questList[105],id); 
+    table.insert(questList[106],id);    
+    table.insert(questList[107],id);
+    minimumQueststatus[id]=3; 
+    maximumQueststatus[id]=9; 
+    questLocation[id]=position(665,415,-3); 
+    radius[id]=100; 
+
+    --Quest 17: The Errant Crusade III
+
+    local id=17;
+    germanTitle[id]="The Errant Crusade III"; 
+    englishTitle[id]="Der fehlgeleitete Kreuzzug III";
+    NPCName[id]="Arenius Batavius"; 
+    statusId[id]=104;
+    germanRace[id]="skeletons"; 
+    englishRace[id]="Skelette";
+    table.insert(questList[111],id); 
+    table.insert(questList[112],id); 
+    table.insert(questList[113],id); 
+    table.insert(questList[114],id); 
+    table.insert(questList[115],id); 
+    minimumQueststatus[id]=11; 
+    maximumQueststatus[id]=23; 
+    questLocation[id]=position(665,415,-3); 
+    radius[id]=100;
+
+    --Quest 18: The Errant Crusade IV
+
+    local id=18;
+    germanTitle[id]="The Errant Crusade IV"; 
+    englishTitle[id]="Der fehlgeleitete Kreuzzug IV";
+    NPCName[id]="Arenius Batavius"; 
+    statusId[id]=104;
+    germanRace[id]="blood mages"; 
+    englishRace[id]="Blutmagier";
+    table.insert(questList[4],id); 
+    minimumQueststatus[id]=26; 
+    maximumQueststatus[id]=28; 
+    questLocation[id]=position(665,415,-3); 
+    radius[id]=100;
+
+    --Quest 19: The Errant Crusade V
+
+    local id=19;
+    germanTitle[id]="The Errant Crusade V"; 
+    englishTitle[id]="Der fehlgeleitete Kreuzzug V";
+    NPCName[id]="Arenius Batavius"; 
+    statusId[id]=104;
+    germanRace[id]="demon skeleton warrior"; 
+    englishRace[id]="D‰monenskelettkrieger";
+    table.insert(questList[173],id); 
+    minimumQueststatus[id]=30; 
+    maximumQueststatus[id]=31; 
+    questLocation[id]=position(665,415,-3); 
+    radius[id]=100;
+
+    --Quest 20: The Errant Crusade VI
+
+    local id=20;
+    germanTitle[id]="The Errant Crusade VI"; 
+    englishTitle[id]="Der fehlgeleitete Kreuzzug VI";
+    NPCName[id]="Arenius Batavius"; 
+    statusId[id]=104;
+    germanRace[id]="ancient fire dragons"; 
+    englishRace[id]="uralter Feuerdrachen";
+    table.insert(questList[264],id); 
+    minimumQueststatus[id]=33; 
+    maximumQueststatus[id]=34; 
+    questLocation[id]=position(665,415,-3); 
+    radius[id]=100;
 
 end
 
