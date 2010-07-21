1687ca4ff7e58238e63fbb1a123819fccb3f1870
blay <blay@illarion.org>
2010-07-21 08:46:52 +0000

    New druid rune (combine gem), removed the possibility to combine gems at altars

--- item/altars.lua
+++ item/altars.lua
@@ -42,87 +42,33 @@ function LookAtItem( User, Item )
 end
 
 function UseItem(User, SourceItem, TargetItem, Counter, param)
-    -- An welchem Altar stehe ich und was brauche ich da f�r Edelsteine?
-    --
-    if SourceItem.data==1 then PosGem={45,283} 		-- Ushara   -> Smaragd, Schwarzstein        -> Data 1 (45,283)
-    elseif SourceItem.data==2 then PosGem={46,283} 	-- Bragon   -> Rubin, Schwarzstein          -> Data 2 (46,283)
-    elseif SourceItem.data==3 then PosGem={285,0} 	-- Eldan    -> Diamant                      -> Data 3 (285)
-    elseif SourceItem.data==4 then PosGem={284,197} 	-- Tanora   -> Blaustein, Amethyst          -> Data 4 (284, 197)
-    elseif SourceItem.data==5 then PosGem={198,197} 	-- Findari  -> Topas, Amethyst              -> Data 5 (198, 197)
-    else PosGem={0,0} end
-	
-
-    -- Habe ich die richtigen Edelsteine und die richtige Anzahl in der Hand?
-    --Wenn ja, dann Edelsteine upgraden
-    --
-    itemL = User:getItemAt(5);
-    itemR = User:getItemAt(6);
-
-    if ( itemL~=nil and ((itemL.id==PosGem[1]) or (itemL.id==PosGem[2])) and itemL.data>0 and itemL.data<10 and itemL.number==3) then
-		-- Werden die Steine zerst�rt?
-		--
-		if destroyGem(itemL.data) then
-			world:gfx(5,SourceItem.pos);
-			world:gfx(37,SourceItem.pos);
-			world:makeSound(24,SourceItem.pos);
-			itemL.data = itemL.data + 1;
-			itemL.number=itemL.number-2;
-			world:changeItem( itemL );
-		else 
-			world:gfx(1,SourceItem.pos);
-			world:makeSound(18,SourceItem.pos);
-			User:inform("Die Edelsteine zerfallen zu Staub. Das hat wohl nicht geklappt...");
-			world:erase(itemL,3);
-			--itemL.number=itemL.number-3;
-			--world:changeItem( itemL );
-		end
-    elseif ( itemR~=nil and ((itemR.id==PosGem[1]) or (itemR.id==PosGem[2])) and itemR.data>0 and itemR.data<10 and itemR.number==3) then
-		-- Werden die Steine zerst�rt?
-        --
-		if destroyGem(itemR.data) then
-			world:gfx(5,SourceItem.pos);
-            world:gfx(37,SourceItem.pos);
-			world:makeSound(24,SourceItem.pos);
-			itemR.data = itemR.data + 1;
-			itemR.number=itemR.number-2;
-			world:changeItem( itemR );
-		else
-			world:gfx(1,SourceItem.pos);
-			world:makeSound(18,SourceItem.pos)
-			User:inform("Die Edelsteine zerfallen zu Staub. Das hat wohl nicht geklappt...");
-			world:erase(itemR,3);
-            --itemR.number=itemR.number-3;
-            --world:changeItem( itemR );
-		end
-    else 
-		god_feel=(math.random(1,50));
-		if god_feel==1 then
-		    base.common.InformNLS(User,
-                "Du f�hlst dich ein wenig wohler.",
-                "You feel better now.");
-		elseif god_feel==2 then
-		    base.common.InformNLS(User,
-                "Der Stein wird warm unter denen H�nden.",
-    	        "The stone become warmer under your hands.");
-		elseif god_feel==3 then
-		    base.common.InformNLS(User,
-                "Du f�hlst dich gl�cklicher.",
-                "You feel more happy.");
-		elseif god_feel==4 then
-		    base.common.InformNLS(User,
-                "Du f�hlst dich spirituell gest�rkt.",
-                "You feel spirituel stronger now.");
-		elseif god_feel==5 then
-		    base.common.InformNLS(User,
-                "Du h�rst ein verr�cktes Lachen in der Ferne.",
-                "You hear a maniac laughter far away.");
-			world:makeSound(25,SourceItem.pos)
-		else
-		    base.common.InformNLS(User,
-            	"Nichts passiert.",
-            	"Nothing happens.");
-		end
-    end
+	god_feel=(math.random(1,50));
+	if god_feel==1 then
+	    base.common.InformNLS(User,
+            "Du f�hlst dich ein wenig wohler.",
+            "You feel better now.");
+	elseif god_feel==2 then
+	    base.common.InformNLS(User,
+            "Der Stein wird warm unter denen H�nden.",
+    	    "The stone become warmer under your hands.");
+	elseif god_feel==3 then
+	    base.common.InformNLS(User,
+            "Du fühlst dich gl�cklicher.",
+            "You feel more happy.");
+	elseif god_feel==4 then
+	    base.common.InformNLS(User,
+            "Du fühlst dich spirituell gest�rkt.",
+            "You feel spirituel stronger now.");
+	elseif god_feel==5 then
+		base.common.InformNLS(User,
+			"Du hörst ein verr�cktes Lachen in der Ferne.",
+			"You hear a maniac laughter far away.");
+		world:makeSound(25,SourceItem.pos)
+	else
+		base.common.InformNLS(User,
+			"Nichts passiert.",
+			"Nothing happens.");
+	end
 	-- User:inform("moep-> "..PosGem[1]);
 	-- User:inform("moep-> "..PosGem[2]);
 
@@ -130,31 +76,4 @@ function UseItem(User, SourceItem, TargetItem, Counter, param)
 		Intelligence=User:increaseAttrib("intelligence",0);
 		User:inform("#w Deine Intelligenz ist: "..Intelligence);
 	end 
-end
-
-function destroyGem (gemData)
-	-- 1 -> 2 =  0% fail
-	-- 2 -> 3 =  0% fail
-	-- 3 -> 4 =  0% fail
-	-- 4 -> 5 = 10% fail
-	-- 5 -> 6 = 20% fail
-	-- 6 -> 7 = 30% fail
-	-- 7 -> 8 = 40% fail
-	-- 8 -> 9 = 50% fail
-	-- 9 ->10 = 60% fail
-	if ( (gemData==4) and (math.random(1,10)<=1) ) then
-		return false;
-	elseif ( (gemData==5) and (math.random(1,10)<=2) ) then
-		return false;
-	elseif ( (gemData==6) and (math.random(1,10)<=3) ) then
-		return false;
-	elseif ( (gemData==7) and (math.random(1,10)<=4) ) then
-		return false;
-	elseif ( (gemData==8) and (math.random(1,10)<=5) ) then
-		return false;
-	elseif ( (gemData==9) and (math.random(1,10)<=6) ) then
-		return false;
-	else
-		return true;
-	end
 end
