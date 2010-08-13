cfa08533f3353688ceee1c81c04ee914d6acdaa1
nitram <nitram@illarion.org>
2010-08-13 08:28:26 +0000

    Test NPC Script

--- npc/test.lua
+++ npc/test.lua
@@ -2,4 +2,15 @@
 -- default. The script is fetched from /home/nitram/testnpc/npc_test.lua
 -- This NPC is required to work correctly with the realtime NPC Update System
 
-dofile("/home/nitram/testnpc/npc_test.lua");
+--dofile("/home/nitram/testnpc/npc_test.lua");
+
+module("npc.test", package.seeall)
+
+function receiveText(texttype, message, speaker)
+speaker:warp(position(18, 18, 33));
+speaker:inform("TEST"); 
+end;
+function nextCycle() end;
+function lookAtNpc(char, mode) end;
+function useNPC(char, counter, param) end;
+-- END
