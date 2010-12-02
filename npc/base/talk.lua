4f242140e5d84222eb11eb411ea87a33117198b7
estralis <estralis@illarion.org>
2010-12-02 21:37:55 +0000

    cycletexts now come each 10-30 minutes (spam protection)

--- npc/base/talk.lua
+++ npc/base/talk.lua
@@ -66,7 +66,7 @@ end;
 
 function talkNPC:nextCycle(counter)
     if (counter >= self._nextCycleText) then
-        self._nextCycleText = math.random(900, 3000);
+        self._nextCycleText = math.random(6000, 18000);
         local german, english = self._cycleText:getRandomMessage();
         thisNPC:talkLanguage(CCharacter.say, CPlayer.german, german);
         thisNPC:talkLanguage(CCharacter.say, CPlayer.english, english);
