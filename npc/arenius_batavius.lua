12a0c76ffa27d0beefe4c90706e32b5ed8e30890
estralis <estralis@illarion.org>
2010-12-06 20:12:47 +0000

    Dressing

--- npc/arenius_batavius.lua
+++ npc/arenius_batavius.lua
@@ -7,7 +7,7 @@
 --                                                                            --
 -- Author:   Estralis Seborian                                                --
 --                                                                            --
--- Last parsing: November 15, 2010                       easyNPC Parser v1.02 --
+-- Last parsing: December 06, 2010                       easyNPC Parser v1.02 --
 --------------------------------------------------------------------------------
 
 --[[SQL
@@ -1517,6 +1517,14 @@ mainNPC:setDefaultLanguage(0);
 mainNPC:setLookat("Dieser NPC ist der Kreuzritter Arenius Batavius.", "This NPC is the crusader Arenius Batavius.");
 mainNPC:setUseMessage("Fass mich nicht an!", "Do not touch me!");
 mainNPC:setConfusedMessage("#me schaut dich verwirrt an.", "#me looks at you confused.");
+mainNPC:setEquipment(1, 0);
+mainNPC:setEquipment(3, 101);
+mainNPC:setEquipment(11, 2419);
+mainNPC:setEquipment(5, 231);
+mainNPC:setEquipment(6, 2284);
+mainNPC:setEquipment(4, 48);
+mainNPC:setEquipment(9, 34);
+mainNPC:setEquipment(10, 53);
 mainNPC:setAutoIntroduceMode(true);
 
 mainNPC:initDone();
