d7e8a114493d1d2c600ffe81f2999b4c3be7c3f3
vilarion <vilarion@illarion.org>
2011-01-08 12:45:46 +0000

    testing world:getMonstersInRangeOf

--- test/vilarion.lua
+++ test/vilarion.lua
@@ -7,8 +7,8 @@ function UseItem( User, SourceItem, TargetItem, counter, Param, ltstate )
         User:inform("e-Vil says: You are not allowed to use this!");
         return;
     end;
-    User:inform("performing world:getMonstersInRangeOf(position(0,0,0),2)");
-    local moblist = world:getMonstersInRangeOf(position(0,0,0),2);
+    User:inform("performing world:getMonstersInRangeOf(position(0,0,0),8)");
+    local moblist = world:getMonstersInRangeOf(position(0,0,0),8);
     User:inform("Type of moblist: "..type(moblist).."; Size of moblist: "..#moblist);
 --    world:createDynamicNPC("Test1",50,position(User.pos.x-1,User.pos.y,User.pos.z),0,"test.vilarion");
 --    world:createDynamicNPC("Test2",1,position(User.pos.x+1,User.pos.y,User.pos.z),0,"test.vilarion");
