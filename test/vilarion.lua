0cc6cc4f35191b8ec9b58c9465c0812c061707ec
vilarion <vilarion@illarion.org>
2011-01-14 14:40:58 +0000

    testing utf-8 server source

--- test/vilarion.lua
+++ test/vilarion.lua
@@ -7,9 +7,9 @@ function UseItem( User, SourceItem, TargetItem, counter, Param, ltstate )
         User:inform("e-Vil says: You are not allowed to use this!");
         return;
     end;
-    User:inform("performing world:getMonstersInRangeOf(position(0,0,0),8)");
-    local moblist = world:getMonstersInRangeOf(position(0,0,0),8);
-    User:inform("Type of moblist: "..type(moblist).."; Size of moblist: "..#moblist);
+    SourceItem.quality = 90;
+    world:changeItem(SourceItem);
+    User:inform("");
 --    world:createDynamicNPC("Test1",50,position(User.pos.x-1,User.pos.y,User.pos.z),0,"test.vilarion");
 --    world:createDynamicNPC("Test2",1,position(User.pos.x+1,User.pos.y,User.pos.z),0,"test.vilarion");
 end
