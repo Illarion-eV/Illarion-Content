d0db3e6d3d2461f5f16133e7a13abe66534a19c3
vilarion <vilarion@illarion.org>
2011-02-22 20:22:22 +0000

    testing talk

--- test/vilarion.lua
+++ test/vilarion.lua
@@ -8,25 +8,7 @@ function UseItem( User, SourceItem, TargetItem, counter, Param, ltstate )
         return;
     end;
 
-    local found, item = User:getBackPack():viewItemNr(2);
-    item.id = 100;
-    world:changeItem(item);
-
-    --[[    
-    if counter == 1 then
-        local found, value;
-        found, value = ScriptVars:find("fancyValue");
-        if found then
-            User:inform( "fancyValue: "..value );
-        else
-            User:inform( "fancyValue is nil" );
-        end;
-    else
-        ScriptVars:set("fancyValue", counter);
-    end;
-    --]]
-
-    -- world:createDynamicNPC("Test1",1,position(User.pos.x-1,User.pos.y,User.pos.z),0,"test.vilarion");
+    world:createDynamicNPC("Test1",1,position(User.pos.x-1,User.pos.y,User.pos.z),0,"test.vilarion");
 
 end
 
@@ -35,6 +17,15 @@ function useNPC(User,counter,param)
 end
 
 function nextCycle()  -- ~10 times per second
+    thisNPC:talk(CCharacter.say, "TALKTEST");
+    if test.vilarion.user then
+        test.vilarion.counter = test.vilarion.counter + 1;
+        if test.vilarion.counter > 100 then
+            thisNPC:talk(CCharacter.say, "TALKTEST");
+            test.vilarion.counter = 0;
+        end;
+    end;
+--[[
     if test.vilarion.user then
         test.vilarion.counter = test.vilarion.counter + 1;
         if test.vilarion.counter > 100 then
@@ -42,6 +33,7 @@ function nextCycle()  -- ~10 times per second
             test.vilarion.user = nil;
         end;
     end;
+--]]
 end
 
 function receiveText(texttype, message, originator)
