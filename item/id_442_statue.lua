d37820ce59ede5792ceb17b0bf429f1c46cfc3ab
estralis <estralis@illarion.org>
2011-03-07 19:50:28 +0000

    Issue #6265

---
+++ item/id_442_statue.lua
@@ -0,0 +1,9 @@
+require("base.common")
+
+module("item.id_442_statue", package.seeall)
+
+-- UPDATE common SET com_script='item.id_442_statue' WHERE com_itemid = 442;
+
+function LookAtItem(User,Item)
+        base.common.InformNLS(User,"GERMAN!","ENGLISH!");
+end;
