2c5164eff78a20d8ee347fbd0fb4245ec9ae1783
vilarion <vilarion@illarion.org>
2011-04-13 13:55:31 +0000

    adding framework for scriptside learning

---
+++ server/learn.lua
@@ -0,0 +1,15 @@
+module("server.learn", package.seeall)
+
+-- called by the server when user:learn(...) is issued by a script
+
+function learn( user, skill, group, actionpoints, opponent, leadAttribute )
+
+end
+
+
+-- invoked every 10s on every user; to be used to reduce MC on a regular basis
+-- user:idleTime() can be used to get the number of seconds a user has been idle to check for inactivity
+
+function reduceMC( user )
+
+end
