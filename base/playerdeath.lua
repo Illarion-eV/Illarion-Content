3a078af4c68f0b7938989fd5c2a9eee499d0c95b
estralis <estralis@illarion.org>
2011-05-20 19:19:26 +0000

    SFX upon playerdeath

--- base/playerdeath.lua
+++ base/playerdeath.lua
@@ -3,16 +3,19 @@ module("base.playerdeath", package.seeall)
 
 -- player got killed by direct a magic spell (summoned flames are no direct magic spells)
 function playerKilledByMagic(Player, Killer)
+world:makeSound(1,Player.pos);
 base.common.TempInformNLS( Player,"[Tod] Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.","[Death] You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.");
 end
 
 -- player got killed by another player / monster
 function playerKilledByFighting(Player, Killer)
+world:makeSound(1,Player.pos);
 base.common.TempInformNLS( Player,"[Tod] Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.","[Death] You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.");
 end
 
 -- death reason unknown
 function playerKilled(Player)
+world:makeSound(1,Player.pos);
 base.common.TempInformNLS( Player,"[Tod] Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.","[Death] You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.");
 end
 
@@ -21,7 +24,7 @@ function monsterKilledByFighting(MonsterID, Killer)
 
 end
 
--- player got killed by direct a magic spell (summoned flames are no direct magic spells)
+-- monster got killed by direct magic spell (summoned flames are no direct magic spells)
 function monsterKilledByMagic(MonsterID, Killer)
 
 end
