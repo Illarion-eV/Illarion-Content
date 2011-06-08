a6c25fd7cd4144e1f7b0e54732507b9e387e9793
martin <martin@illarion.org>
2011-06-08 21:31:18 +0000

    Explorers guild

--- item/id_66_markerstone.lua
+++ item/id_66_markerstone.lua
@@ -8,7 +8,7 @@ function CheckStone(Char,StoneNumber)
     retVal=false;
     StoneBase=math.floor((StoneNumber-1)/32);  -- Stone 0 to 31 -> 0, 32-.. ->2 etc.
     StoneBaseOffset=math.mod(StoneNumber-1,32);  -- StoneNr inside range
-    HasStones=Char:getQuestProgress(10+StoneBase);
+    HasStones=Char:getQuestProgress(130+StoneBase);
     GotStone=LuaAnd(2^(StoneNumber-1),HasStones);
     if GotStone>0 then
         retVal=true;
