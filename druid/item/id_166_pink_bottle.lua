9a3e79ed0b89b5bb05936a90e3c579bce6aab1b1
merung <karten-spiel-haus@hotmail.de>
2011-07-22 23:07:31 +0200

    No "CCharacter" anymore

--- druid/item/id_166_pink_bottle.lua
+++ druid/item/id_166_pink_bottle.lua
@@ -1,9 +1,9 @@
 module("druid.item.id_166_pink_bottle", package.seeall)
 
 function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )  -- DONT EDIT THIS LINE!
-    User:talk(CCharacter.say,"Using");
-    User:talk(CCharacter.say,"Ich setze auf: "..User.lastSpokenText);
+    User:talk(Character.say,"Using");
+    User:talk(Character.say,"Ich setze auf: "..User.lastSpokenText);
     SourceItem:setData("test",User.lastSpokenText);
-    User:talk(CCharacter.say,"gesetzt auf: "..SourceItem:getData("test"));
+    User:talk(Character.say,"gesetzt auf: "..SourceItem:getData("test"));
 end
 	
