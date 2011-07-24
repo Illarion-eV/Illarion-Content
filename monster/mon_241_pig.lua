3d02ee5179b9383445f36237d2c5b7ceb91dd77f
martin <martin.polak@gmx.at>
2011-07-24 21:42:48 +0200

    Color

--- monster/mon_241_pig.lua
+++ monster/mon_241_pig.lua
@@ -2,10 +2,11 @@
 module("monster.mon_241_pig", package.seeall)
 
 function onSpawn(thisPig)
+    var=30;
     red,green,blue=thisPig:getSkinColor();
-    red=math.min(255,red-60+random(120));
-    green=math.min(255,green-60+random(120));
-    blue=math.min(255,blue-60+random(120));
+    red=math.min(255,red-var+math.random(2*var));
+    green=math.min(255,green-var+math.random(2*var));
+    blue=math.min(255,blue-var+math.random(2*var));
     
     thisPig:setSkinColor(red,green,blue);
 end
