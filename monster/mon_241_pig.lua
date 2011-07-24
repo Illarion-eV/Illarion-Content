ba670d37cfc858f6fffb0e384dfe75112d7ac152
martin <martin.polak@gmx.at>
2011-07-24 21:39:21 +0200

    Test color monster

--- monster/mon_241_pig.lua
+++ monster/mon_241_pig.lua
@@ -3,8 +3,12 @@ module("monster.mon_241_pig", package.seeall)
 
 function onSpawn(thisPig)
     red,green,blue=thisPig:getSkinColor();
-    red=red-30+random(60);
-    green=green-30+random(60);
-    blue=blue-30+random(60);
+    --red=red-30+random(60);
+    --green=green-30+random(60);
+    --blue=blue-30+random(60);
+    red=100;
+    green=40;
+    blue=40;
+    
     thisPig:setSkinColor(red,green,blue);
 end
