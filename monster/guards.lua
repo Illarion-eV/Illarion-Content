9120b3ec28f728d34f75604b7db04d9bab604db9
martin <martin.polak@gmx.at>
2011-07-31 12:25:38 +0200

    Testing guards

---
+++ monster/guards.lua
@@ -0,0 +1,75 @@
+module("monster.guards", package.seeall)
+
+-- Set clothes, weapons, hair/beard, colors
+function onSpawn(Guard)
+
+end
+
+-- Check if the enemy should be attacked, return true (did something) or false (nothing),
+-- not called if enemy is in attack range
+function enemyNear(Guard,Enemy)
+
+    return true;
+end
+
+
+-- Who should be attacked, return index of char in candList; return 0 to ignore completely.
+function setTarget(Guard, candList)
+    for key,target in pairs(candList) do                      -- search list for someone
+        target:inform("now checking...");
+        myTar=target;
+        if string.find(target.lastSpokenText,"me")~=nil then    -- who said ".*me.*"
+            target:inform("gotcha");
+            return key;
+        end
+    end
+    
+--[[
+    myTar:inform("check done");
+    if (firstWP==nil) then                          -- if there was noone found
+        monsterPos=Monster.pos;                                 -- let the monster have WP
+        targetPos=position(monsterPos.x+10,monsterPos.y+10,monsterPos.z);
+        WPList={targetPos,monsterPos,targetPos,monsterPos};
+        Monster.waypoints:addFromList(WPList);
+        Monster:setOnRoute(true);
+        firstWP=1;
+        myTar:inform("habe firstWP in setTarget gesetzt!");
+    end
+--]]
+    
+    return 0;
+end
+
+
+function enemyOnSight(Guard,Enemy)
+
+end
+
+
+-- attack back, whoever it is
+function onAttacked(Guard,Enemy)
+
+end
+
+
+-- attack back, whoever it is
+function onCasted(Guard,Enemy)
+
+end
+
+
+-- if someone is talking to you, stand still and talk back (a little)
+function receiveText(Guard, type, text, originator)
+
+end
+
+
+function abortRoute(Guard)
+
+end
+
+
+-- spawn loot
+function onDeath(Guard)
+
+end
