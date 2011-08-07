e6d8109bd3d415933cb720bbd3f9164da933faf2
vilarion <vilarion@illarion.org>
2011-08-07 18:16:23 +0200

    test npc transitions

--- questsystem/firstTest/node0.lua
+++ questsystem/firstTest/node0.lua
@@ -11,9 +11,9 @@ local POSTCONDITION_QUESTSTATE = 1
 
 function receiveText(type, text, player)
   if base.fulfilsPrecondition(player, QUEST_NUMBER, PRECONDITION_QUESTSTATE)
-      and player.getType == Character:player
+      and player.getType == Character.player
       and string.find(text, getNLS(player, NPC_TRIGGER_DE, NPC_TRIGGER_EN)) then
-    thisNPC.talk(Character:say, getNLS(player, NPC_REPLY_DE, NPC_REPLY_EN))
+    thisNPC.talk(Character.say, getNLS(player, NPC_REPLY_DE, NPC_REPLY_EN))
     base.setPostcondition(player, QUEST_NUMBER, POSTCONDITION_QUESTSTATE)
     return true
   end
