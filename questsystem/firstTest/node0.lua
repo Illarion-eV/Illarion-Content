daab1caaaa5628a9e13dd1a4c12b8417d4d095c7
vilarion <vilarion@illarion.org>
2011-08-07 18:41:16 +0200

    test npc transitions

--- questsystem/firstTest/node0.lua
+++ questsystem/firstTest/node0.lua
@@ -4,7 +4,7 @@ module("questsystem.firstTest.node0", package.seeall)
 local QUEST_NUMBER = 10000
 local NPC_TRIGGER_DE = "quest"
 local NPC_TRIGGER_EN = "quest"
-local NPC_REPLY_DE = "Ich habe eine Aufgabe fÃ¼r dich, bitte fang den Hamster der meine VorrÃ¤te frisst! Sag mir wenn du fertig bist."
+local NPC_REPLY_DE = "Ich habe eine Aufgabe für dich, bitte fang den Hamster der meine Vorräte frisst! Sag mir wenn du fertig bist."
 local NPC_REPLY_EN = "I have a task for you, please catch the hamster eating my supplies! Tell me when you are done."
 local PRECONDITION_QUESTSTATE = 0
 local POSTCONDITION_QUESTSTATE = 1
