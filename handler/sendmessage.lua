7d5669aab433b51c03cbd33c7c11b4a353d5c67d
martin <martin.polak@gmx.at>
2011-08-28 20:37:52 +0200

    changed sendmessage-handler

--- handler/sendmessage.lua
+++ handler/sendmessage.lua
@@ -1,16 +1,18 @@
 require("base.class")
+require("base.common")
 
 module("handler.sendmessage", package.seeall)
 
-sendMessage = base.class.class(function(sndMsg, posi, msg, rad)
+sendMessage = base.class.class(function(sndMsg, posi, msgde, msgen, rad)
     sndMsg.pos=posi;
-    sndMsg.message=msg;
+    sndMsg.messageDe=msgde;
+    sndMsg.messageEn=msgen;
     sndMsg.radius=rad;
 end);
 
 function sendMessage:execute()
     plyList=world:getPlayersInRangeOf(self.pos, self.radius);
     for i, player in pairs(plyList) do
-        player:inform(self.message)
+        base.common.InformNLS(player,self.messageDe,self.messageEn)
     end
 end
