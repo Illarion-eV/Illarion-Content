09f91b093c64e20a8e9aecf8ac36892d4a1776ff
martin <martin.polak@gmx.at>
2011-08-31 19:03:50 +0200

    dbezug

--- monster/base/kills.lua
+++ monster/base/kills.lua
@@ -4,5 +4,7 @@ function setLastAttacker(monster,enemy)
     if lastAttacker==nil then
         lastAttacker={};
     end
+    debug("setting last attacker now:")
     lastAttacker[monster.id]=enemy;
+    debug("DONE")
 end
