9572f9fb045aae932e706dec8c15712e20154d90
martin <martin.polak@gmx.at>
2011-09-02 22:37:02 +0200

    debug

--- item/id_2_flour.lua
+++ item/id_2_flour.lua
@@ -56,6 +56,7 @@ function UseItemMartin( User, SourceItem, TargetItem, counter, Param, ltstate )
     triggerlist={};
     for word in string.gmatch(stri, "[^|]+") do 
         table.insert(triggerlist,word)
+        User:inform("inserted "..word)
     end
     
     for key, trigger in triggerlist do
