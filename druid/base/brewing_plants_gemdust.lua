ed8bd3ef0b016b527f8f23b3116bc9a33684402a
merung <karten-spiel-haus@hotmail.de>
2011-09-06 19:40:19 +0200

    changed old data to new data

--- druid/base/brewing_plants_gemdust.lua
+++ druid/base/brewing_plants_gemdust.lua
@@ -19,13 +19,14 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 	   end  
        -- if there is a cauldron, it will become our subject of changes; let's save it
        local cauldron = base.common.GetFrontItem( User );
-
-      
+       local cauldronData = tonumber(cauldron:getData(cauldronData));
+       local potionMarker = cauldron:getData("potionMarker");
+	  
 	  -- check if the SourceItem is a herb
 	  local AlchemyPlant = druid.base.alchemy.CheckIfAlchemyPlant(User,SourceItem);
 	  if AlchemyPlant then
             -- data > 99999999 means that it is a completed potion   
-            if cauldron.data > 99999999 then 
+            if cauldron:getData(potionMarker) ~= nil then 
 		     base.common.InformNLS( User,
                 "Einem fertigen Trank kannst Du nichts mehr beifügen.",
                 "You cannot add something to a completed potion."
@@ -34,15 +35,14 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 			end	
 			-- cauldron.data < 11111111 means that there is no stock 
 		    -- therefore, we will cange it to a proper value to start with
-		    if cauldron.data < 11111111 then
-               cauldron.data = 55555555;
+		    if cauldronData < 11111111 or cauldronData = nil then
+               cauldron:setData("cauldronData", 55555555);
             end
             
 		 -- splitting of plant data and cauldron data (creating a list)
-            local CauldronData = cauldron.data
-			local Plant = SourceItem.id
+            local Plant = SourceItem.id
 			local plusWertPos,minusWertPos = druid.base.alchemy.SplitPlantData(Plant);
-            local dataZList = druid.base.alchemy.SplitCauldronData(User,CauldronData);
+            local dataZList = druid.base.alchemy.SplitCauldronData(User,cauldronData);
 
            -- "overflow" leads to explosion of the stock
             if dataZList[plusWertPos] == 9 or dataZList[minusWertPos] == 1 then
@@ -52,11 +52,12 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 		      world:makeSound(5,User.pos);
 	          world:gfx(9,cauldron.pos);
 		      base.common.InformNLS( User,
-                "Deine letzte Handlung scheint den Sud zerstört zu haben.",
-                "Your last doing seems to have destroyed the stock."
-                       );
-			  cauldron.data = 0
+                "Deine letzte Handlung scheint den Sud zerstört und zu einer Explosion geführt zu haben.",
+                "Your last doing seems to have destroyed the stock and caused an explosion."
+			                );
+			  cauldron:setData("cauldronData", 0);
 			  world:changeItem(cauldron)
+			  User:increaseAttrib("hitpoints", -3000);
 			  return;
 		    end 
 
@@ -76,8 +77,9 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 		    User:increaseAtPos(SourceItem.itempos,-1);
 			
 			-- the new data value is being created
-			cauldron.data = druid.base.alchemy.PasteCauldronData(User,dataZList);
-            world:changeItem(cauldron);
+			local newData = druid.base.alchemy.PasteCauldronData(User,dataZList);
+			cauldron:setData("cauldronData", 55555555);
+			world:changeItem(cauldron);
 	   
 	        User.movepoints=User.movepoints-30 --Delay of 30 movepoints for scaling skillgain and prevent macro abuse. If you change this, also change the movepoints in the learn(...) line in alchemy.lua
 	   end
@@ -86,7 +88,7 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 	  local GemDust = druid.base.alchemy.CheckIfGemDust(User,SourceItem);
 	  if GemDust then
           -- data > 99999999 means that it is a completed potion   
-            if cauldron.data > 99999999 then 
+            if cauldron:getData(potionMarker) ~= nil then 
 		     base.common.InformNLS( User,
                 "Einem fertigen Trank kannst Du nichts mehr beifügen.",
                 "You cannot add something to a completed potion."
@@ -94,7 +96,7 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 		        return;
 			end
 			-- no stock, no potion!	
-		    if cauldron.data < 11111111 then
+		    if cauldronData < 11111111 or cauldronData = nil then
 			   base.common.InformNLS( User,
                 "Im Kessel muss sich ein Sud befinden, um diesen zu verazubern.",
                 "There has to be a stock in the cauldron so that you can enchant it."
@@ -103,27 +105,27 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 			end
 			
 			if SourceItem.id == 446 then --bluestone
-			   PotionMarker = 1
+			   PM = 1
 			elseif SourceItem.id == 447 then  -- ruby
-                   PotionMarker = 2
+                   PM = 2
             elseif SourceItem.id == 448 then  -- emerald
-                   PotionMarker = 3
+                   PM = 3
             elseif SourceItem.id ==	449 then  -- blackstone
-                   PotionMarker = 4
+                   PM = 4
             elseif SourceItem.id == 450 then -- amethyst
-                   PotionMarker = 5
+                   PM = 5
             elseif SourceItem.id == 451 then -- topaz
-                   PotionMarker = 6
+                   PM = 6
             elseif SourceItem.id == 452 then -- diamond
-                   PotionMarker = 7
+                   PM = 7
             end 
             
 			-- change cauldron's data and quality
-			cauldron.data = cauldron.data * 10 + PotionMarker;
-			cauldron.quality = 999; -- note to myself (merung): replace it with a proper calculation
+			cauldron:setData("potionMarker", PM);
+			cauldron.quality = 999; -- !!!!!!!!!!!!!!!!!!!!!! note to myself (merung): replace it with a proper calculation  !!!!!!!!!!!!!!!!!!
 			world:changeItem(cauldron);
-		   
-		   User:increaseAtPos(SourceItem.itempos,-1); -- delete gemdust
+		    
+		    User:increaseAtPos(SourceItem.itempos,-1); -- delete gemdust
 		    
 			User.movepoints=User.movepoints-30 --Delay of 30 movepoints for scaling skillgain and prevent macro abuse. If you change this, also change the movepoints in the learn(...) line in alchemy.lua
 		end
