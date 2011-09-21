9d843da1bbf9aef3a97145a0f997dfebc0a21f4a
estralis <estralis@illarion.org>
2011-09-21 17:38:45 +0200

    New triggerfield for teleporters (test)

---
+++ triggerfield/teleporter.lua
@@ -0,0 +1,17 @@
+--Some GFX and SFX for teleporters
+
+-- Cadomyr: 127 647 0
+-- Runewick: 788 826 0
+-- Galmair: 424 245 0
+-- Wilderland: 684 307 0
+
+require("base.common")
+
+module("triggerfield.teleporter", package.seeall)
+
+function MoveToField(User)
+
+    world:makeSound(13,User.pos);
+    world:gfx(41,User.pos);
+
+end
