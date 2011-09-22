9adda06c832309403981ad65d93a99aab42b669a
Zot <zot@illarion.org>
2011-09-22 18:28:37 +0200

    Bli bla blu, fixed a bug im nu

--- monster/mon_43_redwolfs.lua
+++ monster/mon_43_redwolfs.lua
@@ -2,7 +2,7 @@ require("monster.base.drop")
 require("monster.base.lookat")
 require("monster.base.quests")
 require("base.messages");
-module("monster.mon_42_greywolfs", package.seeall)
+module("monster.mon_43_redwolfs", package.seeall)
 
 
 function ini(Monster)
