ff08dd6f7f531d5573ad67d3748c292433bdf0cd
estralis <estralis@illarion.org>
2011-09-26 19:56:50 +0200

    Debugkram raus.

--- lte/longterm_cooldown.lua
+++ lte/longterm_cooldown.lua
@@ -12,24 +12,23 @@ function addEffect( Effect, Character)
     Effect:addValue("10",0);
 end;
 
-function callEffect( Effect, Char ) -- Effect wird ausgeführt
+function callEffect( Effect, Char ) -- Effect is called
 
-if Char:idleTime() < 300 then --absolutely no regeneration effect if the player is afk for more than five minutes
+    if Char:idleTime() < 300 then --absolutely no regeneration effect if the player is afk for more than five minutes
 
-    --Addition by Estralis: Quest 119/120 (Cadomyr daily missions)
+		--Addition by Estralis: Quest 119/120 (Cadomyr daily missions)
 	
-	theQuestStatus=Char:getQuestProgress(120);
+		theQuestStatus=Char:getQuestProgress(120);
 	
-	if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
-	    Char:setQuestProgress(120,theQuestStatus-1); --cooling!
-    end
+		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
+			Char:setQuestProgress(120,theQuestStatus-1); --cooling!
+		end
 	
-	--Addition end
+		--Addition end
 	
-end --all above is only conducted for players that aren't afk for more than five minutes
+	end --all above is only conducted for players that aren't afk for more than five minutes
 
-    base.common.InformNLS( Char,"LTE 33 called.","LTE 33 called."); --Debugging
-	Effect.nextCalled = 10; --Effect gets called each 5 minutes
+	Effect.nextCalled = 3000; --Effect gets called each 5 minutes
 
 	return true; --No idea why!
 	
