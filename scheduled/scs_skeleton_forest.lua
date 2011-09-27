9cf3aefe8dcef0df0e3417bfb3d5bb23f8e8926a
estralis <estralis@illarion.org>
2011-09-27 19:00:01 +0200

    Irgendwas, was ich nicht verstehe.

--- scheduled/scs_skeleton_forest.lua
+++ scheduled/scs_skeleton_forest.lua
@@ -7,7 +7,7 @@ module("scheduled.scs_skeleton_forest", package.seeall)
 
 function ForestSkells()
     local Charakters = world:getPlayersInRangeOf(position(780,50,0),30);
-    for i, Char in Charakters do
+	for i,char in pairs(Charakters) do    
         RndTry = math.random(0,15)
         if (RndTry == 1) then
             if SpawnSkeleton(Char) then
