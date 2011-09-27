135db70fd1ab05543d28085810205daea2e84ed8
estralis <estralis@illarion.org>
2011-09-27 20:11:48 +0200

    fegsdgeas

--- scheduled/spawn_treasure.lua
+++ scheduled/spawn_treasure.lua
@@ -9,7 +9,7 @@ function spawnTreasure()
 
 	treasurePos=position(703,416,-3); --Salavesh dungeon
 	
-    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 then --only spawn a treasure if nobody is around
+    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around and there is no item on the tile
 	
 		world:createItemFromId(2830,1,treasurePos,false,333,math.random(0,4)); --spawn the chest only if the tile is empty
 		
