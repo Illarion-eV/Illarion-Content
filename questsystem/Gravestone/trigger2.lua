de039ca8706ee5bc422393757b5ab75d84aefc7e
Vitoria <Zanet@.(none)>
2011-10-10 20:27:15 +0200

    create new quest file

---
+++ questsystem/Gravestone/trigger2.lua
@@ -0,0 +1,45 @@
+require("handler.sendmessagetoplayer")
+require("questsystem.base")
+module("questsystem.Gravestone.trigger2", package.seeall)
+
+local QUEST_NUMBER = 10000
+local PRECONDITION_QUESTSTATE = 27
+local POSTCONDITION_QUESTSTATE = 9
+
+local POSITION = position(605, 344, 0)
+local RADIUS = 1
+
+function UseItem( PLAYER, item, TargetItem, counter, Param, ltstate )
+  if PLAYER:isInRangeToPosition(POSITION,RADIUS)
+      and ADDITIONALCONDITIONS(PLAYER)
+      and questsystem.base.fulfilsPrecondition(PLAYER, QUEST_NUMBER, PRECONDITION_QUESTSTATE) then
+    --informNLS(PLAYER, TEXT_DE, TEXT_EN)
+    
+    HANDLER(PLAYER)
+    
+    questsystem.base.setPostcondition(PLAYER, QUEST_NUMBER, POSTCONDITION_QUESTSTATE)
+    return true
+  end
+
+  return false
+end
+
+function informNLS(player, textDe, textEn)
+  if player:getPlayerLanguage() == Player.german then
+    player:inform(player, item, textDe)
+  else
+    player:inform(player, item, textEn)
+  end
+end
+
+-- local TEXT_DE = TEXT -- German Use Text -- Deutscher Text beim Benutzen
+-- local TEXT_EN = TEXT -- English Use Text -- Englischer Text beim Benutzen
+
+
+function HANDLER(PLAYER)
+    handler.sendmessagetoplayer.sendMessageToPlayer(PLAYER, "Du wischt den Schmutz vom Grabstein und erblickst eine Inschrift.", "You clean off the dirt from the tombstone and an inscription appears."):execute()
+end
+
+function ADDITIONALCONDITIONS(PLAYER)
+return true
+end
