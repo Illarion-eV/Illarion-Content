0d51a8d1b83a4d611db5f72d0864d68b03186f47
Vitoria <Zanet@.(none)>
2011-10-11 15:10:30 +0200

    new quest file

---
+++ questsystem/Gravestone/trigger12.lua
@@ -0,0 +1,43 @@
+require("questsystem.base")
+module("questsystem.Gravestone.trigger12", package.seeall)
+
+local QUEST_NUMBER = 10000
+local PRECONDITION_QUESTSTATE = 58
+local POSTCONDITION_QUESTSTATE = 95
+
+local POSITION = position(605, 344, 0)
+local RADIUS = 1
+local LOOKAT_TEXT_DE = "Die Namen am Grabstein, von Mary Sue und William Daloir, leuchten für einen Moment auf."
+local LOOKAT_TEXT_EN = "The names on the gravestone of Mary Sue and William Daloir brighten up for a moment."
+
+function LookAtItem(PLAYER, item)
+  if PLAYER:isInRangeToPosition(POSITION,RADIUS)
+      and ADDITIONALCONDITIONS(PLAYER)
+      and questsystem.base.fulfilsPrecondition(PLAYER, QUEST_NUMBER, PRECONDITION_QUESTSTATE) then
+
+    itemInformNLS(PLAYER, item, LOOKAT_TEXT_DE, LOOKAT_TEXT_EN)
+    
+    HANDLER(PLAYER)
+    
+    questsystem.base.setPostcondition(PLAYER, QUEST_NUMBER, POSTCONDITION_QUESTSTATE)
+    return true
+  end
+
+  return false
+end
+
+function itemInformNLS(player, item, textDe, textEn)
+  if player:getPlayerLanguage() == Player.german then
+    world:itemInform(player, item, textDe)
+  else
+    world:itemInform(player, item, textEn)
+  end
+end
+
+
+function HANDLER(PLAYER)
+end
+
+function ADDITIONALCONDITIONS(PLAYER)
+return true
+end
