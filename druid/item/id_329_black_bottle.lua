1203aca81d3bf1612b1ef464b9cf4cf42f955051
merung <karten-spiel-haus@hotmail.de>
2011-12-09 16:21:57 +0100

    debugging

--- druid/item/id_329_black_bottle.lua
+++ druid/item/id_329_black_bottle.lua
@@ -12,8 +12,8 @@ function DoDruidism(User,SourceItem)
    potionData = tonumber(SourceItem:getData("potionData"));
    
    ListTest = {}
-   ListTest[1] = {1={3,4,5},2={7,8,9}}
-   User:inform(""..ListTest[1][2][1])
+   ListTest[1] = {{3,4,5},{7,8,9}}
+   User:inform(""..ListTest[1][2])
    return
    
    if User.effects:find(329) then
