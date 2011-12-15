47dec6d3b3dee5b219f43b8a20204a7821f045c4
enviyatar <enviyatar@gmx.at>
2011-12-16 00:17:45 +0100

    extended

--- questsystem/information_runewick_2/trigger87.lua
+++ questsystem/information_runewick_2/trigger87.lua
@@ -1,16 +1,15 @@
 require("handler.sendmessagetoplayer")
-require("handler.createplayeritem")
 require("questsystem.base")
-module("questsystem.information_runewick_2.trigger59", package.seeall)
+module("questsystem.information_runewick_2.trigger87", package.seeall)
 
 local QUEST_NUMBER = 622
-local PRECONDITION_QUESTSTATE = 314
-local POSTCONDITION_QUESTSTATE = 257
+local PRECONDITION_QUESTSTATE = 250
+local POSTCONDITION_QUESTSTATE = 307
 
 local NPC_TRIGGER_DE = "."
 local NPC_TRIGGER_EN = "."
-local NPC_REPLY_DE = "Gut, damit könnt ihr nun auch folgende Personen aufsuchen und eure letzte Mission bestreiten: Ruzusss, Lotta Medborgar und Wulfgorda. Ein jeder hat einen Stein für dich, wie ich ihn auch habe."
-local NPC_REPLY_EN = "Good, you are ready to search for following people for your last mission:  Ruzusss, Lotta Medborgar und Wulfgorda. Each of them has a stone for you as I do."
+local NPC_REPLY_DE = "Gut, ihr habt gezeigt, dass ihr ein gewisses Maß an Gefahr bestehen könnt. Bevor die letzte Mission begonnen werden kann, habe ich noch eine Bitte. Und zwar eine Pfeife und zehn Sibanac Blätter!"
+local NPC_REPLY_EN = "Well, you have shown that you can manage some danger. But before you can start your final misison, I have a request. Please, bring me a pipe and ten sibanac leaves."
 
 function receiveText(type, text, PLAYER)
     if ADDITIONALCONDITIONS(PLAYER)
@@ -54,8 +53,7 @@ end
 
 
 function HANDLER(PLAYER)
-    handler.sendmessagetoplayer.sendMessageToPlayer(PLAYER, "Geh nun mit dem erhaltenen Smaragd nach Galmair, Cadomyr und zum Gasthof Hanfschlinge und suche die genannten Personen. ", "Go with the received emerald to Galmair, Cadomyr and the Hemp Necktie Inn and look for these mentioned persons there."):execute()
-    handler.createplayeritem.createPlayerItem(PLAYER, 45, 333, 1):execute()
+    handler.sendmessagetoplayer.sendMessageToPlayer(PLAYER, "Finde einen Schreiner oder Händler über die du die gewünschten Dinge besorgen kannst, wenn du sie nicht selber erzeugen kannst.", "Look for a carpenter or merchant who can supply you with the requested goods, if you cannot produce them by yourself."):execute()
 end
 
 function ADDITIONALCONDITIONS(PLAYER)
--- questsystem/information_runewick_2/trigger91.lua
+++ questsystem/information_runewick_2/trigger91.lua
@@ -1,16 +1,15 @@
 require("handler.sendmessagetoplayer")
-require("handler.createplayeritem")
 require("questsystem.base")
-module("questsystem.information_runewick_2.trigger59", package.seeall)
+module("questsystem.information_runewick_2.trigger91", package.seeall)
 
 local QUEST_NUMBER = 622
-local PRECONDITION_QUESTSTATE = 314
-local POSTCONDITION_QUESTSTATE = 257
+local PRECONDITION_QUESTSTATE = 310
+local POSTCONDITION_QUESTSTATE = 310
 
 local NPC_TRIGGER_DE = "."
 local NPC_TRIGGER_EN = "."
-local NPC_REPLY_DE = "Gut, damit könnt ihr nun auch folgende Personen aufsuchen und eure letzte Mission bestreiten: Ruzusss, Lotta Medborgar und Wulfgorda. Ein jeder hat einen Stein für dich, wie ich ihn auch habe."
-local NPC_REPLY_EN = "Good, you are ready to search for following people for your last mission:  Ruzusss, Lotta Medborgar und Wulfgorda. Each of them has a stone for you as I do."
+local NPC_REPLY_DE = "Bitte bringt mir zehn Sibanac Blätter!"
+local NPC_REPLY_EN = "Please, bring me ten sibanac leaves."
 
 function receiveText(type, text, PLAYER)
     if ADDITIONALCONDITIONS(PLAYER)
@@ -54,8 +53,7 @@ end
 
 
 function HANDLER(PLAYER)
-    handler.sendmessagetoplayer.sendMessageToPlayer(PLAYER, "Geh nun mit dem erhaltenen Smaragd nach Galmair, Cadomyr und zum Gasthof Hanfschlinge und suche die genannten Personen. ", "Go with the received emerald to Galmair, Cadomyr and the Hemp Necktie Inn and look for these mentioned persons there."):execute()
-    handler.createplayeritem.createPlayerItem(PLAYER, 45, 333, 1):execute()
+    handler.sendmessagetoplayer.sendMessageToPlayer(PLAYER, "Und nun beliefere sie mit zehn Sibanac Blättern.", "And now supply her with ten sibanac leaves."):execute()
 end
 
 function ADDITIONALCONDITIONS(PLAYER)
