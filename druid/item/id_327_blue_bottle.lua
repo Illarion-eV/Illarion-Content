d015519ef6a7cd320d178f7739a83af6650596fc
merung <karten-spiel-haus@hotmail.de>
2012-01-03 17:10:59 +0100

    debugging

--- druid/item/id_327_blue_bottle.lua
+++ druid/item/id_327_blue_bottle.lua
@@ -188,7 +188,7 @@ function MoveItemBeforeMove( User, SourceItem, TargetItem )
         return true; -- not thrown at the map
     end
 
-    User:inform(""..SorceItem.pos)
+    User:inform(""..SourceItem.pos)
 	if ((not SourceItem.pos==5) and (not SourceItem.pos==6)) and (SourceItem:getData("missileStatus") == "activated") then
         base.common.TempInformNLS( User,
         "Du musst den Wurfkörper aus der Hand werfen.",
