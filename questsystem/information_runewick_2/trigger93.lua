b0d560eae0717ccb0b54ea41fcac58606356577e
enviyatar <enviyatar@gmx.at>
2012-02-13 00:40:08 +0100

    quests neu exportiert
    Signed-off-by: enviyatar <enviyatar@gmx.at>

---
+++ questsystem/information_runewick_2/trigger93.lua
@@ -0,0 +1,63 @@
+require("handler.sendmessagetoplayer")
+require("handler.createplayeritem")
+require("questsystem.base")
+module("questsystem.information_runewick_2.trigger93", package.seeall)
+
+local QUEST_NUMBER = 622
+local PRECONDITION_QUESTSTATE = 0
+local POSTCONDITION_QUESTSTATE = 184
+
+local NPC_TRIGGER_DE = "Test"
+local NPC_TRIGGER_EN = "Test"
+local NPC_REPLY_DE = "Dann lass uns testen."
+local NPC_REPLY_EN = "Then let test us."
+
+function receiveText(type, text, PLAYER)
+    if ADDITIONALCONDITIONS(PLAYER)
+    and PLAYER:getType() == Character.player
+    and questsystem.base.fulfilsPrecondition(PLAYER, QUEST_NUMBER, PRECONDITION_QUESTSTATE) then
+        if PLAYER:getPlayerLanguage() == Player.german then
+            NPC_TRIGGER=string.gsub(NPC_TRIGGER_DE,'([ ]+)',' .*');
+        else
+            NPC_TRIGGER=string.gsub(NPC_TRIGGER_EN,'([ ]+)',' .*');
+        end
+
+        foundTrig=false
+        
+        for word in string.gmatch(NPC_TRIGGER, "[^|]+") do 
+            if string.find(text,word)~=nil then
+                foundTrig=true
+            end
+        end
+
+        if foundTrig then
+      
+            thisNPC:talk(Character.say, getNLS(PLAYER, NPC_REPLY_DE, NPC_REPLY_EN))
+            
+            HANDLER(PLAYER)
+            
+            questsystem.base.setPostcondition(PLAYER, QUEST_NUMBER, POSTCONDITION_QUESTSTATE)
+        
+            return true
+        end
+    end
+    return false
+end
+
+function getNLS(player, textDe, textEn)
+  if player:getPlayerLanguage() == Player.german then
+    return textDe
+  else
+    return textEn
+  end
+end
+
+
+function HANDLER(PLAYER)
+    handler.createplayeritem.createPlayerItem(PLAYER, 148, 999, 1):execute()
+    handler.sendmessagetoplayer.sendMessageToPlayer(PLAYER, "Gehe nun zur zweiten Zwillingaugen Insel und lege die erhaltene Blume dort auf das Blumenfeld vor dem Grabstein im Süden ab.", "Go to the second Twin Eyes Island now and lay down the flower on the flower field in front of the grave in the south then."):execute()
+end
+
+function ADDITIONALCONDITIONS(PLAYER)
+return true
+end
