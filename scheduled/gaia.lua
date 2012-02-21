255838222a98799ada36d6ba56b6302e44f55009
lennier <Mill@Mill02.(none)>
2012-02-21 19:26:29 +0100

    debugging

--- scheduled/gaia.lua
+++ scheduled/gaia.lua
@@ -62,7 +62,7 @@ function plantdrop()
             check = grnd[auswahl][math.random(1,table.getn(grnd[auswahl]))];
             pflwert = dataval[auswahl];
             ---- Standortbestimmung
-            newpos = position( math.random(0,100), math.random(0,100), 0 );
+            newpos = position( math.random(0,1024), math.random(0,1024), 0 );
             ---- bodentile feststellen
 			theTile=world:getField(newpos);
             local bodenart = base.common.GetGroundType( theTile:tile() );
