2d700dbc242d05a85f25f7561c6aba4b986a0470
lennier <Mill@Mill02.(none)>
2012-02-22 19:33:33 +0100

    debugging

--- scheduled/gaia.lua
+++ scheduled/gaia.lua
@@ -83,8 +83,7 @@ function plantdrop()
             if ((bodenart == check) or (check == 0)) then
 
                 world:createItemFromId(plnt[auswahl],1,newpos,false,333,pflwert);
-				user:inform("Setze Pflanze " ..plnt[auswahl);
-				user:inform("Auf Boden " ..bodenart);
+				user:inform("Setze Pflanze " ..plnt[auswahl].. " Auf Boden " ..bodenart);
 				
             end
         end
