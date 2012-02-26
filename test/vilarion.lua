356e43027516a75725d4f03df9c6cd8bf32fc1f6
vilarion <vilarion@illarion.org>
2012-02-26 21:42:24 +0100

    test dialog system

--- test/vilarion.lua
+++ test/vilarion.lua
@@ -42,7 +42,7 @@ function UseItem( User, SourceItem, TargetItem, counter, Param, ltstate )
         --no_func()
     end
     local c = 42
-    local inputDialog = InputDialog("Enter something cool!", true, 0)
+    local inputDialog = InputDialog("Enter something cool!", true, 0, nil)
     User:requestInputDialog(inputDialog)
 end
 
