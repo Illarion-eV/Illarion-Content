fb4e24f20301e76ff2abddea23934f18a512168e
Zot <zot@illarion.org>
2012-03-21 21:41:29 +0100

    blub

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -6,7 +6,7 @@ module("item.id_456_snowball", package.seeall)
 function MoveItemAfterMove(User, SourceItem, TargetItem)
 	MyTile=world:getField(TargetItem.pos)
 	TileID = MyTile.tile
-	if (TileID==6) then 
+	if (TileID==11) then 
 	User:talkLanguage(Character.say,Player.german,"#me wirft ein Steinchen ins Wasser.");
     User:talkLanguage(Character.say,Player.english,"#me throws a pebble into the water.");
     world:gfx(1,TargetItem.pos);
