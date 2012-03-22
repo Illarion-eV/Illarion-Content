64c0871a5e590e2eb3fa5e0b064877eb79af1656
Zot <zot@illarion.org>
2012-03-22 14:15:18 +0100

    abc

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -1,19 +1,16 @@
 -- UPDATE common SET com_script='item.id_456_snowball' WHERE com_itemid IN (456);
 
-require("base.common")
 module("item.id_456_snowball", package.seeall)
 
 function MoveItemAfterMove(User, SourceItem, TargetItem)
-	MyTile=world:getField(TargetItem.pos)
-	TileID = MyTile.tile
-	if (TileID==11) then 
-	User:talkLanguage(Character.say,Player.german,"#me wirft ein Steinchen ins Wasser.");
-    User:talkLanguage(Character.say,Player.english,"#me throws a pebble into the water.");
-    world:gfx(1,TargetItem.pos);
-    world:makeSound(9,TargetItem.pos);
-    world:erase(Item,1);
-	else 
-	return false
-end
-
+	if Round==nil then
+		Round=1;
+	end
+	if Round==1 then
+        User:talkLanguage(Character.say,Player.german,"#me wirft einen Schneeball.");
+        User:talkLanguage(Character.say,Player.english,"#me throws a snowball.");
+		Round=2;
+	else
+		Round=nil;
+	end
 end
