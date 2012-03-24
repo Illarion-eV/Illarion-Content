20c9b18346f70886b42f77235def01ee3cf42936
Zot <zot@illarion.org>
2012-03-24 18:44:25 +0100

    test

--- item/id_10_teleportgate.lua
+++ item/id_10_teleportgate.lua
@@ -143,12 +143,7 @@ function CharacterOnField( User )
                     "A voice booms: YOU HAVE BEEN WARNED NOT TO ENTER OUR LANDS UNINVITED...WELCOME TO VANIMA" );
                 end;
 
-                if ( SourceItem.wear ~= 255 ) then
-                    if ( SourceItem.quality < 900 ) then
-                        if ( SourceItem.quality > 200 ) then
-                            SourceItem.quality = SourceItem.quality - 100;
-                            world:changeItem( SourceItem );
-                        else
+
                             world:erase( SourceItem, SourceItem.number );
                         end
                     end
