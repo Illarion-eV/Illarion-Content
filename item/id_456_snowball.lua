8273d90afee3fbddb83149a570e1ab3c7d61659d
Zot <zot@illarion.org>
2012-03-26 16:09:07 +0200

    didn't work, trying again.

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -3,15 +3,20 @@
 module("item.id_456_snowball", package.seeall)
 
 function MoveItemBeforeMove(User, SourceItem, TargetItem)
+	if Round==nil then
 	MyTile=world:getField(TargetItem.pos)
 	TileID = MyTile.tile
-	if (TileID==6) then 
-	User:talkLanguage(Character.say,Player.german,"#me wirft ein Steinchen ins Wasser.");
-    User:talkLanguage(Character.say,Player.english,"#me throws a pebble into the water.");
-    world:gfx(1,TargetItem.pos);
-    world:makeSound(9,TargetItem.pos);
-    world:erase(Item,1);
-	else 
-	return false
+	Round=1;
 	end
+	if Round==1 then
+		if (TileID==6) then 
+		User:talkLanguage(Character.say,Player.german,"#me wirft ein Steinchen ins Wasser.");
+		User:talkLanguage(Character.say,Player.english,"#me throws a pebble into the water.");
+		world:gfx(1,TargetItem.pos);
+		world:makeSound(9,TargetItem.pos);
+		world:erase(Item,1);
+	else 
+	Round=nil;	
+	end
+end
 end
