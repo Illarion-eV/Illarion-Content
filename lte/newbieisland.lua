7d56e2e7f661861b08e37442a3c676fc5d6580e0
Merung <karten-spiel-haus@hotmail.de>
2012-03-31 19:01:46 +0200

    debugging

--- lte/newbieisland.lua
+++ lte/newbieisland.lua
@@ -59,13 +59,13 @@ function callEffect(newbieEffect,Character)
 	        "@Rince: text 4 EN");
 	    end 
 		
-		if (Charcater:getQuestProgess(13) == 2) or (Charcater:getQuestProgess(13) == 3) then
+		if (Character:getQuestProgess(13) == 2) or (Charcater:getQuestProgess(13) == 3) then
 		    base.common.InformNLS(Character,
 	        "@Rince: text Schweine töten DE",
 	        "@Rince: text Schweine töten EN");
 	    end
 		
-		if (Charcater:getQuestProgess(12) == 2) then
+		if (Character:getQuestProgess(12) == 2) then
 		    base.common.InformNLS(Character,
 	        "@Rince: text Kohle Erz DE",
 	        "@Rince: text Kohle Erz EN");
