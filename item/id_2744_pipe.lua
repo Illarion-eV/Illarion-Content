b462bee0882aba558cc187e0d025f48319617972
faladron <wingalf@hotmail.de>
2012-04-02 01:59:29 +0200

    pipes

--- item/id_2744_pipe.lua
+++ item/id_2744_pipe.lua
@@ -18,7 +18,7 @@ function UseItem( Character, SourceItem, TargetItem, Counter, Param)
 		end
 		
 	if (Character:countItemAt("belt",155) ~= 0) then 
-	-- Effekt setzen falls noch nicht vorhanden
+	--[[ Effekt setzen falls noch nicht vorhanden
 			find, stonedEffect = Character.effects:find(19);
 			if find then
 			found, stonedIndex = stonedEffect:findValue("stonedIndex");
@@ -28,7 +28,7 @@ function UseItem( Character, SourceItem, TargetItem, Counter, Param)
 			else
 
 			Character.effects:addEffect( LongTimeEffect(19,10) );
-			end
+			end]]
 
 			Character:talkLanguage(Character.say,Player.german ,"#me nimmt einen tiefen Zug von der Pfeife.");
 			Character:talkLanguage(Character.say,Player.english,"#me takes a deep drag from the pipe.");
