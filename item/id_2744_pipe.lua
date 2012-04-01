19c49acc391fbc81e0a20d9d3345b649a72396c0
faladron <wingalf@hotmail.de>
2012-04-02 01:00:20 +0200

    pipes ...

--- item/id_2744_pipe.lua
+++ item/id_2744_pipe.lua
@@ -8,37 +8,40 @@ module("item.id_2744_pipe", package.seeall, package.seeall(item.general.wood))
 -- UPDATE common SET com_script='item.id_2744_pipe' WHERE com_itemid = 2744;
 
 function UseItem( Character, SourceItem, TargetItem, Counter, Param)
-	local AmmountSib = Character:countItemAt("belt",155)
-	local AmmountTob = Character:countItemAt("belt",772)
+	local ammountSib = Character:countItemAt("belt",155)
+	local ammountTob = Character:countItemAt("belt",772)
 	
-	if (AmountTab == 0 and AmountSib == 0) then NoSmoke
 	
-	elseif (AmmountSib ~= 0) then SibSmoke
+	if (amountTab == 0 and AmountSib == 0) then
+	base.common.InformNLS( Character,
+        "1.Du benötigst Sibanac-Blätter oder Tabak um Pfeife rauchen zu können.",
+        "1.You need sibanac-leafs or tobacco to smoke a pipe." );
+	
+		elseif (ammountSib ~= 0) then 
 	-- Effekt setzen falls noch nicht vorhanden
-		function SibSmoke
-		find, stonedEffect = Character.effects:find(19);
-		if find then
-		found, stonedIndex = stonedEffect:findValue("stonedIndex");
-		if found then
-	    stonedEffect:addValue("stonedIndex",(stonedIndex+1));
-		end
-		else
+			find, stonedEffect = Character.effects:find(19);
+			if find then
+			found, stonedIndex = stonedEffect:findValue("stonedIndex");
+			if found then
+			stonedEffect:addValue("stonedIndex",(stonedIndex+1));
+			end
+			else
 
-		Character.effects:addEffect( LongTimeEffect(19,10) );
-		end
+			Character.effects:addEffect( LongTimeEffect(19,10) );
+			end
 
-		Character:talkLanguage(Character.say,Player.german ,"#me nimmt einen tiefen Zug von der Pfeife.");
-		Character:talkLanguage(Character.say,Player.english,"#me takes a deep drag from the pipe.");
+			Character:talkLanguage(Character.say,Player.german ,"#me nimmt einen tiefen Zug von der Pfeife.");
+			Character:talkLanguage(Character.say,Player.english,"#me takes a deep drag from the pipe.");
     
-		Character:eraseItem( 155, 1 );
-		end
+			Character:eraseItem( 155, 1 );
+			end
 	
-	elseif AmmountTob ~= 0 then
+		elseif (AmmountTob ~= 0) then
 		Character:talkLanguage(Character.say,Player.german ,"#me bläst einen Rauchkringel in die Luft.");
 		Character:talkLanguage(Character.say,Player.english,"#me puffs a ring of smoke into the air.");
     
 		Character:eraseItem( 772, 1 );
-		
+		end
 	end
 	
 	 --[[then -- kein Sibanac???
@@ -49,11 +52,7 @@ function UseItem( Character, SourceItem, TargetItem, Counter, Param)
 	end
 end
 	
-function NoSmoke
-	base.common.InformNLS( Character,
-        "1.Du benötigst Sibanac-Blätter oder Tabak um Pfeife rauchen zu können.",
-        "1.You need sibanac-leafs or tobacco to smoke a pipe." );
-	end
+
 	
 	
 function LookAtItem(User, Item)
