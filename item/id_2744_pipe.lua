4aa7939f2a6fbf32a6753695d88171151d8398c9
faladron <wingalf@hotmail.de>
2012-04-02 22:55:38 +0200

    portale

--- item/id_2744_pipe.lua
+++ item/id_2744_pipe.lua
@@ -8,8 +8,8 @@ module("item.id_2744_pipe", package.seeall, package.seeall(item.general.wood))
 -- UPDATE common SET com_script='item.id_2744_pipe' WHERE com_itemid = 2744;
 
 function UseItem( User, SourceItem, TargetItem, Counter, Param)
-	User:createItem(1061, 1, 333, (Destination.1) )
-	
+	TargetItem:setData( "Destination", Counter );
+	base.common.InformNLS( User, "Bla", "Destination set to"..Counter )
 	--local ammountSib = User:countItemAt("belt",155);
 	--local ammountTob = User:countItemAt("belt",772);
 	if (User:countItemAt("belt",155) == 0 and User:countItemAt("belt",772) == 0) then
