c86fe40002594986b839e5742721b887cda43517
faladron <wingalf@hotmail.de>
2012-04-02 22:03:48 +0200

    pipes

--- item/id_2744_pipe.lua
+++ item/id_2744_pipe.lua
@@ -8,19 +8,26 @@ module("item.id_2744_pipe", package.seeall, package.seeall(item.general.wood))
 -- UPDATE common SET com_script='item.id_2744_pipe' WHERE com_itemid = 2744;
 
 function UseItem( User, SourceItem, TargetItem, Counter, Param)
-	User:talkLanguage(Character.say,Player.german ,"#me nimmt einen tiefen Zug von der Pfeife.");
-	User:talkLanguage(Character.say,Player.english,"#me takes a deep drag from the pipe.");
 	
-	--[[local ammountSib = Character:countItemAt("belt",155);
-	local ammountTob = Character:countItemAt("belt",772);
+	
+	--local ammountSib = Character:countItemAt("belt",155);
+	--local ammountTob = Character:countItemAt("belt",772);
 	if (Character:countItemAt("belt",155) == 0 and Character:countItemAt("belt",772) == 0) then
 		base.common.InformNLS( Character,
         "1.Du benötigst Sibanac-Blätter oder Tabak um Pfeife rauchen zu können.",
         "1.You need sibanac-leafs or tobacco to smoke a pipe." );
-		end
 		
-	if (Character:countItemAt("belt",155) ~= 0) then 
-	 Effekt setzen falls noch nicht vorhanden
+		
+	elseif (Character:countItemAt("belt",155) ~= 0) then 
+		User:talkLanguage(Character.say,Player.german ,"#me nimmt einen tiefen Zug von der Pfeife.");
+		User:talkLanguage(Character.say,Player.english,"#me takes a deep drag from the pipe. 'I smoke weed'");
+		Character:eraseItem( 155, 1 );
+			
+	elseif (Character:countItemAt("belt",772) ~= 0) then
+		User:talkLanguage(Character.say,Player.german ,"#me bläst einen Rauchkringel in die Luft.");
+		User:talkLanguage(Character.say,Player.english,"#me puffs a ring of smoke into the air.");
+		Character:eraseItem( 772, 1 );
+	--[[Effekt setzen falls noch nicht vorhanden
 			find, stonedEffect = Character.effects:find(19);
 			if find then
 			found, stonedIndex = stonedEffect:findValue("stonedIndex");
@@ -44,6 +51,7 @@ function UseItem( User, SourceItem, TargetItem, Counter, Param)
     
 		Character:eraseItem( 772, 1 );
 	end]]
+	end
 end
 	
 
