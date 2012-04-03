a68c80f55c7b18e50a92b78b913cdfc46659d34b
faladron <wingalf@hotmail.de>
2012-04-03 22:28:01 +0200

    portals

--- item/id_2744_pipe.lua
+++ item/id_2744_pipe.lua
@@ -8,9 +8,9 @@ module("item.id_2744_pipe", package.seeall, package.seeall(item.general.wood))
 -- UPDATE common SET com_script='item.id_2744_pipe' WHERE com_itemid = 2744;
 
 function UseItem( User, SourceItem, TargetItem, Counter, Param)
-	New = User:createItem (1061, 1, 333, 0)
-	New:setData("destination","1")
-	world:changeItem(New)
+	Blablu = User:createItem(1061, 1, 333, 0)
+	Blablu:setData("destination","1")
+	world:changeItem(Blablu)
 	
 	--local ammountSib = User:countItemAt("belt",155);
 	--local ammountTob = User:countItemAt("belt",772);
