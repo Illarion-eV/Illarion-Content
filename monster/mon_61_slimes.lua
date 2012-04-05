f352673eacff798eb5494a906700a461fb49b4cb
faladron <wingalf@hotmail.de>
2012-04-05 21:02:48 +0200

    Wandling

--- monster/mon_61_slimes.lua
+++ monster/mon_61_slimes.lua
@@ -16,7 +16,17 @@ killer={}; --A list that keeps track of who attacked the monster last
 end
 
 function enemyNear(Monster,Enemy)
-
+	local MonID=Monster:getMonsterType();
+	if(MonID == 611) then
+			Wandling = world:getRace
+			Wandling = 55
+			world:increaseAttrib("hitpoints",0)
+			return true;
+			end
+		else
+		return false;		
+	end
+	
     if init==nil then
         ini(Monster);
     end
@@ -44,17 +54,6 @@ function enemyOnSight(Monster,Enemy)
 end
 
 function onAttacked(Monster,Enemy)
-	local MonID=Monster:getMonsterType();
-	if(MonID == 611) then
-		Wandling = world:getRace
-		Wandling = 55
-		world:increaseAttrib("hitpoints",0)
-		return true;
-		end
-	else
-		return false;		
-	end
-	
     if init==nil then
         ini(Monster);
     end
