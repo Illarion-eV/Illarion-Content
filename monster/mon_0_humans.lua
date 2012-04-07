b68f3a88ff25774b4c1c949e6cd9058a98af0fd7
faladron <wingalf@hotmail.de>
2012-04-07 15:48:30 +0200

    Summons

--- monster/mon_0_humans.lua
+++ monster/mon_0_humans.lua
@@ -53,7 +53,7 @@ function enemyNear(Monster,Enemy)
     if (MonID==3) then
         return ( monster.base.drop.SuddenWarp(Monster,Enemy,true) or monster.base.drop.SuddenWarp(Monster,Enemy,true) or monster.base.drop.CastMonMagic(Monster,Enemy,7,{1000,2000},{{6,5},{9,5},{51,5}},{},40,1,{35,50}) );
     elseif (MonID==4) then
-        return ( monster.base.drop.CastMonster(Monster,Enemy,10,{101,111},40) == true );
+        return ( monster.base.drop.CastMonster(Monster,Enemy,10,{101,111},40) );
     else
         return false;
     end
