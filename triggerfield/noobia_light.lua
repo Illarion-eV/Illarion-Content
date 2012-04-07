bbb564705317b19e86ee2213fce18be3428126e0
merung <karten-spiel-haus@hotmail.de>
2012-04-07 17:07:04 +0200

    debugging

--- triggerfield/noobia_light.lua
+++ triggerfield/noobia_light.lua
@@ -12,7 +12,7 @@ function MoveToField(Character)
 	find, myEffect = Character.effects:find(13)
 	if find then
 	    base.common.InformNLS(Character,
-	    "test",
+	    "Um mit dem Echsenmann zu sprechen, öffne die Sprechkonsole mit Enter, schreibe etwas und drücke wieder Enter. Alle NPCs (NonPlayerCharacters) reagiern auf bestimmte Signal Wörter wenn du in ihrer Nähe stehst. Versuche den Echsenmann um 'Hilfe' zu bitten. Ist die Sprachkonsole leer, kannst du sie mit erneutem drücken der Entertaste schließen.",
 	    "@Rince: ziel erreicht 1 EN");
         myEffect:addValue("noobiaLight",1)
 	end
