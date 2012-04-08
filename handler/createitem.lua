cb78021295fb5c0fce639ebbc6a673ba504ca583
faladron <wingalf@hotmail.de>
2012-04-08 19:16:28 +0200

    Lever testing

--- handler/createitem.lua
+++ handler/createitem.lua
@@ -2,17 +2,17 @@ require("base.class")
 
 module("handler.createitem", package.seeall)
 
-createItem = base.class.class(function(creitem, posi, ID, qual, amnt, wear)
+createItem = base.class.class(function(creitem, posi, ID, qual, amnt, permanentwear)
     creitem.pos=posi;
     creitem.itemId=ID;
     creitem.quality=qual;
     creitem.amount=amnt;
-	creitem.wear=wear;
+	
 end);
 
 function createItem:execute()
     itemCr = world:createItemFromId(self.itemId, self.amount, self.pos,true,self.quality,0);
-	if (wear ~= nil) then
+	if (permanentwear ~= nil) then
 	itemCr.wear=255;
     world:changeItem(itemCr)
 	end
