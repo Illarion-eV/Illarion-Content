3b6ba3271eb2098519455a250477419922040282
faladron <wingalf@hotmail.de>
2012-04-08 19:39:42 +0200

    levers

--- handler/createitem.lua
+++ handler/createitem.lua
@@ -12,7 +12,7 @@ end);
 
 function createItem:execute()
     itemCr = world:createItemFromId(self.itemId, self.amount, self.pos,true,self.quality,0);
-	if (permanentwear ~= nil) then
+	if permanentwear ~= nil then 
 	itemCr.wear=255;
     world:changeItem(itemCr)
 	end
