fd43b3db799a969db35b51a61956761400e1fa96
faladron <wingalf@hotmail.de>
2012-04-08 20:46:58 +0200

    Lever

--- handler/createitem.lua
+++ handler/createitem.lua
@@ -7,18 +7,13 @@ module("handler.createitem", package.seeall)
 -- Added: wear, if wear ~= nil then the wear of the created item is altered!
 -- If it's nil, default wear will be assigned
 
-createItem = base.class.class(function(creitem, posi, ID, qual, amnt, wear)
+createItem = base.class.class(function(creitem, posi, ID, qual, amnt)
     creitem.pos=posi;
     creitem.itemId=ID;
     creitem.quality=qual;
     creitem.amount=amnt;
-	creitem.wear=wear;
 end);
 
 function createItem:execute()
-    itemCr = world:createItemFromId(self.itemId, self.amount, self.pos,true,self.quality,0);
-	if wear ~= nil then		
-			itemCr.wear=wear;
-			world:changeItem(itemCr)
-	end
+    world:createItemFromId(self.itemId, self.amount, self.pos,true,self.quality,0);
 end
