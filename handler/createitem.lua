f47eb9ebac28ad9eff024e0efdd8b15d0f76a9eb
faladron <wingalf@hotmail.de>
2012-04-08 18:56:27 +0200

    Probably won't work

--- handler/createitem.lua
+++ handler/createitem.lua
@@ -2,13 +2,17 @@ require("base.class")
 
 module("handler.createitem", package.seeall)
 
-createItem = base.class.class(function(creitem, posi, ID, qual, amnt)
+createItem = base.class.class(function(creitem, posi, ID, qual, amnt, wear)
     creitem.pos=posi;
     creitem.itemId=ID;
     creitem.quality=qual;
     creitem.amount=amnt;
+	creitem.wear=wear;
 end);
 
 function createItem:execute()
-    world:createItemFromId(self.itemId, self.amount, self.pos,true,self.quality,0);
+    itemCr = world:createItemFromId(self.itemId, self.amount, self.pos,true,self.quality,0);
+	if (wear ~= nil) then
+	itemCr.wear=255;
+    world:changeItem(itemCr)
 end
