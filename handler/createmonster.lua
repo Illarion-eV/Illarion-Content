b203611773597606c9220ec83b5a0239f7d67697
faladron <wingalf@hotmail.de>
2012-04-09 22:38:02 +0200

    Monster spawn handler

--- handler/createmonster.lua
+++ handler/createmonster.lua
@@ -2,12 +2,12 @@ require("base.class")
 
 module("handler.createmonster", package.seeall)
 
-createMonster = base.class.class(function(cremonst, posi, ID, mvpoints)
+createMonsters = base.class.class(function(cremonst, posi, ID, mvpoints)
 	cremonst.pos=posi;
 	cremonst.monsterID=ID;
 	cremonst.movepoints=mvpoints;
 end);
 	
-function createMonster:execute()
+function createMonsters:execute()
 	world:createMonster(self.monsterID, self.pos, self.movepoints);
 end
