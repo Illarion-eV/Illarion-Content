ff365655b10d8f110e4823e67e89996f772c8b10
faladron <wingalf@hotmail.de>
2012-04-09 23:22:41 +0200

    Test failed

--- item/id_2744_pipe.lua
+++ item/id_2744_pipe.lua
@@ -10,12 +10,7 @@ module("item.id_2744_pipe", package.seeall, package.seeall(item.general.wood))
 --[[This item has been claimed by faladron to do his biding and sate his scripting curiosity.
 	It will do anything and everything with the exception of what should be expected from it]]
 function UseItem( User, SourceItem, TargetItem, Counter, Param)
-		local New = SourceItem
-		New.ItemID = 1061
-		New.position = User.position
-		New.quality = 333
-		New.setData("destination", "1");
-		world:createItemFromId(New)
+		
 	--[[ if ( SourceItem.number == 1 ) then  --1 Würfel
 
 		WurfEins = (math.random(6))
