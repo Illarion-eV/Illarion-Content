e7edf52166d51a00f9f1a453215ff029c11043ea
faladron <wingalf@hotmail.de>
2012-04-09 21:57:47 +0200

    Monster handler testing

--- handler/createmonster.lua
+++ handler/createmonster.lua
@@ -4,12 +4,15 @@ module("handler.createmonster", package.seeall)
 
 
 
-createMonster = base.class.class(function(cremonst, posi, ID, mvpoints)
+createMonster = base.class.class(function(cremonst, posi, ID, mvpoints, amnt)
 	cremonst.pos=posi;
 	cremonst.monsterID=ID;
 	cremonst.movepoints=mvpoints;
+	cremonst.amnt=amnt;
 end);
 	
 function createMonster:execute()
+	Amnt=self.amnt
+	for i, amnt in pairs(Amnt) do 
 	world:createMonster(self.monsterID, self.pos, self.movepoints);
 end
