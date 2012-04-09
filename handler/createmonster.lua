c5ec6ea5c96a034accdb5f502fec9e749dcf8395
faladron <wingalf@hotmail.de>
2012-04-09 22:11:56 +0200

    Lever test

--- handler/createmonster.lua
+++ handler/createmonster.lua
@@ -8,11 +8,8 @@ createMonster = base.class.class(function(cremonst, posi, ID, mvpoints, amnt)
 	cremonst.pos=posi;
 	cremonst.monsterID=ID;
 	cremonst.movepoints=mvpoints;
-	cremonst.amnt=amnt;
 end);
 	
 function createMonster:execute()
-	for i, amnt in pairs() do 
 	world:createMonster(self.monsterID, self.pos, self.movepoints);
-	end
 end
