4494881b0c3dee91d68d1dc410c1804fbafb3f4a
faladron <wingalf@hotmail.de>
2012-04-11 20:57:20 +0200

    Endurance Cave

--- triggerfield/enduranceCave.lua
+++ triggerfield/enduranceCave.lua
@@ -15,7 +15,12 @@ function MoveToField(User)
 	User:forceWarp(position(7,8,-15));
 	end
 	
-	if User.pos == (position(20,3,-15)) then
+	if User.pos == (position(20,3,-15))
+				or User.pos == (position  (30, 3, -15))
+				or User.pos == (position  (38,5,-15)) 
+				or User.pos == (position  (38,10,-15)) 
+				or User.pos == (position (38,15,-15))	then
+				
 	local PlayerInCave, t_dest = quest.enduranceCave.InCave(User);
 	local gate = t_dest;
 	if (PlayerInCave) then
@@ -23,21 +28,14 @@ function MoveToField(User)
 	end
 	end
 	
-		if User.pos == (position (20,3,-15)) 
-				or User.pos == (position  (30, 3, -15))
-				or User.pos == (position  (38,5,-15)) 
-				or User.pos == (position  (38,10,-15)) 
-				or User.pos == (position (38,15,-15))
-		then
 		
-			if gate ~= nil then
+	if gate ~= nil then
 	
-			local dest = position( gate[ 1 ], gate[ 2 ], gate[ 3 ] );
-				world:makeSound( 4, dest )
+		local dest = position( gate[ 1 ], gate[ 2 ], gate[ 3 ] );
+			world:makeSound( 4, dest )
                 
-				world:gfx( 41, User.pos )
-				User:warp( dest );
-				world:gfx( 41, User.pos )
-			end
-		end
+			world:gfx( 41, User.pos )
+			User:warp( dest );
+			world:gfx( 41, User.pos )
 	end
+end
