1c36b56ecc2ab6f0f0289970f9a15a564aea804d
faladron <wingalf@hotmail.de>
2012-04-11 20:07:01 +0200

    Endurance Cave

--- quest/enduranceCave.lua
+++ quest/enduranceCave.lua
@@ -19,10 +19,10 @@ if not GlobalsInitialized then
 	magPortal   = {position (20,3,-15), position (30, 3, -15),
 					   position (38,5,-15), position (38,10,-15), position(38,15,-15)};
 
-	StepAmount  = 2; 		   -- the amount of steps the player has to go
+	StepAmount  = 8; 		   -- the amount of steps the player has to go
 						   	   --to reach the end boss
 
-	bosspos    	= {542,681,2}; 	   --warp position of the boss room
+	bosspos    	= {0,0,0}; 	   --warp position of the boss room
 	anteroompos = {7,8,-15}; --warp position of the anterroom
 	
 	CenterPositionOfPortalRoom = position (25,11,-15); -- the center of the room
