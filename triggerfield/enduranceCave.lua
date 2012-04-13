209c88719ae5979ca3dd877a1843147d260c16a7
faladron <wingalf@hotmail.de>
2012-04-13 16:54:04 +0200

    Endurance Cave

--- triggerfield/enduranceCave.lua
+++ triggerfield/enduranceCave.lua
@@ -2,18 +2,33 @@ module("triggerfield.enduranceCave", package.seeall)
 
 require("quest.enduranceCave")
 
--- INSERT INTO triggerfields VALUES (1,1,0,'triggerfield.enduranceCave');
--- INSERT INTO triggerfields VALUES (20,3,-15,'triggerfield.enduranceCave');
--- INSERT INTO triggerfields VALUES (30,3,-15,'triggerfield.enduranceCave');
--- INSERT INTO triggerfields VALUES (38,5,-15,'triggerfield.enduranceCave');
--- INSERT INTO triggerfields VALUES (38,10,-15,'triggerfield.enduranceCave');
--- INSERT INTO triggerfields VALUES (38,15,-15,'triggerfield.enduranceCave');
+-- INSERT INTO triggerfields VALUES (1,1,0,'triggerfield.enduranceCave');   (Entrance to Dungeon)
+-- INSERT INTO triggerfields VALUES (7,8,-15,'triggerfield.enduranceCave');  (Exit From Dungeon)
+-- INSERT INTO triggerfields VALUES (20,3,-15,'triggerfield.enduranceCave'); (Portals)
+-- INSERT INTO triggerfields VALUES (30,3,-15,'triggerfield.enduranceCave'); (Portals)
+-- INSERT INTO triggerfields VALUES (38,5,-15,'triggerfield.enduranceCave'); (Portals)
+-- INSERT INTO triggerfields VALUES (38,10,-15,'triggerfield.enduranceCave'); (Portals)
+-- INSERT INTO triggerfields VALUES (38,15,-15,'triggerfield.enduranceCave'); (Portals)
 
 function MoveToField(User)
-	
-	if User.pos == (position(1,1,0)) then
-	User:forceWarp(position(7,8,-15));
 	local language = User:getPlayerLanguage();
+	
+	if User.pos == position(7,8,-15) then -- Warp exiting the Dungeon
+	world:makeSound( 4, User.pos )
+	world:gfx( 41, User.pos )
+	User:forceWarp(position(1,1,0)
+	world:gfx( 41, User.pos )
+		if language == 1 then
+		User:inform("You hear a faint laughter burrowing in your ears as you step into the portal. Then you are back on the surface.")
+		else
+		User:inform("Du hörst ein fernes Lachen in deinen Ohren dröhnen. Dann bist du wieder auf der Oberwelt.")
+		end
+	end
+	if User.pos == (position(1,1,0)) then -- Warp entering the Dungeon
+	world:makeSound( 4, User.pos )
+	world:gfx( 41, User.pos )
+	User:forceWarp(position(7,8,-15));
+	world:gfx( 41, User.pos )
 		if language == 1 then
 		User:inform("You are warped into a strange room. The fireball in the middle looks strange to you.")
 		else
@@ -21,6 +36,7 @@ function MoveToField(User)
 		end
 	end
 	
+	-- Warp through the Portals inside the dungeon
 	if User.pos == (position(20,3,-15))
 				or User.pos == (position  (30, 3, -15))
 				or User.pos == (position  (38,5,-15)) 
