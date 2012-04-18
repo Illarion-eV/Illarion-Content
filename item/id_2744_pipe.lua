57fb95f094f199b76fac087a8ea897ba9a29544d
faladron <wingalf@hotmail.de>
2012-04-18 22:52:29 +0200

    User Messaging Item done

--- item/id_2744_pipe.lua
+++ item/id_2744_pipe.lua
@@ -17,7 +17,8 @@ function UseItem( User, SourceItem, TargetItem, Counter, Param)
 		char = CheckIfOnline(playername)
 		if char then 
 			User:inform("Noob found!")
-			char:inform("Hier sendet "..message);
+			char:inform("Dir sendet "..User.name"folgende Nachricht: "..message);
+			User:eraseItem( 2744, 1 );
 		else
 		User:inform("Noob not found!")
 		end
