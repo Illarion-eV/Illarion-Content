3f087320c7e27e148cdb1afe530e98ce7fbafb5a
estralis <estralis@illarion.org>
2012-06-03 12:12:51 +0200

    Typos

--- triggerfield/noobia_light.lua
+++ triggerfield/noobia_light.lua
@@ -8,11 +8,11 @@ require("base.common")
 module("triggerfield.noobia_light", package.seeall)
 
 function MoveToField(Character)
-    -- for noobia: the char has to walk to a specific field (this triggerfield); he gets a message and we add a LTEvalue so that we remember he was at the field
+    -- for Noobia: the char has to walk to a specific field (this triggerfield); he gets a message and we add a LTEvalue so that we remember he was at the field
 	--find, myEffect = Character.effects:find(13)
 	--if find then
 	    base.common.InformNLS(Character,
-	    "Um mit dem Echsenmann zu sprechen, öffne die Sprechkonsole mit Enter, schreibe etwas und drücke wieder Enter. Alle NPCs (NonPlayerCharacters) reagiern auf bestimmte Signal Wörter wenn du in ihrer Nähe stehst. Versuche den Echsenmann um 'Hilfe' zu bitten. Ist die Sprachkonsole leer, kannst du sie mit erneutem drücken der Entertaste schließen.",
+	    "Um mit dem Echsenmann zu sprechen, öffne die Sprechkonsole mit Enter, schreibe etwas und drücke wieder Enter. Alle NPCs (NonPlayerCharacters) reagieren auf bestimmte Signalwörter wenn du in ihrer Nähe stehst. Versuche den Echsenmann um 'Hilfe' zu bitten. Ist die Sprachkonsole leer, kannst du sie mit erneutem Drücken der Entertaste schließen.",
 	    "@Rince: ziel erreicht 1 EN");
         --myEffect:addValue("noobiaLight",1)
 	--end
