a3ef61f1658b1a4dad91ec737e0075051bb692ba
estralis <estralis@illarion.org>
2012-06-05 23:35:51 +0200

    Trying to fix this triggerfield

--- triggerfield/noobia_henry.lua
+++ triggerfield/noobia_henry.lua
@@ -13,20 +13,26 @@ require("base.common")
 module("triggerfield.noobia_henry", package.seeall)
 
 function MoveToField(Character)
+
     -- for Noobia: the char has to walk to a specific field (this triggerfield); he gets a message and we add a LTEvalue so that we remember he was at the field
-	local callbackNewbie = function(dialogNewbie)
+
+    found, value = Character.effects:findValue("noobiaHenry");
+
+    if not value then --Didn't visit the triggerfield yet
+
+	    local callbackNewbie = function(dialogNewbie)
            find, myEffect = Character.effects:find(13)
 		   if find then
 		      myEffect:addValue("noobiaHenry",1)
 			end  
+	    end
+	
+	    if Character:getPlayerLanguage() == 0 then
+		    dialogNewbie = MessageDialog("Sehr gut!","Um mit dem Menschen zu sprechen, öffne die Sprachkonsole mit Return, schreibe ein Wort und drücke wieder Enter. Alle NPCs (NonPlayerCharacters) reagieren auf bestimmte Schlüsselwörter wenn du in ihrer Nähe stehst. Versuche den Menschen um 'Hilfe' zu bitten. Ist die Sprachkonsole leer, kannst du sie mit erneutem Drücken der Returntaste schließen.", callbackNewbie)
+	    else	
+		    dialogNewbie = MessageDialog("Very good!", "translation", callbackNewbie)
+	    end	
+	    Character:requestMessageDialog(dialogNewbie)
 	end
-	
-	if Character:getPlayerLanguage() == 0 then
-		dialogNewbie = MessageDialog("Sehr gut!","Um mit dem Menschen zu sprechen, öffne die Sprachkonsole mit Return, schreibe ein Wort und drücke wieder Enter. Alle NPCs (NonPlayerCharacters) reagieren auf bestimmte Schlüsselwörter wenn du in ihrer Nähe stehst. Versuche den Menschen um 'Hilfe' zu bitten. Ist die Sprachkonsole leer, kannst du sie mit erneutem Drücken der Returntaste schließen.", callbackNewbie)
-	else	
-		dialogNewbie = MessageDialog("Very good!", "translation", callbackNewbie)
-	end	
-	Character:requestMessageDialog(dialogNewbie)
-	
 end
 
