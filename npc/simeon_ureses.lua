108f72da05d0e16b4ea08de0ab2ffa1904160d30
estralis <estralis@illarion.org>
2012-06-12 19:43:46 +0200

    Mal schauen

--- npc/simeon_ureses.lua
+++ npc/simeon_ureses.lua
@@ -411,9 +411,65 @@ mainNPC:initDone();
 end;
 
 function receiveText(texttype, message, speaker) 
+
+--ADDITTION BY ESTRALIS
     if string.find(message,"price") or string.find(message,"cost") or string.find(message,"Preis") or string.find(message,"koste") or string.find(message,"repair") or string.find(message,"fix") or string.find(message,"reparier") or string.find(message,"instand") then --initiate repairing with triggers
-        thisNPC:talk(Character.say, "OMG LOL!"); --Message 1
-    else
+
+		--Full repair is the same as buying a new one. Just worth it with special (e.g. gemmed) items.
+		--Round prices to prevent prices like "1273 cp" and to prevent exact durability determination via repairing.
+		
+		theItem=GetFrontItem(thisNPC); --What item shall be repaired?
+		durability=theItem.quality-100*math.floor(theItem.quality/100); --calculate the durability
+		toRepair=99-durability; --the amount of durability points that has to repaired
+		price=math.ceil(theItem.worth*toRepair/1000)*10; --Price rounded up in 10 cp steps
+	    
+		--Defining the messages in english and german. State is used here. Please do NOT simply use the player's language because a german answer on an english request looks weird.
+		
+		--Please edit here!
+        message1={"Please put the item I shall repair on the table.","Packt den Gegenstand, den ich instandsetzen soll, einfach auf den Tisch."}; --No item found
+        message2={"I cannot repair this, sorry.","Entschuldigt, aber das kann ich nicht reparieren."}; --Priceless, perfect or stackable item
+        message3={"For repairing this item, I demand "..price.." copper coins.","Die Reparatur dieses Gegenstandes würde "..price.." Kupferstücke kosten."}; --Saying the price
+        message4={"You don't have enough money I suppose. I demand "..price.." copper coins for repairing this item.","Ihr habt anscheinend nicht genug Geld. Die Reparatur würde "..price.." Kupferstücke kosten."}; --Player is broke
+        message5={"#me repairs the item at a cost of "..price.." copper coins.","#me setzt den Gegenstand für "..price.." Kupferstücke in Stand."};	--...
+		--Stop editing here!
+
+		if string.find(message,"price") or string.find(message,"cost") or string.find(message,"repair") or string.find(message,"fix") then --english triggers
+            language = 1; --english
+		else
+		    language = 2; --german
+		end
+		
+		if theItem.id == 0 or theItem.id == 320 or theItem.id == nil then --there is nothing on the table!
+		
+		    thisNPC:talk(Character.say, message1(language)); --Message 1
+			
+		elseif theItem.worth == 0 or theItem.isStackable or durability==99 then --Cannot repair perfect, priceless or stackable items
+		
+		    thisNPC:talk(Character.say, message2(language)); --Message 2
+			
+		else -- I can repair it!
+		
+	        if string.find(message,"price") or string.find(message,"cost") or  string.find(message,"Preis") or string.find(message,"koste") then --player just wants to know the price
+			
+		        thisNPC:talk(Character.say, message3(language)); --Message 3
+				
+            elseif string.find(message,"repair") or string.find(message,"fix") or string.find(message,"reparier") or string.find(message,"instand") then --player wants to repair the item
+			
+			    if not npc.base.autonpcfunctions.CheckMoney(speaker,price) then --player is broke
+				
+		            thisNPC:talk(Character.say, message4 (language)); --Message 4
+					
+			    else --he has the money
+				
+                    thisNPC:talk(Character.say, message5 (language)); --Message 5
+                    npc.base.autonpcfunctions.PayTheNPC(speaker,price); --pay!
+                    theItem.quality=theItem.quality+toRepair; --repair!
+                    world:changeItem(theItem);
+					
+				end --broke/wealthy	
+			end --price/repair
+		end --there is an item
+	else
         mainNPC:receiveText(speaker, message); 
 	end
 end;
