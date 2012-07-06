87bedf5b0078121c76925026e9d614cd2ad6ed88
pharse <pharse@illarion.org>
2012-07-07 00:43:20 +0200

    testing

--- test/pharse.lua
+++ test/pharse.lua
@@ -23,12 +23,7 @@ testPos = position(0,0,0)
 function UseItem(User,SourceItem,TargetItem,counter,param,ltstate)
     User:inform("test");
     if User.lastSpokenText == "remove grain" then
-        User:inform("in remove grain");
-        User:inform("count: " .. User:countItem(249));
-        User:eraseItem(249, User:countItem(249));
-        User:inform("count: " .. User:countItem(249));
-        User:eraseItem(249, 255);
-        User:inform("count: " .. User:countItem(249));
+        User:eraseItem(259, User:countItem(259));
     end
 end
 
