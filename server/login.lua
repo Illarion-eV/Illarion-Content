2c6cedd523ebec9273fe5cb77d9f4ffacd5cc4bd
grokk <grokk@hotmail.co.uk>
2012-07-09 10:32:34 +1200

    Noobia login message update

--- server/login.lua
+++ server/login.lua
@@ -35,7 +35,7 @@ function onLogin( player )
 		if player:getPlayerLanguage() == 0 then		
 			dialogNewbie = MessageDialog("Willkommen bei Illarion!", "Willkommen bei Illarion, dem kostenlosen Online-Rollenspiel. Dieses Tutorial wird dich auf deinen ersten Schritten begleiten und dir die Bedienung des Spiels beibringen. Du kannst das Tutorial jederzeit überspringen indem du 'Tutorial überspringen' zu einem NPC sagst.", callbackNewbie);
 		else		
-			dialogNewbie = MessageDialog("Welcome to Illarion!", "Welcome to Illarion, the free online roleplaying game. This tutorial will guide you through your first steps and will show you the handling of the game. You may skip the tutorial at any time by saying 'skip tutorial' to a NPC.", callbackNewbie);
+			dialogNewbie = MessageDialog("Welcome to Illarion!", "Welcome to Illarion, the free online roleplaying game. This tutorial will guide you through your first steps and teach you the controls of the game. You may skip the tutorial at any time by saying 'skip tutorial' to a NPC.", callbackNewbie);
 		end	
 		
 		player:requestMessageDialog(dialogNewbie); --showing the welcome text
