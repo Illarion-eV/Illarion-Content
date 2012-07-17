335f3a0b1600288d857818773fc6cb9743402db4
estralis <estralis@illarion.org>
2012-07-17 21:01:17 +0200

    Translated

--- triggerfield/noobia_viola.lua
+++ triggerfield/noobia_viola.lua
@@ -20,7 +20,7 @@ function MoveToField(Character)
 	        local callbackNewbie = function(dialogNewbie) end; --empty callback
 			
 	        if Character:getPlayerLanguage() == 0 then
-		        dialogNewbie = MessageDialog("Tutorial","ÜBERSETZEN", callbackNewbie)
+		        dialogNewbie = MessageDialog("Tutorial","Dies ist nun die letzte Station des Tutorials. Wähle ein Reich aus, welchem dein Charakter zukünftig angehören wird. Gehe hierzu durch eines der Portale auf den kleinen Inseln. Du kannst diese Entscheidung später im Spiel jederzeit revidieren. Viola Baywillow kann dir einiges über die drei Reiche erzählen, frage sie einfach nach 'Hilfe'.", callbackNewbie)
 	        else	
 		        dialogNewbie = MessageDialog("Tutorial", "This is the final station of the tutorial. Please choose which realm you desire to be the home for your character by stepping through the corresponding portal on the three islands. You can reconsider this decision at any time once you have joined the game. Viola Baywillow will provide you with more information on the three available realms, just ask her for 'help'.", callbackNewbie)
 	        end	
