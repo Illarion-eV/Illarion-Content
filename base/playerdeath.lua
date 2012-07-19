c01b5a4de6c21c5b09c46699c07660b75013bbe5
estralis <estralis@illarion.org>
2012-07-19 19:48:37 +0200

    vilarion implemented server.playerdeath

--- base/playerdeath.lua
+++ base/playerdeath.lua
@@ -1,39 +1,19 @@
 require("base.common")
 module("base.playerdeath", package.seeall)
 
--- player got killed by direct a magic spell (summoned flames are no direct magic spells)
+-- This script is redundant to server.playerdeath. Please remove all calls of it!
+
+-- Player got killed by direct a magic spell (summoned flames are no direct magic spells)
 function playerKilledByMagic(Player, Killer)
-world:makeSound(25,Player.pos);
-showDeathDialog(Player);
---base.common.TempInformNLS( Player,"[Tod] Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.","[Death] You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.");
+
 end
 
 -- player got killed by another player / monster
 function playerKilledByFighting(Player, Killer)
-world:makeSound(25,Player.pos);
-showDeathDialog(Player);
---base.common.TempInformNLS( Player,"[Tod] Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.","[Death] You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.");
 end
 
 -- death reason unknown
 function playerKilled(Player)
-world:makeSound(25,Player.pos);
-showDeathDialog(Player);
---base.common.TempInformNLS( Player,"[Tod] Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.","[Death] You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.");
-end
-
-function showDeathDialog(Player)
-
-	local callback = function(nothing) end; --empty callback
-		
-	if Player:getPlayerLanguage() == 0 then		
-		dialog = MessageDialog("Tod", "Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.", callback);
-	else		
-		dialog = MessageDialog("Death", "You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.", callback);
-	end	
-	
-	Player:requestMessageDialog(dialog); --showing the text
-		
 end
 
 -- monster got killed by another player
--- server/playerdeath.lua
+++ server/playerdeath.lua
@@ -1,10 +1,28 @@
--- player death
+-- Player death
 
 -- deadPlayer - The player (character) whose hitpoints have just been set to zero
 
+require("base.common")
 module("server.playerdeath", package.seeall)
 
 function playerDeath(deadPlayer)
-    deadPlayer:talkLanguage(Character.say, Player.english, "I am dead!")
-    deadPlayer:talkLanguage(Character.say, Player.german, "Ich bin tot!")
+    world:makeSound(25,deadPlayer.pos);
+    showDeathDialog(deadPlayer);
+	
+    --vilarion: Please insert the death consequences here.
+	
+end
+
+function showDeathDialog(deadPlayer)
+
+	local callback = function(nothing) end; --empty callback
+		
+	if deadPlayer:getPlayerLanguage() == 0 then		
+		dialog = MessageDialog("Tod", "Du bist gestorben. Die Welt um dich herum verblasst und du bereitest dich darauf vor, den Göttern in Chergas Reich der Toten gegenüberzutreten.", callback);
+	else		
+		dialog = MessageDialog("Death", "You have died. The world around you fades and you prepare yourself to face the Gods in the afterlife of Cherga's Realm.", callback);
+	end	
+	
+	deadPlayer:requestMessageDialog(dialog); --showing the text
+		
 end
