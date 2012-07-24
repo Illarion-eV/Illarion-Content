7581b23f5f2f332767d3bbe74d89ad96985aa9a4
merung <karten-spiel-haus@hotmail.de>
2012-07-24 13:29:07 +0200

    two new scripts to split up the brewing script

---
+++ druid/base/gemdust.lua
@@ -0,0 +1,10 @@
+-- gemdust
+-- alchemy system: using gemdust infront of a cauldron - creating essence brew or enchant a stock
+
+require("base.common")
+require("druid.base.alchemy")
+
+module("druid.base.gemdust", package.seeall)
+
+function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
+end
---
+++ druid/base/herbs.lua
@@ -0,0 +1,10 @@
+-- herbs 
+-- alchemy system: using a herb infront of a cauldron - brewing
+
+require("base.common")
+require("druid.base.alchemy")
+
+module("druid.base.herbs", package.seeall)
+
+function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
+end
