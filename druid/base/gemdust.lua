3c96ff44ec7578458a6feafc0400637b470a7211
merung <karten-spiel-haus@hotmail.de>
2012-07-25 21:02:44 +0200

    new gemdust script filled

--- druid/base/gemdust.lua
+++ druid/base/gemdust.lua
@@ -7,4 +7,72 @@ require("druid.base.alchemy")
 module("druid.base.gemdust", package.seeall)
 
 function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
+
+   -- infront of a cauldron?
+   if base.common.GetFrontItemID(User) == 1008 then
+	  
+        -- is the char an alchemist?
+	    if User:getMagicType() ~= 3 then
+		  base.common.InformNLS( User,
+				"Nur jene, die in die Kunst der Alchemie eingeführt worden sind, können hier ihr Werk vollrichten.",
+				"Only those who have been introduced to the art of alchemy are able to work here."
+				)
+		  return;
+	    end
+
+        gemDust = druid.base.alchemy.CheckIfGemDust(User,SourceItem)
+	    if gemDust then -- security check if it is really gem dust
+	        BrewingGemDust(User,SourceItem,TargetItem,Counter,Param,ltstate)
+	    end
+    
+	else
+	    -- not infront of cauldron, maybe do something else with herbs
+        return
+	end
+end
+
+function BrewingGemDust(User,SourceItem,TargetItem,Counter,Param,ltstate)
+    cauldron = base.common.GetFronItem(User)
+
+	if SourceItem.id == 446 then --bluestone
+	   IdPotion = 165 -- id of the matching potion
+	elseif SourceItem.id == 447 then  -- ruby
+		   IdPotion = 59
+	elseif SourceItem.id == 448 then  -- emerald
+		   IdPotion = 327
+	elseif SourceItem.id ==	449 then  -- blackstone
+		   IdPotion = 329
+	elseif SourceItem.id == 450 then -- amethyst
+		   IdPotion = 166
+	elseif SourceItem.id == 451 then -- topaz
+		   IdPotion = 328
+	elseif SourceItem.id == 452 then -- diamond
+		   IdPotion = 330
+	end 
+	
+	if cauldron:getData("potienIdEffect")~="" then
+	    User:talkLanguage(Character.say, Player.german, "gemdust -> potion, fail")
+		-- define effect
+	
+    elseif cauldron:getData("cauldronFilledWith")=="essenceBrew" then
+	    User:talkLanguage(Character.say, Player.german, "gemdust -> potion, fail")
+		-- define effect
+		
+	elseif cauldron:getData("stockData")~= then
+	    if (SourceItem.id == 447) or (SourceItem.id == 450) then 
+		    potionEffectId = TargetItem:getData("stockData")
+	    else 
+		    potionEffectId = 0
+        end			
+	    cauldron:setData("potionId",IdPotion)
+		cauldron:setData("potionEffectId",potionEffectId)
+	    cauldron:setData("stockData","")
+		potionQuality = 999
+	    cauldron:setData("potionQuality",""..potionQuality)
+
+    elseif cauldron:getData("cauldronFilledWith")=="water" then
+		cauldron:setData("cauldronFilledWith","essenceBrew")
+	
+	else	
+	end	
 end
