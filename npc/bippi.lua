9599d7e95336cd90455cfdf88444dad3b27d0c6c
estralis <estralis@illarion.org>
2012-07-30 20:08:10 +0200

    Issue #6413 resolved.

--- npc/bippi.lua
+++ npc/bippi.lua
@@ -7,7 +7,7 @@
 --                                                                            --
 -- Author:   Ardian/Faladron/Rincewind                                        --
 --                                                                            --
--- Last parsing: March 12, 2011                          easyNPC Parser v1.02 --
+-- Last parsing: July 30, 2012                           easyNPC Parser v1.02 --
 --------------------------------------------------------------------------------
 
 --[[SQL
@@ -21,9 +21,6 @@ require("npc.base.condition.quest")
 require("npc.base.consequence.inform")
 require("npc.base.consequence.state")
 require("npc.base.talk")
-
-require("quest.aquest28");    --the quest file
-
 module("npc.bippi", package.seeall)
 
 function initNpc()
@@ -426,6 +423,34 @@ talkEntry:addResponse("Wenn ihr euch um meine Kühe kümmert, könnt ihr auch etwas
 talkEntry:addResponse("Nun, ich könnte Hände die zupacken können gebrauchen. Wollt ihr mir helfen?");
 talkEntry:addResponse("Könntet ihr mir helfen? Ich kann euch nicht viel dafür geben aber es würde mir viel bedeuten und mir einige Sorgen abnehmen.");
 talkEntry:addConsequence(npc.base.consequence.state.state("=", 1));
+talkingNPC:addTalkingEntry(talkEntry);
+end;
+if (true) then
+local talkEntry = npc.base.talk.talkNPCEntry();
+talkEntry:addTrigger("Yewdale");
+talkEntry:addResponse("Yewdale is the name of this place; just a cottage of peaceful farmers. There is not much to do here except watching the crops grow.");
+talkingNPC:addTalkingEntry(talkEntry);
+end;
+if (true) then
+local talkEntry = npc.base.talk.talkNPCEntry();
+talkEntry:addTrigger("Eibenthal");
+talkEntry:addTrigger("Eibental");
+talkEntry:addResponse("Eibenthal heißt dieser Ort hier; es ist einfach nur ein Hof friedlicher Bauern. Hier kann man nicht viel tun außer dem Gras beim Wachsen zuzusehen.");
+talkingNPC:addTalkingEntry(talkEntry);
+end;
+if (true) then
+local talkEntry = npc.base.talk.talkNPCEntry();
+talkEntry:addTrigger("Nana");
+talkEntry:addTrigger("Winterbutter");
+talkEntry:addCondition(npc.base.condition.language.language("english"));
+talkEntry:addResponse("Nana over there is the local cook. I overheard she might need help with a recipe of her.");
+talkingNPC:addTalkingEntry(talkEntry);
+end;
+if (true) then
+local talkEntry = npc.base.talk.talkNPCEntry();
+talkEntry:addTrigger("Nana");
+talkEntry:addTrigger("Winterbutter");
+talkEntry:addResponse("Nana dort drüben ist hier die Köchin. Ich hörte, sie könnte etwas Hilfe bei einem ihrer Rezepte gebrauchen.");
 talkingNPC:addTalkingEntry(talkEntry);
 end;
 if (true) then
@@ -950,9 +975,9 @@ if (true) then
 local talkEntry = npc.base.talk.talkNPCEntry();
 talkEntry:addTrigger("Bippi");
 talkEntry:addCondition(npc.base.condition.language.language("english"));
-talkEntry:addResponse("That?s my name and milk is my business.");
+talkEntry:addResponse("That's my name and milk is my business.");
 talkEntry:addResponse("Hiho!");
-talkEntry:addResponse("That?s me! Ask me again, I?ll tell ya the same.");
+talkEntry:addResponse("That's me! Ask me again, I?ll tell ya the same.");
 talkingNPC:addTalkingEntry(talkEntry);
 end;
 if (true) then
@@ -968,7 +993,7 @@ local talkEntry = npc.base.talk.talkNPCEntry();
 talkEntry:addTrigger("Yes");
 talkEntry:addResponse("That's true! Let's have a glass of milk to celebrate.");
 talkEntry:addResponse("Absolutely! Even my cows understand.");
-talkEntry:addResponse("#me nods leisurely: 'My grandfather used to say that all the time.");
+talkEntry:addResponse("#me nods leisurely: 'My grandfather used to say that all the time.'");
 talkingNPC:addTalkingEntry(talkEntry);
 end;
 if (true) then
@@ -1022,161 +1047,13 @@ mainNPC:setEquipment(9, 183);
 mainNPC:setEquipment(10, 369);
 mainNPC:setAutoIntroduceMode(true);
 
---------------explicit initialization----------
-    if not ExtraInit then
-		ExtraInit= true;
-    	cowname = {"Betsy", "Mjilka", "Cheeseball"}; --the cow names
-    end
--------------------------------------
-
-
 mainNPC:initDone();
 end;
 
-function receiveText(texttype, message, speaker) 
-if base.common. BasicNPCChecks(speaker,2, thisNPC) then
-	if string.find(message,"reset") then
-		speaker:setQuestProgress(29, 0);
-		speaker:setQuestProgress(28, 0);	
-
-		find, aquest28Effect =  speaker.effects:find(32);
-		if find then
-			speaker.aquest28Effect:removeEffect(32);
-		end
-		speaker:inform("Reset");
-	end	
-	
-	if ( (string.find(message,"Ich helfe euch")~=nil or string.find(message,"I help you")~=nil) and speaker:getQuestProgress(28)==0) then --here starts the quest with first task
-	
-		-- INITIALIZE THE QUEST STATUS 29 WHICH HOLDS VARIABLES OF THE QUEST
-		cowrnd = math.random(3); --choose cow by random
-		local task = 1; local cow = cowrnd; local counter = 6;
-		quest.aquest28.glue_questdata(speaker, cow, task, counter);
-		speaker:setQuestProgress(28, 1);
-
-		--TELL WHAT TO DO AND WHICH COW HE HAS
-		gText="Sehr gut, dann fangen wir an. Um euch nicht zu überfordern müsst ihr euch auch nur um eine Kuh kümmern und zwar um "..cowname[cowrnd]..". Ich werde euch täglich sagen welche Aufgaben ihr zu tun habt.";
-	    eText="Very good, let's start. You only have to take care of one cow namely "..cowname[cowrnd].." so it's not that hard. I will tell you daily what to do.";
-		outText=base.common.GetNLS(speaker,gText,eText);
-        thisNPC:talk(Character.say, outText);
-	
-		-- TELL the TASK TO DO
-		gText,eText = TaskText(task);
-		outText=base.common.GetNLS(speaker,gText,eText);
-        thisNPC:talk(Character.say, outText);
-        
-	elseif ((string.find(message,"[Hh]ilfe")~=nil or string.find(message,"[Hh]elp")~=nil) and speaker:getQuestProgress(28)>0) then
-		gText = "Schlüsselwörter: 'Wie lautet meine Aufgabe?','Gib mir einen Hinweis zur Aufgabe'";
-		eText = "Keyphrases : 'What is my task?','Give me a hint to the task'";
-		base.common.InformNLS(speaker, gText,eText);
-	elseif ((string.find(message,"[Hh]inweis")~=nil or string.find(message,"[Hh]int")~=nil) and speaker:getQuestProgress(28)>0) then
-
-		-- GIVE A HINT TO THE TASK
-		dummy1, task = quest.aquest28.split_questdata(speaker);
-	
-		dummy1,dummy2,gText,eText = TaskText(task);
-		outText=base.common.GetNLS(speaker,gText,eText);
-        thisNPC:talk(Character.say, outText);
-        
-	elseif ((string.find(message,"[Aa]ufgabe")~=nil or string.find(message,"[Tt]ask")~=nil) and speaker:getQuestProgress(28)>0) then
-	
-		aquest28Effect = speaker.effects:find(32); -- does effect #32 already exist?
-		local dummy_1, noTaskYet= quest.aquest28.split_questdata(speaker);
-		if (aquest28Effect == false and noTaskYet == 0) then --time delay of 1 day is finished, creat new task
-			quest.aquest28.chooseTask(speaker);
-		end
-	
-		-- TELL the TASK TO DO
-		dummy1, task = quest.aquest28.split_questdata(speaker);
-		
-		gText,eText = TaskText(task);
-		outText=base.common.GetNLS(speaker,gText,eText);
-        thisNPC:talk(Character.say, outText);
-        
-	else
-		mainNPC:receiveText(speaker, message); 
-	end
-
-
-end
-end;
-
+function receiveText(texttype, message, speaker) mainNPC:receiveText(speaker, message); end;
 function nextCycle() mainNPC:nextCycle(); end;
 function lookAtNpc(char, mode) mainNPC:lookAt(char, mode); end;
 function useNPC(char, counter, param) mainNPC:use(char); end;
-
-
-function TaskText(taskid)
-	if not TaskInit then
-		TaskInit = true;
-		gTask ={};
-		eTask ={};
-		
-        gTask[1] = {
-					desctxt="Meine Arme sind schwach. Heute beim Essen ist mir die Suppenschüssel aus der Hand gefallen. Bitte bürstet und striegelt die Kuh heute.",
-					helptxt="Nun mit der Hand wird das nichts. Sucht euch etwas mit dem bürsten könnt? Und immer mit der Fellrichtung streichen sonst wird die Milch sauer!"
-					};
-
-        eTask[1] = {
-					desctxt="My arms grew weak. I could barely lift the soup bowl while eating today. Please groom the cow.",
-					helptxt="Well, you can't groom with your hand, look for a tool to help you brush? And always comb from front to back or the cow will give sour milk."
-					};
-        gTask[2] = {
-					desctxt="Ich glaube ich bin erkältet. Heute mit kaltem Wasser zu arbeiten wäre mein Tod. Bitte wascht die Kuh heute.",
-					helptxt="Wasser ist zum Waschen da. Und mit den Händen werdet ihr es nicht tragen können. Neben dem Haus ist ein Brunnen aber nimm deinen eigenen Eimer dafür!"
-					};
-
-        eTask[2] = {
-					desctxt="I think I cought a cold. It'd be my death if I had to touch cold water now. Please wash the cow today.",
-					helptxt="You use water to wash, and you can't carry it in your hands. There's a well next to the house but bring your own bucket!"
-					};
-		gTask[3] = {
-					desctxt="Wenn ich singe wird die Milch besonders cremig, aber heute bin ich heiser. Singt der Kuh doch bitte ein Lied vor!",
-					helptxt="Ihr könnt nicht singen? Nun früher hab ich auch öfter mit der Laute vorgespielt aber meine Finger wollen nichtmehr richtig, probiert es damit?"
-					};
-
-        eTask[3] = {
-					desctxt="If I sing for my cows the milk turns out extraordinarily creamy, but I have a sore throat today. Please sing a song to the cow!",
-					helptxt="You can't sing? Well before my fingers got stiff I used to play the lute and they enjoyed that aswell, perhaps you should try that?"
-					};
-		gTask[4] = {
-					desctxt="Das Brunnenwasser ist seit heute morgen so faulig. Klettert bitte hinunter und seht einmal nach was da los ist.",
-					helptxt="Vielleicht hat sich Ungeziefer im Brunnen eingenistet? Ihr müsstet es vertreiben dann wird das Wasser bestimmt wieder klarer."
-					};
-
-        eTask[4] = {
-					desctxt="The water from the well tasted fowl today. Please climb down and check what's wrong.",
-					helptxt="Maybe there's vermin in the well? If so, you'd have to get rid of it, then the water will clear again."
-					};
-		gTask[5] = {
-					desctxt="Mein Kreuz macht mir zu schaffen, ich kann das Futter nicht in den Stall tragen. Bitte füttert heute die Kuh.",
-					helptxt="Sie mag besonders gerne Getreide."
-					};
-
-        eTask[5] = {
-					desctxt="My back aches! I can't carry the fodder to the stable. Please feed the cow today.",
-					helptxt="She likes grain the best."
-					};
-		gTask[6] = {
-					desctxt="Heute kannst du die Früchte deiner Arbeit ernten sozusagen! Du musst die Kuh nur noch melken.",
-					helptxt="Mir persönlich schmeckt die Milch aus der Flasche am besten. Man kann sie auch gleich an der Kuh befüllen."
-					};
-
-        eTask[6] = {
-					desctxt="Today you can reap what you've sown so to speak! All you have to do is milk the cow.",
-					helptxt="I prefer milk from the bottle. You can fill it straight at the cow too."
-					};
-					
-	end
-	if taskid == 0 then
-		gText = "Du hast die heutige Aufgabe schon erledigt, komm morgen wieder.";
-		eText = "You already finished the task for today, come tomorrow again.";
-	    return gText, eText,gText, eText;
-	end
-	return gTask[taskid].desctxt, eTask[taskid].desctxt, gTask[taskid].helptxt, eTask[taskid].helptxt;
-end
-
-
 initNpc();
 initNpc = nil;
 -- END
