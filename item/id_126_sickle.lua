5344cbe45d01be53a94df2889158c12a8ee1d89a
merung <karten-spiel-haus@hotmail.de>
2012-07-31 16:45:41 +0200

    testfunction for race change

--- item/id_126_sickle.lua
+++ item/id_126_sickle.lua
@@ -27,6 +27,12 @@ function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )
 		User:eraseItem(myItemId,deleteAmount)
 	end	
 	
+	if string.sub(User.lastSpokenText,1,7)=="setrace" then
+		length = string.len(User.lastSpokenText)
+		newRace = tonumber((string.sub(User.lastSpokenText,9,length)))
+		User:setAttrib("racetype",newRace)
+	end	
+	
 	if string.sub(User.lastSpokenText,1,14)=="potionEffectId" then
 		length = string.len(User.lastSpokenText)
 		potionEffectId = (string.sub(User.lastSpokenText,16,length))
