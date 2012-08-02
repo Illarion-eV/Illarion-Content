a0691cd616d4d7465225d9446de0cf8cc020ee68
merung <karten-spiel-haus@hotmail.de>
2012-08-02 19:38:22 +0200

    auto me for slime

--- lte/deathaftertime.lua
+++ lte/deathaftertime.lua
@@ -12,7 +12,10 @@ end
 function callEffect(Effect, Character) 
     posOfVic = Character.pos
    
-   findDeathGfx, deathGfx = Effect:findValue("deathGfx")
+   User:talkLanguage(Character.say, Player.german, "#me zerfällt zu einer blauen Pfütze.");
+   User:talkLanguage(Character.say, Player.english, "#me collapses into a blue puddle.");
+  
+  findDeathGfx, deathGfx = Effect:findValue("deathGfx")
    if findDeathGfx then -- in case we defined a Gfx shown on his death
 		world:Gfx(deathGfx,posOfVic)
         myList = world:getPlayersInRangeOf(posOfVic,10) 
