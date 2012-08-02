8f22d11d451813cf8c1d3797a4225ffaf19b19df
merung <karten-spiel-haus@hotmail.de>
2012-08-02 21:32:38 +0200

    debug

--- lte/deathaftertime.lua
+++ lte/deathaftertime.lua
@@ -14,12 +14,14 @@ function callEffect(Effect, Character)
     base.common.TalkNLS(Character, Character.say, "#me zerfällt zu einer blauen Pfütze.", "#me collapses into a blue puddle.")
    
   findDeathGfx, deathGfx = Effect:findValue("deathGfx")
+   local myList = world:getPlayersInRangeOf(cauldron.pos,1) -- we hurt everyone around the cauldron!
+	if not findDeathGfx then
+		for i=1,#myList do
+			base.common.HighInformNLS(myList[i], "fail", "fail")
+		end
+	end	
    if findDeathGfx then -- in case we defined a Gfx shown on his death
 		world:gfx(deathGfx,posOfVic)
-        myList = world:getPlayersInRangeOf(posOfVic,10) 
-        for i=1,#myList do
-		    base.common.InformNLS(myList[i], "debug 1", "debug 1")
-		end	
    end
    Character:increaseAttrib("hitpoints",-10000) -- die!
    return false
