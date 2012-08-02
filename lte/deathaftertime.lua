76f044bc31ee69575df2dae7c34235dc3db7b226
merung <karten-spiel-haus@hotmail.de>
2012-08-02 19:40:51 +0200

    character not user

--- lte/deathaftertime.lua
+++ lte/deathaftertime.lua
@@ -12,8 +12,8 @@ end
 function callEffect(Effect, Character) 
     posOfVic = Character.pos
    
-   User:talkLanguage(Character.say, Player.german, "#me zerfällt zu einer blauen Pfütze.");
-   User:talkLanguage(Character.say, Player.english, "#me collapses into a blue puddle.");
+   Character:talkLanguage(Character.say, Player.german, "#me zerfällt zu einer blauen Pfütze.");
+   Character:talkLanguage(Character.say, Player.english, "#me collapses into a blue puddle.");
   
   findDeathGfx, deathGfx = Effect:findValue("deathGfx")
    if findDeathGfx then -- in case we defined a Gfx shown on his death
