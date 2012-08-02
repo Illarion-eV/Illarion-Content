dfd660ba10d28548c1ce016e53bcf3ebb3e9bbac
merung <karten-spiel-haus@hotmail.de>
2012-08-02 15:40:09 +0200

    new lte

---
+++ lte/deathaftertime.lua
@@ -0,0 +1,28 @@
+-- KillAfterTime, id xxx
+-- to kill a monster (or a char) after some time
+
+
+require("base.common")
+
+function addEffect(Effect, Character)               				
+
+end
+
+function callEffect(Effect, Character) 
+
+   findDeathGfx, deathGfx = Effect:findValue("deathGfx")
+   if findDeathGfx then -- in case we defined a Gfx shown on his death
+		world:Gfx(deathGfx,Character.pos)
+   end
+   Character:increaseAttrib("hitpoints",-10000) -- die!
+   return false
+-- callEffect is only needed once, now 
+end
+
+function removeEffect(Effect,User)
+
+end
+
+function loadEffect(Effect,User)                  			
+
+end
