5875ed47e4b2237369b37937e1792a31a161bf2e
Martin Karing <nitram@illarion.org>
2012-08-21 18:25:57 +0200

    Trying to fix buying problem with the trading NPCs

--- npc/base/trade.lua
+++ npc/base/trade.lua
@@ -119,7 +119,7 @@ end;
 
 function tradeNPC:sellItemToPlayer(npcChar, player, itemIndex, amount)
 	player:inform("Sell item to player! Index: " .. itemIndex);
-    local item = self._sellItems[itemIndex];
+    local item = self._sellItems[itemIndex + 1];
     if (item == nil) then
         base.common.InformNLS(player, "Ein Fehler ist beim Kauf des Items aufgetreten", "An error occurred while buying the item");
         return;
