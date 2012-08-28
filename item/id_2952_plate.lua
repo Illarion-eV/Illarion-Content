80a039caed1801146a249cc6f8d5c1be3de37e71
merung <karten-spiel-haus@hotmail.de>
2012-08-28 21:09:52 +0200

    removed moveitem functions

--- item/id_2952_plate.lua
+++ item/id_2952_plate.lua
@@ -9,12 +9,3 @@ function LookAtItem(User,Item)
         world:itemInform( User,Item,base.common.GetNLS( User, "Teller", "plate") )
     end
 end
-
-function CharacterOnField(User)
-    if User.pos == position(891,390,0) then -- cherga temple, infront of Mirror of Death
-	    User:increaseAttrib("hitpoints",-500)
-		world:gfx(44,User.pos)
-		User:move(6,true)
-		base.common.HighInformNLS(User,"Deine Füße brennen schmerzhaft und du springst schnell weg.","Your feet brun painfully and you leap aside.")
-	end	
-end
