50e06b601a6837f015794b2df0d206af08361f3b
merung <karten-spiel-haus@hotmail.de>
2012-09-01 20:08:20 +0200

    lte for labour camp

---
+++ lte/labourcamp.lua
@@ -0,0 +1,17 @@
+require("base.common")
+module("lte.labourcamp", package.seeall)
+
+-- INSERT INTO longtimeeffects VALUES (55, 'labourcamp', 'lte.labourcamp');
+
+
+function addEffect(Effect, User)
+end
+
+function callEffect(Effect,User)
+end
+
+function removeEffect(Effect,User)
+end
+
+function loadEffect(Effect,User)
+end
