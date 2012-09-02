9809302dd089b979249da87b89df30ea0df0715d
merung <karten-spiel-haus@hotmail.de>
2012-09-02 16:37:53 +0200

    small changes

--- lte/labourcamp.lua
+++ lte/labourcamp.lua
@@ -8,6 +8,18 @@ function addEffect(Effect, User)
 end
 
 function callEffect(Effect,User)
+
+    if User.pos.z ~= -40 then -- security check: char not in labour camp, remove lte
+	    return false
+	end	
+	
+	local findCounter,spamProtection_1 = Effect:findValue("spamProtection_1")
+    if findCounter then
+	   spamProtection_1 = base.common.Limit((spamProtection_1 - 1), 0, 5)
+	   Effect:addValue("spamProtection_1",spamProtection_1)
+    end
+   
+    return true
 end
 
 function removeEffect(Effect,User)
