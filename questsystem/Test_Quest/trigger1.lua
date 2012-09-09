e459458eeb2a729b391a72857f847ef5e9b92456
grokk <grokk@hotmail.co.uk>
2012-09-09 15:58:22 +1200

    EasyQuest test quest version 3

--- questsystem/Test_Quest/trigger1.lua
+++ questsystem/Test_Quest/trigger1.lua
@@ -2,7 +2,7 @@ require("handler.sendmessagetoplayer")
 require("questsystem.base")
 module("questsystem.Illarion.trigger1", package.seeall)
 
-local QUEST_NUMBER = 10000
+local QUEST_NUMBER = 13001
 local PRECONDITION_QUESTSTATE = 0
 local POSTCONDITION_QUESTSTATE = 0
 
