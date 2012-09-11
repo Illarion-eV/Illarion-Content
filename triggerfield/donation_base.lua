3a1f8480bf2c47f17a3ef406ff0369ba7af0f1cd
estralis <estralis@illarion.org>
2012-09-11 19:06:02 +0200

    Community trashcan

---
+++ triggerfield/donation_base.lua
@@ -0,0 +1,11 @@
+require("base.common")
+
+module("triggerfield.donation_base", package.seeall)
+
+function donate(Item,User,FactionID,FactionName,LeaderName,Treasury)
+
+    -- get the item stats
+	-- worthless item -> inform
+	-- item with worth -> inform, donate to treasury
+
+end
