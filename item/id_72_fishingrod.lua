07952b6490f33267307109534ed797e070f49ef0
pharse <pharse@illarion.org>
2012-09-21 15:47:15 +0200

    * added box-muller algo for gaussian distribution; * gathering...

--- item/id_72_fishingrod.lua
+++ item/id_72_fishingrod.lua
@@ -9,105 +9,114 @@ require("content.gathering")
 
 module("item.id_72_fishingrod", package.seeall, package.seeall(item.general.wood))
 
-
-function UseItem(User, SourceItem, TargetItem, Counter, Param, ltstate)
+function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )
 	content.gathering.InitGathering();
-	
-	TargetPos = base.common.GetFrontPosition(User);
-	
-    base.common.ResetInterruption(User, ltstate);
-	if (ltstate == Action.abort) then -- Arbeit unterbrochen
-        if (User:increaseAttrib("sex", 0) == 0) then
-            gText = "seine";
-            eText = "his";
-        else
-            gText = "ihre";
-            eText = "her";
-        end
-        User:talkLanguage(Character.say, Player.german, "#me unterbricht "..gText.." Arbeit.");
-        User:talkLanguage(Character.say, Player.english,"#me interrupts "..eText.." work.");
-        return
-    end
-
-    if not base.common.CheckItem(User, SourceItem) then -- Sicherheitscheck
-        return
-    end
-	
-	-- Angeln unterirdisch nicht möglich
-    if (TargetPos.z < 0) then
-    	base.common.InformNLS(User, "In unterirdischen Wasserlöchern wird das Angeln kaum erfolgreich sein.", "Fishing in underground waterholes wouldn't be successful.");
-		return
-    end
-	
-	if base.common.Encumbrence(User) then -- Behinderung
-        base.common.InformNLS(User,
-        "Deine Rüstung behindert dich beim Fischen.",
-        "Your armour disturbes you while fishing.");
-        return
-    end
-	
-	if (SourceItem:getType() ~= 4) then -- Angel in der Hand?
-        base.common.InformNLS(User,
-        "Du solltest die Angel in der Hand haben, wenn du angeln willst.",
-        "You should have the fishing rod in your hand if you want to catch any fish.");
-        return
-    end
-	
-    if (world:getField(TargetPos):tile() ~= 6) then
-        base.common.InformNLS(User,
-        "Die Chance im Wasser einen Fisch zu fangen ist bedeutend höher als auf dem Land.",
-        "The chance to catch a fish is much higher in the water than on the land.");
-        return
-    end
-	
-	if not base.common.IsLookingAt(User, TargetPos) then -- Drehen wenn nötig
-        base.common.TurnTo(User, TargetPos);
-    end
-	
 	local fishing = content.gathering.fishing;
 	
-	if (ltstate == Action.none) then -- Untätig: Starte Angeln!
-        fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User, SourceItem);
-		User:startAction(fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
-        User:talkLanguage(Character.say, Player.german, "#me beginnt zu fischen.");
-        User:talkLanguage(Character.say, Player.english, "#me starts fishing.");
-        return
-    end
-	
-	if not fishing:FindRandomItem(User) then
+	local TargetPos = base.common.GetFrontPosition(User);
+
+	base.common.ResetInterruption( User, ltstate );
+	if ( ltstate == Action.abort ) then -- Arbeit unterbrochen
+		if (User:increaseAttrib("sex",0) == 0) then
+			gText = "seine";
+			eText = "his";
+		else
+			gText = "ihre";
+			eText = "her";
+		end
+		User:talkLanguage(Character.say, Player.german, "#me unterbricht "..gText.." Arbeit.");
+		User:talkLanguage(Character.say, Player.english,"#me interrupts "..eText.." work.");
+		return
+	end
+
+	if not base.common.CheckItem( User, SourceItem ) then -- security check
 		return
 	end
 	
-	-- Spieler fischt bereits
-	User:learn( fishing.LeadSkill, fishing.LeadSkillGroup, fishing.SavedWorkTime[User.id], 100, User:increaseAttrib(fishing.LeadAttribute,0) );
-	fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User, SourceItem);
-	local chance = math.random(1,10)
-	if(chance <= 3) then -- Skill wird nur noch bei GenWorkTime beachtet, Chance beträgt 30% für Lachs
-		local notcreated = User:createItem(73, 1, 333, 0);
-		if(notcreated > 0) then
-			world:createItemFromId(73, notcreated, User.pos, true, 333, 0);
-			base.common.InformNLS(User, "Du kannst nichts mehr halten!", "You can't carry anymore!");
-			return false
-		end
-	elseif(chance <= 9) then -- Skill wird nur noch bei GenWorkTime beachtet, Chance beträgt 60% für Forelle
-		local notcreated = User:createItem(355, 1, 333, 0);
-		if(notcreated > 0) then
-			world:createItemFromId(355, notcreated, User.pos, true, 333, 0);
-			base.common.InformNLS(User, "Du kannst nichts mehr halten!", "You can't carry anymore!");
-			return false
-		end
+	if (SourceItem:getType() ~= 4) then -- tool in Hand
+		base.common.InformNLS( User,
+		"Du musst die Angel in der Hand haben!",
+		"You need to hold the fishing rod in your hand!" );
+		return
 	end
 	
-	-- GFX + Sound
-	world:gfx(11,TargetPos);
-    world:makeSound(9,TargetPos);
+	-- any other checks?
 	
-	if base.common.ToolBreaks(User, SourceItem, true) then -- Angel beschädigen
-        User:talkLanguage(Character.say, Player.german, "#me lässt die Angel aus den Händen rutschen und die Angel sinkt auf den Grund.");
-        User:talkLanguage(Character.say, Player.english, "#me lets the fishing rod slip out of the hands and it sinks to the ground.");
-        return
-    end
-    User:startAction(fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
+	if (world:getField(TargetPos):tile() ~= 6) then -- fishing only possible on water tiles
+		base.common.InformNLS(User,
+		"Die Chance im Wasser einen Fisch zu fangen ist bedeutend höher als auf dem Land.",
+		"The chance to catch a fish is much higher in the water than on the land.");
+		return
+	end
+	
+	if (TargetPos.z < 0) then -- fishing underground is not possible
+		base.common.InformNLS(User, 
+		"In unterirdischen Wasserlöchern wird das Angeln kaum erfolgreich sein.", 
+		"Fishing in underground waterholes wouldn't be successful.");
+		return
+	end
+
+	if base.common.Encumbrence(User) then
+		base.common.InformNLS( User,
+		"Deine Rüstung behindert Dich beim Fischen.",
+		"Your armour disturbes you when fishing." );
+		return
+	end
+
+	if not base.common.FitForWork( User ) then -- check minimal food points
+		return
+	end
+
+	if not base.common.IsLookingAt( User, TargetPos ) then -- check looking direction
+		base.common.TurnTo( User, TargetPos ); -- turn if necessary
+	end
+
+	if ( ltstate == Action.none ) then -- currently not working -> let's go
+		fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User,nil,false);
+		User:startAction( fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
+		User:talkLanguage( Character.say, Player.german, "#me beginnt zu fischen.");
+		User:talkLanguage( Character.say, Player.english, "#me starts to fish."); 
+		return
+	end
+
+	-- since we're here, we're working
+
+	if fishing:FindRandomItem(User) then
+		return
+	end
+
+	User:learn( fishing.LeadSkill, fishing.LeadSkillGroup, fishing.SavedWorkTime[User.id], 100, User:increaseAttrib(fishing.LeadAttribute,0) );
+	local amount = 1; -- set the amount of items that are produced
+	local fishID = 0; 
+	local chance = math.random(1,10);
+	-- 40% for salmon, 60% for trout
+	if (chance <= 3) then 
+		fishID = 73; -- salmon
+	else
+		fishID = 355; -- trout;
+	end
+	
+	-- GFX + Sound for a splash
+	world:gfx(11,TargetPos);
+	world:makeSound(9,TargetPos);
+	
+	local notCreated = User:createItem( fishID, amount, 333 ,0); -- create the new produced items
+	if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
+		world:createItemFromId( fishID, notCreated, User.pos, true, 333 ,0);
+		base.common.InformNLS(User,
+		"Du kannst nichts mehr halten.",
+		"You can't carry any more.");
+	else -- character can still carry something
+		fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User,nil,false);
+		User:startAction( fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
+	end
+
+	if base.common.ToolBreaks( User, SourceItem, true ) then -- damage and possibly break the tool
+		base.common.InformNLS(User,
+		"Deine alte Angel zerbricht.",
+		"Your old fishing rod breaks.");
+		return
+	end
 end
 
 function LookAtItem( User, Item )
