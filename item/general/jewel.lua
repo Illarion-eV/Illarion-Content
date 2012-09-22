eb96b395d8195d808efff779827456ef1eb418de
merung <karten-spiel-haus@hotmail.de>
2012-09-22 21:29:58 +0200

    new jewle stuff

--- item/general/jewel.lua
+++ item/general/jewel.lua
@@ -14,3 +14,21 @@ module("item.general.jewel", package.seeall)
 function LookAtItem(user, item)
     world:itemInform(user, item, base.lookat.GenerateLookAt(user, item, base.lookat.JEWELERY));
 end;
+
+function UseJewel_67 (User, SourceItem)
+    -- ruby amulett
+
+end
+
+function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )
+    -- list with jewles and the functions belonging to them
+    UseMe={}
+	-- UseMe[ITEMID] = function(...) UseJewl_ITEMID(...) end
+	UseMe[67] = function(User, SourceItem) UseJewel_67(User, SourceItem) end -- ruby amulett
+	
+	if not UseMe[SourceItem.id] then -- security check
+	    return -- if the jewel is not defined yet, we return
+    else
+        UseMe[SourceItem.id](User, SourceItem, TargetItem, Counter, Param, ltstate)
+    end    	
+end 
