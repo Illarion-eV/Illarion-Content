e7554c8ad51f4d70d2fc21064a550919c3e7986d
Miriam <marionline_96@yahoo.de>
2012-09-23 14:28:21 +0200

    bragon's flame  circle for bragon's ruby amulett

--- item/general/jewel.lua
+++ item/general/jewel.lua
@@ -16,14 +16,13 @@ function LookAtItem(user, item)
     world:itemInform(user, item, base.lookat.GenerateLookAt(user, item, base.lookat.JEWELERY));
 end;
 
-function UseJewel_67 (User, SourceItem)
 
 -- ruby amulett
 function UseJewel_67 (User, SourceItem)
 
    if ( SourceItem:getData("BragonAmulettCircle") == "true" ) then
 	  -- use the function CreateCircle , radius 1 and call funktion Event (posi) and cetate a flameillusion cricle
-	  base.common.CreateCircle(User.pos, 1, function(posi) world:createItemFromId(359,1,posi,true,45,nil end)
+	  base.common.CreateCircle(User.pos, 1, function(posi) world:createItemFromId(359,1,posi,true,45,nil end))
    end
 end
 
@@ -32,10 +31,10 @@ function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )
     UseMe={}
 	-- UseMe[ITEMID] = function(...) UseJewl_ITEMID(...) end
 	UseMe[67] = function(User, SourceItem) UseJewel_67(User, SourceItem) end -- ruby amulett
-             	
+
                     if not UseMe[SourceItem.id] then -- security check
 	    return -- if the jewel is not defined yet, we return
     else
         UseMe[SourceItem.id](User, SourceItem, TargetItem, Counter, Param, ltstate)
-    end    	
-end 
+    end
+end
