1fe981933290ae010fc25d009c6e785d8ceea276
estralis <estralis@illarion.org>
2012-10-08 21:29:18 +0200

    Time for each step cut by half.

--- item/id_2715_plane.lua
+++ item/id_2715_plane.lua
@@ -55,91 +55,91 @@ function InitCraftingTool( )
         local CurrentCatID = carpentry:AddCategory( 2715 ) -- Tools and Other Items
     
      --Slicer
-local product = carpentry:AddProduct( CurrentCatID, 2715, { 0, 20}, 1, { 10, 20 } );
+local product = carpentry:AddProduct( CurrentCatID, 2715, { 0, 20}, 1, { 5, 10 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 1 ); -- Apple Wooden Boards: 1x1
 product:AddProductionSteps( { 2535, 1, "all" }, 1 ); -- Iron Ingot: 1x1
 
 --Plate
-product = carpentry:AddProduct( CurrentCatID, 2952, { 0, 20}, 1, { 10, 20 } );
+product = carpentry:AddProduct( CurrentCatID, 2952, { 0, 20}, 1, { 5, 10 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 1 ); -- Apple Wooden Boards: 1x1
 
 --Bowl
-product = carpentry:AddProduct( CurrentCatID, 2935, { 0, 20}, 1, { 10, 20 } );
+product = carpentry:AddProduct( CurrentCatID, 2935, { 0, 20}, 1, { 5, 10 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 1 ); -- Apple Wooden Boards: 1x1
 
 --Wooden Cup
-product = carpentry:AddProduct( CurrentCatID, 2185, { 3, 23}, 1, { 12, 24 } );
+product = carpentry:AddProduct( CurrentCatID, 2185, { 3, 23}, 1, { 6, 12 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 1 ); -- Apple Wooden Boards: 1x1
 
 --Comb
-product = carpentry:AddProduct( CurrentCatID, 2719, { 6, 26}, 1, { 13, 26 } );
+product = carpentry:AddProduct( CurrentCatID, 2719, { 6, 26}, 1, { 6, 12 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 1 ); -- Apple Wooden Boards: 1x1
 
 --Cooking Spoon
-product = carpentry:AddProduct( CurrentCatID, 227, { 9, 29}, 1, { 15, 30 } );
+product = carpentry:AddProduct( CurrentCatID, 227, { 9, 29}, 1, { 7, 14 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 1 ); -- Apple Wooden Boards: 1x1
 
 --Nailboard
-product = carpentry:AddProduct( CurrentCatID, 2659, { 10, 30}, 1, { 15, 30 } );
+product = carpentry:AddProduct( CurrentCatID, 2659, { 10, 30}, 1, { 7, 14 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 1 ); -- Apple Wooden Boards: 1x1
 product:AddProductionSteps( { 2738, 1, "all" }, 1 ); -- Nails: 1x1
 
 --Wooden Shovel
-product = carpentry:AddProduct( CurrentCatID, 312, { 12, 32}, 1, { 16, 32 } );
+product = carpentry:AddProduct( CurrentCatID, 312, { 12, 32}, 1, { 8, 16 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 2 ); -- Apple Wooden Boards: 2x1
 
 --Candle Mold
-product = carpentry:AddProduct( CurrentCatID, 429, { 15, 35}, 1, { 18, 36 } );
+product = carpentry:AddProduct( CurrentCatID, 429, { 15, 35}, 1, { 9, 18 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 2 ); -- Apple Wooden Boards: 2x1
 
 --Bucket
-product = carpentry:AddProduct( CurrentCatID, 51, { 18, 38}, 1, { 19, 38 } );
+product = carpentry:AddProduct( CurrentCatID, 51, { 18, 38}, 1, { 9, 18 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 3 ); -- Apple Wooden Boards: 3x1
 
 --Peel
-product = carpentry:AddProduct( CurrentCatID, 121, { 21, 41}, 1, { 21, 42 } );
+product = carpentry:AddProduct( CurrentCatID, 121, { 21, 41}, 1, { 10, 20 } );
 product:AddProductionSteps( { 2716, 1, "all" }, 2 ); -- Apple Wooden Boards: 2x1
 
 CurrentCatID = carpentry:AddCategory( 2646 ) --bows
 
 --Shortbow
-product = carpentry:AddProduct( CurrentCatID, 65, { 20, 40}, 1, { 20, 40 } );
+product = carpentry:AddProduct( CurrentCatID, 65, { 20, 40}, 1, { 10, 20 } );
 product:AddProductionSteps( { 546, 1, "all" }, 3 ); -- Naldor Wooden Boards: 3x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 
 --Serinjah-Rider's Bow
-product = carpentry:AddProduct( CurrentCatID, 2646, { 30, 50}, 1, { 25, 50 } );
+product = carpentry:AddProduct( CurrentCatID, 2646, { 30, 50}, 1, { 12, 24 } );
 product:AddProductionSteps( { 546, 1, "all" }, 3 ); -- Naldor Wooden Boards: 3x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 
 --Hunting Bow
-product = carpentry:AddProduct( CurrentCatID, 2714, { 40, 60}, 1, { 30, 60 } );
+product = carpentry:AddProduct( CurrentCatID, 2714, { 40, 60}, 1, { 15, 30 } );
 product:AddProductionSteps( { 546, 1, "all" }, 4 ); -- Naldor Wooden Boards: 4x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 
 --Serinjah Firebow
-product = carpentry:AddProduct( CurrentCatID, 2727, { 50, 70}, 1, { 35, 70 } );
+product = carpentry:AddProduct( CurrentCatID, 2727, { 50, 70}, 1, { 17, 34 } );
 product:AddProductionSteps( { 546, 1, "all" }, 4 ); -- Naldor Wooden Boards: 4x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 
 --Ebony Wooden Bow
-product = carpentry:AddProduct( CurrentCatID, 2780, { 60, 80}, 1, { 40, 80 } );
+product = carpentry:AddProduct( CurrentCatID, 2780, { 60, 80}, 1, { 20, 40 } );
 product:AddProductionSteps( { 546, 1, "all" }, 5 ); -- Naldor Wooden Boards: 5x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 
 --Elven Composite Longbow
-product = carpentry:AddProduct( CurrentCatID, 2718, { 70, 90}, 1, { 45, 90 } );
+product = carpentry:AddProduct( CurrentCatID, 2718, { 70, 90}, 1, { 22, 44 } );
 product:AddProductionSteps( { 546, 1, "all" }, 5 ); -- Naldor Wooden Boards: 5x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 
 --Crossbow
-product = carpentry:AddProduct( CurrentCatID, 70, { 80, 100}, 1, { 50, 100 } );
+product = carpentry:AddProduct( CurrentCatID, 70, { 80, 100}, 1, { 25, 50 } );
 product:AddProductionSteps( { 546, 1, "all" }, 6 ); -- Naldor Wooden Boards: 6x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 product:AddProductionSteps( { 2535, 1, "all" }, 2 ); -- Iron Ingots: 2x1
 
 --Magical Elven Bow
-product = carpentry:AddProduct( CurrentCatID, 2685, { 90, 110}, 1, { 55, 110 } );
+product = carpentry:AddProduct( CurrentCatID, 2685, { 90, 110}, 1, { 27, 54 } );
 product:AddProductionSteps( { 546, 1, "all" }, 7 ); -- Naldor Wooden Boards: 7x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 product:AddProductionSteps( { 2551, 1, "all" }, 1 ); -- Pure Air: 1x1
@@ -148,110 +148,110 @@ product:AddProductionSteps( { 284, 1, "all" }, 2 ); -- Bluestone: 2x1
 CurrentCatID = carpentry:AddCategory( 90 ) --instruments
 
 --Horn
-product = carpentry:AddProduct( CurrentCatID, 333, { 10, 20}, 1, { 15, 30 } );
+product = carpentry:AddProduct( CurrentCatID, 333, { 10, 20}, 1, { 7, 14 } );
 product:AddProductionSteps( { 545, 1, "all" }, 2 ); -- Cherry Wooden Boards: 2x1
 
 --Flute
-product = carpentry:AddProduct( CurrentCatID, 90, { 20, 30}, 1, { 20, 40 } );
+product = carpentry:AddProduct( CurrentCatID, 90, { 20, 30}, 1, { 10, 20 } );
 product:AddProductionSteps( { 545, 1, "all" }, 3 ); -- Cherry Wooden Boards: 3x1
 
 --Panpipe
-product = carpentry:AddProduct( CurrentCatID, 532, { 30, 40}, 1, { 20, 40 } );
+product = carpentry:AddProduct( CurrentCatID, 532, { 30, 40}, 1, { 10, 20 } );
 product:AddProductionSteps( { 545, 1, "all" }, 3 ); -- Cherry Wooden Boards: 3x1
 
 --Drum
-product = carpentry:AddProduct( CurrentCatID, 533, { 40, 50}, 1, { 25, 50 } );
+product = carpentry:AddProduct( CurrentCatID, 533, { 40, 50}, 1, { 12, 24 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 2 ); -- Conifer Wooden Boards: 2x1
 product:AddProductionSteps( { 2547, 1, "all" }, 2 ); -- Leather: 2x1
 
 --Lute
-product = carpentry:AddProduct( CurrentCatID, 335, { 50, 60}, 1, { 30, 60 } );
+product = carpentry:AddProduct( CurrentCatID, 335, { 50, 60}, 1, { 15, 30 } );
 product:AddProductionSteps( { 545, 1, "all" }, 2 ); -- Cherry Wooden Boards: 2x1
 product:AddProductionSteps( { 50, 1, "all" }, 2 ); -- Thread: 2x1
 
 --Harp
-product = carpentry:AddProduct( CurrentCatID, 332, { 60, 70}, 1, { 35, 70 } );
+product = carpentry:AddProduct( CurrentCatID, 332, { 60, 70}, 1, { 17, 34 } );
 product:AddProductionSteps( { 545, 1, "all" }, 5 ); -- Cherry Wooden Boards: 5x1
 product:AddProductionSteps( { 51, 1, "all" }, 3 ); -- Thread: 3x1
 
 CurrentCatID = carpentry:AddCategory( 18 ) --shields
 
 --Small wooden Shield
-product = carpentry:AddProduct( CurrentCatID, 2445, { 10, 30}, 1, { 15, 30 } );
+product = carpentry:AddProduct( CurrentCatID, 2445, { 10, 30}, 1, { 7, 14 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 3 ); -- Conifer Wooden Boards: 3x1
 product:AddProductionSteps( { 2547, 1, "all" }, 1 ); -- Leather: 1x1
 
 --Wooden Shield
-product = carpentry:AddProduct( CurrentCatID, 17, { 17, 37}, 1, { 19, 38 } );
+product = carpentry:AddProduct( CurrentCatID, 17, { 17, 37}, 1, { 9, 18 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 3 ); -- Conifer Wooden Boards: 3x1
 product:AddProductionSteps( { 2547, 1, "all" }, 1 ); -- Leather: 1x1
 
 --Light Metal shield
-product = carpentry:AddProduct( CurrentCatID, 18, { 24, 44}, 1, { 22, 44 } );
+product = carpentry:AddProduct( CurrentCatID, 18, { 24, 44}, 1, { 11, 22 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 3 ); -- Conifer Wooden Boards: 3x1
 product:AddProductionSteps( { 2547, 1, "all" }, 1 ); -- Leather: 1x1
 product:AddProductionSteps( { 2535, 1, "all" }, 1 ); -- Iron Ingots: 1x1
 
 --Round Metal shield
-product = carpentry:AddProduct( CurrentCatID, 186, { 31, 51}, 1, { 26, 52 } );
+product = carpentry:AddProduct( CurrentCatID, 186, { 31, 51}, 1, { 13, 26 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 4 ); -- Conifer Wooden Boards: 4x1
 product:AddProductionSteps( { 2547, 1, "all" }, 1 ); -- Leather: 1x1
 product:AddProductionSteps( { 2535, 1, "all" }, 1 ); -- Iron Ingots: 1x1
 
 --Metal shield
-product = carpentry:AddProduct( CurrentCatID, 19, { 38, 58}, 1, { 29, 58 } );
+product = carpentry:AddProduct( CurrentCatID, 19, { 38, 58}, 1, { 14, 28 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 4 ); -- Conifer Wooden Boards: 4x1
 product:AddProductionSteps( { 2547, 1, "all" }, 1 ); -- Leather: 1x1
 product:AddProductionSteps( { 2535, 1, "all" }, 1 ); -- Iron Ingots: 1x1
 
 --Large Metal Shield
-product = carpentry:AddProduct( CurrentCatID, 20, { 45, 65}, 1, { 33, 66 } );
+product = carpentry:AddProduct( CurrentCatID, 20, { 45, 65}, 1, { 16, 32 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 4 ); -- Conifer Wooden Boards: 4x1
 product:AddProductionSteps( { 2547, 1, "all" }, 2 ); -- Leather: 2x1
 product:AddProductionSteps( { 2535, 1, "all" }, 2 ); -- Iron Ingots: 2x1
 
 --Cursed Shield
-product = carpentry:AddProduct( CurrentCatID, 917, { 52, 72}, 1, { 36, 72 } );
+product = carpentry:AddProduct( CurrentCatID, 917, { 52, 72}, 1, { 18, 36 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 5 ); -- Conifer Wooden Boards: 5x1
 product:AddProductionSteps( { 2547, 1, "all" }, 2 ); -- Leather: 2x1
 
 --Red Steel Shield
-product = carpentry:AddProduct( CurrentCatID, 2388, { 59, 79}, 1, { 40, 80 } );
+product = carpentry:AddProduct( CurrentCatID, 2388, { 59, 79}, 1, { 20, 40 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 5 ); -- Conifer Wooden Boards: 5x1
 product:AddProductionSteps( { 2547, 1, "all" }, 2 ); -- Leather: 2x1
 product:AddProductionSteps( { 2535, 1, "all" }, 2 ); -- Iron Ingots: 2x1
 
 --Legionaire's Tower shield
-product = carpentry:AddProduct( CurrentCatID, 2448, { 66, 86}, 1, { 43, 86 } );
+product = carpentry:AddProduct( CurrentCatID, 2448, { 66, 86}, 1, { 21, 42 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 5 ); -- Conifer Wooden Boards: 5x1
 product:AddProductionSteps( { 2547, 1, "all" }, 2 ); -- Leather: 2x1
 
 --Heraldic Shield
-product = carpentry:AddProduct( CurrentCatID, 95, { 73, 93}, 1, { 47, 94 } );
+product = carpentry:AddProduct( CurrentCatID, 95, { 73, 93}, 1, { 23, 46 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 6 ); -- Conifer Wooden Boards: 6x1
 product:AddProductionSteps( { 2547, 1, "all" }, 2 ); -- Leather: 2x1
 
 --Steel Tower shield
-product = carpentry:AddProduct( CurrentCatID, 96, { 80, 100}, 1, { 50, 100 } );
+product = carpentry:AddProduct( CurrentCatID, 96, { 80, 100}, 1, { 25, 50 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 6 ); -- Conifer Wooden Boards: 6x1
 product:AddProductionSteps( { 2547, 1, "all" }, 3 ); -- Leather: 3x1
 product:AddProductionSteps( { 2535, 1, "all" }, 3 ); -- Iron Ingots: 3x1
 
 --Ornate Tower Shield
-product = carpentry:AddProduct( CurrentCatID, 916, { 87, 107}, 1, { 54, 108 } );
+product = carpentry:AddProduct( CurrentCatID, 916, { 87, 107}, 1, { 27, 54 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 6 ); -- Conifer Wooden Boards: 6x1
 product:AddProductionSteps( { 2547, 1, "all" }, 3 ); -- Leather: 3x1
 product:AddProductionSteps( { 2535, 1, "all" }, 3 ); -- Iron Ingots: 3x1
 
 --Shield of the sky
-product = carpentry:AddProduct( CurrentCatID, 2284, { 90, 110}, 1, { 55, 110 } );
+product = carpentry:AddProduct( CurrentCatID, 2284, { 90, 110}, 1, { 27, 54 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 7 ); -- Conifer Wooden Boards: 7x1
 product:AddProductionSteps( { 2547, 1, "all" }, 3 ); -- Leather: 3x1
 product:AddProductionSteps( { 2551, 1, "all" }, 1 ); -- Pure Air: 1x1
 product:AddProductionSteps( { 284, 1, "all" }, 3 ); -- Bluestone: 3x1
 
 --Cloud Shield
-product = carpentry:AddProduct( CurrentCatID, 2439, { 100, 120}, 1, { 60, 120 } );
+product = carpentry:AddProduct( CurrentCatID, 2439, { 100, 120}, 1, { 30, 60 } );
 product:AddProductionSteps( { 2543, 1, "all" }, 7 ); -- Conifer Wooden Boards: 7x1
 product:AddProductionSteps( { 2547, 1, "all" }, 3 ); -- Leather: 3x1
 product:AddProductionSteps( { 2551, 1, "all" }, 1 ); -- Pure Air: 1x1
@@ -260,12 +260,12 @@ product:AddProductionSteps( { 284, 1, "all" }, 5 ); -- Bluestone: 5x1
 CurrentCatID = carpentry:AddCategory( 2194 ) -- leggings
 
 --Short Hardwood Greaves
-product = carpentry:AddProduct( CurrentCatID, 2194, { 30, 50}, 1, { 25, 50 } );
+product = carpentry:AddProduct( CurrentCatID, 2194, { 30, 50}, 1, { 12, 24 } );
 product:AddProductionSteps( { 546, 1, "all" }, 5 ); -- Naldor Wooden Boards: 5x1
 product:AddProductionSteps( { 176, 1, "all" }, 3 ); -- Grey Cloth: 3x1
 
 --Hardwood Greaves
-product = carpentry:AddProduct( CurrentCatID, 2193, { 44, 64}, 1, { 32, 64 } );
+product = carpentry:AddProduct( CurrentCatID, 2193, { 44, 64}, 1, { 16, 32 } );
 product:AddProductionSteps( { 546, 1, "all" }, 7 ); -- Naldor Wooden Boards: 7x1
 product:AddProductionSteps( { 176, 1, "all" }, 4 ); -- Grey Cloth: 4x1 
         
