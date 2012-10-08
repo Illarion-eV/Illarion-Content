236e5deef14367f13829d66536a6139f8fc8979b
estralis <estralis@illarion.org>
2012-10-08 21:20:05 +0200

    Oller Gobaithkram raus

--- item/id_321_depot.lua
+++ item/id_321_depot.lua
@@ -7,68 +7,10 @@ module("item.id_321_depot", package.seeall)
 function LookAtItem(User,Item)
     local lang = User:getPlayerLanguage();
     local LookAtStr = world:getItemName(321,lang).." (";
-    if (Item.data==0) then
-        LookAtStr = LookAtStr.."Troll's Bane";
-    elseif (Item.data==1) then
-        LookAtStr = LookAtStr.."Varshikar";
-    elseif (Item.data==2) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Graue Rose";
-        else
-            LookAtStr = LookAtStr.."Grey Rose";
-        end
-    elseif (Item.data==3) then
-        LookAtStr = LookAtStr.."Greenbriar";
-    elseif (Item.data==4) then
-        LookAtStr = LookAtStr.."Tol Vanima";
-    elseif (Item.data==5) then
-        LookAtStr = LookAtStr.."Farmers Union";
-    elseif (Item.data==6) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Ork-Höhle";
-        else
-            LookAtStr = LookAtStr.."Orc Cave";
-        end
-    elseif (Item.data==7) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Silberbrand";
-        else
-            LookAtStr = LookAtStr.."Silverbrand";
-        end
-    elseif (Item.data==8) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Akademie der Magie";
-        else
-            LookAtStr = LookAtStr.."Academy of Magic";
-        end
-    elseif (Item.data==9) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Eldan-Kloster & Haus der Druiden des Waldes";
-        else
-            LookAtStr = LookAtStr.."Eldan monastery & House of the druids of the forest";
-        end
-    elseif (Item.data==10) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Der Tempel";
-        else
-            LookAtStr = LookAtStr.."The Temple";
-        end
-    elseif (Item.data==11) then
-        LookAtStr = LookAtStr.."Kallahorn";
-    elseif (Item.data==12) then
-        LookAtStr = LookAtStr.."Miners Guild";
-    elseif (Item.data==13) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Nordmark";
-        else
-            LookAtStr = LookAtStr.."Northmark";
-        end
-    elseif (Item.data==14) then
-        LookAtStr = LookAtStr.."Caelum";
 
 --NewIllarion
 
-    elseif (Item.data==101) then
+    if (Item.data==101) then
             LookAtStr = LookAtStr.."Cadomyr";
 
     elseif (Item.data==102) then
