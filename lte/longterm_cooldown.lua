c692000ecff80a42e7087fa400dd48128b1d295b
estralis <estralis@illarion.org>
2012-10-08 18:54:55 +0200

    Added quest "Caravan of Honour"

--- lte/longterm_cooldown.lua
+++ lte/longterm_cooldown.lua
@@ -23,9 +23,38 @@ function callEffect( Effect, Char ) -- Effect is called
 			Char:setQuestProgress(120,theQuestStatus-1); --cooling!
 		end
 	    --Addition end
+		
+		--Addition by Estralis: Quest 127/128/129 Caravan of Honour (Phillip Molitor) Cooldown
+	    theQuestStatus=Char:getQuestProgress(129);
+	
+		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
+			Char:setQuestProgress(129,theQuestStatus-1); --cooling!
+		end
+		--Addition end
 	
 	end --all above is only conducted for players that aren't afk for more than five minutes
 
+	--Addition by Estralis: Quest 127/128/129 Caravan of Honour (Phillip Molitor) Countdown
+	theQuestStatus=Char:getQuestProgress(128);
+	
+	if theQuestStatus == 1 then --Time over!
+	
+        base.common.InformNLS(User,"[Auftragssstatus] Du hast es nicht geschafft, den Auftrag von Philipp Molitor zu erledigen.","[Order status] You failed to fulfill the order of Philipp Molitor."); -- Feedback!
+		Char:setQuestProgress(127,0);
+		
+	end
+	
+	if theQuestStatus == 12 then --One hour left
+	
+	    base.common.InformNLS(User,"[Auftragssstatus] Es verbleibt dir noch eine Stunde, um den Auftrag von Philipp Molitor zu erledigen.","[Order status] You have one hour left to fulfill the order of Philipp Molitor."); -- Feedback!
+	
+	end
+	
+	if theQuestStatus > 0 then --Is there a countdown? Will be reduced even if the player is AFK/idle
+		Char:setQuestProgress(128,theQuestStatus-1); --counting down!
+	end
+	--Addition end
+	
 	Effect.nextCalled = 3000; --Effect gets called each 5 minutes
 
 	return true; --No idea why!
