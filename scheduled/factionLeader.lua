8c0503196c8b7bca585997e465443c48be1f7e45
Lillian <maletzki.lisa@googlemail.com>
2012-10-11 20:33:01 +0200

    fixed if statement..i think

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -7,23 +7,23 @@ module("scheduled.factionLeader", package.seeall)
 function checkFactionLeader()
 
 	players=world:getPlayersOnline()
-	for index, playerName in pairs(players) do
-		if playerName == "Rosaline Edwards" then
-			npcPositions = {position(122, 521, 0), position(237, 104, 0)};
+	for index, player in pairs(players) do
+		if player.name == "Rosaline Edwards" then
+			npcPositions = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)};
 			updatePosition(npcPositions)
-		elseif playerName == "Valerio Guilianni" then
-			npcPositions = {position(337, 215, 0), position(238, 104, 0)};
+		elseif player.name == "Valerio Guilianni" then
+			npcPositions = {usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)};
 			updatePosition(npcPositions)
-		elseif playerName == "Elvaine Morgan" then
-			npcPositions = {position(898, 775, 2), position(239, 104, 0)};
+		elseif player.name == "Elvaine Morgan" then
+			npcPositions = {usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)};
 			updatePosition(npcPositions)
 		end
 	end
 end
 
 function updatePosition(npcPositions)
-	if world:isCharacterOnField(npcPositions[1]) == true then
-		npcCharObject = world:getCharacterOnField(npcPositions[1]);
-		npcCharObject:forceWarp(npcPositions[2]);
+	if world:isCharacterOnField(npcPositions.usualPosition) == true then
+		npcCharObject = world:getCharacterOnField(npcPositions.usualPosition);
+		npcCharObject:forceWarp(npcPositions.newPosition);
 	end
 end
