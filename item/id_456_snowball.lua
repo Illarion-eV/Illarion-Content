a6e92952d9831df4b0ca685c33239eeb523d5dbc
merung <karten-spiel-haus@hotmail.de>
2012-10-11 16:23:15 +0200

    test

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -19,12 +19,19 @@ end
 function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )
 	-- merung's test land -- 
 	local myValue
+	local myValue1
+	local myValue2
 	local myInform = ""
 	if (string.find(User.lastSpokenText,"distribution")~=nil) then
-		for i=1,100 do
+		--[[for i=1,100 do
 		    myValue = base.common.NormalRnd2(1, 9, 10)
 		    myInform = myInform.." "..myValue
 		end	
+		User:inform(myInform)]]
+		for i=1,100 do 
+		    myValue1, myValue2=base.common.NormalBoxMuller(0, 2)
+			myInform = myInform.." "..myValue1
+	    end
 		User:inform(myInform)
     end
 end
