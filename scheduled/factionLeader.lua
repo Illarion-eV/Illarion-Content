a2cd0d992775fe39fc98a6c1a3ca867d407ba68c
Lillian <maletzki.lisa@googlemail.com>
2012-10-11 20:12:37 +0200

    fixed for loop

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -7,25 +7,23 @@ module("scheduled.factionLeader", package.seeall)
 function checkFactionLeader()
 
 	players=world:getPlayersOnline()
-	for player in players do
-		if player.name == "Rosaline Edwards" then
+	for index, playerName in pairs(players) do
+		if playerName == "Rosaline Edwards" then
 			npcPositions = {position(122, 521, 0), position(237, 104, 0)};
-			if world:isCharacterOnField(npcPositions[1]) == true then
-				npcCharObject = world:getCharacterOnField(npcPositions[1]);
-				npcCharObject:forceWarp(npcPositions[2]);
-			end
-		elseif player.name == "Valerio Guilianni" then
+			updatePosition(npcPositions)
+		elseif playerName == "Valerio Guilianni" then
 			npcPositions = {position(337, 215, 0), position(238, 104, 0)};
-			if world:isCharacterOnField(npcPositions[1]) == true then
-				npcCharObject = world:getCharacterOnField(npcPositions[1]);
-				npcCharObject:forceWarp(npcPositions[2]);
-			end
-		elseif player.name == "Elvaine Morgan" then
+u			pdatePosition(npcPositions)
+		elseif playerName == "Elvaine Morgan" then
 			npcPositions = {position(898, 775, 2), position(239, 104, 0)};
-			if world:isCharacterOnField(npcPositions[1]) == true then
-				npcCharObject = world:getCharacterOnField(npcPositions[1]);
-				npcCharObject:forceWarp(npcPositions[2]);
-			end
+			updatePosition(npcPositions)
 		end
+	end
+end
+
+function updatePosition(npcPositions)
+	if world:isCharacterOnField(npcPositions[1]) == true then
+		npcCharObject = world:getCharacterOnField(npcPositions[1]);
+		npcCharObject:forceWarp(npcPositions[2]);
 	end
 end
