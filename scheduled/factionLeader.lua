0861b0270a3a86b51cbba92f971c68d9f1dec3b2
Lillian <maletzki.lisa@googlemail.com>
2012-10-11 21:31:26 +0200

    next try with local information table

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -4,8 +4,9 @@ require("base.common")
 
 module("scheduled.factionLeader", package.seeall)
 
-local informationTable = {{},{},{}}
-informationTable = {"Rosaline Edwards" = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)}, "Valerio Guilianni" = {usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)}, "Elvaine Morgan" = {usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
+local informationTable = {}
+--informationTable = {"Rosaline Edwards" = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)}, "Valerio Guilianni" = {usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)}, "Elvaine Morgan" = {usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
+table.insert(informationTable, "Rosaline Edwards" = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)})
 
 function checkFactionLeader()
 	for name, npcPositions in pairs(informationTable) do
