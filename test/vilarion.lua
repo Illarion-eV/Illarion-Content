28657e1c58bfdea746cef7bcb26583f35babfba6
vilarion <vilarion@illarion.org>
2012-10-12 07:13:22 +0200

    Test Random

--- test/vilarion.lua
+++ test/vilarion.lua
@@ -32,7 +32,7 @@ end
 function UseItem( User, SourceItem, TargetItem, counter, Param, ltstate )
     if (User.lastSpokenText == "random") then
         -- User:inform("uniform: " .. Random:uniform())
-        User:inform("d6: " .. Random:uniform(1, 6))
+        User:inform("d6: " .. Random.uniform(1, 6))
         -- User:inform("normal: " .. Random:normal(0, 1))
     end
 
