9de4dfc486cb0044265c902c8c565e4df65649c2
Lillian <maletzki.lisa@googlemail.com>
2012-10-14 09:08:42 +0200

    updated table for basic information

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -6,9 +6,7 @@ module("scheduled.factionLeader", package.seeall)
 
 
 function checkFactionLeader()
-	informationTable = {["Rosaline Edwards"] = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)}} --, "Valerio Guilianni" = [usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)], "Elvaine Morgan" = [usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)]}
-
-	--table.insert(informationTable, "Rosaline Edwards" = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)})
+	informationTable = {["Rosaline Edwards"] = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)}, ["Valerio Guilianni"] = {usualPosition=posit 0), newPosition=position(238, 104, 0)}, ["Elvaine Morgan"] = {usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
 
 	for name, npcPositions in pairs(informationTable) do
 		if base.common.CheckIfOnline(informationTable.name)[1] ~= nil then
