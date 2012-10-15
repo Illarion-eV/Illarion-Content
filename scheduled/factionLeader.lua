1d4c213b32b69e20c462618b8edc893772b8edfd
Lillian <maletzki.lisa@googlemail.com>
2012-10-15 19:22:45 +0200

    moved table outside of function and fixed a mistake in if statement

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -4,22 +4,27 @@ require("base.common")
 
 module("scheduled.factionLeader", package.seeall)
 
+local informationTable = {
+	["Rosaline Edwards"] = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)},
+	["Valerio Guilianni"] = {usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)},
+	["Elvaine Morgan"] = {usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
 
 function checkFactionLeader()
-	informationTable = {["Rosaline Edwards"] = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)}, ["Valerio Guilianni"] = {usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)}, ["Elvaine Morgan"] = {usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
 	alsaya = base.common.CheckIfOnline("Alsaya")
 	for i=1, #(informationTable) do
 		charObject = base.common.CheckIfOnline(informationTable[i])
-		base.common.InformNLS(alsaya,"name: "..informationTable[i].." ende",".."); --sending the message
+		base.common.InformNLS(alsaya,"name: "..informationTable[i].." ende","..");
 		if charObject.name ~= nil then
-			updatePosition(npcPositions)
+			updatePosition(informationTable[i].usualPosition, informationTable[i].newPosition)
 		end
 	end
 end
 
-function updatePosition(npcPositions)
-	if world:isCharacterOnField(npcPositions.usualPosition) == true then
-		npcCharObject = world:getCharacterOnField(npcPositions.usualPosition);
-		npcCharObject:forceWarp(npcPositions.newPosition);
+function updatePosition(usualPosition, newPosition)
+	if world:isCharacterOnField(usualPosition) == true then
+		npcCharObject = world:getCharacterOnField(usualPosition);
+		if npcCharObject:getType() == 2 then
+			npcCharObject:forceWarp(newPosition);
+		end
 	end
 end
