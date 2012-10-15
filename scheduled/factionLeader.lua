aabe086703d611766d2d290a580448745132b37a
Lillian <maletzki.lisa@googlemail.com>
2012-10-15 20:36:02 +0200

    changed table layout

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -7,7 +7,7 @@ module("scheduled.factionLeader", package.seeall)
 informationTable = {
 	{npcName="Rosaline Edwards", usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)},
 	{npcName="Valerio Guilianni", usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)},
-	{npcName="Elvaine Morgan"], usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
+	{npcName="Elvaine Morgan", usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
 
 function checkFactionLeader()
 	alsaya = base.common.CheckIfOnline("Alsaya")
