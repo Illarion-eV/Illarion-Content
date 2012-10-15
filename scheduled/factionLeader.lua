50418becd933336bb8c439dc5fd3a37a7e8f387f
Lillian <maletzki.lisa@googlemail.com>
2012-10-15 20:54:14 +0200

    added check to see if the npc is on the right place if char is not online

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -10,12 +10,12 @@ informationTable = {
 	{npcName="Elvaine Morgan", usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
 
 function checkFactionLeader()
-	alsaya = base.common.CheckIfOnline("Alsaya")
 	for i=1, #(informationTable) do
 		charObject = base.common.CheckIfOnline(informationTable[i].npcName)
-		base.common.InformNLS(alsaya,"name: "..informationTable[i].npcName.." ende","..");
 		if charObject ~= nil then
 			updatePosition(informationTable[i].usualPosition, informationTable[i].newPosition)
+		else
+			updatePosition(informationTable[i].newPosition, informationTable[i].usualPosition)
 		end
 	end
 end
@@ -23,7 +23,7 @@ end
 function updatePosition(usualPosition, newPosition)
 	if world:isCharacterOnField(usualPosition) == true then
 		npcCharObject = world:getCharacterOnField(usualPosition);
-		if npcCharObject:getType() == 2 then
+		if npcCharObject:getType() == Character.npc then
 			npcCharObject:forceWarp(newPosition);
 		end
 	end
