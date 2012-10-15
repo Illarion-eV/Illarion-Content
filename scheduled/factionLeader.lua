4e224f2efab05a0d0bed8332b323c93b78ebfb0e
Lillian <maletzki.lisa@googlemail.com>
2012-10-15 19:50:43 +0200

    un-localed table

--- scheduled/factionLeader.lua
+++ scheduled/factionLeader.lua
@@ -4,7 +4,7 @@ require("base.common")
 
 module("scheduled.factionLeader", package.seeall)
 
-local informationTable = {
+informationTable = {
 	["Rosaline Edwards"] = {usualPosition=position(122, 521, 0), newPosition=position(237, 104, 0)},
 	["Valerio Guilianni"] = {usualPosition=position(337, 215, 0), newPosition=position(238, 104, 0)},
 	["Elvaine Morgan"] = {usualPosition=position(898, 775, 2), newPosition=position(239, 104, 0)}}
