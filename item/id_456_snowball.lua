a7caac8182b553a16052fb1332710bbafe71b14e
merung <karten-spiel-haus@hotmail.de>
2012-10-17 22:45:41 +0200

    debug

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -243,22 +243,25 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 	    
 		SendMessage(User, 
 		            "You hear a voice, but you cannot tell from where it comes. Maybe it's even just in your head and it says: \"I am the "..teacherEN..". I help those who wish to enter the world of fine matter. If you want to learn about the great secret of alchemy, fullfill my tasks. Go and bring me five "..HerbsTaskOneNameEN[teacherEN][1]..", five "..HerbsTaskOneNameEN[teacherEN][2].." and five "..HerbsTaskOneNameEN[teacherEN][3]..". If you have them, touch me with and you will have my attention.\"", 
-					"Du vernimmst eine Stimme, von der du nicht sagen kannst, woher sie kommt, oder ob sie vielleicht auch nur in deinem Kopf ist und sie spricht: \"Ich bin "..teacherDE..". Ich helfe jenen, die die Welt der Feinstofflichkeit betreten zu wollen. Willst du also das große Geheimnis der Alchmie erfahren, erfülle meine erste Prüfungen. Geh und bringe mir fünf "..HerbsTaskOneNameDE[teacherEN][1]..", fünf "..HerbsTaskOneNameDE[teacherEN][2].." und fünf "..HerbsTaskOneNameDE[teacherEN][3]..". Wenn du sie hast, berühre mich und du wirst meine Aufmerksamkeit haben.\""
+					"Du vernimmst eine Stimme, von der du nicht sagen kannst, woher sie kommt, oder ob sie vielleicht auch nur in deinem Kopf ist und sie spricht: \"Ich bin "..teacherDE..". Ich helfe jenen, die die Welt der Feinstofflichkeit betreten zu wollen. Willst du also das große Geheimnis der Alchmie erfahren, erfülle meine erste Prüfungen. Geh und bringe mir fünf "..HerbsTaskOneNameDE[teacherEN][1]..", fünf "..HerbsTaskOneNameDE[teacherEN][2].." und fünf "..HerbsTaskOneNameDE[teacherEN][3]..". Wenn du sie hast, berühre mich und du wirst meine Aufmerksamkeit haben.\"",
+					teacherEN
 					);
-		User:setQuestprogress(questId,2)
+		User:setQuestProgress(questId,2)
 	    
 	elseif qstPrg == 2 then
 	    
 		if (User:countItem(HerbsTaskOneId[teacherEN][1]) < 5) or (User:countItem(HerbsTaskOneId[teacherEN][2]) < 5) or (User:countItem(HerbsTaskOneId[teacherEN][3]) < 5) then
 		    SendMessage(User, 
 			            "Once again you hear the voice about which source you are still unsure: \"Oh, you are back! You don't have all the herbs I asked you for, though. I asked for five "..HerbsTaskOneNameEN[teacherEN][1]..", five "..HerbsTaskOneNameEN[teacherEN][2].." and five "..HerbsTaskOneNameEN[teacherEN][3]..". Go and bring them to me if you want to learn the great alchemical secret!\"", 
-						"Wieder vernimmst du die Stimme, deren Ursprung dir nicht klar ist: \"Oh, du bist zurück! Du hast aber nicht all die Kräuter, um die ich dich bat. Es waren fünf "..HerbsTaskOneNameDE[teacherEN][1]..", fünf "..HerbsTaskOneNameDE[teacherEN][2].." und fünf "..HerbsTaskOneNameDE[teacherEN][3]..". Geh und bring sie mir, wenn du das alchemistische Geheminis erfahren willst!\""
+						"Wieder vernimmst du die Stimme, deren Ursprung dir nicht klar ist: \"Oh, du bist zurück! Du hast aber nicht all die Kräuter, um die ich dich bat. Es waren fünf "..HerbsTaskOneNameDE[teacherEN][1]..", fünf "..HerbsTaskOneNameDE[teacherEN][2].." und fünf "..HerbsTaskOneNameDE[teacherEN][3]..". Geh und bring sie mir, wenn du das alchemistische Geheminis erfahren willst!\"",
+						teacherEN
 						);
 			return
 		else
 		    SendMessage(User, 
 			           HerbsDeleteTextEN[teacherEN].." For a short amount fo time it seems as if someone would smack their lips, then the voice says: \"Good, very good. But there is an other task for you left. Now bring me two rar herbs - "..HerbsTaskTwoNameEN[teacherEN][1].." and "..HerbsTaskTwoNameEN[teacherEN][2]..". Be successfull and you are a step closer to your goal.", 
-					   HerbsDeleteTextDE[teacherEN].." Für einen Moment glaubst du etwas wie ein Schmatzen zu hören, bevor die Stimme erklingt: \"Sehr gut, sehr gut. Aber es erwartet dich noch eine Aufgabe. Nun sollst du mir zwei seltene Kräuter bringen und zwar "..HerbsTaskTwoNameDE[teacherEN][1].." und "..HerbsTaskTwoNameDE[teacherEN][2]..". Sei erfolgreich und du bist deinem Ziel ein Stück näher."
+					   HerbsDeleteTextDE[teacherEN].." Für einen Moment glaubst du etwas wie ein Schmatzen zu hören, bevor die Stimme erklingt: \"Sehr gut, sehr gut. Aber es erwartet dich noch eine Aufgabe. Nun sollst du mir zwei seltene Kräuter bringen und zwar "..HerbsTaskTwoNameDE[teacherEN][1].." und "..HerbsTaskTwoNameDE[teacherEN][2]..". Sei erfolgreich und du bist deinem Ziel ein Stück näher.",
+					   teacherEN
 					   );
 			User:eraseItem(HerbsTaskOneId[teacherEN][1],5)
 			User:eraseItem(HerbsTaskOneId[teacherEN][2],5)
@@ -270,11 +273,12 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 	    if (User:countItem(HerbsTaskTwoId[teacherEN][1]) < 1) then
 	        SendMessage(User, 
 			            "\"Oh! You don't have the herbs. If you have forgotten what you are supposed to get - "..HerbsTaskTwoNameEN[teacherEN][1].." and "..HerbsTaskTwoNameEN[teacherEN][2]..".\"", 
-						"\"Sowas! Du hast die Kräuter nicht dabei. Falls du es vergessen hast - "..HerbsTaskTwoNameDE[teacherEN][1].." and "..HerbsTaskTwoNameDE[teacherEN][2]..".\""
+						"\"Sowas! Du hast die Kräuter nicht dabei. Falls du es vergessen hast - "..HerbsTaskTwoNameDE[teacherEN][1].." and "..HerbsTaskTwoNameDE[teacherEN][2]..".\"",
+						teacherEN
 						);
             return
 	    else
-		    User:setQuestprogress(questId,4)
+		    User:setQuestProgress(questId,4)
 			SendMessage(User, 
 			            HerbsDeleteTextEN[teacherEN].." For a moment you believe to hear something like: \"Delicious, mh, so delicious...\" Then you hear the familiar voice: \"Yes, you solved this task. Now I have to ask you some questions. When you have answered three, I will tell you the secret of alchemy.\"", 
 						HerbsDeleteTextDE[teacherEN].." Einen Moment lang glaubst du ein Murmeln zu hören: \"Lecker, mh, so lecker...\" Dann erklingt die Stimme wieder auf bekannte Weise: \"Ja, du hast diese Aufgabe bestanden. Als nächstes muss ich dir einige Fragen stellen. Sobald du mir drei beantworten konntest, werde ich dir das Geheimnis verraten.\"",
