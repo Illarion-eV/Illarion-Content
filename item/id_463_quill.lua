f77e85bf3137839923cd20196b05f6f254a5ba6f
merung <karten-spiel-haus@hotmail.de>
2012-10-18 19:57:49 +0200

    quill script

---
+++ item/id_463_quill.lua
@@ -0,0 +1,9 @@
+-- UPDATE common SET com_script='item.id_463_quill' WHERE com_itemid IN (463);
+require("base.common")
+
+
+module("item.id_463_quill", package.seeall)
+
+function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
+
+end
