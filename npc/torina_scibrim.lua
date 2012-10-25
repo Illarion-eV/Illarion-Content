b537aef98124a8dd42e607d6a939c4b6709adde0
Lillian <maletzki.lisa@googlemail.com>
2012-10-25 10:03:19 +0200

    added missing require

--- npc/torina_scibrim.lua
+++ npc/torina_scibrim.lua
@@ -17,6 +17,7 @@ VALUES (0, 106, 553, 0, 4, 'Torina Scibrim', 'npc.torina_scibrim', 1, 1, 0, 255,
 
 require("npc.base.basic")
 require("npc.base.condition.language")
+require("npc.base.condition.town")
 require("npc.base.consequence.inform")
 require("npc.base.consequence.town")
 require("npc.base.talk")
