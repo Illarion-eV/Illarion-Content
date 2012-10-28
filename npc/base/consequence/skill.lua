021cdc55d19a3a952ea9866e068340aa92d09df3
Martin Karing <nitram@illarion.org>
2012-10-28 15:23:20 +0100

    Changed easyNPC framework for new skills

--- npc/base/consequence/skill.lua
+++ npc/base/consequence/skill.lua
@@ -4,10 +4,9 @@ require("npc.base.consequence.consequence")
 module("npc.base.consequence.skill", package.seeall)
 
 skill = base.class.class(npc.base.consequence.consequence.consequence,
-function(self, group, name, mode, value)
+function(self, name, mode, value)
     npc.base.consequence.consequence.consequence:init(self);
     self["value"], self["valuetype"] = npc.base.talk._set_value(value);
-    self["group"] = group;
     self["name"] = name;
     if (mode == "=") then
         self["perform"] = _skill_helper_set;
@@ -24,15 +23,15 @@ function _skill_helper_set(self, npcChar, player)
     local value = npc.base.talk._get_value(self.npc, self.value, self.valuetype);
     local currSkill = player:getSkill(self.name);
     local modSkill = value - currSkill;
-    player:increaseSkill(self.group, self.name, modSkill);
+    player:increaseSkill(self.name, modSkill);
 end;
 
 function _skill_helper_add(self, npcChar, player)
     local value = npc.base.talk._get_value(self.npc, self.value, self.valuetype);
-    player:increaseSkill(self.group, self.name, value);
+    player:increaseSkill(self.name, value);
 end;
 
 function _skill_helper_sub(self, npcChar, player)
     local value = npc.base.talk._get_value(self.npc, self.value, self.valuetype);
-    player:increaseSkill(self.group, self.name, -value);
+    player:increaseSkill(self.name, -value);
 end;
