960d785ae9e4f6d0e0ed220023edef399200d99a
merung <karten-spiel-haus@hotmail.de>
2012-11-10 14:01:47 +0100

    debug

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -380,9 +380,9 @@ function AskQuestion(User,teacherEN)
 	--[[for i=1,#answerList do
 		dialog:addOption(0, answerList[i])
 	end]]
-	dialog:addOption(0, answerList[1])
-	dialog:addOption(0, answerList[2])
-	dialog:addOption(0, answerList[3])
+	dialog:addOption(15, answerList[1])
+	dialog:addOption(15, answerList[2])
+	dialog:addOption(15, answerList[3])
 	User:inform("add list2: "..answerList[3])
 	User:requestSelectionDialog(dialog)
 end
