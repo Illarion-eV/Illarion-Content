5acea232e72ee60f1ffc309dfdb70cba38790e46
vilarion <vilarion@illarion.org>
2012-11-14 19:53:00 +0100

    Test new crafting

--- item/id_311_glassblowpipe.lua
+++ item/id_311_glassblowpipe.lua
@@ -67,72 +67,7 @@ product:addIngredient(46) -- Ruby: 1x
 end --function
 
 
-function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )  -- DONT EDIT THIS LINE!
+function UseItem(User, SourceItem, TargetItem, Counter, Param, ltstate)  -- DONT EDIT THIS LINE!
     Glassblowing = InitCraftingTool()
-    Glassblowing:showDialog(User)
-
---[[    
-    base.common.ResetInterruption( User, ltstate )
-    if ( ltstate == Action.abort ) then
-        if (User:increaseAttrib("sex",0) == 0) then
-            gText = "seine";
-            eText = "his";
-        else
-            gText = "ihre";
-            eText = "her";
-        end
-        User:talkLanguage(Character.say, Player.german, "#me unterbricht "..gText.." Arbeit.");
-        User:talkLanguage(Character.say, Player.english,"#me interrupts "..eText.." work.");
-        Glassblowing:SwapToInactiveItem( User );
-        return
-    end
-
-    if not InitDone then
-        User:inform("Error while loading crafting values.");
-        return
-    end
-    
-    if not Glassblowing:LocationFine( User, ltstate ) then
-        return
-    end
-    
-    if not base.common.CheckItem( User, SourceItem ) then
-        Glassblowing:SwapToInactiveItem( User );
-        return
-    end
-    
-    if ( SourceItem:getType() ~= 4 ) then -- Glasblasrohr in der Hand
-        base.common.InformNLS( User, 
-        "Du musst das Glasblasrohr in die Hand nehmen um damit zu arbeiten.", 
-        "You have to take the glasblowpipe in your hand, to work with it." )
-        return
-    end
-
-    if base.common.Encumbrence(User) then -- Sehr streife Rüstung?
-        base.common.InformNLS( User,
-        "Deine Rüstung behindert beim Glas blasen.",
-        "Your armour disturbs you while blowing glass." );
-        Glassblowing:SwapToInactiveItem( User );
-        return
-    end
-
-    local TargetItem = base.common.GetTargetItem(User, SourceItem);
-	if TargetItem then
-		if Glassblowing:IsProduct(TargetItem.id) then
-			base.common.InformNLS( User,
-			"Du versuchst den Gegenstand in deiner Hand zu bearbeiten.",
-			"You try to work on the item in your hand." );
-			Glassblowing:ToolCreateItem( User, 0, TargetItem, ltstate, SourceItem );
-			return;
-		end
-	end
-
-    if (Param == 0) then
-        Glassblowing:GenerateItemList( User, Param, SourceItem );
-        Glassblowing:SwapToInactiveItem( User );
-        return
-    else
-        Glassblowing:ToolCreateItem( User, Param, nil, ltstate, SourceItem );
-    end
---]]
-end --function
+    Glassblowing:showDialog(User, SourceItem)
+end
