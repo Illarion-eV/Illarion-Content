be8042ace6894b5e820647644b3b5ebfe3b8335b
pharse <pharse@illarion.org>
2012-11-24 19:37:41 +0100

    debug

--- item/id_2836_forge.lua
+++ item/id_2836_forge.lua
@@ -111,7 +111,7 @@ function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )
   -- => the ore that we found at the beginning is the same as in the end.
   local oreItem = nil;
   for _,i in pairs(oreList) do
-    if (User:countItemAt("all",i.id)>=i.amount) then
+    if (User:countItemAt("all",i.ore.id)>=i.ore.amount) then
       if (i.ore.id == 2534) then
         -- that's merinium (should be the last in the list), we need pure fire
         if (User:countItemAt("all",2553) == 0) then
