3d6855152423a9d38ddf9c5fa89a78c30c762001
merung <karten-spiel-haus@hotmail.de>
2012-11-25 17:25:49 +0100

    some changes

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -18,15 +18,35 @@ function MoveItemAfterMove(User, SourceItem, TargetItem)
 end
 
 function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
-    local a, b, quest
-    a,b,quest = string.find(User.lastSpokenText,"getquest (%d+)")
-	if a ~= nil then
-		quest = tonumber(quest)
-		User:inform("Quest "..quest.." has queststatus"..User:getQuestProgress(quest))
-		return
-	end
+    
 	
-	if (User.lastSpokenText == "teacher") then
+	if (User.lastSpokenText == "inform datas") then
+	    TargetItem = base.common.GetFrontItem(User)
+		local stockConc = ""
+		for i=1,8 do 
+		    local currentSubs = TargetItem:getData(wirkstoff[1].."Concentration")
+			if currentSubs == "" then
+			    currentSubs = 5
+			end	
+			stockConc = stockConc..currentSubs
+		end
+		local essenceHerbs = ""
+		for i=1,8 do
+		    local currentHerb = TargetItem:getData("essenceHerb"..i)
+			if currentHerb ~= "" then
+			    essenceHerbs = essenceHerbs.." "..currentHerb
+			end
+        end			
+		local filledWith = TargetItem:getData("filledWith")
+		local potionEffectId = TargetItem:getData("potionEffectId")
+		local potionQuality = TargetItem:getData("potionQuality")
+		
+		local callback = function(dialog) end
+		local dataText = "stockConc: "..stockConc.."\nessenceHerbs: "..essenceHerbs.."\nfilledWith: "..filledWith.."\neffectId: "..potionEffectId.."\npotionQuality: "..potionQuality
+		dialog = MessageDialog("datas", dataText, callback)
+		User:requestMessageDialog(dialog)
+	else
 	alchemy.base.teacher.UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
     end
+	
 end
