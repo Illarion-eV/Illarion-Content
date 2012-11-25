8d6ecdaaec66a206b466a1fc0f2d44a76cfd1897
merung <karten-spiel-haus@hotmail.de>
2012-11-25 12:09:34 +0100

    testfunction

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -18,7 +18,13 @@ function MoveItemAfterMove(User, SourceItem, TargetItem)
 end
 
 function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
- 
-    alchemy.base.teacher.UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
+    local a, b, quest, status
+    a,b,quest = string.find(User.lastSpokenText,"getquest (%d+)")
+	if a ~= nil then
+		User:inform("Quest "..quest.." has queststatus"..User:getQuestProgress(quest))
+		return
+	end
+	
+	alchemy.base.teacher.UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 
 end
