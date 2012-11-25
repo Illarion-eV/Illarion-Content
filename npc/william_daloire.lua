4259a8fa2b76b61a835859a87d91bdc9d28be0a6
Miriam <marionline_96@yahoo.de>
2012-11-25 14:05:01 +0100

    william daloire

--- npc/william_daloire.lua
+++ npc/william_daloire.lua
@@ -261,7 +261,7 @@ talkingNPC:addTalkingEntry(talkEntry);
 end;
 if (true) then
 local talkEntry = npc.base.talk.talkNPCEntry();
-talkEntry:addCondition(npc.base.condition.quest.quest(1000, "=", 8));
+talkEntry:addCondition(npc.base.condition.quest.quest(1000, "=", 9));
 talkEntry:addCondition(npc.base.condition.language.language("english"));
 talkEntry:addTrigger(".");
 talkEntry:addResponse("Oh? ... A letter for me? ... Ah, I see. Please tell Miss Bragolin I'll send her a letter to answer her questions.");
@@ -270,7 +270,7 @@ talkingNPC:addTalkingEntry(talkEntry);
 end;
 if (true) then
 local talkEntry = npc.base.talk.talkNPCEntry();
-talkEntry:addCondition(npc.base.condition.quest.quest(1000, "=", 8));
+talkEntry:addCondition(npc.base.condition.quest.quest(1000, "=", 9));
 talkEntry:addTrigger(".");
 talkEntry:addResponse("Oh?... ein Brief für mich? ... Ah, ich verstehe. Bitte sagt Fräulein Bragolin ich werde ihr einen Brief schreiben um ihre Fragen zu beantworten.");
 talkEntry:addConsequence(npc.base.consequence.quest.quest(1000, "=", 9));
