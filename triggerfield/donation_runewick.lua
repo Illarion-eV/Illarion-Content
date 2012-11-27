c8bbec963172a3d1f864ec9733e2a9154b61e7ca
estralis <estralis@illarion.org>
2012-11-27 19:23:11 +0100

    Left a comment

--- triggerfield/donation_runewick.lua
+++ triggerfield/donation_runewick.lua
@@ -19,7 +19,7 @@ function PutItemOnField(Item,User)
 
     if (donated) and (User:getQuestProgress(205) == 1) then
 		User:setQuestProgress(205, 2); --Quest solved!
-		base.common.InformNLS(User,"[Queststatus] Du hast erfolgreich Recycling betrieben. Sprich mit Neiran el Nyarale, um deine Belohnung einzufordern.","[Quest status] You successfully recyled something. Talk to Neiran el Nyarale to claim your reward."); --sending a message
+		base.common.InformNLS(User,"[Queststatus] Du hast erfolgreich Recycling betrieben <Sorry, das geht so nicht. Bitte schreib ernsthafte Texte ~Estralis>. Sprich mit Neiran el Nyarale, um deine Belohnung einzufordern.","[Quest status] You successfully recyled <--typo! something. Talk to Neiran el Nyarale to claim your reward."); --sending a message
 	end
 
 	-- Quest end
