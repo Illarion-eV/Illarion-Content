154445af775fae84055ddc6517a07ed199eaae3f
merung <karten-spiel-haus@hotmail.de>
2012-11-28 16:44:34 +0100

    debug

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -22,7 +22,7 @@ end
 function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
     if (User.lastSpokenText == "hand") then 
 	    local myItem = User:getItemAt(6)
-		User:inform("debug snow 1")
+		User:inform("debug snow 1: "..myItem.id)
 		alchemy.base.brewing.UseItem( User, myItem, TargetItem, Counter, Param, ltstate )
 	    return
 	end	
