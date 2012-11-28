93f96908d318cae04d5378708f85d0afbe59048d
merung <karten-spiel-haus@hotmail.de>
2012-11-28 17:15:29 +0100

    debug

--- alchemy/base/gemdust.lua
+++ alchemy/base/gemdust.lua
@@ -63,7 +63,7 @@ function GemDustInStock(User,cauldron,gemDust)
 end
 
 function GemDustInWater(User,cauldron,gemDust)
-User:("blaaaah")
+User:inform("blaaaah")
     cauldron:setData("filledWith","essenceBrew")
 	local reGemdust, reCauldron, reBottle = alchemy.base.alchemy.GemDustBottleCauldron(gemDust, nil, nil, nil, User)
 	cauldron.id = reCauldron
