1637ab9d8e85f13e1604aefdd87cb4b819c9760e
vilarion <vilarion@illarion.org>
2012-11-29 21:35:37 +0100

    Show gem bonus in lookat

--- item/gems.lua
+++ item/gems.lua
@@ -110,6 +110,35 @@ function getMagicGemData(level)
     return {gemLevel = level}
 end
 
+-- calculates the gem bonus and returns it in %
+function getGemBonus(item)
+    gemStrength={};
+    gemStrength[1]=extractNum(item:getData("magicalEmerald"));
+    gemStrength[2]=extractNum(item:getData("magicalRuby"));
+    gemStrength[3]=extractNum(item:getData("magicalTopaz"));
+    gemStrength[4]=extractNum(item:getData("magicalAmethyst"));
+    gemStrength[5]=extractNum(item:getData("magicalSapphire"));
+    gemStrength[6]=extractNum(item:getData("magicalObsidian"));
+
+    gemSum=0;
+    gemMin=1000;   -- arbitrarily high number
+
+    for _, gStrength in pairs(gemStrength) do
+        gemSum=gemSum+gStrength;
+        if gStrength<gemMin then gemMin=gStrength end;
+    end
+
+    return gemSum+gemMin*6*2;
+
+end
+
+function extractNum(text)
+    if text=="" then
+        return 0
+    end
+    return tonumber(text)
+end
+
 function LookAtItem(user, item)
     local lookAt = base.lookat.GenerateLookAt(user, item)
 
--- server/standardfighting.lua
+++ server/standardfighting.lua
@@ -32,6 +32,9 @@ require("content.fighting")
 -- Include the regeneration LTE to access the control functions of this LTE
 require("lte.chr_reg");
 
+-- for gem bonus
+require("item.gems")
+
 module("server.standardfighting", package.seeall)
 
 --- Main Attack function. This function is called by the server to start a
@@ -221,7 +224,7 @@ function CalculateDamage(Attacker, Globals)
     DexterityBonus = (Attacker.dexterity - 6) * 1;
     SkillBonus = (Attacker.skill - 20) * 1;
     TacticsBonus = (Attacker.tactics - 20) * 0.5;
-    GemBonus = getGemBonus(Attacker.WeaponItem);
+    GemBonus = item.gems.getGemBonus(Attacker.WeaponItem);
 
     Globals["Damage"] = BaseDamage * (100 + StrengthBonus + PerceptionBonus + DexterityBonus + SkillBonus + TacticsBonus + GemBonus)/100;
     
@@ -948,37 +951,6 @@ function ShowEffects(Attacker, Defender, Globals)
         world:makeSound(31,Defender.Char.pos);
     end;
 end;
-
-
--- calculates the gem bonus and returns it in %
-function getGemBonus(item)
-    gemStrength={};
-    gemStrength[1]=extractNum(item:getData("magicalEmerald"));
-    gemStrength[2]=extractNum(item:getData("magicalRuby"));
-    gemStrength[3]=extractNum(item:getData("magicalTopaz"));
-    gemStrength[4]=extractNum(item:getData("magicalAmethyst"));
-    gemStrength[5]=extractNum(item:getData("magicalSapphire"));
-    gemStrength[6]=extractNum(item:getData("magicalObsidian"));
-    
-    gemSum=0;
-    gemMin=1000;   -- arbitrarily high number
-    
-    for _, gStrength in pairs(gemStrength) do
-        gemSum=gemSum+gStrength;
-        if gStrength<gemMin then gemMin=gStrength end;
-    end
-    
-    return gemSum+gemMin*6*2;
-    
-end
-
-
-function extractNum(text)
-    if text=="" then 
-        return 0
-    end
-    return tonumber(text)
-end
 
 
 -- Parry sounds
