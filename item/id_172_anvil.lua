613aa6a1d04407f9a8ee0e79c9cb79932e854fd8
vilarion <vilarion@illarion.org>
2012-12-03 16:18:41 +0100

    Test crafting by using static tools

--- item/id_172_anvil.lua
+++ item/id_172_anvil.lua
@@ -1,6 +1,6 @@
 require("content.craft.smithing")
 
-module("item.id_172_anvil")
+module("item.id_172_anvil", package.seeall)
 
 function UseItem(User, SourceItem, TargetItem, Counter, Param, ltstate)
     content.craft.smithing.smithing:showDialog(User, SourceItem)
