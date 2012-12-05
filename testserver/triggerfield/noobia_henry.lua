cf72a1753f39fc9af41631bc2276df2ca488d57d
estralis <estralis@illarion.org>
2012-12-05 19:07:12 +0100

    Added a hint how to close the chat box.

--- testserver/triggerfield/noobia_henry.lua
+++ testserver/triggerfield/noobia_henry.lua
@@ -7,7 +7,6 @@
 -- INSERT INTO triggerfields VALUES (38,21,100,'triggerfield.noobia_henry');
 -- INSERT INTO triggerfields VALUES (38,22,100,'triggerfield.noobia_henry');
 
-
 require("base.common")
 
 module("triggerfield.noobia_henry", package.seeall)
@@ -28,9 +27,9 @@ function MoveToField(Character)
 	        local callbackNewbie = function(dialogNewbie) end; --empty callback
 			
 	        if Character:getPlayerLanguage() == 0 then
-		        dialogNewbie = MessageDialog("Tutorial","Um mit dem Menschen zu sprechen, aktiviere die Sprachkonsole mit Return, schreibe ein Wort und drücke wieder Return. Alle NPCs reagieren auf bestimmte Schlüsselwörter wenn du in ihrer Nähe stehst. Wenn es Probleme bei der Kommunikation mit einem NPC gibt, schreibe einfach 'Hilfe' und der NPC antwortet mit einer Liste möglicher Schlüsselwörter.", callbackNewbie)
+		        dialogNewbie = MessageDialog("Tutorial","Um mit dem Menschen zu sprechen, aktiviere die Sprachkonsole mit Return, schreibe ein Wort und drücke wieder Return. Alle NPCs reagieren auf bestimmte Schlüsselwörter wenn du in ihrer Nähe stehst. Wenn es Probleme bei der Kommunikation mit einem NPC gibt, schreibe einfach 'Hilfe' und der NPC antwortet mit einer Liste möglicher Schlüsselwörter. Um die Chatbox zu deaktivieren, drücke einfach nochmal Return ohne Texteingabe.", callbackNewbie)
 	        else	
-		        dialogNewbie = MessageDialog("Tutorial", "In order to speak to the human, activate the chat box with the return key, type the word you wish to say and then hit return again. All non-player characters (NPCs) react on certain keywords if you are standing directly beside them. If you ever get stuck trying while trying to communicate with an NPC, typing 'help' will provide you with a list of recommended commands.", callbackNewbie)
+		        dialogNewbie = MessageDialog("Tutorial", "In order to speak to the human, activate the chat box with the return key, type the word you wish to say and then hit return again. All non-player characters (NPCs) react on certain keywords if you are standing directly beside them. If you ever get stuck trying while trying to communicate with an NPC, typing 'help' will provide you with a list of recommended commands. To deactivate the chat box, hit return again without typing anything.", callbackNewbie)
 	        end	
 	        Character:requestMessageDialog(dialogNewbie)
 		end
