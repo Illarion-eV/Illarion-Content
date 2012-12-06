22ac3a2421de8498ef2d71e86bfc579e51b5a013
vilarion <vilarion@illarion.org>
2012-12-06 19:12:55 +0100

    Test rank condition

--- testserver/npc/base/condition/rank.lua
+++ testserver/npc/base/condition/rank.lua
@@ -26,6 +26,8 @@ end;
 
 function _rank_helper_greater(self, npcChar, player)
     local value = npc.base.talk._get_value(self.npc, self.value, self.valuetype);
+player:inform("Value: "..value)
+player:inform("Rank: "..base.factions.getRankAsNumber(player))
     return base.factions.getRankAsNumber(player) > value;
 end;
 
