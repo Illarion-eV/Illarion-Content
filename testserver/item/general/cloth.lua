abac39f3ed32e21208acab58a80559151b772b25
zot <zot@illarion.org>
2012-12-08 19:38:23 +0100

    Added a few items which someone forgot

--- testserver/item/general/cloth.lua
+++ testserver/item/general/cloth.lua
@@ -3,7 +3,7 @@ require("base.lookat")
 module("item.general.cloth", package.seeall)
 
 -- Normal Items:
--- UPDATE common SET com_script='item.general.cloth' WHERE com_itemid IN (34,48,53,55,180,181,182,183,193,195,196,326,356,357,358,362,363,364,365,366,367,368,369,370,371,385,457,458,459,460,461,547,548,558,2295,2377,2378,2380,2384,2416,2418,2419,2420,2421);
+-- UPDATE common SET com_script='item.general.cloth' WHERE com_itemid IN (34,48,53,55,180,181,182,183,193,195,196,326,356,357,358,362,363,364,365,366,367,368,369,370,371,385,457,458,459,460,461,526,527,547,548,558,697,698,2295,2377,2378,2380,2384,2416,2418,2419,2420,2421);
 
 -- Priest Items:
 -- UPDATE common SET com_script='item.general.cloth' WHERE com_itemid IN ();
