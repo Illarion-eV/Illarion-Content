f13a29b5377697362f5eb0ba01d5ae5cd4540fdb
vilarion <vilarion@illarion.org>
2012-12-09 22:26:27 +0100

    Revert changes to town condition

--- testserver/npc/base/condition/town.lua
+++ testserver/npc/base/condition/town.lua
@@ -8,19 +8,9 @@ town = base.class.class(npc.base.condition.condition.condition,
 function(self, comp ,value)
     npc.base.condition.condition.condition:init(self);
     self["value"] = tonumber(value);
-	if (comp == "<>" or comp == "!=" or comp == "~=") then
-		self["check"] = _town_helper_notequal;
-	else
-		self["check"] = _town_helper_equal;
-	end;
+    self["check"] = _town_helper_equal;
 end);
 
 function _town_helper_equal(self, npcChar, player)
-    player:inform("Town: "..base.factions.getFaction(player).tid)
-    player:inform("Value-Town: "..self.value)
     return (base.factions.getFaction(player).tid == self.value);
-end;
-
-function _town_helper_notequal(self, npcChar, player)
-    return (base.factions.getFaction(player).tid ~= self.value);
 end;
