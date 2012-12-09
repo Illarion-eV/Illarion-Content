6eb93d31dbf83af0d2073ce8b37fd7732cb452a1
vilarion <vilarion@illarion.org>
2012-12-09 22:33:46 +0100

    Revert changes to town condition

--- testserver/npc/base/condition/town.lua
+++ testserver/npc/base/condition/town.lua
@@ -5,7 +5,7 @@ require("npc.base.condition.condition")
 module("npc.base.condition.town", package.seeall)
 
 town = base.class.class(npc.base.condition.condition.condition,
-function(self, comp ,value)
+function(self, value)
     npc.base.condition.condition.condition:init(self);
     self["value"] = tonumber(value);
     self["check"] = _town_helper_equal;
