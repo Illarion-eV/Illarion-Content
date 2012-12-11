400f40f6a40f20b89126754aef286ff053ac51f9
Lillian <maletzki.lisa@googlemail.com>
2012-12-11 22:24:13 +0100

    deactivated custom lookat for now

--- testserver/item/id_293_throwing_spear.lua
+++ testserver/item/id_293_throwing_spear.lua
@@ -6,10 +6,10 @@ module("item.id_293_throwing_spear", package.seeall)
 
 function LookAtItem(User, Item)
 	
-	local customText = base.lookat.GetItemDescription(User,Item,base.lookat.WOOD,false,false);
-	if tonumber(Item:getData("spearData")) > 2^30 then
-		world:itemInform( User, Item, customText );
-	else
+	--local customText = base.lookat.GetItemDescription(User,Item,base.lookat.WOOD,false,false);
+	--if tonumber(Item:getData("spearData")) > 2^30 then
+	--	world:itemInform( User, Item, customText );
+	--else
 		world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.WOOD));
-	end
+	--end
 end
