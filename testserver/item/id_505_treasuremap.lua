48516b1ea7b8184e7e28d6a7f485cd3db22280aa
Lillian <maletzki.lisa@googlemail.com>
2012-12-11 21:41:07 +0100

    fixed lookat

--- testserver/item/id_505_treasuremap.lua
+++ testserver/item/id_505_treasuremap.lua
@@ -12,17 +12,19 @@ function LookAtItem(User, Item)
     local TreasureName = base.treasure.GetTreasureName( math.floor(Item.quality/100), User:getPlayerLanguage(), not dir );
 
     if not dir then
-        world:itemInform( User, Item, base.common.GetNLS( User,
-            "Eine Karte mit einer Markierung auf einer Position irgendwo in deiner unmittelbaren Nähe. Du vermutest, dass es sich um "..TreasureName.." handelt.",
+		base.lookat.SetSpecialDescription(Item, "Eine Karte mit einer Markierung auf einer Position irgendwo in deiner unmittelbaren Nähe. Du vermutest, dass es sich um "..TreasureName.." handelt.",
             "A map that shows a position somewhere really close to your current position. You think it could be "..TreasureName.."." ) );
+        world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.NONE) );
     elseif string.find(dir,"wrong")~=nil then
-        world:itemInform( User, Item, base.common.GetNLS( User,
+        base.lookat.SetSpecialDescription(Item,
             "Du scheinst dich nicht im richtigen Gebiet aufzuhalten.",
             "You don't seem to be in the correct area."));
+		world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.NONE) );
     else
-        world:itemInform( User, Item, base.common.GetNLS( User,
+        base.lookat.SetSpecialDescription(Item,
             "Eine Karte mit einer Markierung, die sich wahrscheinlich von dir aus gesehen "..distance.." im "..dir.." befindet. Du vermutest, dass es sich um "..TreasureName.." handelt.",
             "A map that shows a mark that is probably located somewhere "..distance.." in the "..dir.." of your current position. You believe the map leads to "..TreasureName.."." ) );
+		world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.NONE) );
     end;
 
 end;
