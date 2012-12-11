083e889100277594d772993dddb779538185e7d4
Lillian <maletzki.lisa@googlemail.com>
2012-12-11 22:22:17 +0100

    fixed old data

--- testserver/item/id_293_throwing_spear.lua
+++ testserver/item/id_293_throwing_spear.lua
@@ -7,7 +7,7 @@ module("item.id_293_throwing_spear", package.seeall)
 function LookAtItem(User, Item)
 	
 	local customText = base.lookat.GetItemDescription(User,Item,base.lookat.WOOD,false,false);
-	if Item:getData("spearData") > 2^30 then
+	if tonumber(Item:getData("spearData")) > 2^30 then
 		world:itemInform( User, Item, customText );
 	else
 		world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.WOOD));
