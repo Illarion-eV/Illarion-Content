cba535a6255e7cafa3b4cb990653c72c6b3dd576
Lillian <maletzki.lisa@googlemail.com>
2012-12-11 21:41:45 +0100

    fixed lookat

--- testserver/item/id_505_treasuremap.lua
+++ testserver/item/id_505_treasuremap.lua
@@ -13,17 +13,17 @@ function LookAtItem(User, Item)
 
     if not dir then
 		base.lookat.SetSpecialDescription(Item, "Eine Karte mit einer Markierung auf einer Position irgendwo in deiner unmittelbaren Nähe. Du vermutest, dass es sich um "..TreasureName.." handelt.",
-            "A map that shows a position somewhere really close to your current position. You think it could be "..TreasureName.."." ) );
+            "A map that shows a position somewhere really close to your current position. You think it could be "..TreasureName.."." );
         world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.NONE) );
     elseif string.find(dir,"wrong")~=nil then
         base.lookat.SetSpecialDescription(Item,
             "Du scheinst dich nicht im richtigen Gebiet aufzuhalten.",
-            "You don't seem to be in the correct area."));
+            "You don't seem to be in the correct area.");
 		world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.NONE) );
     else
         base.lookat.SetSpecialDescription(Item,
             "Eine Karte mit einer Markierung, die sich wahrscheinlich von dir aus gesehen "..distance.." im "..dir.." befindet. Du vermutest, dass es sich um "..TreasureName.." handelt.",
-            "A map that shows a mark that is probably located somewhere "..distance.." in the "..dir.." of your current position. You believe the map leads to "..TreasureName.."." ) );
+            "A map that shows a mark that is probably located somewhere "..distance.." in the "..dir.." of your current position. You believe the map leads to "..TreasureName.."." );
 		world:itemInform( User, Item, base.lookat.GenerateLookAt(User, Item, base.lookat.NONE) );
     end;
 
