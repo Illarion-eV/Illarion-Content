79f1bc4bc61079827c00a461851a79c405999b2a
pharse <pharse@illarion.org>
2012-12-12 22:52:49 +0100

    guards

---
+++ testserver/content/guards.lua
@@ -0,0 +1,18 @@
+module("content.guards", package.seeall)
+
+function InitGuards()
+  if (Init == nil) then
+    Init = 1;
+    Guards = {};
+    AddGuard(238, 1, position(114, 639, 0), 3, "cadomyr guard 1");  -- horatio milenus
+  end
+end
+
+function AddGuard(GuardId, FactionId, WarpPos, Radius, AreaName)
+  Guard[guardId] = {
+    faction = factionId,
+    warpPos = warpPos,
+    radius = Radius,
+    areaName = AreaName
+  };
+end
