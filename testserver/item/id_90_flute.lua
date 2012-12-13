a494edc844bb03b7f2f8ecc08d75d0d671dbd07f
vilarion <vilarion@illarion.org>
2012-12-13 18:36:24 +0100

    Fix flute

--- testserver/item/id_90_flute.lua
+++ testserver/item/id_90_flute.lua
@@ -3,9 +3,8 @@
 -- UPDATE common SET com_script='item.id_90_flute' WHERE com_itemid=90;
 
 require("item.base.music")
-require("item.general.wood")
 
-module("item.id_90_flute", package.seeall, package.seeall(item.general.wood))
+module("item.id_90_flute", package.seeall)
 
 item.base.music.addTalkText("#me produces some squeaking sounds on the flute.","#me macht einige quietschende Geräusche auf der Flöte.","flute");
 item.base.music.addTalkText("#me plays a horribly out of tune melody.","#me spielt eine fürchterlich verstimmte Melodie auf der Flöte.","flute");
@@ -14,5 +13,5 @@ item.base.music.addTalkText("#me plays an airy tune on the flute.","#me spielt e
 item.base.music.addTalkText("#me plays a wild tune on the flute.","#me spielt eine wilde Melodie auf der Flöte.","flute");
 
 function UseItem(User,SourceItem,TargetItem,Counter,Param)
-	item.base.music.PlayInstrument(User,SourceItem,"flute");
+	item.base.music.PlayInstrument(User,SourceItem,Character.flute);
 end
