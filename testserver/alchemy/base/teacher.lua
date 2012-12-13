5043faaf719c3e492e56f12826ae0556ab8b4947
merung <karten-spiel-haus@hotmail.de>
2012-12-13 20:44:23 +0100

    debug

--- testserver/alchemy/base/teacher.lua
+++ testserver/alchemy/base/teacher.lua
@@ -480,7 +480,7 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
     if not (SourceItem.pos == position(75,651,0) or SourceItem.pos == position(432,238,0) or SourceItem.pos == position(873,878,0)) then
 	    return
 	end	
-	
+	--[[
 	-- already an alchemist?
     local alchemistCheck = AlchemistCheck(User)
 	if alchemistCheck then
@@ -489,7 +489,7 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 		            "Du hörst eine Stimme, die du nicht zu lokalisieren vermagst: \"Du kennst das große Geheimnis der Alchemie schon. Ich darf dir nicht weiter helfen. Geh weg!\""
 	                )
 	return
-	end				
+	end			]]	
 	-- check if the char belongs to any other mage class
 	local noMagicCheck = NoMagicCheck(User)
 	if not noMagicCheck then
