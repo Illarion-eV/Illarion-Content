78b56ad6f8cdbceec22ade4322b6565f2e2b4a62
merung <karten-spiel-haus@hotmail.de>
2012-12-13 12:04:54 +0100

    lookat reair

--- testserver/item/id_224_goldengoblet.lua
+++ testserver/item/id_224_goldengoblet.lua
@@ -6,24 +6,6 @@ require("base.common")
 module("item.id_224_goldengoblet", package.seeall)
 
 function LookAtItem(User,Item)
-    local text = base.lookat.GetItemDescription(User,Item,4,false,false);
-	local gText;
-	local eText;
-	if (Item.data == 1) then
-		gText = "Alte Sprache Barde des Jahres 26";
-		eText = "Old language bard of the year 26";
-	elseif ( Item.data == 2 ) then
-		gText = "Neue Sprache Barde des Jahres 26";
-		eText = "New language bard of the year 26";
-	elseif Item.data == 10000 then
-		gText = "Auffälliger Kelch";
-		eText = "peculiar goblet";
-	elseif Item.data > 2^30 then
-		world:itemInform(User,Item,text);
-		return;
-	else
-		gText = gText..world:getItemName(Item.id,0);
-		eText = eText..world:getItemName(Item.id,1);
-	end
-	world:itemInform(User,Item,base.common.GetNLS(User,gText,eText));
+    local lookat = base.lookat.GetItemDescription(User,Item,4,false,false);
+	world:itemInform(User,Item,lookat);
 end
