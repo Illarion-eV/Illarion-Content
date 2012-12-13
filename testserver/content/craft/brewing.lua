43db5a4397f9942c62b37d52f1235bc505b94fcc
vilarion <vilarion@illarion.org>
2012-12-14 00:38:25 +0100

    Install original crafting times

--- testserver/content/craft/brewing.lua
+++ testserver/content/craft/brewing.lua
@@ -17,7 +17,7 @@ brewing:addTool(339) -- barrel
 local catId = brewing:addCategory("light alcohol", "Leichter Alkohol") -- Bierflasche(2501), Leichte Alkoholika 
 
 ------------------- FLASCHE MIT CIDER - 2499 -------------------------- 
-local product = brewing:addProduct(catId, 2499, 0, 20, 5, 10)
+local product = brewing:addProduct(catId, 2499, 0, 20, 25, 50)
 product:addIngredient(2498) -- Schritt 4: leere Flasche(2498) 1x
 product:addIngredient(52)
 product:addRemnant(51) -- Schritt 3: Eimer mit Wasser(52) 1x
@@ -25,7 +25,7 @@ product:addIngredient(15, 3) -- Schritte 1 - 2: Apfel(15) 2x
 ------------------ FLASCHE MIT CIDER - FERTIG ------------------------- 
 
 -------------------- FLASCHE MIT MET - 2497 ---------------------------
-product = brewing:addProduct(catId, 2497, 15, 30, 9, 18)
+product = brewing:addProduct(catId, 2497, 15, 30, 45, 90)
 product:addIngredient(2498) -- Schritt 5: leere Flasche(2498) 1x
 product:addIngredient(2529, 3) -- Schritte 1 - 3: Honigwaben(2529) 2x
 product:addIngredient(52)
@@ -33,7 +33,7 @@ product:addRemnant(51) -- Schritt 4: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT MET - FERTIG --------------------------
 
 ------------------- FLASCHE MIT BIER - 2501 ---------------------------
-product = brewing:addProduct(catId, 2501, 32, 50, 12, 24)
+product = brewing:addProduct(catId, 2501, 32, 50, 60, 120)
 product:addIngredient(2498) -- Schritt 7: leere Flasche(2498) 1x
 product:addIngredient(259, 2) -- Schritte 1 - 2: Getreide(259) 1x
 product:addIngredient(154) -- Schritte 3 - 4: Hopfen(154) 1x
@@ -42,7 +42,7 @@ product:addRemnant(51) -- Schritt 4: Eimer mit Wasser(52) 1x
 ------------------ FLASCHE MIT BIER - FERTIG --------------------------
 
 ------------------- FLASCHE MIT WEIN - 2500 ---------------------------
-product = brewing:addProduct(catId, 2500, 47, 65, 16, 32, 2)
+product = brewing:addProduct(catId, 2500, 47, 65, 80, 160, 2)
 product:addIngredient(2498) -- Schritt 5: leere Flasche(2498) 1x
 product:addIngredient(388, 3) -- Schritte 1 - 3: Weinbeeren(388) 2x
 product:addIngredient(52)
@@ -52,7 +52,7 @@ product:addRemnant(51) -- Schritt 4: Eimer mit Wasser(52) 1x
 catId = brewing:addCategory("heavy alcohol", "Harter Alkohol") -- Das harte Zeug 
 
 ------------------- FLASCHE MIT RUM - 517 ---------------------------
-product = brewing:addProduct(catId, 517, 52, 70, 17, 34)
+product = brewing:addProduct(catId, 517, 52, 70, 102, 204)
 product:addIngredient(518) -- Schritt 6: leere Flasche(1317) 1x
 product:addIngredient(778, 4) -- Schritte 1 - 4: Zuckerrohr(778) 1x
 product:addIngredient(52)
@@ -60,7 +60,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT RUM - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT BEERENSCHNAPS - 1315 ---------------------------
-product = brewing:addProduct(catId, 1315, 54, 75, 19, 38)
+product = brewing:addProduct(catId, 1315, 54, 75, 114, 228)
 product:addIngredient(1317) -- Schritt 6: leere Flasche(1317) 1x
 product:addIngredient(81, 2*2) -- Schritt 1 - 2: Beeren(81) 2x
 product:addIngredient(147, 2*2) -- Schritt 3 - 4: Brombeeren(147) 2x
@@ -69,7 +69,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT BEERENSCHNAPS - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT BÄRENTÖTER - 1316 ---------------------------
-product = brewing:addProduct(catId, 1316, 56, 75, 19, 38)
+product = brewing:addProduct(catId, 1316, 56, 75, 114, 228)
 product:addIngredient(1317) -- Schritt 6: leere Flasche(1317) 1x
 product:addIngredient(15, 4*2) -- Schritt 1 - 4: Apfel(15) 2x
 product:addIngredient(2529, 2) -- Schritt 5: Honig(2529) x2
@@ -79,7 +79,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT BÄRENTÖTER - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT ELBENWEIN - 1318 ---------------------------
-product = brewing:addProduct(catId, 1318, 87, 100, 27, 54)
+product = brewing:addProduct(catId, 1318, 87, 100, 135, 270)
 product:addIngredient(1317) -- Schritt 6: leere Flasche(1317) 1x
 product:addIngredient(759, 2) -- Schritt 1 - 2: Nüsse(759) x1
 product:addIngredient(80, 3) -- Schritt 3: Beeren(81) x3
@@ -87,7 +87,7 @@ product:addIngredient(2529, 2) -- Schritt 4: Honig(2529) x2
 ------------------- FLASCHE MIT ELBENWEIN - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT KIRSCHSCHNAPS - 1319 ---------------------------
-product = brewing:addProduct(catId, 1319, 77, 95, 24, 48)
+product = brewing:addProduct(catId, 1319, 77, 95, 144, 288)
 product:addIngredient(1317) -- Schritt 6: leere Flasche(1317) 1x
 product:addIngredient(302, 4*2) -- Schritt 1 - 4: Kirschen(302) 2x
 product:addIngredient(52)
@@ -97,7 +97,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 catId = brewing:addCategory("juices", "Säfte") -- Tee, Smoothies, Saft :p
 
 ------------------- FLASCHE MIT BROMBEERSAFT - 783 Dunkelblau ---------------------------
-product = brewing:addProduct(catId, 783, 63, 85, 21, 42)
+product = brewing:addProduct(catId, 783, 63, 85, 126, 252)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(157, 3*2) -- Schritt 1 - 3: Brombeeren(157) 2x
 product:addIngredient(778) -- Schritt 4: Zuckerrohr(778) 1x
@@ -106,7 +106,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT BROMBEERSAFT - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT MANDARINENSAFT - 784 ---------------------------
-product = brewing:addProduct(catId, 784, 68, 90, 22, 44)
+product = brewing:addProduct(catId, 784, 68, 90, 132, 264)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(199, 3*2) -- Schritt 1 - 3: Mandarinen(199) 2x
 product:addIngredient(778) -- Schritt 4: Zuckerrohr(778) 1x
@@ -115,7 +115,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT MANDARINENSAFT - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT BANANENSAFT - 785 ---------------------------
-product = brewing:addProduct(catId, 785, 78, 100, 25, 50)
+product = brewing:addProduct(catId, 785, 78, 100, 150, 300)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(80, 3*2) -- Schritt 1 - 3: Bananen(80) 2x
 product:addIngredient(778) -- Schritt 4: Zuckerrohr(778) 1x
@@ -124,7 +124,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT BANANENSAFT - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT KOHLSAFT - 786 ---------------------------
-product = brewing:addProduct(catId, 786, 33, 50, 12, 24)
+product = brewing:addProduct(catId, 786, 33, 50, 72, 144)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(155, 4*2) -- Schritt 1 - 4: Kohl(290) 2x
 product:addIngredient(52)
@@ -132,7 +132,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT KOHLSAFT - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT JUNGFERNKRAUTTEE - 787 --------------------------- 
-product = brewing:addProduct(catId, 787, 29, 45, 11, 22)
+product = brewing:addProduct(catId, 787, 29, 45, 110, 220)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(144, 3*2) -- Schritt 1 - 3: Jungfernkraut(144) 2x
 product:addIngredient(157, 2) -- Schritt 4: Brombeeren(157) 2x
@@ -142,7 +142,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT TEE - FERTIG --------------------------- 
 
 ------------------- FLASCHE MIT KAROTTENSAFT - 788 ---------------------------
-product = brewing:addProduct(catId, 788, 57, 75, 19, 38)
+product = brewing:addProduct(catId, 788, 57, 75, 114, 228)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(2493, 3*2) -- Schritt 1 - 3: Karotten(2493) 2x
 product:addIngredient(778) -- Schritt 4: Zuckerrohr(778) 1x
@@ -151,7 +151,7 @@ product:addRemnant(51) -- Schritt 7: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT KAROTTENSAFT - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT ERDBEERSAFT - 789 ---------------------------
-product = brewing:addProduct(catId, 789, 73, 95, 24, 48)
+product = brewing:addProduct(catId, 789, 73, 95, 144, 288)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(151, 3*2) -- Schritt 1 - 3: Erdbeeren(151) 2x
 product:addIngredient(778) -- Schritt 4: Zuckerrohr(778) 1x
@@ -160,7 +160,7 @@ product:addRemnant(51) -- Schritt 5: Eimer mit Wasser(52) 1x
 ------------------- FLASCHE MIT ERDBEERSAFT - FERTIG ---------------------------
 
 ------------------- FLASCHE MIT TRAUBENSAFT - 791 ---------------------------
-product = brewing:addProduct(catId, 791, 83, 100, 25, 50)
+product = brewing:addProduct(catId, 791, 83, 100, 150, 300)
 product:addIngredient(790) -- Schritt 6: leere Flasche(790) 1x
 product:addIngredient(388, 3*2) -- Schritt 1 - 3: Trauben(388) 2x
 product:addIngredient(778) -- Schritt 4: Zuckerrohr(778) 1x
--- testserver/content/craft/gemcutting.lua
+++ testserver/content/craft/gemcutting.lua
@@ -21,7 +21,7 @@ product:addIngredient(251) -- Schritt 1: Rohe Amethysten(251) 1x
 -------------- AMETHYST - FERTIG ---------------
 
 ---------------- AMETHYSTSTAUB - 450 ----------------
-product = gemcutting:addProduct(catDust, 450, 0, 30, 10, 20)
+product = gemcutting:addProduct(catDust, 450, 0, 30, 20, 40)
 product:addIngredient(197, 4) -- Schritt 1: Amethyst(197) 4x
 -------------- AMETHYSTSTAUB - FERTIG ---------------
 
@@ -31,7 +31,7 @@ product:addIngredient(255) -- Schritt 1: Rohe Rubine(255) 1x
 --------------- RUBIN - FERTIG -----------------
 
 ---------------- RUBINSTAUB - 447 ----------------
-product = gemcutting:addProduct(catDust, 447, 10, 40, 15, 30)
+product = gemcutting:addProduct(catDust, 447, 10, 40, 30, 60)
 product:addIngredient(46, 4) -- Schritt 1: Rubin(46) 4x
 -------------- RUBINSTAUB - FERTIG ---------------
 
@@ -41,7 +41,7 @@ product:addIngredient(253) -- Schritt 1: Roher Saphir(253) 1x
 ------------- BLAUSTEIN - FERTIG ---------------
 
 --------------- BLAUSTEINSTAUB - 446 ----------------
-product = gemcutting:addProduct(catDust, 446, 20, 50, 20, 40)
+product = gemcutting:addProduct(catDust, 446, 20, 50, 40, 80)
 product:addIngredient(284, 4) -- Schritt 1: Saphir(284) 4x
 ------------- BLAUSTEINSTAUB - FERTIG ---------------
 
@@ -51,7 +51,7 @@ product:addIngredient(252) -- Schritt 1: Roher Obsidian(252) 1x
 ----------- SCHWARZSTEIN - FERTIG --------------
 
 ------------- SCHWARZSTEINSTAUB - 449 ---------------
-product = gemcutting:addProduct(catDust, 449, 30, 60, 25, 50)
+product = gemcutting:addProduct(catDust, 449, 30, 60, 50, 100)
 product:addIngredient(283, 4) -- Schritt 1: Obsidian(283) 4x
 ----------- SCHWARZSTEINSTAUB - FERTIG --------------
 
@@ -61,7 +61,7 @@ product:addIngredient(256) -- Schritt 1: Roher Smaragd(256) 1x
 ------------- SMARAGD - FERTIG -----------------
 
 ---------------- SMARAGDSTAUB - 448 ------------------
-product = gemcutting:addProduct(catDust, 448, 40, 80, 30, 60)
+product = gemcutting:addProduct(catDust, 448, 40, 80, 60, 120)
 product:addIngredient(45, 4) -- Schritt 1: Smaragd(45) 4x
 ------------- SMARAGDSTAUB - FERTIG -----------------
 
@@ -71,7 +71,7 @@ product:addIngredient(257) -- Schritt 1: Roher Topas(257) 1x
 --------------- TOPAS - FERTIG -----------------
 
 --------------- TOPASSTAUB - 451 --------------------
-product = gemcutting:addProduct(catDust, 451, 50, 100, 35, 70)
+product = gemcutting:addProduct(catDust, 451, 50, 100, 70, 140)
 product:addIngredient(198, 4) -- Schritt 1: Topas(198) 4x
 --------------- TOPASSTAUB - FERTIG -----------------
 
@@ -81,6 +81,6 @@ product:addIngredient(254) -- Schritt 1: Rohdiamanten(254) 1x
 --------------- DIAMANT - FERTIG -----------------
 
 --------------- DIAMANTSTAUB - 452 --------------------
-product = gemcutting:addProduct(catDust, 452, 60, 120, 40, 80)
+product = gemcutting:addProduct(catDust, 452, 60, 120, 80, 160)
 product:addIngredient(285, 4) -- Schritt 1: Diamant(285) 4x
 --------------- DIAMANTSTAUB - FERTIG -----------------
