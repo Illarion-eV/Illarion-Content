cc7b91030c9f23f3d36d7d9ca70603a9b7b9d6a7
vilarion <vilarion@illarion.org>
2012-12-13 18:45:10 +0100

    Fix instrument learning

--- testserver/item/base/music.lua
+++ testserver/item/base/music.lua
@@ -17,6 +17,6 @@ function PlayInstrument(User,Item,Skill)
     local PlayVal=base.common.Limit(math.floor((Skl+(Qual*5))/120*table.getn(TalkTexts[Skill])*(math.random(8,13)/10)),1,table.getn(TalkTexts[Skill]));
     User:talkLanguage( Character.say, Player.german, TalkTexts[Skill][PlayVal][2]);
     User:talkLanguage( Character.say, Player.english, TalkTexts[Skill][PlayVal][1]);
-    User:learn(Skill,30,100,User:increaseAttrib("dexterity",0));
+    User:learn(Skill,30,100)
 	User.movepoints=User.movepoints-30;
 end
