eb9a0d9965f961539b644562ebbd5053e4264d7b
vilarion <vilarion@illarion.org>
2012-12-13 17:01:12 +0100

    Fix depot lookat

--- testserver/item/id_321_depot.lua
+++ testserver/item/id_321_depot.lua
@@ -2,47 +2,24 @@
 
 -- UPDATE common SET com_script='item.id_321_depot' WHERE com_itemid=321;
 
+require("base.common")
+require("base.lookat")
+
 module("item.id_321_depot", package.seeall)
 
-function LookAtItem(User,Item)
-    local lang = User:getPlayerLanguage();
-    local LookAtStr = world:getItemName(321,lang).." (";
+function LookAtItem(User, Item)
+    local lookAt = base.lookat.GenerateLookAt(User, Item)
+    local depotId = tonumber(Item.getData("depot"))
 
---NewIllarion
-
-    if (Item.data==101) then
-            LookAtStr = LookAtStr.."Cadomyr";
-
-    elseif (Item.data==102) then
-            LookAtStr = LookAtStr.."Runewick";
-
-    elseif (Item.data==103) then
-            LookAtStr = LookAtStr.."Galmair";
-
-    elseif (Item.data==104) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Gasthof zur Hanfschlinge";
-        else
-            LookAtStr = LookAtStr.."The Hemp Necktie Inn";
-        end
-
-    elseif (Item.data==1337) then
-        if (lang==0) then
-            LookAtStr = LookAtStr.."Estralis - Hand ab, sonst Hand ab!";
-        else
-            LookAtStr = LookAtStr.."Estralis - Hands off or hands off!";
-        end
-
-    else
-
-        theOwner=getCharForId(Item.data);
-        if theOwner then   
-            LookAtStr = LookAtStr..theOwner.name;
-        else
-            LookAtStr = LookAtStr.."Debugging: Unknown depot";
-        end
+    if depotId == 101 then
+        lookAt.description = "Cadomyr"
+    elseif depotId == 102 then
+        lookAt.description = "Runewick"
+    elseif depotId == 103 then
+        lookAt.description = "Galmair"
+    elseif depotId == 104 then
+        lookAt.description = base.common.GetNLS(User, "Gasthof zur Hanfschlinge", "The Hemp Necktie Inn")
     end
     
-    LookAtStr = LookAtStr..")";
-    world:itemInform(User,Item,LookAtStr);
+    world:itemInform(User, Item, lookAt)
 end
