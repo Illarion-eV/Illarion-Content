e694b77cfeb0311249029549d18dc5da7510816e
Dyluck <dyluckedina@hotmail.com>
2012-12-14 00:19:07 -0800

    Testing Script

--- testserver/item/id_90_flute.lua
+++ testserver/item/id_90_flute.lua
@@ -16,7 +16,35 @@ item.base.music.addTalkText("#me plays an airy tune on the flute.","#me spielt e
 item.base.music.addTalkText("#me plays a wild tune on the flute.","#me spielt eine wilde Melodie auf der Flöte.", skill);
 
 function UseItem(User,SourceItem,TargetItem,Counter,Param)
-	item.base.music.PlayInstrument(User,SourceItem,skill);
+	item.base.music.PlayInstrument(User,SourceItem,"flute");
+	
+	--Testing fireball, only activates if flute's data key name=fireball -Dyluck
+	local targetPos
+	local targetChar
+	
+	if ( SourceItem:getData(name) = "fireball") then --if book is fireball
+		User:talk(Character.say, "#me casts 'Fireball';
+		
+		if ( User:getFaceTo() = 0) then --if facing north
+			targetPos = position(User.pos.x, User.pos.y + 3, User.pos.z);
+			world:gfx(9, targetPos);
+			world:makeSound(5, targetPos);
+			
+			if world:isCharacterOnField(targetPos) then --if there's a target char on target position
+				targetChar = world:getCharacterOnField(targetPos); --find the char
+				target:increaseAttrib("hitpoints", -1000);
+				world:makeSound(1, targetPos);
+			end
+		end
+	end			
+	--End Test
+	
 end
 
-LookAtItem = item.general.wood.LookAtItem
+--Testing fireball lookat -Dyluck
+function LookAtItem(User,Item)
+	if ( Item:getData(name) = "fireball") then --if book is fireball
+		world:itemInform(User,Item,"Spellbook 'Fireball'");
+	end
+end
+--End Test
