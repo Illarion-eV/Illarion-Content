20be44cde6cef3686a0679b58bb9a00c78c8e1fe
Lillian <maletzki.lisa@googlemail.com>
2012-12-15 19:27:03 +0100

    changed input string for rankpointmodification

--- testserver/gm/items/id_382_ceilingtrowel.lua
+++ testserver/gm/items/id_382_ceilingtrowel.lua
@@ -360,8 +360,8 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param)
             return;
           end
           local inputString = dialog:getInput();
-          if (string.find(inputString,"(%a+) ((%d+))+") ~= nil) then
-            a, b, modifier,value,faction,radius = string.find(inputString,"(%a+) ((%d+))+");
+          if (string.find(inputString,"(%a+) (%d+)[(%s)(%d+)]*") ~= nil) then
+            a, b, modifier,value,faction,radius = string.find(inputString,"(%a+) (%d+)[(%s)(%d+)]*");
             value=tonumber(value);
 			User:inform("[Debug] Fitting string found "..modifier..value)
             ChangeRankpoints(User,modifier,value,faction,radius);
