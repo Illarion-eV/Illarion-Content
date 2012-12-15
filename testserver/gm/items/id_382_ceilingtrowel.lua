8a625a1f09b087f67ec1d8ae8542171c4c60d416
Lillian <maletzki.lisa@googlemail.com>
2012-12-15 19:56:14 +0100

    changed debug messages

--- testserver/gm/items/id_382_ceilingtrowel.lua
+++ testserver/gm/items/id_382_ceilingtrowel.lua
@@ -363,12 +363,12 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param)
           if (string.find(inputString,"(%a+) (%d+)") ~= nil) then
             a, b, modifier,value,faction,radius = string.find(inputString,"(%a+) (%d+)");
             value=tonumber(value);
-			User:inform("[Debug] Fitting string found "..modifier..value..faction..radius)
+			User:inform("[Debug] Fitting string found "..modifier..value)
             ChangeRankpoints(User,modifier,value,faction,radius);
 		  elseif (string.find(inputString,"(%a+) (%d+) (%d+)") ~= nil) then
             a, b, modifier,value,faction,radius = string.find(inputString,"(%a+) (%d+) (%d+)");
             value=tonumber(value);
-			User:inform("[Debug] Fitting string found "..modifier..value..faction..radius)
+			User:inform("[Debug] Fitting string found "..modifier..value..faction)
             ChangeRankpoints(User,modifier,value,faction,radius);
 		  elseif (string.find(inputString,"(%a+) (%d+) (%d+) (%d+)") ~= nil) then
             a, b, modifier,value,faction,radius = string.find(inputString,"(%a+) (%d+) (%d+) (%d+)");
