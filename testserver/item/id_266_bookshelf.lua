be87d0086d64cf6d6ebec3a091b44afdf599d220
vilarion <vilarion@illarion.org>
2012-12-17 05:25:34 +0100

    Change description of bookshelves

--- testserver/item/id_266_bookshelf.lua
+++ testserver/item/id_266_bookshelf.lua
@@ -8,12 +8,26 @@ ALCHEMY = 0
 CRAFTING = 1
 FAIRYTALES = 2
 
-function addBookshelf(pos, books)
+function addBook(id, germanTitle, englishTitle)
+    books = books or {}
+    books[id] = {german = germanTitle, english = englishTitle}
+end
+
+function addBookshelf(pos, containedBooks)
     bookshelves = bookshelves or {}
     bookshelves[pos.x] = bookshelves[pos.x] or {}
     bookshelves[pos.x][pos.y] = bookshelves[pos.x][pos.y] or {}
-    bookshelves[pos.x][pos.y][pos.z] = books
+    local bookshelf = bookshelves[pos.x][pos.y][pos.z]
+    bookshelf = {}
+
+    for i=1, #containedBooks do
+        table.insert(bookshelf, containedBooks[i])
+    end
 end
+
+addBook(ALCHEMY, "Alchemie", "Alchemy")
+addBook(CRAFTING, "Handwerk", "Crafting")
+addBook(FAIRYTALES, "Märchen", "Fairy Tales")
 
 addBookshelf(position(10, 10, 0), {CRAFTING, FAIRYTALES})
 addBookshelf(position(7, 7, 0), {CRAFTING})
@@ -24,17 +38,22 @@ function LookAtItem(user, item)
     
     if item:getType() == scriptItem.field then 
         local pos = item.pos
-        local bookshelf = bookshelves and bookshelves[pos.x] and bookshelves[pos.x][pos.y] and bookshelves[pos.x][pos.y][pos.z]
+        local bookshelf = getBookshelf(pos)
 
         if bookshelf then
-            local books = #bookshelf
+            local bookCount = #bookshelf
 
-            if books == 0 then
+            if bookCount == 0 then
                 lookAt.description = base.common.GetNLS(user, "Dieses Regal ist leer.", "This bookshelf is empty.")
-            elseif books == 1 then
-                lookAt.description = base.common.GetNLS(user, "Hier steht ein einzelnes Buch.", "There is one single book.")
+            elseif bookCount == 1 then
+                lookAt.description = base.common.GetNLS(user, "Hier steht ein einzelnes Buch:", "There is one single book:")
+                lookAt.description = lookAt.description .. "\n" .. base.common.GetNLS(user, books[bookshelf[1]].german, books[bookshelf[1]].english) 
             else
-                lookAt.description = base.common.GetNLS(user, "Hier stehen " .. books .. " Bücher.", "There are " .. books .. " books here.")
+                lookAt.description = base.common.GetNLS(user, "Hier stehen " .. bookCount .. " Bücher:", "There are " .. bookCount .. " books here:")
+                
+                for i=1, bookCount do
+                    lookAt.description = lookAt.description .. "\n" .. base.common.GetNLS(user, books[bookshelf[i]].german, books[bookshelf[i]].english)
+                end
             end
         else
             lookAt.description = base.common.GetNLS(user, "Dieses Regal ist leer.", "This bookshelf is empty.")
@@ -45,5 +64,11 @@ function LookAtItem(user, item)
 end
 
 function UseItem(user, item, target, counter, param, ltstate)
+    if item:getType() ~= scriptItem.field then
+        return
+    end
+end
 
+function getBookshelf(pos)
+    return bookshelves and bookshelves[pos.x] and bookshelves[pos.x][pos.y] and bookshelves[pos.x][pos.y][pos.z]
 end
