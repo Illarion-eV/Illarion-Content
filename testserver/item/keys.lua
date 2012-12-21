47ae67bcc1afcbfd46ff6f4879b7e21052dd5fcd
Lillian <maletzki.lisa@googlemail.com>
2012-12-21 14:03:40 +0100

    added debug messages

--- testserver/item/keys.lua
+++ testserver/item/keys.lua
@@ -16,9 +16,12 @@ function UseItem(User,SourceItem,TargetItem,counter,param)
 	end	
 
 	if base.keys.CheckKey(SourceItem,DoorItem) then	
-        if base.keys.LockDoor(DoorItem) then
+		local lockDoor = base.keys.LockDoor(DoorItem);
+		local unlockDoor = base.keys.UnlockDoor(DoorItem);
+		debug("lockDoor "..lockDoor) 
+        if lockDoor then
             base.common.InformNLS(User,"Du sperrst die Tür ab.","You lock the door.");
-        elseif base.keys.UnlockDoor(DoorItem) then            
+        elseif unlockDoor then            
             base.common.InformNLS(User,"Du sperrst die Tür auf.","You unlock the door.");
         end
     else
