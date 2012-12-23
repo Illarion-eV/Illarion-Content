63c67e9001b235295ddfcd68f6565fc2073e93f1
merung <karten-spiel-haus@hotmail.de>
2012-12-23 23:17:04 +0100

    new pos

--- testserver/npc/madeline_clarbelis.lua
+++ testserver/npc/madeline_clarbelis.lua
@@ -2,8 +2,8 @@
 -- NPC Name: Madeline Clarbelis                                      Runewick --
 -- NPC Job:  Druid Apprentice                                                 --
 --                                                                            --
--- NPC Race: human                      NPC Position:  898, 835, 1            --
--- NPC Sex:  female                     NPC Direction: south                  --
+-- NPC Race: human                      NPC Position:  951, 766, 1            --
+-- NPC Sex:  female                     NPC Direction: west                   --
 --                                                                            --
 -- Author:   Grokk                                                            --
 --                                                       easyNPC Parser v1.21 --
@@ -11,7 +11,7 @@
 
 --[[SQL
 INSERT INTO "npc" ("npc_type", "npc_posx", "npc_posy", "npc_posz", "npc_faceto", "npc_name", "npc_script", "npc_sex", "npc_hair", "npc_beard", "npc_hairred", "npc_hairgreen", "npc_hairblue", "npc_skinred", "npc_skingreen", "npc_skinblue") 
-VALUES (0, 898, 835, 1, 4, 'Madeline Clarbelis', 'npc.madeline_clarbelis', 1, 1, 0, 255, 236, 139, 255, 211, 155);
+VALUES (0, 951, 766, 1, 6, 'Madeline Clarbelis', 'npc.madeline_clarbelis', 1, 1, 0, 255, 236, 139, 255, 211, 155);
 ---]]
 
 require("npc.base.basic")
