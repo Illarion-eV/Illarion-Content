7af9ff3253297aa9ee1e6326529057c5466470f7
estralis <estralis@illarion.org>
2012-12-23 12:19:17 +0100

    Weiter

--- testserver/item/id_3108_bookrest.lua
+++ testserver/item/id_3108_bookrest.lua
@@ -11,11 +11,14 @@ function LookAtItem(User,Item)
     if (Item.pos == position(741,406,-3)) then
 	
 	    lookAt = ItemLookAt();
-	
+		lookAt.rareness = ItemLookAt.epicItem;
+        	
 	    if (User:getPlayerLanguage()==0) then
 	        lookAt.name = "Tagebuch des Abtes Elzechiel";
+			lookAt.description = "Dieses Buch ist von einer schaurigen Schönheit. Du bist versucht, es dennoch zu lesen..."
 	    else
 	        lookAt.name = "Journal of Abbot Elzechiel";
+			lookAt.description = "This item has an evil presence. You are tempted to read it, though..."
 	    end
 		
         world:itemInform(User, Item, lookAt); --custom lookAt
@@ -27,7 +30,7 @@ end
 
 function UseItem(User,SourceItem,TargetItem,counter,param)
 
-    if (Item.pos == position(741,406,-3)) then
+    if (SourceItem.pos == position(741,406,-3)) then
 	    base.common.InformNLS(User,"Bitte hier das Lesen des Salavesh-Buches implementieren!","Please implement reading the Salavesh book here!");
 	    --Please implement reading the Salavesh book here!
 	end
