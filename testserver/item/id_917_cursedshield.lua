b6983515ac1edca30a62f1edbd640a41f0bd8f23
Lillian <maletzki.lisa@googlemail.com>
2012-12-24 10:32:19 +0100

    replaced old data and added translation

--- testserver/item/id_917_cursedshield.lua
+++ testserver/item/id_917_cursedshield.lua
@@ -7,17 +7,17 @@ module("item.id_917_cursedshield", package.seeall, package.seeall(item.general.m
 
 function MoveItemBeforeMove( User, SourceItem, TargetItem )
     -- if shield was purified, then no possibility of curse
-    if ( SourceItem.data == 1 ) then return true; end;
+    if ( tonumber(SourceItem:getData("cursedShield")) == 1 ) then return true; end;
     
     -- if the shield is cursed, make it impossible to unequip
-    if ( SourceItem.data == 2 ) and ( ( SourceItem.itempos == 5 ) or ( SourceItem.itempos == 6 ) ) then
+    if ( tonumber(SourceItem:getData("cursedShield")) == 2 ) and ( ( SourceItem.itempos == 5 ) or ( SourceItem.itempos == 6 ) ) then
         -- if successfully removed
         if ( math.random( 2000 ) <= User:increaseAttrib( "willpower", 0 ) * 4 ) then
-            base.common.InformNLS( User, "German", "With a strong will and perserverance, you manage to detach the cursed shield from your hand." )
+            base.common.InformNLS( User, "Mit deinem starken Willen und Ausdauer schaffst du es, das verfluchte Schild von deiner Hand zu lösen.", "With a strong will and perseverance, you manage to detach the cursed shield from your hand." )
             return true;
         end;
         -- else unable to remove shield
-        base.common.InformNLS( User, "German", "Some kind of dark energy seems to prohibit you from releasing the shield." );
+        base.common.InformNLS( User, "Eine dunkle Energie scheint dich daran zu hindern das Schild loszulassen.", "Some kind of dark energy seems to prohibit you from releasing the shield." );
         return false;
     end;
     
@@ -30,12 +30,12 @@ function MoveItemAfterMove( User, SourceItem, TargetItem )
         -- if curse gets in effect
         local curseChance = math.random( 5 + User:increaseAttrib( "essence", 0 ) + math.floor( User:getSkill(Character.magicResistance) / 5 ) );
         -- if shield curse had already been effected, but user managed to remove it
-        if ( SourceItem.data == 2 ) then curseChance = 1; end;
+        if ( tonumber(SourceItem:getData("cursedShield")) == 2 ) then curseChance = 1; end;
         
         if ( curseChance == 1 ) then
-            TargetItem.data = 2;
+            tonumber(TargetItem:getData("cursedShield")) = 2;
             world:changeItem( TargetItem );
-            base.common.InformNLS( User, "German", "A sudden dark energy emenates from the shield and seems to clutch to your hand." );
+            base.common.InformNLS( User, "Eine plötzliche dunkle Energie strömt aus dem Schild und scheint deine Hand zu packen.", "A sudden dark energy emenates from the shield and seems to clutch to your hand." );
             return true;
         end;
     end;
