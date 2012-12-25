92730ca18c4ae8cf00c5ccfd12f71cebf114a260
martin <martin@illarion.org>
2012-12-25 21:33:02 +0100

    debug fighting

--- testserver/server/standardfighting.lua
+++ testserver/server/standardfighting.lua
@@ -828,6 +828,7 @@ function LoadWeapons(CharStruct)
 		if rWType==10 or rWType==11 or rWType==14 then -- Ammo or shield in right hand: switch r and l hand!
 			isRWp=0;
 		end
+		debug("*** FOUND WP IN R!");
 	end
 	
 	if lAttFound then
@@ -835,9 +836,11 @@ function LoadWeapons(CharStruct)
 		if lWType==10 or lWType==11 or lWType==14 then -- Ammo or shield in right hand: switch r and l hand!
 			isLWp=0;
 		end
+		debug("*** FOUND WP IN L!");
 	end
 	
 	if isRWp==0 and isLWp==1 then 	-- switch weapons
+	debug("*** SWITCHING WEAPONS NOW!"); 
 		local dItem=rItem;
 		local dAttFound=rAttFound;
 		local dAttWeapon=rAttWeapon;
