885506350063d447a222f23995c2b85f75106b0b
Lillian <maletzki.lisa@googlemail.com>
2012-12-26 18:01:08 +0100

    fixed lookat

--- testserver/questsystem/Geld_in_Hecke/trigger2.lua
+++ testserver/questsystem/Geld_in_Hecke/trigger2.lua
@@ -18,7 +18,7 @@ function LookAtItem(PLAYER, item)
       and questsystem.base.fulfilsPrecondition(PLAYER, QUEST_NUMBER, PRECONDITION_QUESTSTATE) then
 
 	base.lookat.SetSpecialName(item, LOOKAT_TEXT_DE, LOOKAT_TEXT_EN)
-	world:itemInform(PLAYER,Item,base.lookat.GenerateLookAt(PLAYER, Item, base.lookat.NONE));
+	world:itemInform(PLAYER,item,base.lookat.GenerateLookAt(PLAYER, item, base.lookat.NONE));
     
     HANDLER(PLAYER)
     
