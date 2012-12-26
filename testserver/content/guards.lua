4acd443ccf80b0f62003d61a68b5a50bd0f0fb21
pharse <pharse@illarion.org>
2012-12-26 10:31:41 +0100

    debug

--- testserver/content/guards.lua
+++ testserver/content/guards.lua
@@ -6,7 +6,7 @@ function InitGuards()
     Guards = {};
     AddGuard("Horatio Milenus", 1, position(114, 643, 0), 6, "cadomyr guard 1");
     AddGuard("Bre Southstar", 3, position(423, 250, 0), 6, "galmair guard 1");
-    AddGuard("Gavin Northstar", 3, position(386, 335, 0), 8, "galmair guard 1");
+    AddGuard("Gavin Northstar", 3, position(386, 335, 0), 8, "galmair guard 2");
     AddGuard("Brassius Meres", 2, position(834, 821, 0), 8, "runewick guard 1");
   end
 end
