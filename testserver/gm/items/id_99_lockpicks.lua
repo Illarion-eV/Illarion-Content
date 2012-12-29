320cd60f6d69b885d5109232f28ea34977273658
Lillian <maletzki.lisa@googlemail.com>
2012-12-29 23:58:08 +0100

    try on change skill mode

--- testserver/gm/items/id_99_lockpicks.lua
+++ testserver/gm/items/id_99_lockpicks.lua
@@ -173,7 +173,7 @@ function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
 			end
 			local sdSkill = SelectionDialog("Select skill", "What skill do you wish to change for "..chosenPlayer.name.."?", skillDialog)
 			for _,skill in ipairs(skillNames) do 
-				sdSkill:addOption(0,User:getSkillName(User.dodge).." value: "..chosenPlayer:getSkillValue(User.dodge).major);
+				sdSkill:addOption(0,User:getSkillName(dodge).." value: "..chosenPlayer:getSkillValue(dodge).major);
 			end		
 			User:requestSelectionDialog(sdSkill)
 		end
