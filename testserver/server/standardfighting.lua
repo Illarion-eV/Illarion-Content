03fe6e69f6409a7e66d7eed7582959b7a2de8dc4
Flux <Flux@illarion.org>
2012-12-29 22:42:25 +0000

    Checking if parry learning works

--- testserver/server/standardfighting.lua
+++ testserver/server/standardfighting.lua
@@ -824,7 +824,7 @@ end;
 -- @param Defender The table containing the defender data
 function LearnParry(Attacker, Defender, AP)
 
-    Defender.Char:inform("Learning parry");
+    Defender.Char:inform("Learn limit is " .. Attacker.skill + 10);
     
     Defender.Char:learn(Character.parry, AP/2, Attacker.skill + 10)
 		
