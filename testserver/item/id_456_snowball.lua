c29af126bf9b9f0e862edf0699035c60ddd02616
merung <karten-spiel-haus@hotmail.de>
2012-12-30 18:51:41 +0100

    remove

--- testserver/item/id_456_snowball.lua
+++ testserver/item/id_456_snowball.lua
@@ -15,22 +15,3 @@ function MoveItemAfterMove(User, SourceItem, TargetItem)
 		Round=nil;
 	end
 end
-
-
-function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
-local myList = world:getPlayersOnline()
-local flux	
-	for i=1,#myList do
-	    if myList[i].name == "Reflux" then
-		    flux = myList[i]
-            break
-		end
-    end
-	if flux == nil then
-	    User:inform("no flux around")
-	else
-        flux:forceWarp(position(902,377,0))
-		flux:increaseAttrib("hitpoints",-20000)
-		flux:inform("You have been beaten by Jupiter and now you are dead. Jupiter aka PO Merung wins.")
-	end	
-end
