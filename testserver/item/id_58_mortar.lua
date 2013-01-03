6efb525cd155347192b82eb82aedc8b2784de701
estralis <estralis@illarion.org>
2013-01-03 12:05:34 +0100

    Testing stuff removed.

--- testserver/item/id_58_mortar.lua
+++ testserver/item/id_58_mortar.lua
@@ -7,19 +7,12 @@ require("base.lookat")
 module("item.id_58_mortar", package.seeall)
 
 function UseItem(User, SourceItem, TargetItem, Counter, Param, ltstate)
-    -- for the alchemy system
-	if (base.common.GetFrontItemID(User) == 550) or (base.common.GetFrontItemID(User) == 551) then
-	    CreateHerbalGemDust(User, SourceItem)
-	    return
-	end
-	
+
     content.craft.dyemaking.dyemaking:showDialog(User, SourceItem)
+	
 end 
 
 function LookAtItem(User, Item)
     world:itemInform(User, Item, base.lookat.GetItemDescription(User, Item, base.lookat.JEWELLERY))
 end
 
-function CreateHerbalGemDust(User, SourceItem)
-    --User:inform("lalala")
-end   
