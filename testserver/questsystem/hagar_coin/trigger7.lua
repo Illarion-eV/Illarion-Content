f3ea3411e834b207852456f0ec2721dddccdcfdc
Lillian <maletzki.lisa@googlemail.com>
2013-01-04 18:15:43 +0100

    fixed receive function

--- testserver/questsystem/hagar_coin/trigger7.lua
+++ testserver/questsystem/hagar_coin/trigger7.lua
@@ -10,8 +10,8 @@ local NPC_TRIGGER_EN = "quest"
 local NPC_REPLY_DE = "Psst... Ich habe da etwas munkeln gehört von einer Botschaft, die in eine Kiefer eingeritzt wurde... etwas südöstlich vom Tor zu Galmair, in der Nähe des Wassers."
 local NPC_REPLY_EN = "Psst... I heard rumors about something that was carved into a pine tree, in the southeast of the gate to Galmair, near the water."
 
-function receiveText(type, text, player)
-  --[[if questsystem.base.fulfilsPrecondition(player, QUEST_NUMBER, PRECONDITION_QUESTSTATE)
+function receiveText(thisNPC, texttype, text, player)
+  if questsystem.base.fulfilsPrecondition(player, QUEST_NUMBER, PRECONDITION_QUESTSTATE)
       and player:getType() == Character.player
       and string.find(text, getNLS(player, NPC_TRIGGER_DE, NPC_TRIGGER_EN)) then
     thisNPC:talk(Character.say, getNLS(player, NPC_REPLY_DE, NPC_REPLY_EN))
@@ -20,7 +20,7 @@ function receiveText(type, text, player)
     questsystem.base.setPostcondition(player, QUEST_NUMBER, POSTCONDITION_QUESTSTATE)
 
     return true
-  end]]
+  end
 
   return false
 end
