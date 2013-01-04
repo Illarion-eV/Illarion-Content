e9e37ff88311c5e8d462d860fc121ddc6f333092
Lillian <maletzki.lisa@googlemail.com>
2013-01-04 17:48:37 +0100

    removed debug messages, resolved issue 7857

--- testserver/npc/base/consequence/rankpoints.lua
+++ testserver/npc/base/consequence/rankpoints.lua
@@ -8,7 +8,6 @@ rankpoints = base.class.class(npc.base.consequence.consequence.consequence,
 function(self, mode, value)
     npc.base.consequence.consequence.consequence:init(self);
     self["value"], self["valuetype"] = npc.base.talk._set_value(value);
-    debug("value: "..value.."mode: "..mode)
     if (mode == "=") then
             self["perform"] = _rankpoints_helper_set;
     elseif (mode == "+") then
@@ -29,11 +28,8 @@ end;
 
 function _rankpoints_helper_add(self, npcChar, player)
     local value = npc.base.talk._get_value(self.npc, self.value, self.valuetype);
-	debug("value: "..value)
     local rankpoints = base.factions.getRankpoints(player);
-	debug("Old rankpoints: "..rankpoints)
     rankpoints = rankpoints + value;
-	debug("New rankpoints: "..rankpoints)
     base.factions.setRankpoints(player,rankpoints);
 end;
 
