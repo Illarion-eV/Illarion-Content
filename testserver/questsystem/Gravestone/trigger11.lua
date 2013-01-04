1a58efd77fddcd08c5ac8be877df965f592a695d
Lillian <maletzki.lisa@googlemail.com>
2013-01-04 19:12:37 +0100

    lookat fix and precondition fix

--- testserver/questsystem/Gravestone/trigger11.lua
+++ testserver/questsystem/Gravestone/trigger11.lua
@@ -5,6 +5,7 @@ module("questsystem.Gravestone.trigger11", package.seeall)
 local QUEST_NUMBER = 10000
 local PRECONDITION_QUESTSTATE = 88
 local POSTCONDITION_QUESTSTATE = 99
+local ADDITIONAL_QUESTSTATE = 104
 
 local POSITION = position(605, 344, 0)
 local RADIUS = 1
@@ -14,7 +15,7 @@ local LOOKAT_TEXT_EN = "The names of the lovers brighten up for a brief moment."
 function LookAtItem(PLAYER, item)
   if PLAYER:isInRangeToPosition(POSITION,RADIUS)
       and ADDITIONALCONDITIONS(PLAYER)
-      and questsystem.base.fulfilsPrecondition(PLAYER, QUEST_NUMBER, PRECONDITION_QUESTSTATE) then
+      and PLAYER:getQuestProgress(QUEST_NUMBER) < ADDITIONAL_QUESTSTATE then
 
 	base.lookat.SetSpecialDescription(item, LOOKAT_TEXT_DE, LOOKAT_TEXT_EN)
 	world:itemInform(PLAYER,item,base.lookat.GenerateLookAt(PLAYER, item, base.lookat.NONE));
