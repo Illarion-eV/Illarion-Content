667e86b3fa920979d678df414d23addfaf831641
merung <karten-spiel-haus@hotmail.de>
2013-01-05 20:48:48 +0100

    better tes func

--- testserver/test/merung.lua
+++ testserver/test/merung.lua
@@ -11,22 +11,24 @@ function UseItem(User,SourceItem,TargetItem,counter,param)
     end
 	
 	if (User.lastSpokenText == "drop") then
-	    local drop = function(posi)
-		    world:createItemFromId(15,1,posi,false,333,nil)
-	    end
-		for i=1,5 do
-		    base.common.CreateCircle(User.pos, i, drop)
-		end	
-	end
-	if (User.lastSpokenText == "remove") then
-	    local delete = function(posi)
-		    local theItem = world:getItemOnField(posi)
-			if theItem.id == 15 then 
-			    world:erase(theItem,1)
+	   for i = 1,10 do
+		   local drop = function(posi)
+				world:createItemFromId(15,1,posi,false,333,nil)
+			end
+			for j=1,5 do
+				base.common.CreateCircle(User.pos, i, drop)
 			end	
-        end
-		for i=1,5 do
-		    base.common.CreateCircle(User.pos, i, delete)
-		end	
+
+			local delete = function(posi)
+				local theItem = world:getItemOnField(posi)
+				if theItem.id == 15 then 
+					world:erase(theItem,1)
+				end	
+			end
+			for k=1,5 do
+				base.common.CreateCircle(User.pos, i, delete)
+			end	
+	    end 
+		User:inform("drop end")
 	end		
 end
