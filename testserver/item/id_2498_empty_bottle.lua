bdc4bdab67df0d1024b5390108348e72378c67c5
merung <karten-spiel-haus@hotmail.de>
2013-01-05 12:59:49 +0100

    ltstate added

--- testserver/item/id_2498_empty_bottle.lua
+++ testserver/item/id_2498_empty_bottle.lua
@@ -7,7 +7,7 @@
 require("base.common");
 module("item.id_2498_empty_bottle", package.seeall)
 
-function UseItem(User,SourceItem,TargetItem,Counter,Param)
+function UseItem(User,SourceItem,TargetItem,Counter,Param,ltstate)
    local foundSource
   -- check for well or fountain
   TargetItem = base.common.GetItemInArea(User.pos, 2207);
