96a841d467f92f2100ec07fffe5e331e74df286a
martin <martin@illarion.org>
2013-01-06 10:05:20 +0100

    gems

--- testserver/test/martin.lua
+++ testserver/test/martin.lua
@@ -27,7 +27,7 @@ function receiveGems(gemRecipient)
 	-- first check if there was a switch already: 
 	local fnd, lastSwitch = ScriptVars:find("SwitchedToPayment"..town)
 	--fnd=1
-	lastSwitch=1
+	--lastSwitch=1
 	if fnd then gemRecipient:inform("lastSwitch: = "..lastSwitch) end
 	if fnd and tonumber(lastSwitch)<timeStmp then
 		gemRecipient:inform("now switching!")
