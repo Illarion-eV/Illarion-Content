602735097d8182bbbe581abb262be3302e05046d
merung <karten-spiel-haus@hotmail.de>
2013-01-06 20:32:59 +0100

    msg

--- testserver/item/id_1005_beehive.lua
+++ testserver/item/id_1005_beehive.lua
@@ -149,6 +149,7 @@ function UseItem( User, SourceItem, TargetItem, Counter, Param, ltstate )
 	else -- character can still carry something
 		if amount > 0 then  -- there are still items we can work on
 			honeygathering.SavedWorkTime[User.id] = honeygathering:GenWorkTime(User,nil);
+			User:inform("before start action amount is "..amount)
 			User:startAction( honeygathering.SavedWorkTime[User.id], 0, 0, 0, 0);
 		else -- no items left
 			-- only inform for non farming items. Farming items with amount==0 should already be erased.
