656df04d23a9d5c0a3ced11224e4278b94019a40
pharse <pharse@illarion.org>
2013-01-10 17:02:24 +0100

    genWorkTime depends only on maxTime and minTime.

--- testserver/base/gatheringcraft.lua
+++ testserver/base/gatheringcraft.lua
@@ -248,12 +248,12 @@ function GatheringCraft:GenWorkTime(User, toolItem)
   -- apply the quality bonus
   if ( toolItem ~= nil ) then
     local qual = math.min(9,math.max(1,math.floor(toolItem.quality/100))) - 1; -- quality integer in [0,8]
-    maxTime = maxTime - 20*(qual/8);
+    maxTime = maxTime - maxTime*0.25*(qual/8);
   end
 
   -- apply time bonus: reduce maxTime
   if (timeBonus>0) then
-    maxTime = maxTime - math.ceil(30*timeBonus);
+    maxTime = maxTime - math.ceil(maxTime*0.5*timeBonus);
   end
   -- apply skill bonus: increase skill
   if (skillBonus>0 and skill<100) then
