2d4efd2c0189f3f7ef31fc35eda0b7d2f540773f
merung <karten-spiel-haus@hotmail.de>
2013-01-11 22:53:35 +0100

    get array length

--- testserver/test/merung.lua
+++ testserver/test/merung.lua
@@ -131,7 +131,7 @@ function PutPlantOnField(rare)
 		if myList[groundType] == nil then
 		    return false
 		end	
-		myPlant = myList[groundType][math.random(1,myList[groundType])]
+		myPlant = myList[groundType][math.random(1,#myList[groundType])]
 		world:createItemFromId(myPlant,1,myPos,false,333,nil)
 		return true
 	end
