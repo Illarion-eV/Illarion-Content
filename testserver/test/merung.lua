9977e62a876772ebfb9adffd09ecd1d49feb13e4
merung <karten-spiel-haus@hotmail.de>
2013-01-12 00:33:25 +0100

    change list

--- testserver/test/merung.lua
+++ testserver/test/merung.lua
@@ -35,51 +35,51 @@ function Init(User)
 	
 	-- normal herbs; collectable with sickle
 	AddPlant(144, {gt.rocks},false,User)                         -- virgins weed
-    AddPlant(137, {gt.sand},false)                          -- flamegoblet blossom
-    AddPlant(135, {gt.grass},false)                         -- yellow weed
-    AddPlant(148, {gt.forest},false)                        -- firnis blossom
-    AddPlant(763, {gt.dirt},false)                          -- mash flower
-    AddPlant(767, {gt.water},false)                          -- water blossom
-	AddPlant(140, {gt.forest},false)                        -- donf blade
-    AddPlant(156, {gt.sand},false)                          -- steppe fern
-    AddPlant(153, {gt.grass},false)                         -- foot leaf
-    AddPlant(752, {gt.rock},false)                         -- mandrake
-	AddPlant(147, {gt.forest},false)                       -- blackberry
-    AddPlant(142, {gt.sand},false)                         -- sandberry
-    AddPlant(143, {gt.grass},false)                         -- red elder
-    AddPlant(136, {gt.dirt},false)                          -- anger berry
-    AddPlant(134, {gt.grass},false)                         -- fourleafed oneberry
-    AddPlant(155, {gt.water},false)                         -- sibanac leaf
-    AddPlant(151, {gt.forest},false)                        -- strawberry
-    AddPlant(141, {gt.rocks},false)                         -- black thistle
-    AddPlant(145, {gt.grass},false)                         -- heath flower
-    AddPlant(151, {gt.forest},false)                        -- strawberry
-    AddPlant(141, {gt.rocks},false)                         -- black thistle
-    AddPlant(133, {gt.grass},false)                         -- sun herb
-    AddPlant(753, {gt.grass,gt.forest},false)              -- blue bird's berry
-    AddPlant(159, {gt.forest},false)						-- toadstool
-    AddPlant(160, {gt.forest},false)						-- red head
-    AddPlant(161, {gt.grass},false)						-- herder's mushroom
-    AddPlant(162, {gt.grass},false)						-- birth mushroom
-    AddPlant(163, {gt.forest},false)						-- champignon
+    AddPlant(137, {gt.sand},false,User)                          -- flamegoblet blossom
+    AddPlant(135, {gt.grass},false,User)                         -- yellow weed
+    AddPlant(148, {gt.forest},false,User)                        -- firnis blossom
+    AddPlant(763, {gt.dirt},false,User)                          -- mash flower
+    AddPlant(767, {gt.water},false,User)                          -- water blossom
+	AddPlant(140, {gt.forest},false,User)                        -- donf blade
+    AddPlant(156, {gt.sand},false,User)                          -- steppe fern
+    AddPlant(153, {gt.grass},false,User)                         -- foot leaf
+    AddPlant(752, {gt.rock},false,User)                         -- mandrake
+	AddPlant(147, {gt.forest},false,User)                       -- blackberry
+    AddPlant(142, {gt.sand},false,User)                         -- sandberry
+    AddPlant(143, {gt.grass},false,User)                         -- red elder
+    AddPlant(136, {gt.dirt},false,User)                          -- anger berry
+    AddPlant(134, {gt.grass},false,User)                         -- fourleafed oneberry
+    AddPlant(155, {gt.water},false,User)                         -- sibanac leaf
+    AddPlant(151, {gt.forest},false,User)                        -- strawberry
+    AddPlant(141, {gt.rocks},false,User)                         -- black thistle
+    AddPlant(145, {gt.grass},false,User)                         -- heath flower
+    AddPlant(151, {gt.forest},false,User)                        -- strawberry
+    AddPlant(141, {gt.rocks},false,User)                         -- black thistle
+    AddPlant(133, {gt.grass},false,User)                         -- sun herb
+    AddPlant(753, {gt.grass,gt.forest},false,User)              -- blue bird's berry
+    AddPlant(159, {gt.forest},false,User)						-- toadstool
+    AddPlant(160, {gt.forest},false,User)						-- red head
+    AddPlant(161, {gt.grass},false,User)						-- herder's mushroom
+    AddPlant(162, {gt.grass},false,User)						-- birth mushroom
+    AddPlant(163, {gt.forest},false,User)						-- champignon
 	
 	-- rar herbs; not collectable with sickle
-    AddPlant(138, {gt.dirt},true)                        -- night angels blossom
-	AddPlant(146, {gt.sand},true)                        -- deser sky capsule
-	AddPlant(152, {gt.grass, gt.forest},true)            -- life root
-	AddPlant(754, {gt.grass},true)                        -- oneleaved fourberry
-	AddPlant(755, {gt.rocks},true)                        -- fire root
-	AddPlant(756, {gt.grass,gt.forest},true)             -- pious berry
-	AddPlant(757, {gt.forest},true)                        -- tybalt star
-	AddPlant(758, {gt.rocks},true)                        -- heart blood
-	AddPlant(760, {gt.snow},true)                        -- ice leaf
-	AddPlant(761, {gt.grass},true)                        -- rain weed
-	AddPlant(762, {gt.rocks},true)                        -- gold crack herb
-	AddPlant(764, {gt.forest},true)                        -- dark moos
-	AddPlant(765, {gt.forest},true)                        -- day tream
-	AddPlant(766, {gt.forest},true)                        -- con blossom
-	AddPlant(768, {gt.forest},true)                        -- wolverine fern
-	AddPlant(769, {gt.sand},true)                         -- desert berry
+    AddPlant(138, {gt.dirt},true,User)                        -- night angels blossom
+	AddPlant(146, {gt.sand},true,User)                        -- deser sky capsule
+	AddPlant(152, {gt.grass, gt.forest},true,User)            -- life root
+	AddPlant(754, {gt.grass},true,User)                        -- oneleaved fourberry
+	AddPlant(755, {gt.rocks},true,User)                        -- fire root
+	AddPlant(756, {gt.grass,gt.forest},true,User)             -- pious berry
+	AddPlant(757, {gt.forest},true,User)                        -- tybalt star
+	AddPlant(758, {gt.rocks},true,User)                        -- heart blood
+	AddPlant(760, {gt.snow},true,User)                        -- ice leaf
+	AddPlant(761, {gt.grass},true,User)                        -- rain weed
+	AddPlant(762, {gt.rocks},true,User)                        -- gold crack herb
+	AddPlant(764, {gt.forest},true,User)                        -- dark moos
+	AddPlant(765, {gt.forest},true,User)                        -- day tream
+	AddPlant(766, {gt.forest},true,User)                        -- con blossom
+	AddPlant(768, {gt.forest},true,User)                        -- wolverine fern
+	AddPlant(769, {gt.sand},true,User)                         -- desert berry
 end
 
 function AddPlant(ItemID,Grounds,rare,User)
@@ -94,7 +94,13 @@ function AddPlant(ItemID,Grounds,rare,User)
 		    myList[Grounds] = {}
 		end
 		table.insert(myList[Grounds],ItemID)
-	end	
+	end
+	-- exchange old list with new list
+	if rare then -- rare herb
+	    RarePlantByGround = myList
+	else -- normal herb
+        NormalPlantByGround = myList
+    end	
 end
 
 function plantdrop(User)
