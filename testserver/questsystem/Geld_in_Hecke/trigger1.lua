2cbf093ba684840bf1d38faa726691f86f7e3f50
Enviyatar <enviyatar@gmx.at>
2013-01-13 22:40:18 +0100

    updated

--- testserver/questsystem/Geld_in_Hecke/trigger1.lua
+++ testserver/questsystem/Geld_in_Hecke/trigger1.lua
@@ -9,7 +9,7 @@ local PRECONDITION_QUESTSTATE = 14
 local POSTCONDITION_QUESTSTATE = 2
 
 local POSITION = position(685, 316, 0)
-local RADIUS = 2
+local RADIUS = 3
 local LOOKAT_TEXT_DE = "Ganz unten auf dem Boden des Fasses siehst du eine Notiz, die folgendes besagt: \"Das Geld liegt wie immer in der Hecke.\""
 local LOOKAT_TEXT_EN = "On the very bottom of the barrel is a hidden note. You can read the following: \"The money is as always in the hedge!\""
 
