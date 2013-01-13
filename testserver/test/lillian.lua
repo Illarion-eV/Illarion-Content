779faac3b6c12233deb5948d9646d1161d730dc9
Lillian <maletzki.lisa@googlemail.com>
2013-01-13 21:41:24 +0100

    test

--- testserver/test/lillian.lua
+++ testserver/test/lillian.lua
@@ -32,7 +32,7 @@ function getRanklist(User, NPC, message)
 	local place = 2;
 	
 	for i=1, 5 do -- get the top 5
-		found, arenaEntry = ScriptVars:find("ArenaList"..town);
+		found, arenaEntry = ScriptVars:find("ArenaListCadomyr");
 		if found then
 			arenaList = split(arenaEntry, ";");
 		elseif found == false or table.getn(arenaList) ~= 10 then
