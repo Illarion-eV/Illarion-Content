8c8ae887380043c3b8894fe95cf7107b3426ab2f
pharse <pharse@illarion.org>
2013-01-14 01:57:17 +0100

    debug

--- testserver/item/seeds.lua
+++ testserver/item/seeds.lua
@@ -143,7 +143,8 @@ function MoveItemBeforeMove(User, SourceItem, TargetItem)
     amount = tonumber(amount);
     debug("move seed, amount " .. amount);
     world:createItemFromId( SourceItem.id, amount, TargetItem.pos, true, 333, nil );
-    world:erase(TargetItem, SourceItem.number);
+    world:erase(SourceItem, SourceItem.number);
+    return false;
   end
   
   -- local amount = SourceItem:getData("amount");
