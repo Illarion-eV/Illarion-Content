e28fa0fc9febbd525f1a3fc8d36c72b373783c2c
estralis <estralis@illarion.org>
2013-01-21 19:57:35 +0100

    Folder "old" removed.

--- testserver/old/kill_sheep.lua
+++
@@ -1,8 +0,0 @@
--- Skript f&uuml;r das Schlachten eines Schafes
-function onDeath(monster)
-
-        pos = monster.pos;
-        --world:createMonster(1,pos,10);
-        world:createItemFromId(173,1,pos,true,333,0)
-end
-
