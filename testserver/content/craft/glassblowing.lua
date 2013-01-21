3bb3f9cc22f5b248661efce882464254a1daaf94
zot <zot@illarion.org>
2013-01-21 21:17:23 +0100

    Minor change again

--- testserver/content/craft/glassblowing.lua
+++ testserver/content/craft/glassblowing.lua
@@ -38,11 +38,11 @@ product:addIngredient(41) -- Glass Ingot: 3x
 
 --Glass
 product = glassblowing:addProduct(catId, 2055, 50, 90, 17, 34)
-product:addIngredient(41) -- Glass Ingot: 2x
+product:addIngredient(41) -- Glass Ingot: 1x
 
 --Glass Mug
 product = glassblowing:addProduct(catId, 1908, 60, 100, 20, 40)
-product:addIngredient(41) -- Glass Ingot: 2x
+product:addIngredient(41) -- Glass Ingot: 1x
 
 catId = glassblowing:addCategory("decoration", "Dekoration")
 
