b320ae3766ccc97aa460e2174c6c8896a3668ed0
vilarion <vilarion@illarion.org>
2013-01-29 01:25:28 +0100

    test lte

--- testserver/test/vilarion.lua
+++ testserver/test/vilarion.lua
@@ -224,7 +224,7 @@ Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
     --end;
 
     if (User.lastSpokenText == "ltelarge") then
-        User.effects:addEffect(LongTimeEffect(777, 30))
+        User.effects:addEffect(LongTimeEffect(777, 100))
     end
 
     --pos = User.pos
@@ -339,13 +339,13 @@ end
 
 function addEffect( e, c )
     c:inform("--- effect added ---");
-    e:addValue("n",4294967294);
+    e:addValue("n",4000000000);
 end
 
 function callEffect( e, c )
     local found, n = e:findValue("n");
     c:inform("--- effect called, n == "..n.." ---");
-    e.nextCalled = 30;
+    e.nextCalled = 100;
     return true;
 --[[
     c:inform("idleTime: "..c:idleTime());
