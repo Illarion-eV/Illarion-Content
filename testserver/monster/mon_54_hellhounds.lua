8055b3fc157e0cc913d463cc1dc096a3a809182d
Lillian <maletzki.lisa@googlemail.com>
2013-02-01 12:47:55 +0100

    messages added

--- testserver/monster/mon_54_hellhounds.lua
+++ testserver/monster/mon_54_hellhounds.lua
@@ -14,6 +14,7 @@ monster.base.quests.iniQuests();
 killer={}; --A list that keeps track of who attacked the monster last
 
 --Random Messages
+msgs:addMessage("#me hat Schaum vor dem Mund.", "#me foams at the mouth.");
 
 end
 
