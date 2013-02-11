6026acb6bc4ad12fb652bc686adcd4ff69d042fe
Lillian <maletzki.lisa@googlemail.com>
2013-02-11 21:15:04 +0100

    repair consequence and test npc

---
+++ testserver/npc/base/consequence/repair.lua
@@ -0,0 +1,15 @@
+require("base.class")
+require("npc.base.consequence.consequence")
+require("development.repair")
+
+module("npc.base.consequence.repair", package.seeall)
+
+repair = base.class.class(npc.base.consequence.consequence.consequence,
+function(self)
+    npc.base.consequence.consequence.consequence:init(self);
+    self["perform"] = _repair_helper;
+end);
+
+function _trade_helper(self, npcChar, player)
+    development.repair.repairDialog(npcChar, player); 
+end;
