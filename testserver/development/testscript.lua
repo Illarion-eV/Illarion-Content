1260860c764938a837b7ccddda22cd92b88cbe42
Flux <Flux@illarion.org>
2013-02-12 22:36:00 +0000

    No more killing people with arrows so fast

--- testserver/development/testscript.lua
+++ testserver/development/testscript.lua
@@ -323,7 +323,7 @@ function CauseDamage(Attacker, Defender, Globals)
 	
     if base.character.IsPlayer(Defender.Char) 
         and base.character.WouldDie(Defender.Char, Globals.Damage + 1)
-        and (Attacker.AttackKind ~= 4)
+        --and (Attacker.AttackKind ~= 4)
         and not base.character.AtBrinkOfDeath(Defender.Char) then
         -- Character would die. Nearly killing him and moving him back in case it's possible
         base.character.ToBrinkOfDeath(Defender.Char);
