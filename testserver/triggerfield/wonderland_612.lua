e5d5e3b3558afa0d87b2a7ad1df0fc691774301f
Enviyatar <enviyatar@gmx.at>
2013-02-12 21:01:16 +0100

    update quest test

--- testserver/triggerfield/wonderland_612.lua
+++ testserver/triggerfield/wonderland_612.lua
@@ -9,5 +9,6 @@ function MoveToField(Character)
 	if Character:getQuestProgress(612) == 0 then
 	Character:warp(position(900,580,0))
 	Character:inform("Du hörst ein Lachen und eine krächzende Stimme sagen: \"HAHA! Du gehörst nun mir!\" Nach einer Weile hörst du eine andere Stimme aus dem Nordwesten: \"Geh weg von mir!\"","You hear a laughter and a scratchy voice is saying: \"HAHA! You are mine now!\" After a while you hear another voice from the northwest: \"Go away from me!\"")  
+	Character:setQuestProgress(612,1)
 	end
 end
