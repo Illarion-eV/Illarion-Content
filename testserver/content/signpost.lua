704f2d0968a1459b329d51083491d9540263affd
Enviyatar <enviyatar@gmx.at>
2013-02-14 16:47:43 +0100

    update

--- testserver/content/signpost.lua
+++ testserver/content/signpost.lua
@@ -35,7 +35,7 @@ function InitWegweiser()
 
 
     
-AddWeg({116,589,0},"Taverne 'Einhörniger Löwe'","Tavern 'Unicorn Lion'",0,0);
+
     AddWeg({91,586,0},"Gasthaus 'Seeufer'","Inn 'Lakeshore'",0,0);
     AddWeg({123,566,0},"Palast Ihrer Majestät","Palace of Her Majesty",0,0);
     AddWeg({120,566,0},"Palast Ihrer Majestät","Palace of Her Majesty",0,0);
