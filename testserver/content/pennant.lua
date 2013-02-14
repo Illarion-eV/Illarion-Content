0d4de2950ebd19e40c9e40c171caadd7bf99db09
Enviyatar <enviyatar@gmx.at>
2013-02-14 16:48:21 +0100

    update

--- testserver/content/pennant.lua
+++ testserver/content/pennant.lua
@@ -52,7 +52,7 @@ function InitPennant()
     AddPennant({134,585,0},"Werkstatt","Workshop",2,0);
     AddPennant({126,611,0},"Marktplatz","Marketplace",1,0);
     AddPennant({129,563,0},"Zhambratempel","Temple of Zhambra",2,0);
-    AddPennant({114,587,0},"Taverne 'Einhörniger Löwe'","Tavern 'Unicorn Lion'",1,0);
+    AddPennant({114,588,0},"Taverne 'Einhörniger Löwe'","Tavern 'Unicorn Lion'",1,0);
 
     AddPennant({121,519,0},"In schimmernden Rot und Weiß ziert das Wappen von Cadomyr den Banner.","The crest of Cadomyr adorns the banner in shimmering red and white.", 1,0); --palace
 	AddPennant({122,519,0},"In schimmernden Rot und Weiß ziert das Wappen von Cadomyr den Banner.","The crest of Cadomyr adorns the banner in shimmering red and white.", 1,0); --palace
