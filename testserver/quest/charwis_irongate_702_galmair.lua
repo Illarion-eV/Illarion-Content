adaa6dbbcfc497bc3c5f92b86b5a321e44706355
Lillian <maletzki.lisa@googlemail.com>
2013-02-16 15:05:09 +0100

    umlaut fix

--- testserver/quest/charwis_irongate_702_galmair.lua
+++ testserver/quest/charwis_irongate_702_galmair.lua
@@ -1,4 +1,4 @@
--- INSERT INTO quests SET qst_script = 'quest.charwis_irongate_702_galmair' WHERE qst_id = QUESTID;
+-- INSERT INTO quests SET qst_script = 'quest.charwis_irongate_702_galmair' WHERE qst_id = 702;
 
 require("base.common")
 module("quest.charwis_irongate_702_galmair", package.seeall)
@@ -16,35 +16,35 @@ Title[ENGLISH] = "Message Delivery"
 Description = {}
 Description[GERMAN] = {}
 Description[ENGLISH] = {}
-Description[GERMAN][1] = "Charwis Irongate bat dich eine Nachricht an Nik Nilo zu Ã¼berbringen, einem HÃ¤ndler in Galmair der seine Waren auf dem Marktplatz in Galmair verkauft."
+Description[GERMAN][1] = "Charwis Irongate bat dich eine Nachricht an Nik Nilo zu überbringen, einem Händler in Galmair der seine Waren auf dem Marktplatz in Galmair verkauft."
 Description[ENGLISH][1] = "Charwis Irongate asked you to bring a message Nik Nilo, a merchant in Galmair who sells his good on the Galmair marketplace."
-Description[GERMAN][2] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung fÃ¼r deinen Dienst abzuholen."
+Description[GERMAN][2] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung für deinen Dienst abzuholen."
 Description[ENGLISH][2] = "You should talk to Charwis Irongate again to collect the reward for your service."
-Description[GERMAN][3] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe fÃ¼r dich."
+Description[GERMAN][3] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe für dich."
 Description[ENGLISH][3] = "Talk to Charwis Irongate again, he probably has another task for you to do."
-Description[GERMAN][4] = "Morri, ein HÃ¤ndler auf dem Marktplatz von Galmair, hat NÃ¤gel bei Charwis Irongate bestellt. Frag ihn ob er die Bestellung noch haben mÃ¶chte."
+Description[GERMAN][4] = "Morri, ein Händler auf dem Marktplatz von Galmair, hat Nägel bei Charwis Irongate bestellt. Frag ihn ob er die Bestellung noch haben möchte."
 Description[ENGLISH][4] = "Morri, a merchant on the Galmair marketplace, ordered pins from Charwis Irongate. Ask him if he still wants his order."
-Description[GERMAN][5] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung fÃ¼r deinen Dienst abzuholen."
+Description[GERMAN][5] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung für deinen Dienst abzuholen."
 Description[ENGLISH][5] = "You should talk to Charwis Irongate again to collect the reward for your service."
-Description[GERMAN][6] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe fÃ¼r dich."
+Description[GERMAN][6] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe für dich."
 Description[ENGLISH][6] = "Talk to Charwis Irongate again, he probably has another task for you to do."
 Description[GERMAN][7] = "Gretel Goldhair hat vielleicht die Hammersammlung von Charwis Irongate entsorgt. Frag sie danach."
 Description[ENGLISH][7] = "Gretel Goldhair might have trown away the colleection of hammers of Charwis Irongate. Ask her about it."
-Description[GERMAN][8] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung fÃ¼r deinen Dienst abzuholen."
+Description[GERMAN][8] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung für deinen Dienst abzuholen."
 Description[ENGLISH][8] = "You should talk to Charwis Irongate again to collect the reward for your service."
-Description[GERMAN][9] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe fÃ¼r dich."
+Description[GERMAN][9] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe für dich."
 Description[ENGLISH][9] = "Talk to Charwis Irongate again, he probably has another task for you to do."
-Description[GERMAN][10] = "Charwis Irongate bat dich, den groÃŸen Goldnugget zu nehmen und ihn Pheritaleth aus Cadomyr zu zeigen um seine Ehre zu verteildigen."
+Description[GERMAN][10] = "Charwis Irongate bat dich, den großen Goldnugget zu nehmen und ihn Pheritaleth aus Cadomyr zu zeigen um seine Ehre zu verteildigen."
 Description[ENGLISH][10] = "Charwis Irongate asked you to show the big gold nugget to Pheritaleth of Cadomyr to restore his honor."
-Description[GERMAN][11] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung fÃ¼r deinen Dienst abzuholen."
+Description[GERMAN][11] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung für deinen Dienst abzuholen."
 Description[ENGLISH][11] = "You should talk to Charwis Irongate again to collect the reward for your service."
-Description[GERMAN][12] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe fÃ¼r dich."
+Description[GERMAN][12] = "Rede erneut mit Charwis Irongate, er hat sicher noch eine Aufgabe für dich."
 Description[ENGLISH][12] = "Talk to Charwis Irongate again, he probably has another task for you to do."
-Description[GERMAN][13] = "Bringe Elvaine Morgan, der FÃ¼hrer von Runewick, den Stab der Luft zurÃ¼ck den Charwis Irongate gefunden hat."
+Description[GERMAN][13] = "Bringe Elvaine Morgan, der Führer von Runewick, den Stab der Luft zurÜck den Charwis Irongate gefunden hat."
 Description[ENGLISH][13] = "Bring back the wand of air, which Charwis Irongate found, back to Elvaine Morgan, the leader of Runewick."
-Description[GERMAN][14] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung fÃ¼r deinen Dienst abzuholen."
+Description[GERMAN][14] = "Du solltest zu Charwis Irongate gehen und dir deine Belohnung für deinen Dienst abzuholen."
 Description[ENGLISH][14] = "You should talk to Charwis Irongate again to collect the reward for your service."
-Description[GERMAN][15] = "Du hast alle Aufgaben von Charwis Irongate erfÃ¼llt."
+Description[GERMAN][15] = "Du hast alle Aufgaben von Charwis Irongate erfüllt."
 Description[ENGLISH][15] = "You have fulfilled all tasks of Charwis Irongate."
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
