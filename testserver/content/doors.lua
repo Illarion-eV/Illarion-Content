255993e4a5f30006d27f2b4be50008fcb202a3f1
Lillian <maletzki.lisa@googlemail.com>
2013-02-17 10:04:06 +0100

    runewick doors. issue 8448

--- testserver/content/doors.lua
+++ testserver/content/doors.lua
@@ -13,6 +13,33 @@ function initDoors()
 	AddDoor({341, 207, 0}, 100,"Golden Dreams, the private chamber of the Don","Goldträume, die Privatkammer des Dons"); -- Don's private room
 
     -- Runewick 2xx
+    --Tower of Fire 20x
+    AddDoor({901, 763, 2}, 200,"Archmage Elvaine Morgan's rooms", "Die Räumlichkeiten von Erzmagier Elvaine Morgan"); -- Archmage's room
+    AddDoor({901, 764, 2}, 200,"Archmage Elvaine Morgan's rooms", "Die Räumlichkeiten von Erzmagier Elvaine Morgan"); -- Archmage's room
+ 
+	AddDoor({902, 770, 4}, 201,"Tower of Fire Apartment 1","Turm des Feuers Appartment 1") -- 2 Persons
+    AddDoor({902, 771, 4}, 201,"Tower of Fire Apartment 1","Turm des Feuers Appartment 1") -- 2 Persons
+
+    AddDoor({902, 777, 4}, 202,"Tower of Fire Apartment 1","Turm des Feuers Appartment 2") -- 3 Persons	+ Balcony	
+    AddDoor({903, 777, 4}, 202,"Tower of Fire Apartment 1","Turm des Feuers Appartment 2") -- 3 Persons	+ Balcony	
+            
+    
+    --Tower of Air 21x
+    AddDoor({907, 833, 4}, 210,"Tower of Air Apartment 1","Turm der Luft Appartment 1") -- 2 Persons
+    AddDoor({906, 833, 4}, 210,"Tower of Air Apartment 1","Turm der Luft Appartment 1") -- 2 Persons
+    
+    AddDoor({898, 834, 4}, 211,"Tower of Air Apartment 2","Turm der Luft Appartment 2") -- 3 Persons
+    AddDoor({898, 835, 4}, 211,"Tower of Air Apartment 2","Turm der Luft Appartment 2") -- 3 Persons
+    
+    --Tower of Earth 22x
+    AddDoor({946, 763, 4}, 220,"Tower of Earth Apartment 1","Turm der Erde Appartment 1") -- 3 Persons
+    AddDoor({947, 763, 4}, 220,"Tower of Earth Apartment 1","Turm der Erde Appartment 1") -- 3 Persons
+            
+    AddDoor({946, 759, 4}, 221,"Tower of Earth Apartment 2","Turm der Erde Appartment 2") -- 1 Person
+    AddDoor({947, 759, 4}, 221,"Tower of Earth Apartment 2","Turm der Erde Appartment 2") -- 1 Person
+    
+    AddDoor({944, 764, 2}, 222,"Tower of Earth Apartment 3","Turm der Erde Appartment 3") -- 2 Persons + Balcony
+    AddDoor({944, 765, 2}, 222,"Tower of Earth Apartment 3","Turm der Erde Appartment 3") -- 2 Persons + Balcony
 
 	-- Cadomyr 3xx
 	
