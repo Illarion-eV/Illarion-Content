51a3267884b9d6e8e0d3e1925d1e07570538f587
Flux <Flux@illarion.org>
2013-02-20 23:42:26 +0000

    test

--- testserver/lte/chr_reg.lua
+++ testserver/lte/chr_reg.lua
@@ -49,6 +49,36 @@ if Char:idleTime() < 300 then --absolutely no regeneration effect if the player 
         base.common.InformNLS( Char,
             "Deine aktuelle Position ist: "..Char.pos.x..", "..Char.pos.y..", "..Char.pos.z.."",
             "Your current position is: "..Char.pos.x..", "..Char.pos.y..", "..Char.pos.z.."" );
+
+			local displacement,xpos,ypos;
+			for displacement = 0, 27, 3 do
+				for xpos = 909, 911, 1 do
+					for ypos = 760, 765, 1 do
+						if User.pos == position(xpos+displacement,ypos,1) then
+
+							local pillar1 = world:getField(position(910+displacement, 760,1));
+							local flame1 = pillar1:getStackItem(pillar1:countItems());
+							local pillar2 = world:getField(position(910+displacement, 765,1));
+							local flame2 = pillar2:getStackItem(pillar2:countItems());
+
+							--base.common.InformNLS(User,"Working:"..flame1.id.." and also"..flame2.id.." k?");
+
+							if pillar1.id == 2805 then
+								local newflame = world:createItemFromId(259,1,position(910+displacement,760,1),true,333);
+								newflame.wear = 1;
+							end;
+
+							if pillar2.id == 2805 then
+								local newflame = world:createItemFromId(259,1,position(910+displacement,765,1),true,333);
+								newflame.wear = 1;
+							end;
+
+						end;
+
+					end;
+				end;
+			end;
+
     end
     ------------------------POSITION ANZEIGEN FERTIG--------------------------
 
