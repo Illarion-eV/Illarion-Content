19fea13dcc48357f501cbfcc2a482c367071199d
Flux <Flux@illarion.org>
2013-02-20 23:30:35 +0000

    Again

--- testserver/server/standardfighting.lua
+++ testserver/server/standardfighting.lua
@@ -169,7 +169,7 @@ function quicktest()
 					local pillar2 = world:getField(position(910+displacement, 765,1));
 					local flame2 = pillar2:getStackItem(pillar2:countItems());
 
-					base.common.InformNLS(User,"Working:"..pillar1.id.." and also"..pillar2.id.." k?");
+					base.common.InformNLS(User,"Working:"..flame1.id.." and also"..flame2.id.." k?");
 
 					if pillar1.id == 2805 then
 						local newflame = world:createItemFromId(259,1,position(910+displacement,760,1),true,333);
