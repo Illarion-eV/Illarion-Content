a6e422d82b62cb165f32fa02bb5a4a9c409d8062
Flux <Flux@illarion.org>
2013-02-20 21:39:29 +0000

    Fix

--- testserver/triggerfield/runewick_bridges.lua
+++ testserver/triggerfield/runewick_bridges.lua
@@ -6,11 +6,14 @@ require("base.common")
 module("triggerfield.runewick_bridges", package.seeall)
 
 function MoveToField( User )
+	
 
 	for displacement = 0, 27, 3 do
 		for xpos = 909, 911, 1 do
 			for ypos = 760, 765, 1 do
 				if User.pos == position(xpos+displacement,ypos,1) then
+
+					User.char:inform("Test");
 					local pillar1 = world:getField(position(910+displacement, 760,1));
 					local flame1 = pillar1:getStackItem(pillar1:countItems());
 					local pillar2 = world:getField(position(910+displacement, 765,1));
