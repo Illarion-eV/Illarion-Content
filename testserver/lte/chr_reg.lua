230905442fcfa22e1e46b81af743c30da9fbe50a
Flux <Flux@illarion.org>
2013-02-20 23:54:16 +0000

    Blah

--- testserver/lte/chr_reg.lua
+++ testserver/lte/chr_reg.lua
@@ -55,12 +55,12 @@ if Char:idleTime() < 300 then --absolutely no regeneration effect if the player 
 				for xpos = 909, 911, 1 do
 					for ypos = 760, 765, 1 do
 						if Char.pos.x == xpos+displacement then
-							
-							base.common.InformNLS( Char,
+							if Char.pos.y == ypos then
+
+								base.common.InformNLS( Char,
 										"Deine aktuelle Position ist: "..Char.pos.x..", "..Char.pos.y..", "..Char.pos.z.."",
 										"Should be working1" );
 
-							if Char.pos.y == ypos then
 								if Char.pos.z == 1 then
 
 									base.common.InformNLS( Char,
