5b404c6551858338f595f53acc413e2c99ce702e
Flux <Flux@illarion.org>
2013-02-21 17:12:33 +0000

    Fix

--- testserver/triggerfield/runewick_bridges.lua
+++ testserver/triggerfield/runewick_bridges.lua
@@ -12,18 +12,26 @@ function MoveToField( User )
 		for xpos = 909, 911, 1 do
 			for ypos = 760, 765, 1 do
 				if User.pos == position(xpos+displacement,ypos,1) then
+					local flametype;
+
+					if displacement%2 == 0 then
+						flametype = 359;
+					else
+						flametype = 360;
+					end;
+
 					local testthing = 910+displacement;
 					local pillar1 = world:getField(position(testthing, 760,1));
 					local flame1 = pillar1:getStackItem(pillar1:countItems()-1);
 					local pillar2 = world:getField(position(testthing, 764,1));
 					local flame2 = pillar2:getStackItem(pillar2:countItems()-1);
 					if flame1.id == 2805 then
-						local newflame = world:createItemFromId(359,1,position(testthing,760,1),true,333,nil);
+						local newflame = world:createItemFromId(flametype,1,position(testthing,760,1),true,333,nil);
 						newflame.wear = 1;
 						world:changeItem(newflame);
 					end;
 					if flame2.id == 2805 then
-						local newflame = world:createItemFromId(359,1,position(testthing,764,1),true,333,nil);
+						local newflame = world:createItemFromId(flametype,1,position(testthing,764,1),true,333,nil);
 						newflame.wear = 1;
 						world:changeItem(newflame);
 					end;
@@ -37,18 +45,23 @@ function MoveToField( User )
 		for xpos = 943, 948, 1 do
 			for ypos = 770, 772, 1 do
 				if User.pos == position(xpos,ypos+displacement,1) then
+					if displacement%2 == 0 then
+						flametype = 359;
+					else
+						flametype = 360;
+					end;
 					local testthing = 770+displacement;
 					local pillar1 = world:getField(position(944, testthing,1));
 					local flame1 = pillar1:getStackItem(pillar1:countItems()-1);
 					local pillar2 = world:getField(position(948, testthing,1));
 					local flame2 = pillar2:getStackItem(pillar2:countItems()-1);
 					if flame1.id == 2805 then
-						local newflame = world:createItemFromId(359,1,position(944, testthing,1),true,333,nil);
+						local newflame = world:createItemFromId(flametype,1,position(944, testthing,1),true,333,nil);
 						newflame.wear = 1;
 						world:changeItem(newflame);
 					end;
 					if flame2.id == 2805 then
-						local newflame = world:createItemFromId(359,1,position(948, testthing,1),true,333,nil);
+						local newflame = world:createItemFromId(flametype,1,position(948, testthing,1),true,333,nil);
 						newflame.wear = 1;
 						world:changeItem(newflame);
 					end;
