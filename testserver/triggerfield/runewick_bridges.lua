e6520730fe07fd6f8f5c2012970c57d8bae2a702
Flux <Flux@illarion.org>
2013-02-21 04:05:07 +0000

    Okay

--- testserver/triggerfield/runewick_bridges.lua
+++ testserver/triggerfield/runewick_bridges.lua
@@ -6,39 +6,30 @@ require("base.common")
 module("triggerfield.runewick_bridges", package.seeall)
 
 function MoveToField( User )
-	
 	local displacement,xpos,ypos;
 	for displacement = 1, 28, 3 do
 		for xpos = 909, 911, 1 do
 			for ypos = 760, 765, 1 do
 				if Char.pos == position(xpos+displacement,ypos,1) then
-
 					local testthing = 910+displacement;
-
 					local pillar1 = world:getField(position(testthing, 760,1));
 					local flame1 = pillar1:getStackItem(pillar1:countItems()-1);
 					local pillar2 = world:getField(position(testthing, 765,1));
 					local flame2 = pillar2:getStackItem(pillar2:countItems()-1);
-
 					if flame1.id == 2805 then
 						local newflame = world:createItemFromId(359,1,position(testthing,760,1),true,333,nil);
 						newflame.wear = 2;
 						world:changeItem(newflame);
 					end;
-
 					if flame2.id == 2805 then
 						local newflame = world:createItemFromId(359,1,position(testthing,765,1),true,333,nil);
 						newflame.wear = 1;
 						world:changeItem(newflame);
 					end;
-
-
 				end;
-
 			end;
 		end;
 	end;
- 
 end
 
 -- INSERT INTO triggerfields VALUES (909,760,1,'triggerfield.runewick_bridges');
