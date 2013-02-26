16ff06bd9d6baf881c177d255c82f224a8052e22
merung <karten-spiel-haus@hotmail.de>
2013-02-26 17:48:23 +0100

    test teleporter by silverwing

--- testserver/test/merung.lua
+++ testserver/test/merung.lua
@@ -1,5 +1,6 @@
 require("base.common")
 require("base.lookat")
+require("base.money")
 require("content.areas")
 require("triggerfield.potionToTeacher")
 
@@ -8,52 +9,34 @@ require("triggerfield.potionToTeacher")
 module("test.merung", package.seeall)
 
 function UseItem(User, SourceItem)
-    if (User.lastSpokenText == "glas") then
-        User:increaseSkill(Character.glassBlowing,100)
-    end
-	if (User.lastSpokenText == "farming") then
-        User:increaseSkill(Character.farming,100)
-    end
-	if (User.lastSpokenText == "carpentry") then
-        User:increaseSkill(Character.carpentry,100)
-    end
-	if (User.lastSpokenText == "attribute") then
-        User:increaseAttrib("essence",10)
-		User:increaseAttrib("perception",10)
-		User:increaseAttrib("intelligence",10)
-    end
-	local a,b,lockId = string.find(User.lastSpokenText,"lockId (%d+)")
-    if a ~= nil then
-	    local myItem=base.common.GetFrontItem(User)
-		myItem:setData("lockId",lockId)
-		world:changeItem(myItem)
-	end
-    local a,b,monsterId = string.find(User.lastSpokenText,"monster (%d+)")
-    if a ~= nil then
-	    local monsterId = tonumber(monsterId)
-		local posi = base.common.GetFrontPosition(User, 5)
-		world:createMonster(monsterId, posi, 10)
-	end
-	if (User.lastSpokenText == "kill") then
-        myTargets = world:getMonstersInRangeOf(User.pos,8)
-		for i=1,#myTargets do
-		    myTargets[i]:increaseAttrib("hitpoints",-15000)
-        end			
-    end
-    
-    if (User.lastSpokenText == "area") then
-        local checkArea = content.areas.PointInArea(User.pos, "cadomyr flat 1")
-		if checkArea then
-		    User:inform("im gebiet")
-		else
-          	User:inform("nicht im gebiet")
-        end			
-	end
 	
-	local a,b,recipe = string.find(User.lastSpokenText,"recipe (%d+)")
-    if a ~= nil then
-	    local recipe = tonumber(recipe)
-		triggerfield.potionToTeacher.TellRecipe(User, recipe)
-	end
+	local names = {"Runewick","Galmair","Cadomyr","Hanfschlinge"}
+	local items = {105,61,2701,1909}
+	local callback = function(dialog)
+	targetPos = {position(900,800,0), postion(220,110,1),position(1,1,0),position(450,450,0)}
 	
+		success = dialog:getSuccess()
+		if success then
+			selected = dialog:getSelectedIndex()
+			if  base.money.CharHasMoney(User,1000) then
+				
+				User:inform("Ihr habt euch dazu entschlossen nach " ..names[selected+1].. " zu Reisen")
+				base.money.TakeMoneyFromChar(User,1000)
+				
+				User:warp(targetPos[selceted+1])
+			else
+				User:inform("Ihr verfügt nicht über genug Geld für eine solche Reise")
+			end
+	    end
+	end 
+	local dialog = SelectionDialog("Teleporter", "Wähle eine Stadt aus", callback)
+  
+    for i=1,#items do
+        dialog:addOption(items[i], names[i])
+    end
+  
+	User:requestSelectionDialog(dialog)
+	
+	
+	end
 end
