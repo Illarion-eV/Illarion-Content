75cc230b2a42e8e26fa95f26c8d7351742306324
merung <karten-spiel-haus@hotmail.de>
2013-02-27 12:02:00 +0100

    merge bookrests

---
+++ testserver/item/bookrests.lua
@@ -0,0 +1,9 @@
+require("base.common")
+
+-- UPDATE common SET com_script='item.bookrests' WHERE com_itemid = 3104;
+-- UPDATE common SET com_script='item.bookrests' WHERE com_itemid = 3105;
+-- UPDATE common SET com_script='item.bookrests' WHERE com_itemid = 3106;
+-- UPDATE common SET com_script='item.bookrests' WHERE com_itemid = 3107;
+-- UPDATE common SET com_script='item.bookrests' WHERE com_itemid = 3108;
+
+module("item.bookrests", package.seeall)
