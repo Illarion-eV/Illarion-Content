3eabce3760a0eb312f655a39ec70121d94a1c073
merung <karten-spiel-haus@hotmail.de>
2013-03-02 15:34:00 +0100

    debug

--- testserver/server/learn.lua
+++ testserver/server/learn.lua
@@ -83,9 +83,10 @@ end
 -- user:idleTime() can be used to get the number of seconds a user has been idle to check for inactivity
 
 function reduceMC( user )
-    
+    if is Testserver() then user:inform("idle inform: "..user:idleTime()) end
 	if user:idleTime() < 300 then --Has the user done any action or spoken anything within the last five minutes?
-     	user:increaseMentalCapacity(-1*math.floor(user:getMentalCapacity()*0.00025+0.5)); --reduce MC-points by 0.025%, rounded correctly.
+     	if is Testserver() then user:inform("reset mc") end
+		user:increaseMentalCapacity(-1*math.floor(user:getMentalCapacity()*0.00025+0.5)); --reduce MC-points by 0.025%, rounded correctly.
 	end
 	
 end
