f64a425ded3230b226e2d26925e5de6e22779f52
merung <karten-spiel-haus@hotmail.de>
2013-03-02 17:17:12 +0100

    debug

--- testserver/test/merung.lua
+++ testserver/test/merung.lua
@@ -11,15 +11,10 @@ module("test.merung", package.seeall)
 
 function UseItem(User, SourceItem)
 	
-	if User.lastSpokenText == "inform" then
-	    local myItem = world:getItemOnField(position(432,238,0))
-		alchemy.base.teacher.LookAtItem(User, myItem)
-	end
+	
     
 end
 
 function LookAtItem(player, item)
-    if lookAt then
-	    world:itemInform(User,myItem,lookAt)
-	end	
+    
 end	
