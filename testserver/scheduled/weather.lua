4d52023deffc1135c1cc071cc1733c881ecb1bdc
Lillian <maletzki.lisa@googlemail.com>
2013-03-08 07:52:45 +0100

    test

--- testserver/scheduled/weather.lua
+++ testserver/scheduled/weather.lua
@@ -89,7 +89,7 @@ function setClouds(actClouds,thisMonth)     -- much in winter, less in summer
     typicalClearDayProb=math.floor(40-9*math.cos(thisMonth*math.pi/8)); -- 78% in summer, 60 in winter (69)
     clearTest=math.random(1,100);
 	if isTestserver() then
-	    clearTest=math.random(1,80);
+	    --clearTest=math.random(1,80);
 	end
     if clearTest>=typicalClearDayProb then  -- no clear day
         typicalClouds=50;                   -- if cloudy, then typically 50%
@@ -111,7 +111,7 @@ function getRain(thisMonth,actClouds)
         probabilityToRain=math.floor(60-20*math.cos(thisMonth*math.pi/8));  -- 35% in summer, 15 in winter (realistic values), 70% clear sky!
         doesItRain=math.random(0,100);
 		if isTestserver() then
-			doesItRain=math.random(0,125);
+			--doesItRain=math.random(0,125);
 		end
         if doesItRain<probabilityToRain then    -- rain positive
             retRain=math.random(0,100);   
