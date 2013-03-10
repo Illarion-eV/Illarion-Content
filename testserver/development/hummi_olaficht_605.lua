30b1568ee6bdb63e5062f7ac50c8714eec5c7195
Enviyatar <enviyatar@gmx.at>
2013-03-10 16:54:20 +0100

    update

--- testserver/development/hummi_olaficht_605.lua
+++ testserver/development/hummi_olaficht_605.lua
@@ -20,7 +20,7 @@ Description[GERMAN][1] = "Finde Frizza beim Teleporter in Cadomyr and sprich mit
 Description[ENGLISH][1] = "Find Frizza at the teleporter in Cadomyr and talk to her."
 Description[GERMAN][2] = "Da kannst nun mit Frizza sprechen. Frage nach 'Hilfe' wenn du nicht weißt nach was du fragen sollst!\nDu kannst auch zurück zu Hummi gehen um deine Belohnung abzuholen und später nochmals vorbei kommen. Frizza hat nämlich auch mindestens eine Aufgabe für dich."
 Description[ENGLISH][2] = "You can talk with Frizza now. Ask for 'help' if you do not know what to say!\nYou can also go back to Hummi to collect your reward and come back later. Frizza has also at least one task for you."
-Description[GERMAN][3] = " Hast du bereits nach den beiden anderen NPCs die Hummi erwähnt gefragt und sie auch besucht? Elesil and Iradona? Wenn nein, dann solltest du das jetzt tun. Wenn ja, dann hat Hummi einen zusätzlichen Auftrag für dich."
+Description[GERMAN][3] = "Hast du bereits nach den beiden anderen NPCs die Hummi erwähnt gefragt und sie auch besucht? Elesil and Iradona? Wenn nein, dann solltest du das jetzt tun. Wenn ja, dann hat Hummi einen zusätzlichen Auftrag für dich."
 Description[ENGLISH][3] = "Have you already asked for and visited the two other NPCs that Hummi mentions? Elesil and Iradona? If not, you should do it now. If yes, Hummi has an additional task for you."
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
