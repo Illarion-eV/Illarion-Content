290c14459d3c5b264d22ef7f589a0663579581a3
zot <zot@illarion.org>
2013-03-10 19:25:15 +0100

    Minor change

--- testserver/development/lilli_gallfing.lua
+++ testserver/development/lilli_gallfing.lua
@@ -24,7 +24,7 @@ require("npc.base.consequence.inform")
 require("npc.base.consequence.item")
 require("npc.base.consequence.quest")
 require("npc.base.talk")
-module("npc.lilli_gallfing", package.seeall)
+module("development.lilli_gallfing", package.seeall)
 
 function initNpc()
 mainNPC = npc.base.basic.baseNPC();
