7f6a65904ea24fb3e135d3813a5348a80bbf1025
Lillian <maletzki.lisa@googlemail.com>
2013-03-10 12:25:32 +0100

    Module

--- testserver/development/florain_dreyndel_820_runewick.lua
+++ testserver/development/florain_dreyndel_820_runewick.lua
@@ -1,7 +1,7 @@
--- INSERT INTO quests SET qst_script = 'quest.florain_dreyndel_820_runewick' WHERE qst_id = QUESTID;
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (820, 'quest.florain_dreyndel_820_runewick');
 
 require("base.common")
-module("quest.florain_dreyndel_820_runewick", package.seeall)
+module("development.florain_dreyndel_820_runewick", package.seeall)
 
 GERMAN = Player.german
 ENGLISH = Player.english
