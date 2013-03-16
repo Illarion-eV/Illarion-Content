5cb08fe82d3b0ad94864db29fa807081e207f280
slightly <mooseysc21@hotmail.com>
2013-03-16 23:35:41 +0000

    EN check

--- testserver/development/hummi_olaficht_607.lua
+++ testserver/development/hummi_olaficht_607.lua
@@ -21,9 +21,9 @@ Description[ENGLISH][1] = "Find Tronuk and ask him about a 'story' in order to g
 Description[GERMAN][2] = "Du hast Tronuk gefunden. Nun wähle eine seiner beiden Geschichten. Höre dir die ganze Geschichte an. Du wirst eine Information erhalten sobald du fertig bist.\nFalls du die falsche Geschichte gewählt hast wirst du dir die andere auch anhören müssen."
 Description[ENGLISH][2] = "You found Tronuk. Now, choose one of his two stories. Listen the whole story. You will receive an information as soon as you are done.\nYou might also have to listen to the second story if you chose the wrong one."
 Description[GERMAN][3] = "Du weißt nun den Namen. Wenn du zurück bei Hummi bist, nenne ihm den Namen des Orks der gewonnen hat. Der Name beginnt mit einem 'J'. Du kannst dir zur Not die Geschichte mittels 'Turnier' nochmals anhören."
-Description[ENGLISH][3] = "You know the name now. When you are back at Hummi, tell him the name of the ork who won. The name starts with a 'J'. If necessary, you can listen to the story another time with 'tournament'."
+Description[ENGLISH][3] = "You know the name now. When you get back to Hummi, tell him the name of the orc who won. The name starts with a 'J'. If necessary, you can listen to the story another time by saying 'tournament'."
 Description[GERMAN][4] = "Hummi hat keine anderen Aufgaben für dich. Lediglich mehr Informationen. Aber suche nach weiteren Aufgaben bei anderen NPCs."
-Description[ENGLISH][4] = "Hummi does not have any other quests for you. Just more information. However, you can look for other NPCs with quests for you."
+Description[ENGLISH][4] = "Hummi does not have any other quests for you, just more information. However, you can look for other NPCs with quests for you."
 
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
