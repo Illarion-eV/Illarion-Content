cf3b43c8cccf88e9d691154f14157e5d1768692d
Lillian <maletzki.lisa@googlemail.com>
2013-03-18 18:48:50 +0100

    max gemlevel 5 reversed

--- testserver/server/login.lua
+++ testserver/server/login.lua
@@ -344,7 +344,7 @@ function PayOutWage(Recipient,town)
 			while RankedWage>0 do
 				local randomGem=math.random(1,2);
 				local maxGemLevel=math.floor(RankedWage^(1/3))
-				local gemLevel= base.common.Limit(math.random(1,maxGemLevel), 1, 5)
+				local gemLevel= base.common.Limit(math.random(1,maxGemLevel), 1, 10)
 				
 				local gemsByTown={};
 				gemsByTown["Cadomyr"]={item.gems.TOPAZ, item.gems.AMETHYST}
