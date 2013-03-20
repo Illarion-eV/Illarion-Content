c1323b0ce654bde79497df4f5b4d6ac84d8a4123
Flux <Flux@illarion.org>
2013-03-20 03:41:34 +0000

    Tweaking a conditional

--- testserver/development/testscript.lua
+++ testserver/development/testscript.lua
@@ -546,7 +546,7 @@ function CheckAttackOK(CharStruct)
         return false;
     end;
 
-	if (CharStruct.WeaponItem:getData("RareWeapon"<0) then -- Item is a broken artifact
+	if (CharStruct.WeaponItem:getData("RareWeapon")<0) then -- Item is a broken artifact
         return false;
     end;
 
