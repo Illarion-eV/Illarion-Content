ca288d63bf8c5f220745c4a7cacaeeeda1c657b7
Flux <Flux@illarion.org>
2013-03-21 18:31:31 +0000

    This won't work but I'll try anyway

--- testserver/item/base/crafts.lua
+++ testserver/item/base/crafts.lua
@@ -315,9 +315,9 @@ function Craft:loadDialog(dialog, user)
 			local continue = true;
 			if isTestserver() then
 				
-				local special = product.data.RareArmour;
+				local special = product.item.data.RareArmour;
 				if(special==nil) then
-					special = product.data.RareWeapon;
+					special = product.item.data.RareWeapon;
 				end
 
 				if not special==nil then
