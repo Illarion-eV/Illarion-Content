40c169624a0a22793c08b84ceaab215d4e02f513
Flux <Flux@illarion.org>
2013-03-21 04:21:50 +0000

    Fix

--- testserver/content/craft/smithing.lua
+++ testserver/content/craft/smithing.lua
@@ -671,7 +671,7 @@ product:addIngredient(2535) -- Iron Ingot: 1x1
 if isTestserver() then
 	catId = smithing:addCategory("Rare Items", "Rare Items")
 	
-	product = smithing:addProduct(catId, 189, 0, 40, 1, 1, 1,{"RareWeapon",1})
-	product:addIngredient(189,1,{"RareWeapon",-1});
+	product = smithing:addProduct(catId, 189, 0, 40, 1, 1, 1,{"RareWeapon","1"})
+	product:addIngredient(189,1,{"RareWeapon","-1"});
 end
 
