092e1c6306d382365cd536e93b1e9a7e5ef4f6fc
merung <karten-spiel-haus@hotmail.de>
2013-03-26 18:04:28 +0100

    testing!

--- testserver/test/merung.lua
+++ testserver/test/merung.lua
@@ -1,21 +1,33 @@
 require("development.recipe_creation")
+require("alchemy.base.brewing")
 
 -- UPDATE common SET com_script = 'test.merung' WHERE com_itemid = 1266;
 
 module("test.merung", package.seeall)
 
 function UseItem(User, SourceItem)
+	User:increaseAttrib("essence",30)
 	
-	if SourceItem:getType()==4 then
-	    if SourceItem.itempos == 5 or SourceItem.itempos == 6 then
-		    User:increaseAttrib("essence",30)
-			development.recipe_creation.FirstMenu(User, ingredientsList)
-	    else
-		    User:inform("Du musst ihn in die Hand nehmen","You have to hold it in your hands")
-	    end
-	else
-        User:inform("Du musst ihn in die Hand nehmen","You have to hold it in your hands")
-    end		
+	if User.lastSpokenText == "rezept" then
+		if SourceItem:getType()==4 then
+			if SourceItem.itempos == 5 or SourceItem.itempos == 6 then
+				development.recipe_creation.FirstMenu(User, ingredientsList)
+			else
+				User:inform("Du musst ihn in die Hand nehmen","You have to hold it in your hands")
+			end
+		else
+			User:inform("Du musst ihn in die Hand nehmen","You have to hold it in your hands")
+		end	
+	end	
+	
+	if User.lastSpokenText == "brau" then
+		theItem = User:getItemAt(5)
+	    if theItem.id == 0 then
+            return
+        end
+		alchemy.base.brewing.UseItem(User, SourceItem)
+	end	
+            		
 	
 	
 end
