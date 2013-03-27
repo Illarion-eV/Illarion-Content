5fd0fbb101aeb5ffcaa26a577f92578cd333bd17
merung <karten-spiel-haus@hotmail.de>
2013-03-27 17:27:59 +0100

    debuG

--- testserver/development/id_2745_parchment.lua
+++ testserver/development/id_2745_parchment.lua
@@ -25,7 +25,29 @@ function AlchemyRecipe(User, SourceItem, ltstate)
 end
 
 function UseRecipe(User, SourceItem, ltstate)
-    User:inform("still to do")
+    -- herbs ok
+	-- gemdust ok
+	-- water ok
+	-- empty bottle ok
+	-- stock ok
+	-- [red, pink, white, black, yellow, dark blue] ok
+	-- blue ok
+	
+	-- is the char an alchemist?
+	local anAlchemist = alchemy.base.alchemy.CheckIfAlchemist(User,"Nur jene, die in die Kunst der Alchemie eingeführt worden sind, können hier ihr Werk vollrichten.","Only those who have been introduced to the art of alchemy are able to work here.")
+	if not anAlchemist then
+		return
+	end
+	
+	if ( User:increaseAttrib("perception",0) + User:increaseAttrib("essence",0) + User:increaseAttrib("intelligence",0) ) < 30 then 
+		User:inform("Verstand, ein gutes Auge und ein Gespür für die feinstofflichen Dinge - dir fehlt es daran, als dass du hier arbeiten könntest.",
+		            "Mind, good eyes and a feeling for the world of fine matter - with your lack of those, you are unable to work here."
+	                )
+		return
+    end
+	
+	local ingredientsList = getIngredients(SourceItem) 
+	
 end
 
 function ViewRecipe(User, SourceItem)
@@ -48,5 +70,4 @@ function getIngredients(SourceItem)
 		end
 	end
     return ingredientsList
-
 end
