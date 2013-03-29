cf8070d065ac0481e421a29f77706aea88527543
Enviyatar <enviyatar@gmx.at>
2013-03-29 20:48:15 +0100

    update

--- testserver/development/frizza.lua
+++ testserver/development/frizza.lua
@@ -22,7 +22,7 @@ require("npc.base.condition.town")
 require("npc.base.consequence.inform")
 require("npc.base.consequence.quest")
 require("npc.base.talk")
-module("npc.frizza", package.seeall)
+module("development.frizza", package.seeall)
 
 function initNpc()
 mainNPC = npc.base.basic.baseNPC();
