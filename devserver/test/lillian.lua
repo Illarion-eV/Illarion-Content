559efb13e1d411834a31ffc7f02e584650497c30
Lillian <maletzki.lisa@googlemail.com>
2013-04-07 11:35:18 +0200

    test dice prototype

--- devserver/test/lillian.lua
+++ devserver/test/lillian.lua
@@ -6,24 +6,20 @@ require("base.factions")
 module("test.lillian", package.seeall)
 
 function UseItem(User, SourceItem, ltstate)
-	ScriptVars:set("ArenaListCadomyr", "Hans Zwei;543;Peter Drei;321;Wolfgang Eins;987;Klaus Fünf;111;Holger Vier;222")
-	local testTable = {"Hans Zwei", 543, "Peter Drei", 321, "Wolfgang Eins", 987, "Klaus Fünf", 111, "Holger Vier", 222}
-	if (User.lastSpokenText == "list") then
-		getRanklist(User, User, true)
-	end
 	
-	if (User.lastSpokenText == "set") then
-		 local list = setRanklist(User, User, 666);
-		 User:inform("List: "..list);
-	end
+	if(User.lastSpokenText == "dice") then
+		local numberOfDice = chooseNumberOfDice(User);
+		local thrownNumbers = math.random(1,6);
 		
-	if(User.lastSpokenText == "special") then
-		base.factions.setSpecialRank(User, 12)
-		User:inform("SpecialRank set");
+		for i=1, numberOfDice-1 do
+			thrownNumbers = thrownNumbers..", "..math.random(1,6);
+		end
+		
+		local text = base.common.GetNLS(User,"#me wirft "..numberOfDice.." Würfel und wirft: "..thrownNumbers ,"#me throws "..numberOfDice.." dice and gets: "..thrownNumbers);
+		
+		User:talk(Character.talk, text);
 	end
-	
-	
-	
+
 end
 
 function LookAtItem(User, Item)
@@ -32,111 +28,22 @@ function LookAtItem(User, Item)
     return true    
 end
 
-arenaInformations = {{playerPos=position(0,0,0), monsterPos=position(0,0,0), npcName="Alsaya", town="Cadomyr", quest=801}, 
-					{playerPos=position(0,0,0), monsterPos=position(0,0,0), npcName="Test", town="Runewick", quest=802}, 
-					{playerPos=position(0,0,0), monsterPos=position(0,0,0), npcName="Test", town="Galmair", quest=803}}
+function chooseNumberOfDice(User)
+	local title = base.common.GetNLS(User,"Würfel", "Dice");
+	local text = base.common.GetNLS(User,"Bitte gibt ein, wieviele Würfel ihr zu werfen wünscht." , "Please type in how many dice you wish to throw.");
 
-function getRanklist(User, NPC, message)
-	local arena = getArena(User, NPC);
-	local town = arenaInformations[arena].town;
-	local found = false;
-	local arenaEntry;
-	local arenaList = {};
-	local list = " ";
-	local place = 2;
-	
-	for i=1, 5 do  -- get the top 5
-		found, arenaEntry = ScriptVars:find("ArenaListCadomyr");
-		if found then
-			arenaList = sortTable(base.common.split(arenaEntry, ";"));
-		elseif found == false or table.getn(arenaList) ~= 10 then
-			User:inform("[ERROR] An error occured please contact a developer.")
+	local cbInputDialog = function (dialog)
+		if (not dialog:getSuccess()) then
+			return;
 		end
-	end
-	
-	if message then
-		local mdList = function(dialog)
-			if (not dialog:getSuccess()) then
-				return;
-			end
-		end
-		if User:getPlayerLanguage() == 0 then
-			list = "Platz 1: "..arenaList[1].." mit "..arenaList[2].." Punkten.\n";
-			for i=3,#(arenaList),2 do
-				list = list.."Platz "..place.." : "..arenaList[i].." mit "..arenaList[i+1].." Punkten.\n";
-				place = place +1;
-			end
-			mdList = MessageDialog("Top fünf Kämpfer des Reiches", list, nil);			
+		local inputNumber = dialog:getInput();
+		if (string.find(inputNumber,"(%d+)") ~= nil) then
+			a, b, number = string.find(inputNumber,"(%d+)");
+			return number;
 		else
-			list = "Place 1: "..arenaList[1].." with "..arenaList[2].." points.\n";
-			for i=3, #(arenaList),2 do
-				list = list.."Place "..place.." : "..arenaList[i].." with "..arenaList[i+1].." points.\n";
-				place = place +1;
-			end
-			mdList = MessageDialog("Top five fighters of the realm", list, nil);
-		end
-		User:requestMessageDialog(mdList);
-	else
-		return arenaList;
-	end
-end
-
-function getArena(User, NPC)
-	for i=1, #(arenaInformations) do
-		if arenaInformations[i].npcName == NPC.name then
-			return i;
-		else
-			return "";
+			User:inform("Not a valid number. Please try again.");
+			User:requestInputDialog(InputDialog(title, text ,false, 255, cbInputDialog))
 		end
 	end
-end
-
-function sortTable(inputTable)
-	local numberTable = {}
-	local sortedTable = {}
-	for i=2, #(inputTable), 2 do
-		table.insert(numberTable, inputTable[i]);
-	end	
-	table.sort(numberTable);
-	for i= 1, #(numberTable) do
-		for j=2, #(inputTable), 2 do
-			if inputTable[j] == numberTable[i] then
-				table.insert(sortedTable, 1, inputTable[j]);
-				table.insert(sortedTable, 1, inputTable[j-1]);
-			end
-		end
-	end
-	return sortedTable;
-end
-
---[[
-Saves the points of the player and if he reached the 
-top five, also saves the new top five.
-]]
-function setRanklist(User, NPC, points) 
-	local ranklist = getRanklist(User, NPC, false)
-	local arena = getArena(User, NPC);
-	local town = arenaInformations[arena].town;
-	local quest = arenaInformations[arena].quest;
-	local newRanklist = {};
-	local arenaListName = "ArenaList"..town;
-	
-	User:setQuestProgress(quest, points);
-	
-	if ranklist[table.getn(ranklist)] > tostring(points) then
-		return;
-	else
-		for i=2, #(ranklist), 2 do
-			if ranklist[i] < tostring(points) then
-				table.insert(ranklist, i-1, points);
-				table.insert(ranklist, i-1, User.name);
-				table.remove(ranklist, table.getn(ranklist));
-				table.remove(ranklist, table.getn(ranklist));
-				break;
-			end
-		end
-		stringList = base.common.join(ranklist, ";");
-		ScriptVars:set(arenaListName, stringList)
-		return stringList;
-	end
+	User:requestInputDialog(InputDialog(title, text ,false, 255, cbInputDialog))
 end
