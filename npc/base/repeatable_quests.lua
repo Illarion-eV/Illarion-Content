290f9ba9ab75c10a92fe546c29267e656d2836ae
Lillian <maletzki.lisa@googlemail.com>
2013-04-21 19:19:50 +0200

    debug

--- npc/base/repeatable_quests.lua
+++ npc/base/repeatable_quests.lua
@@ -5,7 +5,7 @@ module("npc.base.repeatable_quests", package.seeall)
 function checkIfTimesExpired(User, quest, monthToRepeat, dayToRepeat, hourToRepeat) 
 	local questState, questLastChanged = User:getQuestProgress(quest);
 	debug("lastChanged: "..questLastChanged)
-	local year, month, day, hour, minute, second = base.common.TimestampToDate(questLastChanged);
+	local year, month, day, hour, minute, second = base.common.TimestampToDate(questLastChanged*3600);
 
 	debug("year: "..year.." month: "..month.." day: "..day.." hour: "..hour);
 	hour = hour + hourToRepeat;
@@ -34,7 +34,7 @@ function checkIfTimesExpired(User, quest, monthToRepeat, dayToRepeat, hourToRepe
 	end
 	
 	debug("year: "..year.." month: "..month.." day: "..day.." hour: "..hour);
-	local newTimestamp = base.common.GetCurrentTimestampForDate(year, month, day, hour, minute, second)	
+	local newTimestamp = (base.common.GetCurrentTimestampForDate(year, month, day, hour, minute, second)/ 3600)	
 	debug("new: "..newTimestamp)
 	local currentTimestamp = world:getTime("unix")
 	debug("current: "..currentTimestamp)
