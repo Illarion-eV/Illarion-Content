8fbde41b21b9279cbf30834c542b019217969765
Lillian <maletzki.lisa@googlemail.com>
2013-04-22 20:49:24 +0200

    test

--- test/lillian.lua
+++ test/lillian.lua
@@ -21,20 +21,19 @@ function LookAtItem(User, Item)
 end
 
 daysPerMonth = {31,28,31,30,31,30,31,31,30,31,30,31}
-TIMEZONE=-5
+TIMEZONE=+1
 DST=0
 
 function getRLDateFromUnixTimestamp(timestamp)
-	y=math.floor(1970+ timestamp /31556926)
+	year=math.floor(1970+ timestamp /31556926)
 	ds=((1970+timestamp/31556926)-y)*31556926
-	m=math.floor(ds/2629743)+1
+	month=math.floor(ds/2629743)+1
 	d=math.floor(ds/86400)+1
-	md=math.floor(((ds/2629743+1)-m)*daysPerMonth[m])+1
-	wd=d%7+6
+	day=math.floor(((ds/2629743+1)-m)*daysPerMonth[m])+1
 	if(m==3)then if(md>=14)then DST=1 else DST=0 end end
 	if(m==11)then if(md>=7)then DST=0 else DST=1 end end
-	h=math.floor(math.fmod(timestamp,60*60*24)/3600) + 5 + (TIMEZONE) + (DST)
-	mn= math.floor(math.fmod(timestamp,60*60*24)/60 - 60*(h-DST))
-	s= math.floor(math.fmod(math.fmod(timestamp,60*60*24),60))
-	debug("Date: [dd.mm.yyyy hh:mm:ss]: "..d.."."..m.."."..y.." "..h..":"..mn..":"..s.." or "..md)
+	hour=math.floor(math.fmod(timestamp,60*60*24)/3600) + 5 + (TIMEZONE) + (DST)
+	minute= math.floor(math.fmod(timestamp,60*60*24)/60 - 60*(h-DST))
+	second= math.floor(math.fmod(math.fmod(timestamp,60*60*24),60))
+	debug("Date: [dd.mm.yyyy hh:mm:ss]: "..day.."."..month.."."..year.." "..hour..":"..minute..":"..second..)
 end
