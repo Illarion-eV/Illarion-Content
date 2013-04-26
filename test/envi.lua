cdca5e48347e5c3d60ebc99ea9365d2eb78f1615
Enviyatar <enviyatar@gmx.at>
2013-04-26 15:49:40 +0200

    test

--- test/envi.lua
+++ test/envi.lua
@@ -12,8 +12,8 @@ function MoveToField(char)
 	if math.random(0,99)< 99  and char:increaseAttrib("hitpoints",0)>0 then --Chance of 10% and Hitpoints above 0
         	world:gfx(41,position(10,11,0));
         	world:createMonster(101,position(10,11,0),0); --smelly mummy
-		world:gfx(41,position(10,12,0));
-        	world:createMonster(101,position(10,12,0),0); --mummified temple servant
+		world:gfx(41,position(12,12,0));
+        	world:createMonster(101,position(12,12,0),0); --mummified temple servant
 		world:gfx(41,position(10,13,0));
         	world:createMonster(101,position(10,13,0),0); --Cherga's servant
         	world:gfx(41,position(10,10,0));
@@ -22,14 +22,11 @@ function MoveToField(char)
 	else
 			char:warp(position(8,16,0))
 			world:gfx(37,char.pos);
-			world:makeSound(13,char.pos);			
-			world:gfx(37,char.pos);
 			world:makeSound(13,char.pos);
 			char:inform("Die krächzende Stimme meldet sich wieder: \"Keine Chance!\" Worauf eine Stimme aus dem Südwesten sagt: \"Lauf weg!\"", "The jagged voice says: \"No Chance!\". A voice from the southwest replies: \"Run away!\"")  
 			world:makeSound(25,char.pos);			
 			world:gfx(13,char.pos);
-			world:makeSound(25,char.pos);
-			char:increaseAttrib("hitpoints", -500)
+			char:increaseAttrib("hitpoints", -5000)
 	end
 
 end
