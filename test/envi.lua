1002750526d4e94d2798a2fc3f3ed5e79e5e091d
Enviyatar <enviyatar@gmx.at>
2013-04-26 15:34:40 +0200

    test monster creation

--- test/envi.lua
+++ test/envi.lua
@@ -6,10 +6,20 @@ module("test.envi", package.seeall)
 
 
 function MoveToField(char)
-	if char:getType() ~= Character.player then
+	if char:getType() ~= Character.player then --Monsters will be ingored
 		return
 	end
-	if math.random(0,99)< 10 then -- riddle has been solved yet; the voice one more time; chance be warped
+	if math.random(0,99)< 10  and char:increaseAttrib("hitpoints",0)>0 then --Chance of 10% and Hitpoints above 0
+        	world:gfx(41,position(10,10,0));
+        	world:createMonster(101,position(10,10,0),0); --another mummy
+        	world:gfx(41,position(10,11,0));
+        	world:createMonster(152,position(10,11,0),0); --smelly mummy
+		world:gfx(41,position(10,12,0));
+        	world:createMonster(172,position10,12,0),0); --mummified temple servant
+		world:gfx(41,position(10,13,0));
+        	world:createMonster(701,position(10,13,0),0); --Cherga's servant
+	else
+			char:warp(position(8,16,0))
 			world:gfx(37,char.pos);
 			world:makeSound(13,char.pos);			
 			world:gfx(37,char.pos);
@@ -19,8 +29,6 @@ function MoveToField(char)
 			world:gfx(13,char.pos);
 			world:makeSound(25,char.pos);
 			char:increaseAttrib("hitpoints", -500)
-	else
-			char:warp(position(8,16,0))
 		return; 
 	end
 
