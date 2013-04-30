96bec90453187a3a280c14ff38959aa43c498ed1
merung <karten-spiel-haus@hotmail.de>
2013-04-30 13:31:12 +0200

    test

--- test/merung.lua
+++ test/merung.lua
@@ -5,65 +5,29 @@ require("alchemy.base.brewing")
 
 module("test.merung", package.seeall)
 
-
-function ltstateCheck(User, ltstate)
-
-    if (ltstate == Action.none) then
+function ltstateCheck(User, ltstate, counter)
+ 
+	if (ltstate == Action.none) then
 		User:startAction(50,21,5,15,25);
 		return
 	end
 	if ( ltstate == Action.abort ) then
 		User:inform("Nevermind that shit. Here comes Mongo")
-		return
+     	return
 	end
-	if counter == nil then
-	    counter = 1
-	else
-		counter = counter + 1
-	end	
 	User:inform("counter: "..counter)
+   
+	ltstateCheck(User, ltstate, counter+1)
 	
-	User:startAction(50,21,5,15,25)
-	
-end	
-
+end    
+ 
 function UseItem(User, SourceItem,ltstate)
-		User:inform("useitem")
-	if User.lastSpokenText == "ltstate" then	
-		ltstateCheck(User, ltstate)
-	end	
-	--[[
-	if User.lastSpokenText == "rezept" then
-		if SourceItem:getType()==4 then
-			if SourceItem.itempos == 5 or SourceItem.itempos == 6 then
-				development.recipe_creation.FirstMenu(User, ingredientsList)
-			else
-				User:inform("Du musst ihn in die Hand nehmen","You have to hold it in your hands")
-			end
-		else
-			User:inform("Du musst ihn in die Hand nehmen","You have to hold it in your hands")
-		end	
-	end	
-	
-	if User.lastSpokenText == "brau" then
-		theItem = User:getItemAt(5)
-	    if theItem.id == 0 then
-            return
-        end
-		if (ltstate == Action.none) then
-		   User:startAction(50,21,5,15,25);
-		   return
-	    end
-		if ( ltstate == Action.abort ) then
-		    User:inform("Nevermind that shit. Here comes Mongo")
-	        return
-	    end
-		alchemy.base.brewing.UseItem(User, theItem)
-	end	
-    ]]        		
-	
-	
+
+	if User.lastSpokenText == "ltstate" then       
+	    ltstateCheck(User, ltstate, 1)
+	end    
 end
+
 
 function LookAtItem(player, item)
     
