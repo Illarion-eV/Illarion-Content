41fe37b8dd8c1699158f30b01badf3784b6ea1ad
merung <karten-spiel-haus@hotmail.de>
2013-05-06 16:01:01 +0200

    testing

--- test/merung.lua
+++ test/merung.lua
@@ -5,6 +5,10 @@ require("alchemy.base.brewing")
 
 module("test.merung", package.seeall)
 
+HERB_LIST={15,80,154,200,201,259,290,302,772,778,2493,138,146,152,754,755,756,757,758,760,761,762,764,765,766,768,769,81,133,134,135,136,137,140,141,142,143,144,145,147,148,149,151,153,155,156,158,159,160,161,162,163,199,388,752,753,759,763,767}
+ 
+ 
+ 
  
 function UseItem(User, SourceItem,ltstate)
     if User.lastSpokenText == "alchemist" then
@@ -14,7 +18,11 @@ function UseItem(User, SourceItem,ltstate)
 		return
 	end
 	
-	
+	if User.lastSpokenText == "herbs" then
+	    for i=1,#HERB_LIST do then
+		    User:createItem(HERB_LIST[i],20,333,nil)
+		end
+	end
 	
 end
 
