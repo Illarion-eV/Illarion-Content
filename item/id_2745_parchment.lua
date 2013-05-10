40190e46d6694dea538cccfe2a0e49b2728a3d7d
merung <karten-spiel-haus@hotmail.de>
2013-05-10 16:24:14 +0200

    debug

--- item/id_2745_parchment.lua
+++ item/id_2745_parchment.lua
@@ -131,7 +131,7 @@ function StartBrewing(User,SourceItem,ltstate,checkVar)
 	end
 	
 	CallBrewFunctionAndDeleteItem(User,deleteItem, deleteId,cauldron)
-	if alchemy.base.alchemy.CheckExplosionAndCleanList() then
+	if alchemy.base.alchemy.CheckExplosionAndCleanList(User) then
 	    User:inform("Du brichst deine Arbeit vor dem "..USER_POSITION_LIST[User.id]..". Arbeitsschritt ab.", "You abort your work before the "..USER_POSITION_LIST[User.id]..". work step.")
 		return
 	end	
