1a25ee02bc6c68660fb086e0b7967faa2bbe372a
Enviyatar <enviyatar@gmx.at>
2013-05-12 23:18:30 +0200

    test

--- test/envi.lua
+++ test/envi.lua
@@ -26,9 +26,10 @@ function MoveToField(char)
 		world:gfx(53,position(6,6,0));
         	world:createMonster(942,position(6,6,0),0);
 				local monsters = world:getMonstersInRangeOf(char.pos, 20);
+				char:inform("Hui!", "Hui!")
 				for _,monsters in ipairs(monsters) do
 					monsters:increaseAttrib("hitpoints", -10000)
-				char:inform("Hui!", "Hui!")
+
 				end
 		char:inform("Fertig!", "Done!")
 		elseif base.factions.getMembership(char) == 3 then
