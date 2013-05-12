717b8e8231a1abc0ad456ed9e28c38b79bc8da7f
Enviyatar <enviyatar@gmx.at>
2013-05-13 01:33:14 +0200

    debug

--- development/hummi_olaficht_607.lua
+++ development/hummi_olaficht_607.lua
@@ -22,8 +22,10 @@ Description[GERMAN][2] = "Du hast Tronuk gefunden. Nun wähle eine seiner beiden 
 Description[ENGLISH][2] = "You found Tronuk. Now, choose one of his two stories. Listen the whole story. You will receive an information as soon as you are done.\nYou might also have to listen to the second story if you chose the wrong one."
 Description[GERMAN][3] = "Du weißt nun den Namen. Wenn du zurück bei Hummi bist, nenne ihm den Namen des Orks, der gewonnen hat. Der Name beginnt mit einem 'J'. Du kannst dir zur Not die Geschichte mittels 'Turnier' nochmals anhören."
 Description[ENGLISH][3] = "You know the name now. When you get back to Hummi, tell him the name of the orc who won. The name starts with a 'J'. If necessary, you can listen to the story another time by saying 'tournament'."
-Description[GERMAN][4] = "Hummi hat keine anderen Aufgaben für dich. Lediglich mehr Informationen. Aber suche nach weiteren Aufgaben bei anderen NPCs."
-Description[ENGLISH][4] = "Hummi does not have any other quests for you, just more information. However, you can look for other NPCs with quests for you."
+Description[GERMAN][4] = "Du weißt nun den Namen. Wenn du zurück bei Hummi bist, nenne ihm den Namen des Orks, der gewonnen hat. Der Name beginnt mit einem 'J'. Du kannst dir zur Not die Geschichte mittels 'Turnier' nochmals anhören."
+Description[ENGLISH][4] = "You know the name now. When you get back to Hummi, tell him the name of the orc who won. The name starts with a 'J'. If necessary, you can listen to the story another time by saying 'tournament'."
+Description[GERMAN][5] = "Hummi hat keine anderen Aufgaben für dich. Lediglich mehr Informationen. Aber suche nach weiteren Aufgaben bei anderen NPCs."
+Description[ENGLISH][5] = "Hummi does not have any other quests for you, just more information. However, you can look for other NPCs with quests for you."
 
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
@@ -31,9 +33,10 @@ QuestTarget = {}
 QuestTarget[1] = {position(164, 621, 0)} -- Tronruk
 QuestTarget[2] = {position(164, 621, 0)} -- Tronruk
 QuestTarget[3] = {position(681, 318, 0)} -- Hummi
+QuestTarget[4] = {position(681, 318, 0)} -- Hummi
 
 -- Insert the quest status which is reached at the end of the quest
-FINAL_QUEST_STATUS = 4
+FINAL_QUEST_STATUS = 5
 
 
 function QuestTitle(user)
