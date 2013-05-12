e08038b9b2cd1a1e7c0b9b9c8d6786c31b88e455
Enviyatar <enviyatar@gmx.at>
2013-05-12 20:12:03 +0200

    test

--- item/id_2763_pickaxe.lua
+++ item/id_2763_pickaxe.lua
@@ -311,30 +311,27 @@ Radius 15
 	
 	----------- Digging Projects (temporarly) -----------
 	----------- Galmair Tunnel Project -----------
-	AddArea( 9, position(427,206,0), 5 );
-		AddStone( 9, 914 );
-		AddStone( 9, 232 );
-		AddStone( 9, 1245 );
-	AddArea( 9, position(427,187,0), 5 );
-		AddStone( 9, 914 );
-		AddStone( 9, 232 );
-		AddStone( 9, 1245 );
-	AddArea( 9, position(410,187,0), 5 );
-		AddStone( 9, 914 );
-		AddStone( 9, 232 );
-		AddStone( 9, 1245 );
-	AddArea( 9, position(393,187,0), 5 );
-		AddStone( 9, 914 );
-		AddStone( 9, 232 );
-		AddStone( 9, 1245 );
-	AddArea( 9, position(393,169,0), 5 );
-		AddStone( 9, 914 );
-		AddStone( 9, 232 );
-		AddStone( 9, 1245 );
-	AddArea( 9, position(425,226,0), 5 );
-		AddStone( 9, 914 );
-		AddStone( 9, 232 );
-		AddStone( 9, 1245 );    			
+	AddArea( 91, position(427,206,0), 5 );
+		AddStone( 91, 914 );
+		AddStone( 91, 232 );
+		AddStone( 91, 1245 );
+	AddArea( 92, position(427,187,0), 5 );
+		AddStone( 92, 914 );
+		AddStone( 92, 232 );
+		AddStone( 92, 1245 );
+	AddArea( 93, position(410,187,0), 5 );
+		AddStone( 93, 914 );
+		AddStone( 93, 232 );
+		AddStone( 93, 1245 );
+	AddArea( 94, position(393,187,0), 5 );
+		AddStone( 94, 914 );
+		AddStone( 94, 232 );
+		AddStone( 94, 1245 );
+	AddArea( 95, position(393,169,0), 5 );
+		AddStone( 95, 914 );
+		AddStone( 95, 232 );
+		AddStone( 95, 1245 );
+		
 
 
     InitDone = true;
