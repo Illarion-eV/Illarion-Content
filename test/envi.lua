c114b2f5bdf7dde0e30f20a468bdb80326c19e58
Enviyatar <enviyatar@gmx.at>
2013-05-13 17:40:00 +0200

    test

--- test/envi.lua
+++ test/envi.lua
@@ -24,6 +24,39 @@ function MoveToField(char)
         	world:createMonster(941,position(6,5,0),0);
 		world:gfx(53,position(6,6,0));
         	world:createMonster(942,position(6,6,0),0);
+--[[		if base.factions.getMembership(char) == 2 then
+			char:inform("funkt1!", "funkt1!")
+			local monsters = world:getMonstersInRangeOf(char.pos, 30);
+			char:inform("funkt2!", "funkt2!")
+			for i,mon in ipairs(monsters) do
+			char:inform("funkt3!", "funkt3!")
+				mon:increaseAttrib("hitpoints", -10000)
+			char:inform("funkt4!", "funkt4!")
+			end
+		elseif base.factions.getMembership(char) == 0 then
+		char:inform("Ohoh, ein Hinterhalt für Vogelfreie!", "Ohoh, an ambush for outlaws!")
+		world:gfx(53,position(10,10,0)); 
+		world:gfx(2,position(11,11,0));
+		world:gfx(46,position(10,10,0));
+		else
+		char:inform("Ohoh, ein Hinterhalt!", "Ohoh, an ambush!")
+		end	]]--
+	else
+			char:warp(position(8,16,0))
+			world:gfx(37,char.pos);
+			world:makeSound(13,char.pos);
+			char:inform("Die krächzende Stimme meldet sich wieder: \"Keine Chance!\" Worauf eine Stimme aus dem Südwesten sagt: \"Lauf weg!\"", "The jagged voice says: \"No Chance!\". A voice from the southwest replies: \"Run away!\"")  
+			world:makeSound(25,char.pos);			
+			world:gfx(13,char.pos);
+			char:increaseAttrib("hitpoints", -5000)
+	end
+
+end
+
+function MoveFromField(char)
+	if char:getType() ~= Character.player then --Monsters will be ingored
+		return
+	end
 		if base.factions.getMembership(char) == 2 then
 			char:inform("funkt1!", "funkt1!")
 			local monsters = world:getMonstersInRangeOf(char.pos, 30);
@@ -32,10 +65,6 @@ function MoveToField(char)
 			char:inform("funkt3!", "funkt3!")
 				mon:increaseAttrib("hitpoints", -10000)
 			char:inform("funkt4!", "funkt4!")
-		--	for _,monster in ipairs(monsters) do
-		--	char:inform("funkt5!", "funkt5!")
-		--		monster:increaseAttrib("hitpoints", -10000)
-		--	char:inform("funkt6!", "funkt6!")
 			end
 		elseif base.factions.getMembership(char) == 0 then
 		char:inform("Ohoh, ein Hinterhalt für Vogelfreie!", "Ohoh, an ambush for outlaws!")
@@ -56,4 +85,3 @@ function MoveToField(char)
 	end
 
 end
-
