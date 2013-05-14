f40a2b1901d492f1704dcae16a588f48203ac0a0
Enviyatar <enviyatar@gmx.at>
2013-05-15 00:49:31 +0200

    test

--- triggerfield/galmair_bridges1_660.lua
+++ triggerfield/galmair_bridges1_660.lua
@@ -32,7 +32,7 @@ function MoveToField(char)
 		return
 		end
     		Init(); --Initialising
-		local shutup = 0
+		shutup = 0
 		local level = math.random(1,3)
 		local enemy1 = monster[level][math.random(1,table.getn(monster[level]))]
 		local enemy2 = monster[level][math.random(1,table.getn(monster[level]))]
@@ -82,7 +82,7 @@ function MoveFromField(char)
 			
 		else
 			player:inform("Du wirfst einen Blick zur Stadtmauer von Galmair und siehst die Wachen dort wie sie dich und dein Schicksal beobachten. Was, wenn du nur dem Don deine Steuern zahlen würdest?", "You look to the town wall of Galmair and see guards on the wall watching your faith. What if you would pay your taxes to the Don?")	
-			local shutup = 1
+			shutup = 1
 		end
 		end	
 	else
