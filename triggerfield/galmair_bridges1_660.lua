d711adcda6c49ab381a6979864987cebec086c62
Enviyatar <enviyatar@gmx.at>
2013-05-15 04:18:23 +0200

    test

--- triggerfield/galmair_bridges1_660.lua
+++ triggerfield/galmair_bridges1_660.lua
@@ -87,6 +87,14 @@ function MoveFromField(char)
 	if shutup ~= 0 then --stop spam
 		return
 		end
+	local hero = world:getPlayersInRangeOf(char.pos, 10); --lets see if there is a player around
+		for i,player in ipairs(hero) do
+		if base.factions.getMembership(player) == 3 then
+			luckybunch = 1 --if non-galmairians are together with galmairians
+			else
+			return
+			end
+		end
 	if char:getType() ~= Character.player then --monster start moving
 		local hero = world:getPlayersInRangeOf(char.pos, 10); --lets see if there is a player around
 		for i,player in ipairs(hero) do
@@ -97,7 +105,6 @@ function MoveFromField(char)
 			base.character.DeathAfterTime(char,math.random(10,20),0,1) --kill trigger monster
 			player:inform("Bevor du auch noch reagieren kannst, schießen Pfeile an dir vorbei und töten deine Widersacher. Du blickst in die Richtung von wo die Pfeile kamen und siehst die Wachen auf der Stadtmauer von Galmair dir mit ihren Armbrüste zuwinken. Gut, dass du dem Don deine Steuern zahlst und er dich beschützt!", "Even before you are able to react, arrows shoot around you and take down your enemies. You look to the direction the arrows originated from and see guards on the town wall of Galmair waving to you with their crossbows. Good, you have paid your taxes to the Don and he protects you!")	--praise the don message for the player
 			shutup = 1 --stop spam in the future
-			luckybunch = 1 --if non-galmairians are together with galmairians
 			player:setQuestProgress(660,math.random(40,55)) --lte set
 			local monsters = world:getMonstersInRangeOf(player.pos, 20); --get all monster in player range 
 				for i,mon in ipairs(monsters) do
