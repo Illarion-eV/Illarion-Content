8cd1798597db6a916b2960218012452907216bb3
Lillian <maletzki.lisa@googlemail.com>
2013-05-20 15:31:12 +0200

    repeatable quests with timestamp

--- npc/base/repeatable_quests.lua
+++ npc/base/repeatable_quests.lua
@@ -1,45 +1,36 @@
 require("base.common")
+require("base.time")
 
 module("npc.base.repeatable_quests", package.seeall)
 
 function checkIfTimesExpired(User, quest, monthToRepeat, dayToRepeat, hourToRepeat) 
 	local questState, questLastChanged = User:getQuestProgress(quest);
 	debug("lastChanged: "..questLastChanged)
-	local year, month, day, hour, minute, second-- = base.time.getRLDateFromUnixTimestamp(questLastChanged);
 
-	debug("year: "..year.." month: "..month.." day: "..day.." hour: "..hour);
-	hour = hour + hourToRepeat;
-	if hour > 24 then
-		day = day + 1;
-		hour = hour - 24;
-	end
+	local month, day, hour;
 	
-	day = day + dayToRepeat;
-	if day > 24 then
-		month = month + 1;
-		day = day - 24;
-	end
+	local y, mo, d, h, mi, sec = base.time.getRLDateFromUnixTimestamp(questLastChanged);
 	
-	month = month + monthToRepeat;
-	if month > 16 then
-		year = year + 1
-		month = month - 16;
-	end
+	debug(d.."."..mo.."."..y.." "..h..":"..mi..":"..sec)
 	
-	--fix days for mas
-	if month == 16 and day > 5 then
-		month = month + 1;
-		year = year + 1;
-		day = day - 5;
-	end
+	if base.time.daysPerMonth(mo) == 28 then
+		month = monthToRepeat * 2419200;
+	elseif base.time.daysPerMonth(mo) == 31 then
+		month = monthToRepeat * 2678400;
+	else 
+		month = monthToRepeat * 2592000;
+	end	
 	
-	debug("year: "..year.." month: "..month.." day: "..day.." hour: "..hour);
-	local newTimestamp = base.common.GetCurrentTimestampForDate(year, month, day, hour, minute, second)	
-	debug("new: "..newTimestamp)
-	local currentTimestamp = base.common.GetCurrentTimestamp()
-	debug("current: "..currentTimestamp)
-	year, month, day, hour, minute, second = base.common.TimestampToDate(currentTimestamp);
-	debug("current year: "..year.." month: "..month.." day: "..day.." hour: "..hour);
+	day = dayToRepeat * 86400;
+	hour = hourToRepeat * 3600;
+	
+	local newTimestamp = questLastChanged + month + day + hour;
+	
+	y, mo, d, h, mi, sec = base.time.getRLDateFromUnixTimestamp(newTimestamp)
+	debug(d.."."..mo.."."..y.." "..h..":"..mi..":"..sec)
+	
+	local currentTimestamp = world:getTime("unix");
+	
 	if newTimestamp >= currentTimestamp then
 		return true; 
 	else 
