c45f5597dfc259ae50071ea590e5e9f187e4547c
merung <karten-spiel-haus@hotmail.de>
2013-05-24 14:17:37 +0200

    script

---
+++ scheduled/alchemy.lua
@@ -0,0 +1,13 @@
+
+module("scheduled.gaia", package.seeall)
+
+-- INSERT INTO scheduledscripts VALUES('scheduled.alchemy', 5, 5, 'startAlchemy');
+
+
+-- This script is used for character independet events lasting over some time
+
+
+function startAlchemy()
+
+
+end
