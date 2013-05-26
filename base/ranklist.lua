a2064c9d913d3a32d09c36ac11133eefa8369260
martin <martin.polak@gmx.at>
2013-05-26 20:25:53 +0200

    Debugging ranklist

--- base/ranklist.lua
+++ base/ranklist.lua
@@ -74,6 +74,7 @@ function setRanklist(User, listName, points)
 	local newRanklist = {}
 	local joinedRanklist = {}
 	
+	User:inform("ranklist nr: "..#ranklist)
 	for i=1, #ranklist, 2 do 
 		table.insert(newRanklist, {["name"] = ranklist[i]; ["points"] = ranklist[i+1]}); 
 		User:inform("inserting "..ranklist[i])
