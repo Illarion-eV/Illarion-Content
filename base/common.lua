1c7a24cb948d04904a4dc0ff5944eefb5199b951
Martin Karing <nitram@illarion.org>
2013-05-27 22:27:51 +0200

    Improved performance of base.common.split

--- base/common.lua
+++ base/common.lua
@@ -1170,24 +1170,32 @@ end
     @param str - the string which should be splitted
     @return pat - the pattern around what the string should be splittet
     example = split("hello:my:split:world",":") returns {"hello","my","split","world"}
+	
+	In case the pattern is only one character it is better to use string.gmatch.
 ]]
 function split(str, pat)
-    local t = {n = 0}  -- NOTE: use {n = 0} in Lua-5.0
-    local fpat = "(.-)" .. pat
-    local last_end = 1
-    local s, e, cap = str:find(fpat, 1)
-    while s do
-        if s ~= 1 or cap ~= "" then
-            table.insert(t, cap);
-        end;
-        last_end = e + 1;
-        s, e, cap = str:find(fpat, last_end);
-   end;
-   if (last_end <= str:len()) then
-      cap = str:sub(last_end);
-      table.insert(t, cap);
-   end
-   return t;
+    local t = {}
+	if (string.len(pat) == 1) then
+		for element in string.gmatch(str, "([^" .. pat .. "]+)[" .. pat .. "]?") do
+			table.insert(t, element);
+		end;
+	else
+		local fpat = "(.-)" .. pat
+		local last_end = 1
+		local s, e, cap = str:find(fpat, 1)
+		while s do
+			if s ~= 1 or cap ~= "" then
+				table.insert(t, cap);
+			end;
+			last_end = e + 1;
+			s, e, cap = str:find(fpat, last_end);
+		end;
+		if (last_end <= str:len()) then
+			cap = str:sub(last_end);
+			table.insert(t, cap);
+		end;
+	end;
+	return t;
 end
 
 --[[
