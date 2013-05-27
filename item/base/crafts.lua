7f08ff7499606d5fd40f828b52bf8d6ef256e3ed
Lillian <maletzki.lisa@googlemail.com>
2013-05-27 21:30:30 +0200

    try to resolve problem with gilding weapons

--- item/base/crafts.lua
+++ item/base/crafts.lua
@@ -479,8 +479,6 @@ function Craft:checkMaterial(user, productId)
     for i = 1, #product.ingredients do
         local ingredient = product.ingredients[i]
 		
-		debug("Data of Ingredient: "..ingredient.data)
-		
 		if ingredient.data ~= {} then
 			local available = user:countItemAt("all", ingredient.item, ingredient.data)
 		else
