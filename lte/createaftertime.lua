26829478e805d5195470f6bc3b39c2bca3b6a4a4
Enviyatar <enviyatar@gmx.at>
2013-06-05 12:44:38 +0200

    test

--- lte/createaftertime.lua
+++ lte/createaftertime.lua
@@ -139,7 +139,7 @@ function callEffect(Effect, Character)
 	end
 
 	if createItemText==2 then -- if we have defined a sound for the created item
-		Character:inform("" ..TextDE[createItemText].. " " ..PosItemDE[PosItem]"."," " ..TextEN[createItemText].. " " ..PosItemEN[PosItem] ".") --inform player about result and the direction
+		Character:inform("" ..TextDE[createItemText] " " ..PosItemDE[PosItem]"."," " ..TextEN[createItemText] " " ..PosItemEN[PosItem] ".") --inform player about result and the direction
 	end
 
 	createItemTimeA=0  --prepare for next script call
