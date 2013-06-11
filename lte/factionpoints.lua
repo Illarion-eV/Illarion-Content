63e143cad95cba7fe860533fc86a027e41c39577
Enviyatar <enviyatar@gmx.at>
2013-06-11 19:33:02 +0200

    new experiements

---
+++ lte/factionpoints.lua
@@ -0,0 +1,34 @@
+-- CreateAfterTime, id 81
+-- regulates factionpoint-reward
+
+
+require("base.common")
+module("lte.factionpoints", package.seeall)
+
+TextDE={};
+TextEN={};
+TextDE[1]="Es sieht nicht danach aus als würde eine Fee heute ein Element verlieren.";
+TextEN[1]="It does not look like as any fairy would drop an element today.";
+
+
+PosItemDE={};
+PosItemEN={};
+PosItemDE[1]=" nordwestlich von dir.";
+PosItemEN[1]=" northwest of you.";
+
+
+function addEffect(Effect, Character)               				
+
+end
+
+function callEffect(Effect, Character)
+
+end
+
+function removeEffect(Effect,User)
+
+end
+
+function loadEffect(Effect,User)                  			
+
+end
