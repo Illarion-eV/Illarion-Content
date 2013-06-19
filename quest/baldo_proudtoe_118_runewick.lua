951dfefb75c7d174e9eb0f6c639a435acba2a339
Silverwing <Silverwing@illarion.org>
2013-06-19 11:06:47 +0200

    Questlogs for NPCs Baldo_Proudtoe and Lilith_needlehand

---
+++ quest/baldo_proudtoe_118_runewick.lua
@@ -0,0 +1,58 @@
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (118, 'quest.baldo_proudtoe_118_runewick');
+
+require("base.common")
+module("quest.baldo_proudtoe_118_runewick", package.seeall)
+
+GERMAN = Player.german
+ENGLISH = Player.english
+
+-- Insert the quest title here, in both languages
+Title = {}
+Title[GERMAN] = "Die Gärten von Runewick"
+Title[ENGLISH] = "Runewick Gardens"
+
+-- Insert an extensive description of each status here, in both languages
+-- Make sure that the player knows exactly where to go and what to do
+Description = {}
+Description[GERMAN] = {}
+Description[ENGLISH] = {}
+Description[GERMAN][1] = "Sammel zehn Äpfel und bringe diese Baldo Proudtoe im Garten. Um Äpfel zu sammeln stelle dich vor einen Apfelbaum und benutze diesen."
+Description[ENGLISH][1] = "Collect ten apples for Baldo Proudtoe in Garden. You should start by collecting apples by standing infront of an appletree using the appletree."
+Description[GERMAN][2] = "Geh zu Baldo Proudtoe, der sich im Garten aufhält. Er hat bestimmt noch eine Aufgabe für dich."
+Description[ENGLISH][2] = "Go back to Baldo Proudtoe in the Garden, he is sure to have another task for you."
+Description[GERMAN][3] = "Sammel zehn Weintrauben und bringe diese Baldo Proudtoe im Garten. Um Weintrauben zu sammeln stelle dich vor einen Strauch und benutze diesen."
+Description[ENGLISH][3] = "Collect ten grapes for Baldo Proudtoe in Garden. You should start by collecting grapes by standing infront of an bush using the bush."
+Description[GERMAN][4] = "Du hast alle Aufgaben von Baldo Proudtoe erfüllt."
+Description[ENGLISH][4] = "You have fulfilled all the tasks for Baldo Proudtoe."
+
+
+-- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
+QuestTarget = {}
+QuestTarget[1] = {position(941, 790, 0), position(792, 819, 0), position(792, 777, 0)} -- Apple tree
+QuestTarget[2] = {position(941, 790, 0)} 
+QuestTarget[3] = {position(941, 790, 0), position(958, 785, 0)} -- Bush
+QuestTarget[4] = {position(941, 790, 0)} 
+
+
+-- Insert the quest status which is reached at the end of the quest
+FINAL_QUEST_STATUS = 
+
+
+function QuestTitle(user)
+    return base.common.GetNLS(user, Title[GERMAN], Title[ENGLISH])
+end
+
+function QuestDescription(user, status)
+    local german = Description[GERMAN][status] or ""
+    local english = Description[ENGLISH][status] or ""
+
+    return base.common.GetNLS(user, german, english)
+end
+
+function QuestTargets(user, status)
+    return QuestTarget[status]
+end
+
+function QuestFinalStatus()
+    return FINAL_QUEST_STATUS
+end
---
+++ quest/lilith_needlehand_73_runewick.lua
@@ -0,0 +1,65 @@
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (73, 'quest.Lilith_Needlehand_73_runewick');
+     
+require("base.common")
+module("quest.Lilith_Needlehand_73_runewick", package.seeall)
+     
+GERMAN = Player.german
+ENGLISH = Player.english
+     
+-- Insert the quest title here, in both languages
+Title = {}
+Title[GERMAN] = "Das faule Schneiderlein"
+Title[ENGLISH] = "The Lazy Tailoress"
+     
+-- Insert an extensive description of each status here, in both languages
+-- Make sure that the player knows exactly where to go and what to do
+Description = {}
+Description[GERMAN] = {}
+Description[ENGLISH] = {}
+Description[GERMAN][1] = "Sammel zwanzig Wollballen und bringe sie zurück zu Lilith. Wenn du Schafe mit einer Schere scherst, bekommst du Wolle."
+Description[ENGLISH][1] = "Collect twenty bundles of wool and bring them back to Lilith. You can shear a sheep with scissors to get wool."
+Description[GERMAN][2] = "Kehre zurück zu Lilith Needlehand und frage, ob sie noch eine Aufgabe für dich hat."
+Description[ENGLISH][2] = "Go back to Lilith Needlehand, she is sure to have another task for you." 
+Description[GERMAN][3] = "Besorge fünfzehn Spulen Garn und bringe sie zurück zu Lilith. Garn kannst du mit einer Schere am Spinnrad aus Wolle oder Sibanac herstellen."
+Description[ENGLISH][3] = "Produce fifteen bobbins of thread and bring them back to Lilith. You can produce thread out of wool or sibanac leaf by using a spinning wheel with scissors." 
+Description[GERMAN][4] = "Kehre zurück zu Lilith Needlehand und frage, ob sie noch eine Aufgabe für dich hat."
+Description[ENGLISH][4] = "Go back to Lilith Needlehand, she is sure to have another task for you."  
+Description[GERMAN][5] = "Besorge fünf Bahnen roten Stoff und bringe sie zurück zu Lilith. Du kannst weißen oder grauen Stoff mit einen Farbeimer und einen Färbestab an einen Fass einfärben."
+Description[ENGLISH][5] = "Produce five bolts of red cloth and bring them back to Lilith. You can dye white or gray cloth with a dyeing rod and a bucket red dye, when you stand infront of a barral." 
+Description[GERMAN][6] = "Du hast alle Aufgaben von Lilith Needlehand erfüllt."
+Description[ENGLISH][6] = "You have fulfilled all the tasks for Lilith Needlehand." 
+
+
+    
+-- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
+QuestTarget = {}
+QuestTarget[1] = {position(852, 792, 0), position(894, 762, 0} -- Sheeps
+QuestTarget[2] = {position(894, 762, 0)} -- Lilith Needlehand 
+QuestTarget[3] = {position(911, 783, 0), position(894, 762, 0} -- spinning wheel
+QuestTarget[4] = {position(894, 762, 0)} -- Lilith Needlehand 
+QuestTarget[5] = {position(909, 778, 0), position(894, 762, 0} -- barrel
+QuestTarget[6] = {position(894, 762, 0)} -- Lilith Needlehand 
+     
+-- Insert the quest status which is reached at the end of the quest
+FINAL_QUEST_STATUS = 6
+     
+     
+function QuestTitle(user)
+    return base.common.GetNLS(user, Title[GERMAN], Title[ENGLISH])
+end
+     
+function QuestDescription(user, status)
+    local german = Description[GERMAN][status] or ""
+    local english = Description[ENGLISH][status] or ""
+     
+    return base.common.GetNLS(user, german, english)
+end
+     
+function QuestTargets(user, status)
+    return QuestTarget[status]
+end
+     
+function QuestFinalStatus()
+    return FINAL_QUEST_STATUS
+end
+
