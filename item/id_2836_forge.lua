5e0b4d14e55d3e72a9035822e098608068b1dfd8
Enviyatar <enviyatar@gmx.at>
2013-06-22 15:03:00 +0200

    update

--- item/id_2836_forge.lua
+++ item/id_2836_forge.lua
@@ -48,6 +48,7 @@ function UseItem(User, SourceItem, ltstate)
 	base.licence.licence(User); --checks if user is citizen or has a licence, future
 	if base.licence.stopcraftingnolicence then -- avoids crafting if user is neither citizen nor has a licence
 		return
+		else
 	end
 	content.gathering.InitGathering();
 	local oremelting = content.gathering.oremelting;
