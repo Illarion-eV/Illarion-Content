6a695b0e45d00eab6284bc9a544fd767dacbac70
Enviyatar <enviyatar@gmx.at>
2013-06-22 12:29:03 +0200

    test

--- base/licence.lua
+++ base/licence.lua
@@ -9,16 +9,13 @@ licenceGalmairPos={position(383,276,0),position(408,263,0),position(376,233,0)};
 --licenceGalmairPos[3]={position(376,233,0)}	--Galmair kitchen (Crest)
 
 function licence(char)
-	local i = 3
-	base.common.InformNLS(char,"test","test");
-	char:inform("i: " ..i,"i: " ..i)
-	local licenceGalmairPos = licenceGalmairPos[i]
---	char:inform("licenceGalmairPos: " ..licenceGalmairPos,"licenceGalmairPos: " ..licenceGalmairPos)
---	for i = 1,3 do	--set second number according to Galmairs sum of positions (current 3)
+
+	for i = 1,3 do	--set second number according to Galmairs sum of positions (current 3)
+		char:inform("i: " ..i,"i: " ..i)
+		local licenceGalmairPos = licenceGalmairPos[i]
 		local licenceGalmairDistance = char:distanceMetricToPosition (licenceGalmairPos)
 --		local licenceGalmairDistance = char:distanceMetricToPosition (position(383,276,0))
 		if licenceGalmairDistance < 11 then
---			char:inform("Range: Yes, in range" ..licenceGalmairPos,"Range: Yes, in range" ..licenceGalmairPos)
 			char:inform("Range: Yes, in range","Range: Yes, in range")	
 			if base.factions.getMembership(char) == 3 or char:getQuestProgress(812) > 0 then	
 				base.common.InformNLS(char,"Du hast eine Lizenz.","You habe a licence.");
@@ -28,8 +25,7 @@ function licence(char)
 			char:inform("Faction: " ..base.factions.getMembership(char),"Faction: " ..base.factions.getMembership(char))
 			char:inform("Quest812: " ..char:getQuestProgress(812),"Quest812: " ..char:getQuestProgress(812))
 		else
---			char:inform("Range: No, out of range" ..licenceGalmairPos,"Range: No, out of range" ..licenceGalmairPos)
 			char:inform("Range: No, out of range","Range: No, out of range")
 		end
---	end
+	end
 end
