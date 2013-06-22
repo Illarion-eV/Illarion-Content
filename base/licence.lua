65ca11efc96a9c0ab5219ff2870d34bf7c94fdc5
Enviyatar <enviyatar@gmx.at>
2013-06-22 11:41:07 +0200

    implemented licence for test

---
+++ base/licence.lua
@@ -0,0 +1,28 @@
+require("base.common")
+require("base.factions")
+
+module("base.licence", package.seeall)
+
+licenceGalmairPos={};
+licenceGalmairPos[1]=383,276,0; --Galmair workshop
+licenceGalmairPos[2]=408,263,0;	--Galmair alchemy hous
+licenceGalmairPos[2]=376,233,0;	--Galmair kitchen (Crest)
+
+function licence(char)
+	local i = 1
+--	for i = 1,3 do	--set second number according to Galmairs sum of positions (current 3)
+		local licenceGalmairDistance = char:distanceMetricToPosition (licenceGalmairPos[i])
+		if licenceGalmairDistance < 11 then
+			char:inform("Range: Yes, in range" ..licenceGalmairPos[i],"Range: Yes, in range" ..licenceGalmairPos[i])
+			char:inform("Faction: "..getMembership(char),"Faction: "..getMembership(char))
+			char:inform("Quest812: "..char:getQuestProgress(812),"Quest812: "..char:getQuestProgress(812))	
+			if base.factions.getMembership(char) == 3 or char:getQuestProgress(812) > 0 then	
+				base.common.InformNLS(char,"Du hast eine Lizenz.","You habe a licence.");
+			else
+				base.common.InformNLS(char,"Du solltest eine Lizenz erwerbern.","You should purchase a licence.");
+			end
+		else
+			char:inform("Range: No, out of range" ..licenceGalmairPos[i],"Range: No, out of range" ..licenceGalmairPos[i])
+		end
+--	end
+end
--- item/id_2836_forge.lua
+++ item/id_2836_forge.lua
@@ -14,6 +14,7 @@
 -- UPDATE common SET com_objectafterrot=2836 WHERE com_itemid = 2835;
 
 require("base.common")
+require("base.licence")
 require("content.gathering")
 
 module("item.id_2836_forge", package.seeall)
@@ -44,6 +45,7 @@ OreList = {
 CoalItem = CreateItem(21);
 
 function UseItem(User, SourceItem, ltstate)
+	base.licence(); --checks if user is citizen or has a licence
 	content.gathering.InitGathering();
 	local oremelting = content.gathering.oremelting;
 	
