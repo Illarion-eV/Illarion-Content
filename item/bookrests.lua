d667de8228e6e27d00e9bb49958a0893958f2953
Enviyatar <enviyatar@gmx.at>
2013-06-30 02:18:51 +0200

    test

--- item/bookrests.lua
+++ item/bookrests.lua
@@ -245,7 +245,7 @@ function Ferry(User, SourceItem)
 					for i,player in ipairs(travler) do
 						world:gfx(1,User.pos)
 						world:makeSound(9,User.pos);
-						travler:warp(targetPos[selected+1])
+						User:warp(targetPos[selected+1])
 						world:gfx(11,User.pos)
 						world:makeSound(9,User.pos);	
 					end
