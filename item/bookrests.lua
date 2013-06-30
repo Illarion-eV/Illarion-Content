735a340b5fde40377a2390f95dec7366a453534b
Enviyatar <enviyatar@gmx.at>
2013-06-30 13:46:12 +0200

    update

--- item/bookrests.lua
+++ item/bookrests.lua
@@ -157,10 +157,10 @@ ferrySourceItemPos={position(101,790,0),position(727,809,0),position(888,485,0),
 
 ferryDE={}
 ferryEN={}
-ferryDE[1]={"Cadomyr Hafen","Cadomyr Verlorener Hafen","Einsame Inseln","Runewick Hafen","Ostland Hafen","Nördlicher Hafen"}
-ferryEN[1]={"Cadomyr Harbour","Cadomyr Lost Harbour","Lonely Islands","Runewick Harbour","Eastland Harbour","Northern Harbour"}
-ferryDE[2]={"Cadomyr Hafen","Cadomyr Verlorener Hafen","Einsame Inseln","Runewick Hafen","Ostland Hafen","Nördlicher Hafen"}
-ferryEN[2]={"Cadomyr Harbour","Cadomyr Lost Harbour","Lonely Islands","Runewick Harbour","Eastland Harbour","Northern Harbour"}
+ferryDE[1]={"Cadomyr Hafen","Cadomyr Verlorener Hafen","Leuchtturm","Einsame Inseln","Runewick Hafen","Ostland Hafen","Nördlicher Hafen"}
+ferryEN[1]={"Cadomyr Harbour","Cadomyr Lost Harbour","Lighthouse","Lonely Islands","Runewick Harbour","Eastland Harbour","Northern Harbour"}
+ferryDE[2]={"Cadomyr Hafen","Cadomyr Verlorener Hafen","Leuchtturm","Einsame Inseln","Runewick Hafen","Ostland Hafen","Nördlicher Hafen"}
+ferryEN[2]={"Cadomyr Harbour","Cadomyr Lost Harbour","Lighthouse","Lonely Islands","Runewick Harbour","Eastland Harbour","Northern Harbour"}
 ferryDE[3]={"Cadomyr Hafen","Runewick Hafen","Ostland Hafen","Nördlicher Hafen"}
 ferryEN[3]={"Cadomyr Harbour","Runewick Harbour","Eastland Harbour","Northern Harbour"}
 ferryDE[4]={"Cadomyr Hafen","Runewick Hafen","Ostland Hafen","Nördlicher Hafen","Böser Fels"}
@@ -170,18 +170,18 @@ ferryEN[5]={"Galmair Harbour","Northern Islands -  Ra","Northern Islands -  Hept
 
 
 ferryItem={}
-ferryItem[1]={2701,272,3099,105,2760,308}
-ferryItem[2]={2701,272,3099,105,2760,308}
+ferryItem[1]={2701,272,467,3099,105,2760,308}
+ferryItem[2]={2701,272,467,3099,105,2760,308}
 ferryItem[3]={2701,105,2760,308}
 ferryItem[4]={2701,105,2760,308,915}
 ferryItem[5]={61,359,360,372,474}
 
 ferryTargetPos={}
-ferryTargetPos[1]={position(102,790,0), position(105,832,0),position(616,859,0),position(728,809,0),position(888,486,0),position(870,286,0)}
-ferryTargetPos[2]={position(102,790,0), position(105,832,0),position(616,859,0),position(728,809,0),position(888,486,0),position(870,286,0)}
+ferryTargetPos[1]={position(102,790,0),position(105,832,0),position(553,658,0),position(616,859,0),position(728,809,0),position(888,486,0),position(870,286,0)}
+ferryTargetPos[2]={position(102,790,0),position(105,832,0),position(553,658,0),position(616,859,0),position(728,809,0),position(888,486,0),position(870,286,0)}
 ferryTargetPos[3]={position(102,790,0),position(728,809,0),position(888,486,0),position(870,286,0)}
 ferryTargetPos[4]={position(102,790,0),position(728,809,0),position(888,486,0),position(870,286,0),position(987,256,0)}
-ferryTargetPos[5]={position(450,95,0), position(364,49,0),position(415,85,0),position(478,34,0),position(682,45,0)}
+ferryTargetPos[5]={position(450,95,0),position(364,49,0),position(415,85,0),position(478,34,0),position(682,45,0)}
 
 
 function Ferry(User, SourceItem)
