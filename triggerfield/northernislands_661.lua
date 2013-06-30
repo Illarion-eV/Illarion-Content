85c7cff2f943e9ec3dcbe38325b5493c70eb9303
Enviyatar <enviyatar@gmx.at>
2013-06-30 19:06:27 +0200

    test

--- triggerfield/northernislands_661.lua
+++ triggerfield/northernislands_661.lua
@@ -34,6 +34,8 @@ function MoveToField(char)
 	for i = 1,3 do
 		local AmountPos = table.getn(NorthPos[i])
 		for j = 1,AmountPos do
+			User:inform("items: "..i,"items: "..i)
+			User:inform("items: "..j,"items: "..j)
 	   		if (char.pos == Northpos[i][j]) then
 				NorthitemsID = createNorthItemID[i]
 				NorthItemIDB = createNorthItemIDB[i]
