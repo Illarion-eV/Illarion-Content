df2993ea3b8e596eeec449d109b296b96a3e760a
Enviyatar <enviyatar@gmx.at>
2013-07-04 21:15:59 +0200

    update, implemented base.pirates.lua

---
+++ base/pirates.lua
@@ -0,0 +1,18 @@
+require("base.common")
+require("item.bookrests")
+
+module("base.pirates", package.seeall)
+
+
+function piratesattack(User)
+
+					for i,player in ipairs(item.bookrests.Ferry.travler) do
+						player:inform("Piraten nähern sich eurem Schiff, dir stehen folgende Möglichkeiten zur Wahl.", "Pirates are coming closer to your ship. You can choose between following options.")
+						world:gfx(1,player.pos)
+						world:makeSound(9,player.pos);
+						player:warp(position(352,870,1))
+						world:gfx(11,player.pos)
+						world:makeSound(9,player.pos);	
+					end
+
+end
--- item/bookrests.lua
+++ item/bookrests.lua
@@ -1,5 +1,5 @@
 require("base.common")
---require("base.pirates")
+require("base.pirates")
 
 -- UPDATE common SET com_script='item.bookrests' WHERE com_itemid = 3104;
 -- UPDATE common SET com_script='item.bookrests' WHERE com_itemid = 3105;
@@ -217,21 +217,14 @@ function Ferry(User, SourceItem)
 				
 
 					base.money.TakeMoneyFromChar(User,10000)
+					travler = world:getPlayersInRangeOf(SourceItem.pos, 5); 
+					if math.random(1,100)< 51 then
 
-					if math.random(1,100)< 26 then
-
-					for i,player in ipairs(travler) do
-						player:inform("Piraten nähern sich eurem Schiff, dir stehen folgende Möglichkeiten zur Wahl.", "Pirates are coming closer to your ship. You can choose between following options.")
-						world:gfx(1,player.pos)
-						world:makeSound(9,player.pos);
-						player:warp(position(352,870,1))
-						world:gfx(11,player.pos)
-						world:makeSound(9,player.pos);	
-					end
+						base.pirates.piratesattack(User)
 
  					else
 
-					travler = world:getPlayersInRangeOf(SourceItem.pos, 5); 
+
 					for i,player in ipairs(travler) do
 						player:inform("Du hast dich dazu entschlossen nach " ..names[selected+1].. " zu Reisen.", "You have chosen to travel to " ..names[selected+1]..".")
 						world:gfx(1,player.pos)
