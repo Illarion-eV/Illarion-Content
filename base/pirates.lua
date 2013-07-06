d5dd5c5c227985d2084314598523e0e0c57b9ee6
Enviyatar <enviyatar@gmx.at>
2013-07-07 01:14:26 +0200

    test

--- base/pirates.lua
+++ base/pirates.lua
@@ -48,7 +48,7 @@ function piratesattack(User,SourceItem,travler)
 	else
 		dialog = SelectionDialog("Pirates attack", "Pirates are coming closer to your ship. You can choose between following options.", callback)
 	end
-	dialog:setCloseOnMove()
+--	dialog:setCloseOnMove()
 	
 	for i=1,Amountoptions do
 		dialog:addOption(items[i],options[i])
