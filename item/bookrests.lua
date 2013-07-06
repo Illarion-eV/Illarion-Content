a62a9db2bc8d2a5734ad5a58a0806b5793833662
Enviyatar <enviyatar@gmx.at>
2013-07-07 01:30:00 +0200

    test

--- item/bookrests.lua
+++ item/bookrests.lua
@@ -224,19 +224,6 @@ function Ferry(User, SourceItem)
 
 						base.pirates.piratesattack(User,SourceItem,travler)
 
-User:requestSelectionDialog(dialog)
-	local callback = function(dialog)
-	debug("..selected ")
-		success = dialog:getSuccess() 
-		if success then
-			selected = dialog:getSelectedIndex() 
-			User:inform("Success, you selected option "..selected+1) 
-		else
-			User:inform("Selection aborted!") 
-		end
-	debug("..selected2 ")
-	end
-
  					else
 
 
