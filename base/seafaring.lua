b0b02cf224f116e1e92f263d6b59355c06f2bbdc
Enviyatar <enviyatar@gmx.at>
2013-07-07 13:56:05 +0200

    test

--- base/seafaring.lua
+++ base/seafaring.lua
@@ -80,6 +80,16 @@ function Ferry(User, SourceItem)
 					travler = world:getPlayersInRangeOf(SourceItem.pos, 5); 
 					if math.random(1,100)< 91 then
 
+for i,player in ipairs(travler) do
+		player:inform("Piraten nähern sich eurem Schiff, dir stehen folgende Möglichkeiten zur Wahl.", "Pirates are coming closer to your ship. You can choose between following options.")
+		world:gfx(1,player.pos)
+		world:makeSound(9,player.pos);
+		player:warp(position(352,870,1))
+		world:gfx(11,player.pos)
+		world:makeSound(9,player.pos);	
+	end
+
+
 						piratesAttack(User,SourceItem,travler)
 
 
