e32022ecb2e8c82be69416fed5f500d79437132d
Enviyatar <enviyatar@gmx.at>
2013-07-07 02:16:49 +0200

    test

--- item/bookrests.lua
+++ item/bookrests.lua
@@ -223,7 +223,7 @@ function Ferry(User, SourceItem)
 					if math.random(1,100)< 91 then
 
 						base.pirates.piratesattack(User,SourceItem,travler)
-
+User:inform("Success, you selected option "..selected+1)
  					else
 
 
