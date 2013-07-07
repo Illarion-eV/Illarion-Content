4d78d537b372ab2817b305989455101baae21556
Enviyatar <enviyatar@gmx.at>
2013-07-07 13:53:03 +0200

    test

--- base/seafaring.lua
+++ base/seafaring.lua
@@ -130,14 +130,14 @@ end
 
 
 function piratesAttack(User,SourceItem,travler)
-	for i,player in ipairs(travler) do
+	--[[for i,player in ipairs(travler) do
 		player:inform("Piraten nähern sich eurem Schiff, dir stehen folgende Möglichkeiten zur Wahl.", "Pirates are coming closer to your ship. You can choose between following options.")
 		world:gfx(1,player.pos)
 		world:makeSound(9,player.pos);
 		player:warp(position(352,870,1))
 		world:gfx(11,player.pos)
 		world:makeSound(9,player.pos);	
-	end
+	end]]
 
 	local options
 	local Amountoptions = table.getn(pirateOptionsDE)	
