04337fb91ad4dbac9d0bf30f5c48df2960a73b7b
Enviyatar <enviyatar@gmx.at>
2013-07-07 02:24:41 +0200

    test

--- item/bookrests.lua
+++ item/bookrests.lua
@@ -186,6 +186,11 @@ ferryTargetPos[4]={position(102,790,0),position(728,809,0),position(888,486,0),p
 ferryTargetPos[5]={position(450,95,0),position(364,49,0),position(415,85,0),position(478,34,0),position(682,45,0)}
 
 
+optionsDE={"Kämpfe","Flieh","Zahl"}
+optionsEN={"Fight","Flee","Pay"}
+
+items={1,53,61}
+
 function Ferry(User, SourceItem)
 
 	local names
@@ -222,7 +227,58 @@ function Ferry(User, SourceItem)
 					travler = world:getPlayersInRangeOf(SourceItem.pos, 5); 
 					if math.random(1,100)< 91 then
 
-						base.pirates.piratesattack(User,SourceItem,travler)
+--						base.pirates.piratesattack(User,SourceItem,travler)
+
+
+
+
+for i,player in ipairs(travler) do
+		player:inform("Piraten nähern sich eurem Schiff, dir stehen folgende Möglichkeiten zur Wahl.", "Pirates are coming closer to your ship. You can choose between following options.")
+		world:gfx(1,player.pos)
+		world:makeSound(9,player.pos);
+		player:warp(position(352,870,1))
+		world:gfx(11,player.pos)
+		world:makeSound(9,player.pos);	
+	end
+
+	local options
+	local Amountoptions = table.getn(optionsDE)	
+	for m = 1,Amountoptions do
+	   	if  User:getPlayerLanguage() == Player.german then
+			options = optionsDE
+		else
+			options = optionsEN
+		end
+	end
+
+
+	local callpirates = function(dialog2)
+	debug("..selected ")
+		success = dialog2:getSuccess() 
+		if success then
+			selected = dialog2:getSelectedIndex() 
+			User:inform("Success, you selected option "..selected+1) 
+		else
+			User:inform("Selection aborted!") 
+		end
+	debug("..selected2 ")
+	end
+--	debug("..selected ")
+	local dialog2
+	if User:getPlayerLanguage() == Player.german then
+		dialog2 = SelectionDialog("Piraten Angriff", "Piraten nähern sich eurem Schiff, dir stehen folgende Möglichkeiten zur Wahl.", callpirates)
+	else
+		dialog2 = SelectionDialog("Pirates attack", "Pirates are coming closer to your ship. You can choose between following options.", callpirates)
+	end
+--	dialog:setCloseOnMove()
+	
+	for m=1,Amountoptions do
+		dialog2:addOption(items[m],options[m])
+	end
+	User:requestSelectionDialog(dialog2)
+
+
+
 
  					else
 
