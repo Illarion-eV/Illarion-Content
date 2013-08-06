26175122b8e4a4dd0e8b96e94cf19a1a479373a1
nalcaryos <serra@gmx.at>
2013-08-06 15:28:40 +0200

    DE check done. I also added missing questatus messages. 0009017. Small quality change in on reward.

--- quest/mirarie_bragolin_317_cadomyr.lua
+++ quest/mirarie_bragolin_317_cadomyr.lua
@@ -17,25 +17,33 @@ Description = {}
 Description[GERMAN] = {}
 Description[ENGLISH] = {}
 Description[GERMAN][1] = "Besorge fünf Wasserflaschen und bringe sie Mirarie. Du kannst sie bei einem Händler kaufen oder große leere Flaschen an einem Brunnen oder Gewässer füllen."
-Description[ENGLISH][1] = "Produce five bottles of water and bring them to Hector. You can buy them from a merchant or fill a large empty bottle on a fountain or on the water."
+Description[ENGLISH][1] = "Produce five bottles of water and bring them to Mirarie. You can buy them from a merchant or fill a large empty bottle on a fountain or on the water."
 Description[GERMAN][2] = "Geh zu Mirarie Bragolin in Cadomyr. Sie hat bestimmt noch eine Aufgabe für dich."
-Description[ENGLISH][2] = "Go back to Mirarie Bragolin in Cadomyr, she certainly have another task for you."
-Description[GERMAN][3] = "Sprich Thomas Dalenus auf dem Marktplatz von Cadomyr wegen des Briefes an. Kehre dann zu Mirarie zurück."
-Description[ENGLISH][3] = "Talk to Thomas Dalenus on the market place of Cadomyr concerning the letter. Then return to Mirarie."
-Description[GERMAN][4] = "Geh zu Mirarie Bragolin in Cadomyr. Sie hat bestimmt noch eine Aufgabe für dich."
-Description[ENGLISH][4] = "Go back to Mirarie Bragolin in Cadomyr, she certainly have another task for you."
-Description[GERMAN][5] = "Sprich Grakamesh an der Mauer von Cadomyr wegen der Nachricht an. Kehre dann zu Mirarie zurück."
-Description[ENGLISH][5] = "Talk to Grakamesh out of the walls of Cadomyr concerning the message. Then return to Mirarie."
-Description[GERMAN][6] = "Geh zu Mirarie Bragolin in Cadomyr. Sie hat bestimmt noch eine Aufgabe für dich."
-Description[ENGLISH][6] = "Go back to Mirarie Bragolin in Cadomyr, she certainly have another task for you."
-Description[GERMAN][7] = "Sprich William Daloire, einem Kräuterkundigen aus Runewick, wegen der Nachricht an. Kehre dann zu Mirarie zurück."
-Description[ENGLISH][7] = "Talk to William Daloire a herbalist in Runewick concerning the message. Then return to Mirarie."
-Description[GERMAN][6] = "Geh zu Mirarie Bragolin in Cadomyr. Sie hat bestimmt noch eine Aufgabe für dich."
-Description[ENGLISH][6] = "Go back to Mirarie Bragolin in Cadomyr, she certainly have another task for you."
-Description[GERMAN][9] = "Sprich Valerio Guilianni, den Don von Galmair, wegen der Nachricht an. Kehre dann zu Mirarie zurück."
-Description[ENGLISH][9] = "Talk to Don Valerio Guilianni in Galmair concerning the message. Then return to Mirarie."
-Description[GERMAN][10] = "Du hast alle Aufgaben von Mirarie Bragolin erfüllt."
-Description[ENGLISH][10] = "You have fulfilled all the tasks for Mirarie Bragolin."
+Description[ENGLISH][2] = "Go back to Mirarie Bragolin in Cadomyr, she certainly has another task for you."
+Description[GERMAN][3] = "Sprich Thomas Dalenus auf dem Marktplatz von Cadomyr wegen des Briefes an."
+Description[ENGLISH][3] = "Talk to Thomas Dalenus on the market place of Cadomyr concerning the letter."
+Description[GERMAN][4] = "Kehre zu Mirarie zurück."
+Description[ENGLISH][4] = "Return to Mirarie."
+Description[GERMAN][5] = "Geh zu Mirarie Bragolin in Cadomyr. Sie hat bestimmt noch eine Aufgabe für dich."
+Description[ENGLISH][5] = "Go back to Mirarie Bragolin in Cadomyr, she certainly have another task for you."
+Description[GERMAN][6] = "Sprich Grakamesh an der Mauer von Cadomyr wegen der Nachricht an."
+Description[ENGLISH][6] = "Talk to Grakamesh out of the walls of Cadomyr concerning the message."
+Description[GERMAN][7] = "Kehre zu Mirarie zurück."
+Description[ENGLISH][7] = "Return to Mirarie."
+Description[GERMAN][8] = "Geh zu Mirarie Bragolin in Cadomyr. Sie hat bestimmt noch eine Aufgabe für dich."
+Description[ENGLISH][8] = "Go back to Mirarie Bragolin in Cadomyr, she certainly have another task for you."
+Description[GERMAN][9] = "Sprich William Daloire, einem Kräuterkundigen aus Runewick, wegen der Nachricht an."
+Description[ENGLISH][9] = "Talk to William Daloire a herbalist in Runewick concerning the message."
+Description[GERMAN][10] = "Kehre zu Mirarie zurück."
+Description[ENGLISH][10] = "Return to Mirarie."
+Description[GERMAN][11] = "Geh zu Mirarie Bragolin in Cadomyr. Sie hat bestimmt noch eine Aufgabe für dich."
+Description[ENGLISH][11] ="Go back to Mirarie Bragolin in Cadomyr, she certainly have another task for you."
+Description[GERMAN][12] = "Sprich Valerio Guilianni, den Don von Galmair, wegen der Nachricht an."
+Description[ENGLISH][12] = "Talk to Don Valerio Guilianni in Galmair concerning the message."
+Description[GERMAN][13] = "Kehre zu Mirarie zurück."
+Description[ENGLISH][13] = "Return to Mirarie."
+Description[GERMAN][14] =  "Du hast alle Aufgaben von Mirarie Bragolin erfüllt."
+Description[ENGLISH][14] = "You have fulfilled all the tasks for Mirarie Bragolin."
 
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
@@ -44,15 +52,19 @@ QuestTarget[1] = {position(128, 621, 0), position(113, 574, 0), position(117, 60
 QuestTarget[2] = {position(128, 621, 0)} 
 QuestTarget[3] = {position(128, 621, 0), position(113, 611, 0)} 
 QuestTarget[4] = {position(128, 621, 0)} 
-QuestTarget[5] = {position(128, 621, 0), position(133, 638, 0)} 
-QuestTarget[6] = {position(128, 621, 0)} 
-QuestTarget[7] = {position(128, 621, 0), position(897, 794, 1)} 
+QuestTarget[5] = {position(128, 621, 0)} 
+QuestTarget[6] = {position(128, 621, 0), position(133, 638, 0)}
+QuestTarget[7] = {position(128, 621, 0)} 
 QuestTarget[8] = {position(128, 621, 0)} 
-QuestTarget[9] = {position(128, 621, 0), position(337, 215, 0)} 
+QuestTarget[9] = {position(128, 621, 0), position(897, 794, 1)} 
 QuestTarget[10] = {position(128, 621, 0)} 
+QuestTarget[11] = {position(128, 621, 0)}
+QuestTarget[12] = {position(128, 621, 0), position(337, 215, 0)} 
+QuestTarget[13] = {position(128, 621, 0)}
+QuestTarget[14] = {position(128, 621, 0)}
 
 -- Insert the quest status which is reached at the end of the quest
-FINAL_QUEST_STATUS = 10
+FINAL_QUEST_STATUS = 14
 
 
 function QuestTitle(user)
