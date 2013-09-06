41135a07b2faa3b42ade11a5937928a7fe6a9823
nalcaryos <serra@gmx.at>
2013-09-06 21:32:16 +0200

    Correct a small typo in the standartfighting script.

--- server/standardfighting.lua
+++ server/standardfighting.lua
@@ -1256,7 +1256,7 @@ function Specials(Attacker, Defender, Globals)
                     "#me thrusts out, delivering a powerful, piercing attack.");
         elseif(Globals.criticalHit==7) then -- Dist
             base.common.TalkNLS(Attacker.Char, Character.say,
-                    "#me zielt etwas länger uns er trifft dadurch sein Ziel an einer ungeschützen Stelle.",
+                    "#me zielt etwas länger und er trifft dadurch sein Ziel an einer ungeschützen Stelle.",
                     "#me takes careful aim, hitting "..hisher.." target with precision and power.");
         elseif(Globals.criticalHit==8) then -- Wrest
             base.common.TalkNLS(Attacker.Char, Character.say,
