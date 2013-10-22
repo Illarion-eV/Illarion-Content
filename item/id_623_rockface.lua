ff202e342a75f84bc7e04efca427dca793354b9b
Caldarion <garinger@gmx.de>
2013-10-22 17:54:58 +0200

    add a rock wall gate that opens on a keyword (Mantis 8909)
    - works on rockwall id 623  at position 894, 627, 0
    - lte to close wall after 30sec again

---
+++ item/id_623_rockface.lua
@@ -0,0 +1,40 @@
+require("base.common")
+require("base.lookat")
+
+-- UPDATE common SET com_script='item.id_623_rockface' WHERE com_itemid IN (623);
+
+module("item.id_623_rockface", package.seeall)
+
+function UseItem(User, SourceItem, ltstate)
+
+  -- entrance to the cave
+  if (SourceItem.pos ~= position(894, 627, 0)) then
+		return
+  end
+
+  -- open on keyword
+  local spokenText = User.lastSpokenText;
+  if (string.find(spokenText, "[Ff][Oo][Xx]") ~= nil or
+	  string.find(spokenText, "[Ff][Uu][Cc][Hh][Ss]") ~= nil) then
+
+	world:erase(SourceItem, 1);
+
+	base.common.HighInformNLS(User,
+      "Die Steinwand verschwindet als du sie berührst!",
+      "The rock wall disappears as you touch it!");
+
+	local myEffect = LongTimeEffect(3000,300); -- close after 30sec
+	User.effects:addEffect(myEffect);
+  else
+	base.common.HighInformNLS(User,
+      "Nichts passiert als du die Steinwand berührst.",
+      "Nothing happens as you touch the rock wall.");
+  end
+end
+
+function LookAtItem(User, Item)
+  if (Item.pos == position(894, 627, 0)) then
+		base.lookat.SetSpecialDescription(Item, "Eine merkwürdige Steinwand.", "A peculiar rock wall.");
+  end
+  world:itemInform(User,Item,base.lookat.GenerateLookAt(User, Item, base.lookat.NONE));
+end
