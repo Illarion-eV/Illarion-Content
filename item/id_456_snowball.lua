74d190b655306ad597246e2ac8f295ebf80e551d
merung <karten-spiel-haus@hotmail.de>
2014-01-09 13:25:48 +0100

    Add input dialog test

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -29,3 +29,48 @@ function MoveItemAfterMove(User, SourceItem, TargetItem)
 		Round=1;
 	end
 end
+
+function UseItem(User,SourceItem)
+
+	local callback = function (dialog)
+		if not dialog:getSuccess() then
+			User:inform("Abort selection")
+		else
+			local index = dialog:getSelectedIndex()+1;
+			local multipleLines = false
+			if index == 2 then
+				multipleLines = true
+			end
+			openInputDialog(User,multipleLines)
+		end
+	end
+	local dialog = SelectionDialog("Input Dialog", "Select an input dialog with one or multiple lines", callback);
+	dialog:addOption(0,"Input dialog with ONE line");
+	dialog:addOption(0,"Input dialog with MULTIPLE line");
+	
+	User:requestSelectionDialog(dialog)
+end
+
+function openInputDialog(User,multipleLines)
+
+	-- input dialog
+	local callback = function(dialog)
+		if not dialog:getSuccess() then
+			User:inform("Abort input dialog.")
+		else
+			openMessageDialog(User,dialog:getInput())			
+		end
+	end
+	local dialog = InputDialog("Input dialog", "Write your text. Maximum is set to 250 characters.", multipleLines, 250, callback)
+	User:requestInputDialog(dialog)
+end
+
+function openMessageDialog(User, userText)
+
+	local callback = function(dialog)
+		-- empty callback
+	end
+	local dialog = MessageDialog("Message Dialog",userText,callback)
+	User:requestMessageDialog(dialog)
+
+end
