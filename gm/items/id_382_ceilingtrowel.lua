e618247ae9055526d096647c22bf0a9a397d265b
Martin Karing <nitram@illarion.org>
2014-01-12 17:24:56 +0100

    Added missing texts to faction gm tool

--- gm/items/id_382_ceilingtrowel.lua
+++ gm/items/id_382_ceilingtrowel.lua
@@ -399,10 +399,12 @@ debug("radius"..radius)
           end
           local firstFaction = factionIds[dialog:getSelectedIndex()+1];
           local modeStrings = {};
+          modeStrings[base.factions.RELATION_SELF] = "self";
           modeStrings[base.factions.RELATION_NEUTRAL] = "neutral";
           modeStrings[base.factions.RELATION_HOSTILE] = "hostile";
           modeStrings[base.factions.RELATION_AGGRESSIVE] = "aggressive";
           modeStrings[base.factions.RELATION_FRIENDLY] = "friendly";
+          modeStrings[base.factions.RELATION_ACCEPTED] = "accepted player";
           local modeValues = {base.factions.RELATION_FRIENDLY, base.factions.RELATION_NEUTRAL, base.factions.RELATION_HOSTILE, base.factions.RELATION_AGGRESSIVE};
           local cbSecondFaction = function (dialog)
             if (not dialog:getSuccess()) then
