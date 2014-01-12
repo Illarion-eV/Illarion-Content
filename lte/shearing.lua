8352568fb11fe000dfd2e275db7ea2f9bd1c80a4
Caldarion <garinger@gmx.de>
2014-01-12 18:35:08 +0100

    limit resource: only allow gathing 100 wool per sheep per 30min

---
+++ lte/shearing.lua
@@ -0,0 +1,41 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+-- Long Time Effect Script: Shearing sheep
+-- Effect ID: 402
+
+
+require("base.common");
+module("lte.shearing", package.seeall)
+
+function addEffect(shearingEffect, Animal)
+
+end
+
+function callEffect(shearingEffect, Animal)
+
+	local foundAmount, gatherAmount = shearingEffect:findValue("gatherAmount");
+	if (not foundAmount) then
+		return false;
+	end
+	if (gatherAmount <= 0) then
+		return false;
+	end
+
+	shearingEffect:addValue("gatherAmount", gatherAmount - 10);
+	shearingEffect.nextCalled = 1800; -- call every 3min
+	return true;
+end
