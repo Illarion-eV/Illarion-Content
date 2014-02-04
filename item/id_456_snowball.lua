9ca427e650d57de05cac9363b9ee2e7f901a5124
merung <karten-spiel-haus@hotmail.de>
2014-02-04 19:11:54 +0100

    Add waypoint check

--- item/id_456_snowball.lua
+++ item/id_456_snowball.lua
@@ -19,58 +19,10 @@ require("base.common")
 
 module("item.id_456_snowball", package.seeall)
 
-Round=1;
-
-function MoveItemAfterMove(User, SourceItem, TargetItem)
-	if Round==1 then
-        User:talk(Character.say, "#me wirft einen Schneeball.", "#me throws a snowball.")
-		Round=2;
-	else
-		Round=1;
-	end
-end
-
 function UseItem(User,SourceItem)
 
-	local callback = function (dialog)
-		if not dialog:getSuccess() then
-			User:inform("Abort selection")
-		else
-			local index = dialog:getSelectedIndex()+1;
-			local multipleLines = false
-			if index == 2 then
-				multipleLines = true
-			end
-			openInputDialog(User,multipleLines)
-		end
-	end
-	local dialog = SelectionDialog("Input Dialog", "Select an input dialog with one or multiple lines", callback);
-	dialog:addOption(0,"Input dialog with ONE line");
-	dialog:addOption(0,"Input dialog with MULTIPLE line");
+	local oldSlime = world:createMonster(1055,position(8,2,0),0)
+	oldSlime.waypoints:addWaypoint(position(10,6,0))
+	oldSlime:setOnRoute(true)
 	
-	User:requestSelectionDialog(dialog)
-end
-
-function openInputDialog(User,multipleLines)
-
-	-- input dialog
-	local callback = function(dialog)
-		if not dialog:getSuccess() then
-			User:inform("Abort input dialog.")
-		else
-			openMessageDialog(User,dialog:getInput())			
-		end
-	end
-	local dialog = InputDialog("Input dialog", "Write your text. Maximum is set to 250 characters.", multipleLines, 250, callback)
-	User:requestInputDialog(dialog)
-end
-
-function openMessageDialog(User, userText)
-
-	local callback = function(dialog)
-		-- empty callback
-	end
-	local dialog = MessageDialog("Message Dialog",userText,callback)
-	User:requestMessageDialog(dialog)
-
 end
--- monster/mon_105_transparentslimes.lua
+++ monster/mon_105_transparentslimes.lua
@@ -48,9 +48,12 @@ DELETED = false
 function abortRoute(theSlime)
 	-- Slime feeding quest in Runewick
 	if theSlime:getMonsterType()==1055 then
-	local tSF = triggerfield.slimeFeeding
-		if theSlime.pos == tSF.WARP_TO_SLIME_POSITION then
-			if world:isItemOnField(tSF.WARP_TO_SLIME_POSITION) and DELETED == false then
+		if theSlime.pos == position(10,6,0) then
+			theSlime.waypoints:addWaypoint(position(12,2,0))
+			theSlime:setOnRoute(true)
+		
+		
+			--[[if world:isItemOnField(tSF.WARP_TO_SLIME_POSITION) and DELETED == false then
 				theSlime:talk(Character.say, "#mes schleimige Masse gleitet über das Futter und absorbiert es. Sein Körper wabbelt kurz ein Objekt tritt aus diesem raus, welches über die Ansperrung kataplutiert.",
 				"#me's slimy mass flows over the feed and absorbs it. Its body wobbles for a short period of time and an oject emerges from it, which is catapulted over the boundary.")
 				local feeding = world:getItemOnField(tSF.WARP_TO_SLIME_POSITION)
@@ -61,14 +64,15 @@ function abortRoute(theSlime)
 				theSlime.movepoints = theSlime.movepoints -30
 			end
 			theSlime.waypoints:addWaypoint(tSF.SLIME_CAVE_POSITION)
-			theSlime:setOnRoute(true)
+			theSlime:setOnRoute(true)]]
 			
-		elseif theSlime.pos == tSF.SLIME_CAVE_POSITION then
-			theSlime:talk(Character.say, "#me fließt in die Höhlennische zurück.",
+		elseif theSlime.pos == position(12,2,0) then
+			theSlime:increaseAttrib("hitpoints", -10000)
+			--[[theSlime:talk(Character.say, "#me fließt in die Höhlennische zurück.",
 			"#me flows back into the small hole.")
 			theSlime:increaseAttrib("hitpoints", -10000)
 			tSF.FEEDING_IN_PROGRESS = false
-			DELETED = false
+			DELETED = false]]
 		end
 	end
 	-- Slime feeding quest in Runewick END
