ea2826c33ee14c1b8d0093495b8828aec66ff0be
Caldarion <garinger@gmx.de>
2014-02-26 13:57:42 +0100

    allow to start sewing via using static tool

---
+++ item/id_102_tailoringtable.lua
@@ -0,0 +1,30 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+-- UPDATE common SET com_script='item.id_102_tailoringtable' WHERE com_itemid IN (102,103);
+
+require("content.craft.tailoring")
+require("base.licence")
+
+module("item.id_102_tailoringtable", package.seeall)
+
+function UseItem(User, SourceItem, ltstate)
+	if base.licence.licence(User) then --checks if user is citizen or has a licence
+		return -- avoids crafting if user is neither citizen nor has a licence
+	end
+
+    content.craft.tailoring.tailoring:showDialog(User, SourceItem)
+end
