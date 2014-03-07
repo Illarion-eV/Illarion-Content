b152065a819d8480481accbd2ac467ea5ac829d6
Caldarion <garinger@gmx.de>
2014-03-07 16:50:52 +0100

    * allow start fishing via using the shoal

---
+++ content/gatheringcraft/fishing.lua
@@ -0,0 +1,147 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+-- shoal in water ( 1170 )
+
+-- additional tool: fishingrod ( 72 )
+
+require("base.common")
+require("content.gathering")
+require("scheduled.placeShoal")
+
+module("content.gatheringcraft.fishing", package.seeall)
+
+function StartGathering(User, SourceItem, ltstate)
+
+	content.gathering.InitGathering();
+	local fishing = content.gathering.fishing;
+
+	base.common.ResetInterruption( User, ltstate );
+	if ( ltstate == Action.abort ) then -- work interrupted
+		if (User:increaseAttrib("sex",0) == 0) then
+			gText = "seine";
+			eText = "his";
+		else
+			gText = "ihre";
+			eText = "her";
+		end
+		User:talk(Character.say, "#me unterbricht "..gText.." Arbeit.", "#me interrupts "..eText.." work.")
+		return
+	end
+
+	if not base.common.CheckItem( User, SourceItem ) then -- security check
+		return
+	end
+
+	-- additional tool item is needed
+	if (User:countItemAt("all",72)==0) then
+		base.common.HighInformNLS( User,
+		"Du brauchst eine Angel um zu fischen.",
+		"You need a fishing rod for catching fish." );
+		return
+	end
+	local toolItem = User:getItemAt(5);
+	if ( toolItem.id ~= 72 ) then
+		toolItem = User:getItemAt(6);
+		if ( toolItem.id ~= 72 ) then
+			base.common.HighInformNLS( User,
+			"Du musst die Angel in der Hand haben!",
+			"You have to hold the fishing rod in your hand!" );
+			return
+		end
+	end
+
+	if not base.common.FitForWork( User ) then -- check minimal food points
+		return
+	end
+
+	if not base.common.IsLookingAt( User, SourceItem.pos ) then -- check looking direction
+		base.common.TurnTo( User, SourceItem.pos ); -- turn if necessary
+	end
+
+	-- since we're here, we're working
+	-- check the amount
+	local MaxAmount = 20
+	local changeItem = false;
+	local amountStr = SourceItem:getData("amount");
+	local amount = 0;
+	if ( amountStr ~= "" ) then
+		amount = tonumber(amountStr);
+	elseif ( SourceItem.wear == 255 ) then
+		amount = MaxAmount;
+	end
+
+	if ( ltstate == Action.none ) then -- currently not working -> let's go
+		fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User,nil);
+		User:startAction( fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
+		User:talk(Character.say, "#me beginnt zu fischen.", "#me starts to fish.")
+		return
+	end
+
+	if fishing:FindRandomItem(User) then
+		return
+	end
+
+
+	-----------
+	User:learn( fishing.LeadSkill, fishing.SavedWorkTime[User.id], fishing.LearnLimit);
+	local fished = 1; -- set the amount of items that are produced
+	local fishID = 0;
+	local chance = math.random(1,10);
+	-- 40% for salmon, 60% for trout
+	if (chance <= 4) then
+		fishID = 73; -- salmon
+	else
+		fishID = 355; -- trout;
+	end
+
+	-- GFX + Sound for a splash
+	world:gfx(11,SourceItem.pos);
+	world:makeSound(9,SourceItem.pos);
+
+	amount = amount - 1
+	SourceItem:setData("amount",amount)
+	world:changeItem(SourceItem)
+
+	local notCreated = User:createItem( fishID, fished, 333, nil ); -- create the new produced items
+	if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
+		world:createItemFromId( fishID, notCreated, User.pos, true, 333, nil );
+		base.common.HighInformNLS(User,
+		"Du kannst nichts mehr halten und der Rest fällt zu Boden.",
+		"You can't carry any more and the rest drops to the ground.");
+	else -- character can still carry something
+		if amount > 0 then  -- there are still items we can work on
+			fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User,nil);
+			User:changeSource(SourceItem);
+			User:startAction( fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
+		end
+	end
+	if amount == 0 then
+		table.insert(scheduled.placeShoal.shoalPositions,{counter = Random.uniform(15,20),shoalPosition = SourceItem.pos})
+		world:erase(SourceItem,1)
+		User:inform("Du scheinst hier alles leergefischt zu haben.",
+			        "You seem to have caught all the fish here.",Player.highPriority)
+		return
+	end
+
+	if base.common.GatheringToolBreaks( User, toolItem ) then -- damage and possibly break the tool
+		base.common.HighInformNLS(User,
+		"Deine alte Angel zerbricht.",
+		"Your old fishing rod breaks.");
+		return
+	end
+end
--- item/id_72_fishingrod.lua
+++ item/id_72_fishingrod.lua
@@ -16,13 +16,13 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 -- UPDATE common SET com_script='item.id_72_fishingrod' WHERE com_itemid=72;
 
-require("item.general.wood")
 require("base.common")
-require("content.gathering")
-require("scheduled.placeShoal")
+require("content.gatheringcraft.fishing")
+require("item.general.wood")
 
 module("item.id_72_fishingrod", package.seeall)
 
+LookAtItem = item.general.wood.LookAtItem
 
 function getWaterTilePosition(User)
 	local targetPos = base.common.GetFrontPosition(User);
@@ -64,41 +64,9 @@ function getShoal(User)
 	return nil;
 end
 
-LookAtItem = item.general.wood.LookAtItem
+
 
 function UseItem(User, SourceItem, ltstate)
-	content.gathering.InitGathering();
-	local fishing = content.gathering.fishing;
-
-	local TargetPos = base.common.GetFrontPosition(User);
-
-	base.common.ResetInterruption( User, ltstate );
-	if ( ltstate == Action.abort ) then -- work interrupted
-		if (User:increaseAttrib("sex",0) == 0) then
-			gText = "seine";
-			eText = "his";
-		else
-			gText = "ihre";
-			eText = "her";
-		end
-		User:talk(Character.say, "#me unterbricht "..gText.." Arbeit.", "#me interrupts "..eText.." work.")
-		return
-	end
-
-	if not base.common.CheckItem( User, SourceItem ) then -- security check
-		return
-	end
-
-	if (SourceItem:getType() ~= 4) then -- tool in Hand
-		base.common.HighInformNLS( User,
-		"Du musst die Angel in der Hand haben!",
-		"You have to hold the fishing rod in your hand!" );
-		return
-	end
-
-	if not base.common.FitForWork( User ) then -- check minimal food points
-		return
-	end
 
 	if (getWaterTilePosition(User) == nil) then -- fishing only possible on water tiles
 		base.common.HighInformNLS(User,
@@ -107,94 +75,20 @@ function UseItem(User, SourceItem, ltstate)
 		return
 	end
 
-	if (TargetPos.z < 0) then -- fishing underground is not possible
+	if (base.common.GetFrontPosition(User).z < 0) then -- fishing underground is not possible
 		base.common.HighInformNLS(User,
 		"In unterirdischen Wasserlöchern wird das Angeln kaum erfolgreich sein.",
 		"Fishing in underground waterholes wouldn't be successful.");
 		return
 	end
 
-	local shoal = getShoal(User);
-	if (shoal == nil) then
+	local shoalItem = getShoal(User);
+	if (shoalItem == nil) then
 		base.common.HighInformNLS(User,
 		"Hier scheinen sich keine Fische zu befinden. Halte Ausschau nach einem Fischschwarm.",
 		"There seems to be no fish here. Look for a shoal.");
 		return
 	end
 
-	if not base.common.IsLookingAt( User, shoal.pos ) then -- check looking direction
-		base.common.TurnTo( User, shoal.pos ); -- turn if necessary
-	end
-
-	-- since we're here, we're working
-	-- check the amount
-	local MaxAmount = 20
-	local changeItem = false;
-	local amountStr = shoal:getData("amount");
-	local amount = 0;
-	if ( amountStr ~= "" ) then
-		amount = tonumber(amountStr);
-	elseif ( shoal.wear == 255 ) then
-		amount = MaxAmount;
-	end
-
-	if ( ltstate == Action.none ) then -- currently not working -> let's go
-		fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User,nil);
-		User:startAction( fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
-		User:talk(Character.say, "#me beginnt zu fischen.", "#me starts to fish.")
-		return
-	end
-
-	if fishing:FindRandomItem(User) then
-		return
-	end
-
-
-	-----------
-	User:learn( fishing.LeadSkill, fishing.SavedWorkTime[User.id], fishing.LearnLimit);
-	local fished = 1; -- set the amount of items that are produced
-	local fishID = 0;
-	local chance = math.random(1,10);
-	-- 40% for salmon, 60% for trout
-	if (chance <= 4) then
-		fishID = 73; -- salmon
-	else
-		fishID = 355; -- trout;
-	end
-
-	-- GFX + Sound for a splash
-	world:gfx(11,TargetPos);
-	world:makeSound(9,TargetPos);
-
-	local notCreated = User:createItem( fishID, fished, 333, nil ); -- create the new produced items
-	amount = amount - 1
-	if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
-		world:createItemFromId( fishID, notCreated, User.pos, true, 333, nil );
-		base.common.HighInformNLS(User,
-		"Du kannst nichts mehr halten und der Rest fällt zu Boden.",
-		"You can't carry any more and the rest drops to the ground.");
-	else -- character can still carry something
-		if amount > 0 then  -- there are still items we can work on
-			fishing.SavedWorkTime[User.id] = fishing:GenWorkTime(User,nil);
-			User:changeSource(SourceItem);
-			User:startAction( fishing.SavedWorkTime[User.id], 0, 0, 0, 0);
-		end
-	end
-	if amount == 0 then
-		table.insert(scheduled.placeShoal.shoalPositions,{counter = Random.uniform(15,20),shoalPosition = shoal.pos})
-		world:erase(shoal,1)
-		User:inform("Du scheinst hier alles leergefischt zu haben.",
-			        "You seem to have caught all the fish here.",Player.highPriority)
-		return
-	else
-		shoal:setData("amount",amount)
-		world:changeItem(shoal)
-	end
-
-	if base.common.GatheringToolBreaks( User, SourceItem ) then -- damage and possibly break the tool
-		base.common.HighInformNLS(User,
-		"Deine alte Angel zerbricht.",
-		"Your old fishing rod breaks.");
-		return
-	end
+	content.gatheringcraft.fishing.StartGathering(User, shoalItem, ltstate);
 end
