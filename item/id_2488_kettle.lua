9db1b7a7851466a1deef9058c5b805350aeb705d
Caldarion <garinger@gmx.de>
2014-03-07 21:05:42 +0100

    allow to start cooking, via using the kettle

---
+++ item/id_2488_kettle.lua
@@ -0,0 +1,30 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+-- UPDATE common SET com_script='item.id_2488_kettle' WHERE com_itemid IN (2488);
+
+require("content.craft.cooking")
+require("base.licence")
+
+module("item.id_2488_kettle", package.seeall)
+
+function UseItem(User, SourceItem, ltstate)
+	if base.licence.licence(User) then --checks if user is citizen or has a licence
+		return -- avoids crafting if user is neither citizen nor has a licence
+	end
+
+    content.craft.cooking.cooking:showDialog(User, SourceItem)
+end
