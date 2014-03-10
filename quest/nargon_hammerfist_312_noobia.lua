4991beed81751e74e6800fdf2f817a5667c1aa02
estralis <estralis@illarion.org>
2014-03-10 15:39:45 +0100

    Add quest logs for Noobia

---
+++ quest/nargon_hammerfist_312_noobia.lua
@@ -0,0 +1,74 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>. 
+]]
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (312, 'nargon_hammerfist_312_noobia');
+
+require("base.common")
+module("quest.nargon_hammerfist_312_noobia", package.seeall)
+
+GERMAN = Player.german
+ENGLISH = Player.english
+
+-- Insert the quest title here, in both languages
+Title = {}
+Title[GERMAN] = "Einführung - Sammeln"
+Title[ENGLISH] = "Tutorial - Gathering"
+
+-- Insert an extensive description of each status here, in both languages
+-- Make sure that the player knows exactly where to go and what to do
+Description = {}
+Description[GERMAN] = {}
+Description[ENGLISH] = {}
+Description[GERMAN][1] = "An dieser Station lernst du, wie man Rohstoffe abbaut. Der Augenmerk liegt auf Bergbau aber im Prinzip laufen alle Handwerke nach dem selben Schema ab. Nun, wer ist wohl der Richtige, um dir zu zeigen, wie man nach Gold gräbt? Natürlich ein Zwerg wie Nargon Hammerfist! Rohstoffe werden durch das Benutzen eines geeigneten Werkzeuges in der Nähe einer Ressourcenquelle gesammelt."
+Description[ENGLISH][1] = "At this station, you will learn how to gather resources. This station will focus on mining, though similar principles are applicable to the other crafts. Now, who might be the best to teach you how to mine for gold? Why, a dwarf like Nargon Hammerfist of course. You can gather resources by using a suitable tool while standing close to a material source."
+Description[GERMAN][2] = "Arr, mein Name ist Nargon, Nargon Hammerfist. Wilste 'nen Taler? Harr. Nur mit harter Arbeit kannste reich werden. Geh in die Mine hinter mir und sammel fünf Brocken Kohle mit deiner Spitzhacke. Um zu schürfen, stell dich vor einen Stein und benutze die in der Hand gehaltene Spitzhacke."
+Description[ENGLISH][2] = "Arr, the name is Nargon, Nargon Hammerfist. Ye want coin? Harr. Only through hard labour can ye become wealthy. Head into the mine behind me and gather five lumps of coal with your pick axe. To mine, stand in front of a rock and use the pick-axe which must be held in your hands."
+Description[GERMAN][3] = "Du hast die Einführung über das Sammeln abgeschlossen."
+Description[ENGLISH][3] = "You finished the tutorial on gathering."
+
+-- Insert the position of the quest start here (probably the position of an NPC or item)
+Start = {42, 55, 100}
+
+-- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
+QuestTarget = {}
+QuestTarget[2] = {position(43, 49, 100), position(50, 50, 100)}
+
+-- Insert the quest status which is reached at the end of the quest
+FINAL_QUEST_STATUS = 3
+
+
+function QuestTitle(user)
+    return base.common.GetNLS(user, Title[GERMAN], Title[ENGLISH])
+end
+
+function QuestDescription(user, status)
+    local german = Description[GERMAN][status] or ""
+    local english = Description[ENGLISH][status] or ""
+
+    return base.common.GetNLS(user, german, english)
+end
+
+function QuestStart()
+    return Start
+end
+
+function QuestTargets(user, status)
+    return QuestTarget[status]
+end
+
+function QuestFinalStatus()
+    return FINAL_QUEST_STATUS
+end
---
+++ quest/nimbur_goldhand_313_noobia.lua
@@ -0,0 +1,74 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>. 
+]]
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (313, 'nimbur_goldhand_313_noobia');
+
+require("base.common")
+module("quest.nimbur_goldhand_313_noobia", package.seeall)
+
+GERMAN = Player.german
+ENGLISH = Player.english
+
+-- Insert the quest title here, in both languages
+Title = {}
+Title[GERMAN] = "Einführung - Handwerk"
+Title[ENGLISH] = "Tutorial - Crafting"
+
+-- Insert an extensive description of each status here, in both languages
+-- Make sure that the player knows exactly where to go and what to do
+Description = {}
+Description[GERMAN] = {}
+Description[ENGLISH] = {}
+Description[GERMAN][1] = "An dieser Station wird dir beigebracht, wie man einem Handwerk nachgeht. Nimbur Goldhand ist ein sehr begabter Artisan, der die nötigen Kenntnisse vermittelt, die du benötigst, um die Laufbahn des Handwerkers einzuschlagen. Handwerke erfordern ebenso wie das Sammeln von Ressourcen ein Handwerkzeug und einen Arbeitsplatz sowie die nötigen Rohstoffe."
+Description[ENGLISH][1] = "At this station, you will learn how to craft items. Nimbur Goldhand is a very skilled artisan who can teach you the necessary steps to get started with a crafting profession. Crafting requires a hand tool, just like gathering, and a static tool. You also need the necessary resources."
+Description[GERMAN][2] = "Arr, richtig. Hör zu! Ich werd' dir alles, was ich weiß, über's Schmieden beibringen. Hier, nimm diesen Hammer und diesen Barren und geh rein und komm nicht eher raus, bis du fünfzehn Nägel in deiner Tasche hast! Um zu schmieden, stell dich vor einen Amboss und benutze den in der Hand gehaltenen Hammer. Wähle dann ein Produkt aus dem Menü, um es zu fertigen."
+Description[ENGLISH][2] = "Arr... right. Listen up! I tell ye everything about blacksmithing ye need to know. Here, take this hammer and these ingots. Step inside and don't dare come out without fifteen pins in yer bag! Blacksmithing requires you to stand in front of an anvil and use a hammer, which must be held in your hands. Then, choose the pins from the menu to start working on it."
+Description[GERMAN][3] = "Du hast die Einführung über das Handwerk abgeschlossen."
+Description[ENGLISH][3] = "You finished the tutorial on crafting."
+
+-- Insert the position of the quest start here (probably the position of an NPC or item)
+Start = {42, 83, 100}
+
+-- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
+QuestTarget = {}
+QuestTarget[2] = {position(47, 81, 100)}
+
+-- Insert the quest status which is reached at the end of the quest
+FINAL_QUEST_STATUS = 3
+
+
+function QuestTitle(user)
+    return base.common.GetNLS(user, Title[GERMAN], Title[ENGLISH])
+end
+
+function QuestDescription(user, status)
+    local german = Description[GERMAN][status] or ""
+    local english = Description[ENGLISH][status] or ""
+
+    return base.common.GetNLS(user, german, english)
+end
+
+function QuestStart()
+    return Start
+end
+
+function QuestTargets(user, status)
+    return QuestTarget[status]
+end
+
+function QuestFinalStatus()
+    return FINAL_QUEST_STATUS
+end
