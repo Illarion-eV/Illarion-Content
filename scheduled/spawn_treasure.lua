b389714dfdbeaa8d572f9fa390843a6f55a5485b
estralis <estralis@illarion.org>
2014-03-29 15:48:19 +0100

    Spawn treasure chests instead of items, fix of coordinates.

--- scheduled/spawn_treasure.lua
+++ scheduled/spawn_treasure.lua
@@ -23,11 +23,11 @@ module("scheduled.spawn_treasure", package.seeall)
 
 function spawnTreasure()
 
-	treasurePos=position(703,422,-3); --Salavesh dungeon
+	treasurePos=position(703,421,-3); --Salavesh dungeon
 	
     if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around and there is no item on the tile
 	
-		world:createItemFromId(2830,1,treasurePos,false,333,{trsCat=math.random(0,4)}); --spawn the chest only if the tile is empty
+		world:createItemFromId(2830,1,treasurePos,false,333,{trsCat=tostring(math.random(0,4))}); --spawn the chest only if the tile is empty
 		
 	end
 	
