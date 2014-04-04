a717922a5319dce622db363c5bec46018e86aa4f
estralis <estralis@illarion.org>
2014-04-04 16:57:40 +0200

    Send "your tool will break soon" more often.

--- base/common.lua
+++ base/common.lua
@@ -442,7 +442,7 @@ function GatheringToolBreaks(user, item)
     item.quality = quality * 100 + durability;
     world:changeItem(item);
     
-    if (durability == 10) then 
+    if (durability < 10) then 
       InformNLS(user,
       "Das Werkzeug wird nicht mehr lange halten. Du solltest dich nach einem neuen umschauen.",
       "The tool looks like it could break soon. You should try to get a new one.");
