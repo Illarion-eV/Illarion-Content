b13c2a9d66ee047c8f92a52338ef45eea8ed6ba0
estralis <estralis@illarion.org>
2014-04-12 13:41:31 +0200

    Be less tolerant while placing treasures.

--- scheduled/spawn_treasure.lua
+++ scheduled/spawn_treasure.lua
@@ -26,32 +26,32 @@ function spawnTreasure()
 
 	treasurePos=position(703,421,-3); --Salavesh dungeon
 	
-    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around and there is no item on the tile
+    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 then --only spawn a treasure if nobody is around
 	
-		world:createItemFromId(2830,1,treasurePos,false,333,{trsCat=math.random(0,4)}); --spawn the chest only if the tile is empty
+		world:createItemFromId(2830,1,treasurePos,true,333,{trsCat=math.random(0,4)}); --spawn the chest
 		
 	end
 	
 	treasurePos=position(161,666,-4); --Sir Reginald's tomb
 	
-    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around and there is no item on the tile
+    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around
 	
-		world:createItemFromId(2830,1,treasurePos,false,333,{trsCat=math.random(0,1)}); --spawn the chest only if the tile is empty
+		world:createItemFromId(2830,1,treasurePos,true,333,{trsCat=math.random(0,1)}); --spawn the chest
 		
 	end
 		treasurePos=position(769,705,0); --A cave in the woods
 	
-    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around and there is no item on the tile
+    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around
 	
-		world:createItemFromId(2830,1,treasurePos,false,333,{trsCat=math.random(0,1)}); --spawn the chest only if the tile is empty
+		world:createItemFromId(2830,1,treasurePos,true,333,{trsCat=math.random(0,1)}); --spawn the chest
 		
 	end
 	
 	treasurePos=position(312,374,-6); --Galmair sewers
 	
-    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around and there is no item on the tile
+    if table.getn(world:getPlayersInRangeOf(treasurePos,20)) == 0 and world:isItemOnField(treasurePos) == false then --only spawn a treasure if nobody is around
 	
-		world:createItemFromId(2830,1,treasurePos,false,333,{trsCat=math.random(0,1)}); --spawn the chest only if the tile is empty
+		world:createItemFromId(2830,1,treasurePos,true,333,{trsCat=math.random(0,1)}); --spawn the chess
 		
 	end
 	
