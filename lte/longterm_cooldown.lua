7927b58bbe8124d1046263ea8bd643435bdac7d2
caldarion <garinger@gmx.de>
2014-05-08 00:09:53 +0200

    finalize new npc

--- lte/longterm_cooldown.lua
+++ lte/longterm_cooldown.lua
@@ -12,7 +12,7 @@ PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
 details.
 
 You should have received a copy of the GNU Affero General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>. 
+with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 -- Longterm Cooldown
 -- by Estralis
@@ -35,18 +35,18 @@ function callEffect( Effect, Char ) -- Effect is called
 
 		--Addition by Envi: Quest 680 (Evil Rock Reward)
 	    theQuestStatus=Char:getQuestProgress(680);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(680,theQuestStatus-1); --cooling!
 			if theQuestStatus == 1 then --only for this quest. the player should know when he can do it again.
-				base.common.InformNLS(Char,"[Info] Die Zeit scheint reif, um dein Glück auf der Insel 'Böser Fels' erneut zu versuchen.","[Info] It seems it is time to try your luck again on the island 'Evilrock'."); 
+				base.common.InformNLS(Char,"[Info] Die Zeit scheint reif, um dein Glück auf der Insel 'Böser Fels' erneut zu versuchen.","[Info] It seems it is time to try your luck again on the island 'Evilrock'.");
 			end
 		end
 	    --Addition end
 
 		--Addition by Envi: Quest 665 (Evil Rock Vision)
 	    theQuestStatus=Char:getQuestProgress(665);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(665,theQuestStatus-1); --cooling!
 		end
@@ -54,7 +54,7 @@ function callEffect( Effect, Char ) -- Effect is called
 
 		--Addition by Envi: Quest 673 (Feed Galmair's bear)
 	    theQuestStatus=Char:getQuestProgress(673);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(673,theQuestStatus-1); --cooling!
 		end
@@ -63,44 +63,44 @@ function callEffect( Effect, Char ) -- Effect is called
 
 		--Addition by Envi: Quest 811 (Licence Cadomyr)
 	    theQuestStatus=Char:getQuestProgress(811);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(811,theQuestStatus-1); --cooling!
 			if theQuestStatus == 1 then --only for this quest. the player should know when he can do it again.
-				base.common.InformNLS(Char,"Deine Lizenz für die Benutzung der Geräte von Cadomyr ist abgelaufen. Du kannst sie im Zensusbüro von Cadomyr erneuern.","Your licence for Cadomyr's static tools has expired. You can get a new one at the census office of Cadomyr."); 
+				base.common.InformNLS(Char,"Deine Lizenz für die Benutzung der Geräte von Cadomyr ist abgelaufen. Du kannst sie im Zensusbüro von Cadomyr erneuern.","Your licence for Cadomyr's static tools has expired. You can get a new one at the census office of Cadomyr.");
 			end
 		end
 	    --Addition end
 
 		--Addition by Envi: Quest 812 (Licence Runewick)
 	    theQuestStatus=Char:getQuestProgress(812);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(812,theQuestStatus-1); --cooling!
 			if theQuestStatus == 1 then --only for this quest. the player should know when he can do it again.
-				base.common.InformNLS(Char,"Deine Lizenz für die Benutzung der Geräte von Runewick ist abgelaufen. Du kannst sie im Zensusbüro von Runewick erneuern.","Your licence for Runewick's static tools has expired. You can get a new one at the census office of Runewick."); 
+				base.common.InformNLS(Char,"Deine Lizenz für die Benutzung der Geräte von Runewick ist abgelaufen. Du kannst sie im Zensusbüro von Runewick erneuern.","Your licence for Runewick's static tools has expired. You can get a new one at the census office of Runewick.");
 			end
 		end
 	    --Addition end
 
 		--Addition by Envi: Quest 811 (Licence Galmair)
 	    theQuestStatus=Char:getQuestProgress(813);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(813,theQuestStatus-1); --cooling!
 			if theQuestStatus == 1 then --only for this quest. the player should know when he can do it again.
-				base.common.InformNLS(Char,"Deine Lizenz für die Benutzung der Geräte von Galmair ist abgelaufen. Du kannst sie im Zensusbüro von Galmair erneuern.","Your licence for Galmair's static tools has expired. You can get a new one at the census office of Galmair."); 
+				base.common.InformNLS(Char,"Deine Lizenz für die Benutzung der Geräte von Galmair ist abgelaufen. Du kannst sie im Zensusbüro von Galmair erneuern.","Your licence for Galmair's static tools has expired. You can get a new one at the census office of Galmair.");
 			end
 		end
 	    --Addition end
 
 		--Addition by Envi: Quest 613 (Paula Bronzeblatt)
 	    theQuestStatus=Char:getQuestProgress(613);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(613,theQuestStatus-1); --cooling!
 			if theQuestStatus == 1 then --only for this quest. the player should know when he can do it again.
-				base.common.InformNLS(Char,"Eine Taube wirft ein Pergamentstück neben dir ab. Du liest: 'Ich sollte morgen wieder eine Schatzkarte für dich haben. -Paula Bronzeblatt'","A dove drops a parchment next to you. You read: 'I should have another treasure map for you tomorrow. -Paula Bronzeblatt'"); 
+				base.common.InformNLS(Char,"Eine Taube wirft ein Pergamentstück neben dir ab. Du liest: 'Ich sollte morgen wieder eine Schatzkarte für dich haben. -Paula Bronzeblatt'","A dove drops a parchment next to you. You read: 'I should have another treasure map for you tomorrow. -Paula Bronzeblatt'");
 			end
 		end
 	    --Addition end
@@ -108,11 +108,11 @@ function callEffect( Effect, Char ) -- Effect is called
 
 		--Addition by Envi: Quest 614 (Arno Unterland)
 	    theQuestStatus=Char:getQuestProgress(614);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(614,theQuestStatus-1); --cooling!
 			if theQuestStatus == 1 then --only for this quest. the player should know when he can do it again.
-				base.common.InformNLS(Char,"Eine Taube wirft ein Pergamentstück neben dir ab. Du liest: 'Morgen können wir wieder etwas drehen. Kommt vorbei! -Arno Unterland'","A dove drops a parchment next to you. You read: 'Tomorrow, we can do it again. Visit me! -Arno Unterland'"); 
+				base.common.InformNLS(Char,"Eine Taube wirft ein Pergamentstück neben dir ab. Du liest: 'Morgen können wir wieder etwas drehen. Kommt vorbei! -Arno Unterland'","A dove drops a parchment next to you. You read: 'Tomorrow, we can do it again. Visit me! -Arno Unterland'");
 			end
 		end
 	    --Addition end
@@ -120,7 +120,7 @@ function callEffect( Effect, Char ) -- Effect is called
 
 		--Addition by Envi: Quest 661 (Elstree Forest)
 	    theQuestStatus=Char:getQuestProgress(661);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(661,theQuestStatus-1); --cooling!
 		end
@@ -128,7 +128,7 @@ function callEffect( Effect, Char ) -- Effect is called
 
 		--Addition by Envi: Quest 660 (Galmair bridges)
 	    theQuestStatus=Char:getQuestProgress(660);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(660,theQuestStatus-1); --cooling!
 		end
@@ -136,139 +136,147 @@ function callEffect( Effect, Char ) -- Effect is called
 
 		--Addition by Estralis: Quest 119/120 (Cadomyr daily missions)
 	    theQuestStatus=Char:getQuestProgress(120);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(120,theQuestStatus-1); --cooling!
 		end
 	    --Addition end
-		
+
 		--Addition by Estralis: Quest 152/153 (Galmair daily missions)
 	    theQuestStatus=Char:getQuestProgress(153);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(153,theQuestStatus-1); --cooling!
 		end
 	    --Addition end
-		
+
 		--Addition by Estralis: Quest 160/161 (Runewick daily missions)
 	    theQuestStatus=Char:getQuestProgress(161);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(161,theQuestStatus-1); --cooling!
 		end
 	    --Addition end
-		
+
 		--Addition by Merung: Quest 162 (Runewick daily missions II)
 	    theQuestStatus=Char:getQuestProgress(165);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(165,theQuestStatus-1); --cooling!
 		end
 	    --Addition end
-		
+
 		--Addition by Estralis: Quest 127/128/129 Caravan of Honour (Phillip Molitor) Cooldown
 	    theQuestStatus=Char:getQuestProgress(129);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(129,theQuestStatus-1); --cooling!
 		end
 		--Addition end
-	
+
 		--Addition by Estralis: Quest 162/163/164 Glorious Bounty (Palis Nestros) Cooldown
 	    theQuestStatus=Char:getQuestProgress(164);
-	
+
 		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
 			Char:setQuestProgress(164,theQuestStatus-1); --cooling!
 		end
 		--Addition end
-		
+
+		--Addition by Caldarion: Quest 181/182/183 Spy Informant (Bula Glasha) Cooldown
+	    theQuestStatus=Char:getQuestProgress(183);
+
+		if theQuestStatus > 0 then --Is there a cooldown? Will only be reduced if the player isn't AFK/idle
+			Char:setQuestProgress(183,theQuestStatus-1); --cooling!
+		end
+		--Addition end
+
 	end --all above is only conducted for players that aren't afk for more than five minutes
 
 	--Addition by Estralis: Quest 127/128/129 Caravan of Honour (Phillip Molitor) Countdown
 	theQuestStatus=Char:getQuestProgress(128);
-	
+
 	if theQuestStatus == 1 then --Time over!
-	
+
         base.common.InformNLS(Char,"[Auftragssstatus] Du hast es nicht geschafft, den Auftrag von Philipp Molitor zu erledigen.","[Order status] You failed to fulfil the order of Philipp Molitor."); -- Feedback!
 		Char:setQuestProgress(127,0);
-		
+
 	end
 
 	if theQuestStatus == 3 then --Ten minutes left!
-	
+
         base.common.InformNLS(Char,"[Auftragssstatus] Es verbleiben dir noch zehn Minuten, um den Auftrag von Philipp Molitor zu erledigen.","[Order status] You have ten minutes left to fulfil the order of Philipp Molitor."); -- Feedback!
-		
+
 	end
-	
+
 	if theQuestStatus == 13 then --One hour left
-	
+
 	    base.common.InformNLS(Char,"[Auftragssstatus] Es verbleibt dir noch eine Stunde, um den Auftrag von Philipp Molitor zu erledigen.","[Order status] You have one hour left to fulfil the order of Philipp Molitor."); -- Feedback!
-	
+
 	end
-	
+
 	if theQuestStatus > 0 then --Is there a countdown? Will be reduced even if the player is AFK/idle
 		Char:setQuestProgress(128,theQuestStatus-1); --counting down!
 	end
 	--Addition end
-	
+
 	--Addition by Estralis: Quest 162/163/164 Glorious Bounty (Palis Nestros) Cooldown
 	theQuestStatus=Char:getQuestProgress(163);
-	
+
 	if theQuestStatus == 1 then --Time over!
-	
+
         base.common.InformNLS(Char,"[Kopfgeldstatus] Das Kopfgeld von Palis Nestros kannst du dir abschminken.","[Bounty status] You can kiss the bounty of Palis Nestros goodbye."); -- Feedback!
 		Char:setQuestProgress(162,0);
-		
+
 	end
 
 	if theQuestStatus == 3 then --Ten minutes left!
-	
+
         base.common.InformNLS(Char,"[Kopfgeldstatus] In zehn Minuten läuft dein Kopfgeldvertrag mit Palis Nestros aus.","[Bounty status] In ten minutes, your bounty contract with Palis Nestros will expire."); -- Feedback!
-		
+
 	end
-	
+
 	if theQuestStatus == 13 then --One hour left
-	
+
 	    base.common.InformNLS(Char,"[Kopfgeldstatus] Eine Stunde verbleibt dir, um das Kopfgeld von Palis Nestros zu verdienen.","[Bounty status] One hour left for earning the bounty of Palis Nestros."); -- Feedback!
-	
+
 	end
-	
+
 	if theQuestStatus > 0 then --Is there a countdown? Will be reduced even if the player is AFK/idle
 		Char:setQuestProgress(163,theQuestStatus-1); --counting down!
 	end
 	--Addition end
-	
-		--Addition by Caldarion: Quest 181/182/183 Spy Informant (Bula Glasha) Cooldown
+
+	--Addition by Caldarion: Quest 181/182/183 Spy Informant (Bula Glasha) Cooldown
 	theQuestStatus=Char:getQuestProgress(182);
-	
+
 	if theQuestStatus == 1 then --Time over!
-	
-        base.common.InformNLS(Char,"[Information Status] Sie waren zu langsam Sammeldaten, können Sie Ihre Gebühr verloren.","[Information status] You were too slow collecting data, you have lost your fee."); -- Feedback!
+
+        base.common.InformNLS(Char,"[Informanten Status] Du warst zu langsam um die Informationen zu beschaffen, du bekommst keine Belohnung.","[Information status] You were too slow collecting data, you have lost your fee."); -- Feedback!
 		Char:setQuestProgress(181,0);
-		
+
 	end
 
 	if theQuestStatus == 3 then --Ten minutes left!
-	
-        base.common.InformNLS(Char,"[Information status] In zehn Minuten ist Ihre Chance, um Informationen zu sammeln für Bula Glasha vorbei sein.","[Information status] In ten minutes, your chance to collect information for Bula Glasha will be over."); -- Feedback!
-		
+
+        base.common.InformNLS(Char,"[Informanten Status] In zehn Minuten ist deine Chance vestrichen, um Informationen für Bula Glasha zu sammeln.","[Information status] In ten minutes, your chance to collect information for Bula Glasha will be over."); -- Feedback!
+
 	end
-	
+
 	if theQuestStatus == 13 then --One hour left
-	
-	    base.common.InformNLS(Char,"[Information status] Eine Stunde für das Sammeln von links die Informationen Gebühr von Bula Glasha.","[Information status] One hour left for earning the information fee of Bula Glasha."); -- Feedback!
-	
+
+	    base.common.InformNLS(Char,"[Informanten Status] Eine Stunde verbleibt um Informationen für Bula Glasha zu sammeln.","[Information status] One hour left for earning the information fee of Bula Glasha."); -- Feedback!
+
 	end
-	
+
 	if theQuestStatus > 0 then --Is there a countdown? Will be reduced even if the player is AFK/idle
 		Char:setQuestProgress(182,theQuestStatus-1); --counting down!
 	end
 	--Addition end
-	
+
 	Effect.nextCalled = 3000; --Effect gets called each 5 minutes
 
 	return true; --No idea why!
-	
+
 end
 
 
--- monster/base/quests.lua
+++ monster/base/quests.lua
@@ -1286,7 +1286,7 @@ function iniQuests()
     maximumQueststatus[id]=132;
     questLocation[id]=position(852, 497, -6);
     radius[id]=200;
-	
+
 	--Quest 50: Spy Informant
     local id=50;
     germanTitle[id]="Spitzel Informant";
@@ -1301,10 +1301,10 @@ function iniQuests()
 	table.insert(questList[274],id); --All wasps
 	table.insert(questList[278],id); --All wasps
     minimumQueststatus[id]=1;
-    maximumQueststatus[id]=5;	
+    maximumQueststatus[id]=5;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 51: Spy Informant
     local id=51;
     germanTitle[id]="Spitzel Informant";
@@ -1319,10 +1319,10 @@ function iniQuests()
 	table.insert(questList[274],id); --All wasps
 	table.insert(questList[278],id); --All wasps
     minimumQueststatus[id]=6;
-    maximumQueststatus[id]=14;	
+    maximumQueststatus[id]=14;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 52: Spy Informant
     local id=52;
     germanTitle[id]="Spitzel Informant";
@@ -1337,10 +1337,10 @@ function iniQuests()
 	table.insert(questList[274],id); --All wasps
 	table.insert(questList[278],id); --All wasps
     minimumQueststatus[id]=15;
-    maximumQueststatus[id]=27;	
+    maximumQueststatus[id]=27;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 53: Spy Informant
     local id=53;
     germanTitle[id]="Spitzel Informant";
@@ -1363,10 +1363,10 @@ function iniQuests()
 	table.insert(questList[1052],id); -- all slimes
 	table.insert(questList[1053],id); -- all slimes
     minimumQueststatus[id]=28;
-    maximumQueststatus[id]=32;	
+    maximumQueststatus[id]=32;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 54: Spy Informant
     local id=54;
     germanTitle[id]="Spitzel Informant";
@@ -1389,10 +1389,10 @@ function iniQuests()
 	table.insert(questList[1052],id); -- all slimes
 	table.insert(questList[1053],id); -- all slimes
     minimumQueststatus[id]=33;
-    maximumQueststatus[id]=41;	
+    maximumQueststatus[id]=41;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 55: Spy Informant
     local id=55;
     germanTitle[id]="Spitzel Informant";
@@ -1415,10 +1415,10 @@ function iniQuests()
 	table.insert(questList[1052],id); -- all slimes
 	table.insert(questList[1053],id); -- all slimes
     minimumQueststatus[id]=42;
-    maximumQueststatus[id]=54;	
+    maximumQueststatus[id]=54;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 56: Spy Informant
     local id=56;
     germanTitle[id]="Spitzel Informant";
@@ -1437,10 +1437,10 @@ function iniQuests()
 	table.insert(questList[971],id); --All dogs
 	table.insert(questList[972],id); --All dogs
     minimumQueststatus[id]=55;
-    maximumQueststatus[id]=59;	
+    maximumQueststatus[id]=59;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 57: Spy Informant
     local id=57;
     germanTitle[id]="Spitzel Informant";
@@ -1459,10 +1459,10 @@ function iniQuests()
 	table.insert(questList[971],id); --All dogs
 	table.insert(questList[972],id); --All dogs
     minimumQueststatus[id]=60;
-    maximumQueststatus[id]=68;	
+    maximumQueststatus[id]=68;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 58: Spy Informant
     local id=58;
     germanTitle[id]="Spitzel Informant";
@@ -1481,10 +1481,10 @@ function iniQuests()
 	table.insert(questList[971],id); --All dogs
 	table.insert(questList[972],id); --All dogs
     minimumQueststatus[id]=69;
-    maximumQueststatus[id]=81;	
+    maximumQueststatus[id]=81;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 59: Spy Informant
     local id=59;
     germanTitle[id]="Spitzel Informant";
@@ -1500,10 +1500,10 @@ function iniQuests()
 	table.insert(questList[1021],id); --All foxes
 	table.insert(questList[1022],id); --All foxes
     minimumQueststatus[id]=82;
-    maximumQueststatus[id]=86;	
+    maximumQueststatus[id]=86;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 60: Spy Informant
     local id=60;
     germanTitle[id]="Spitzel Informant";
@@ -1519,10 +1519,10 @@ function iniQuests()
 	table.insert(questList[1021],id); --All foxes
 	table.insert(questList[1022],id); --All foxes
     minimumQueststatus[id]=87;
-    maximumQueststatus[id]=95;	
+    maximumQueststatus[id]=95;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 61: Spy Informant
     local id=61;
     germanTitle[id]="Spitzel Informant";
@@ -1538,178 +1538,178 @@ function iniQuests()
 	table.insert(questList[1021],id); --All foxes
 	table.insert(questList[1022],id); --All foxes
     minimumQueststatus[id]=96;
-    maximumQueststatus[id]=108;	
+    maximumQueststatus[id]=108;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 62: Spy Informant
     local id=62;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="ratten";
+    germanRace[id]="Ratten";
     englishRace[id]="rats";
-	table.insert(questList[921],id; -- all rats
+	table.insert(questList[921],id); -- all rats
 	table.insert(questList[922],id); --all rats
 	table.insert(questList[931],id); --all rats
 	table.insert(questList[932],id); --all rats
 	table.insert(questList[941],id); --all rats
 	table.insert(questList[942],id); --all rats
     minimumQueststatus[id]=109;
-    maximumQueststatus[id]=113;	
+    maximumQueststatus[id]=113;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 63: Spy Informant
     local id=63;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="ratten";
+    germanRace[id]="Ratten";
     englishRace[id]="rats";
-	table.insert(questList[921],id; -- all rats
+	table.insert(questList[921],id); -- all rats
 	table.insert(questList[922],id); --all rats
 	table.insert(questList[931],id); --all rats
 	table.insert(questList[932],id); --all rats
 	table.insert(questList[941],id); --all rats
 	table.insert(questList[942],id); --all rats
     minimumQueststatus[id]=114;
-    maximumQueststatus[id]=122;	
+    maximumQueststatus[id]=122;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 64: Spy Informant
     local id=64;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="ratten";
+    germanRace[id]="Ratten";
     englishRace[id]="rats";
-	table.insert(questList[921],id; -- all rats
+	table.insert(questList[921],id); -- all rats
 	table.insert(questList[922],id); --all rats
 	table.insert(questList[931],id); --all rats
 	table.insert(questList[932],id); --all rats
 	table.insert(questList[941],id); --all rats
 	table.insert(questList[942],id); --all rats
     minimumQueststatus[id]=123;
-    maximumQueststatus[id]=135;	
+    maximumQueststatus[id]=135;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 65: Spy Informant
     local id=65;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="Barin";
+    germanRace[id]="Bären";
     englishRace[id]="bears";
-	table.insert(questList[491],id; -- all bears
+	table.insert(questList[491],id); -- all bears
 	table.insert(questList[492],id); --all bears
 	table.insert(questList[501],id); --all bears
 	table.insert(questList[502],id); --all bears
 	table.insert(questList[511],id); --all bears
 	table.insert(questList[512],id); --all bears
     minimumQueststatus[id]=136;
-    maximumQueststatus[id]=140;	
+    maximumQueststatus[id]=140;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 66: Spy Informant
     local id=66;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="Barin";
+    germanRace[id]="Bären";
     englishRace[id]="bears";
-	table.insert(questList[491],id; -- all bears
+	table.insert(questList[491],id); -- all bears
 	table.insert(questList[492],id); --all bears
 	table.insert(questList[501],id); --all bears
 	table.insert(questList[502],id); --all bears
 	table.insert(questList[511],id); --all bears
 	table.insert(questList[512],id); --all bears
     minimumQueststatus[id]=141;
-    maximumQueststatus[id]=149;	
+    maximumQueststatus[id]=149;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 67: Spy Informant
     local id=67;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="Barin";
+    germanRace[id]="Bären";
     englishRace[id]="bears";
-	table.insert(questList[491],id; -- all bears
+	table.insert(questList[491],id); -- all bears
 	table.insert(questList[492],id); --all bears
 	table.insert(questList[501],id); --all bears
 	table.insert(questList[502],id); --all bears
 	table.insert(questList[511],id); --all bears
 	table.insert(questList[512],id); --all bears
     minimumQueststatus[id]=150;
-    maximumQueststatus[id]=162;	
+    maximumQueststatus[id]=162;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 68: Spy Informant
     local id=68;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="trolle";
+    germanRace[id]="Trolle";
     englishRace[id]="trolls";
-	table.insert(questList[91]],id; --all trolls
+	table.insert(questList[91],id); --all trolls
 	table.insert(questList[92],id); --all trolls
 	table.insert(questList[93],id); --all trolls
 	table.insert(questList[94],id); --all trolls
 	table.insert(questList[95],id); --all trolls
 	table.insert(questList[96],id); --all trolls
     minimumQueststatus[id]=163;
-    maximumQueststatus[id]=167;	
+    maximumQueststatus[id]=167;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 69: Spy Informant
     local id=69;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="trolle";
+    germanRace[id]="Trolle";
     englishRace[id]="trolls";
-	table.insert(questList[91]],id; --all trolls
+	table.insert(questList[91],id); --all trolls
 	table.insert(questList[92],id); --all trolls
 	table.insert(questList[93],id); --all trolls
 	table.insert(questList[94],id); --all trolls
 	table.insert(questList[95],id); --all trolls
 	table.insert(questList[96],id); --all trolls
     minimumQueststatus[id]=168;
-    maximumQueststatus[id]=176;	
+    maximumQueststatus[id]=176;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
-	
+
 	--Quest 70: Spy Informant
     local id=70;
     germanTitle[id]="Spitzel Informant";
     englishTitle[id]="Spy Informant";
     NPCName[id]="Bula Glasha";
     statusId[id]=181;
-    germanRace[id]="trolle";
+    germanRace[id]="Trolle";
     englishRace[id]="trolls";
-	table.insert(questList[91]],id; --all trolls
+	table.insert(questList[91],id); --all trolls
 	table.insert(questList[92],id); --all trolls
 	table.insert(questList[93],id); --all trolls
 	table.insert(questList[94],id); --all trolls
 	table.insert(questList[95],id); --all trolls
 	table.insert(questList[96],id); --all trolls
     minimumQueststatus[id]=177;
-    maximumQueststatus[id]=189;	
+    maximumQueststatus[id]=189;
 	questLocation[id]=position(514, 178, 0);
     radius[id]=250;
 
