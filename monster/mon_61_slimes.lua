66541d9e8bf2414e6a7e15c3664e74fa6cc0f616
caldarion <garinger@gmx.de>
2014-05-26 15:44:55 +0200

    fix a typo

--- monster/mon_61_slimes.lua
+++ monster/mon_61_slimes.lua
@@ -42,7 +42,7 @@ msgs:addMessage("#me richtet sich auf.", "#me rears up.");
 msgs:addMessage("#me hinterlässt eine schleimige Spur.", "#me leaves a mucilaginous trail behind.");
 msgs:addMessage("#me riecht nach Abfall.", "#me reeks of dump.");
 msgs:addMessage("#me spuckt Schleimbatzen.", "#me spits out lumps of slime.");
-msgs:addMessage("#mes Gestalt wandelt sich in einen Ball.", "#me's shape morphs int a ball.");
+msgs:addMessage("#mes Gestalt wandelt sich in einen Ball.", "#me's shape morphs into a ball.");
 msgs:addMessage("#me drückt sich flach auf den Boden.", "#me flattens itself plain to the ground.");
 end
 
