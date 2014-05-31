ac59373dfe7d178ba7b12288a9bcc04d93b7c5bf
caldarion <garinger@gmx.de>
2014-05-31 15:43:01 +0200

    rename npc files

--- quest/fokous_onyxbrow_189_galmair.lua
+++ quest/fokous_onyxbrow_189_galmair.lua
@@ -14,7 +14,7 @@ details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>. 
 ]]
--- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (187, 'quest.fokous_onyxbrow_189_galmair');
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (189, 'quest.fokous_onyxbrow_189_galmair');
 
 require("base.common")
 require("base.factions")
