0f25eac29252aebff5bc955044d1977bb896304f
EviePryler <eviepryler@gmail.com>
2014-06-08 02:00:27 -0500

    Attempt to give Human monsters hair and beards

--- monster/mon_0_humans.lua
+++ monster/mon_0_humans.lua
@@ -25,7 +25,37 @@ require("base.arena")
 require("base.messages");
 module("monster.mon_0_humans", package.seeall)
 
-
+function onSpawn(theHuman)
+    if theHuman:getMonsterType()<6 then
+        mySex=0;
+    else
+        mySex=1;
+    end
+    var=20; -- variation of color, +/- var
+    baseR=245;  -- baseRed
+	baseG=200;  -- baseGreen
+	baseB=150;  -- baseBlue
+    red=math.min(255,baseR-var+math.random(2*var));
+    green=math.min(255,baseG-var+math.random(2*var));
+    blue=math.min(255,baseB-var+math.random(2*var));
+    myHair={};
+    myHair[0]={1,2,3};    -- list of possible hair IDs
+    myHair[1]={1,4,7};
+	myBeard={};
+	myBeard[0]={1,3,4,5,6,8}  -- list of possible beard IDs
+	myBeard[1]={0}
+    hairBlonde=(210,200,10) -- Blonde hair Red,Green,Blue
+	hairBlack=(10,10,10) -- Black hair Red,Green,Blue
+	hairRed=(205,30,30) -- Red hair Red,Green,Blue
+	hairBrunet=(90,50,10) -- Brunet hair Red,Green,Blue
+	myhairColor={hairBlonde,hairBlack,hairRed,hairBrunet}
+	theHuman:setAttrib("sex",mySex);
+    theHuman:setSkinColor(red,green,blue);
+    theHuman:setHair( myHair[mySex][math.random(1,#myHair[mySex])] );
+    theHuman:setHairColor(math.random[myhairColor])
+	theHuman:setBeardColor=HairColor
+ end
+ 
 function ini(Monster)
 
 init=true;
