c5334255f9d640ad03a01323462c1847e757a86d
caldarion <garinger@gmx.de>
2014-09-16 16:49:38 +0200

    bring back test npc

---
+++ npc/test.lua
@@ -0,0 +1,21 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>. 
+]]
+-- This is the test LUA NPC. This located at 15, 15, 33 and does nothing by
+-- default. The script is fetched from /home/nitram/testnpc/npc_test.lua
+-- This NPC is required to work correctly with the realtime NPC Update System
+
+dofile("../../../../../../home/nitram/testnpc/npc_test.lua");
