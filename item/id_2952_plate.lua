b9836b57d2b6947059222bfe6be6772c7a7482cc
merung <karten-spiel-haus@hotmail.de>
2014-10-28 20:53:09 +0100

    Adjust item.id_2952_plate to new modules!

--- item/id_2952_plate.lua
+++ item/id_2952_plate.lua
@@ -16,5 +16,7 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 local common = require("base.common")
 
-module("item.id_2952_plate", package.seeall)
+local M = {}
+
+return M
 
