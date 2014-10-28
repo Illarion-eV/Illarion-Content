c7eef80dc5a065716110f803437a5ce551736268
merung <karten-spiel-haus@hotmail.de>
2014-10-28 22:47:10 +0100

    Remove old module statemeant

--- handler/closedoor.lua
+++ handler/closedoor.lua
@@ -17,7 +17,6 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 local class = require("base.class")
 local doors = require("base.doors")
 
-module("handler.closedoor", package.seeall)
 
 -- A door is locked if its quality is 333 and unlocked if it is 233. Data has to be ~=0.
 -- it makes sound 19 (world:makeSound(19, Door.pos);) when the door locks.
