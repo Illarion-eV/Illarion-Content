17dd34b56ffd18fd5504125ebbefc31d2b1ee6ee
merung <karten-spiel-haus@hotmail.de>
2014-10-28 20:12:51 +0100

    Adjust content.areas to new modules

--- content/dailymessage.lua
+++ content/dailymessage.lua
@@ -14,7 +14,7 @@ details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>. 
 ]]
-module("content.dailymessage", package.seeall);
+local M = {}
 
 GoodBadDayMessages = {};
 
@@ -678,3 +678,5 @@ end;
 initMessages();
 
 initMessages = nil;
+
+return M
