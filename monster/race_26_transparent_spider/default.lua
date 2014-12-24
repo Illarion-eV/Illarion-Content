a0d5e86f2b4025e8f0f8023a55835d68d9d4c99b
Martin Karing <nitram@illarion.org>
2014-12-24 23:48:39 +0100

    Fixing naming of monster group

--- monster/race_26_transparent_spider/default.lua
+++ monster/race_26_transparent_spider/default.lua
@@ -16,5 +16,5 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 --ID 261, Nightmarespider, Level: 7, Armourtype: heavy, Weapontype: distance
 
-local transparentSpiders = require("monster.race_26_tranparent_spider.base")
+local transparentSpiders = require("monster.race_26_transparent_spider.base")
 return transparentSpiders.generateCallbacks()
