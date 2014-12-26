7819edd87d80046def0ce18be3e9b728edc2adc1
EviePryler <eviepryler@gmail.com>
2014-12-26 15:14:56 -0600

    change Galmair well location due to market remodel

--- content/climbing.lua
+++ content/climbing.lua
@@ -55,7 +55,7 @@ function M.climbDown(User)
 		elseif (TargetItem.pos == position(105, 600, 0)) then --Cadomyr
 			User:talk(Character.say, "#me klettert an einem Seil den Brunnen hinunter.", "#me climbs down into the well on a rope.")
 			User:warp(position(106, 582, -6));
-		elseif (TargetItem.pos == position(359, 273, 0)) then --Galmair
+		elseif (TargetItem.pos == position(357, 272, 0)) then --Galmair
 			User:talk(Character.say, "#me klettert an einem Seil den Brunnen hinunter.", "#me climbs down into the well on a rope.")
 			User:warp(position(359, 481, -6));
 		elseif (TargetItem.pos == position(849, 841, 0)) then --Runewick
