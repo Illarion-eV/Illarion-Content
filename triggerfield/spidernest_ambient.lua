f7b2ce68c5ad6b22b71a3574517d1a5533ca68c1
caldarion <garinger@gmx.de>
2015-01-29 18:07:00 +0100

    reduce code duplication

--- triggerfield/spidernest_ambient.lua
+++ triggerfield/spidernest_ambient.lua
@@ -27,6 +27,7 @@ INSERT INTO triggerfields VALUES (909,521,-6,'triggerfield.spidernest_ambient');
 ]]
 
 local common = require("base.common")
+local ambient_base = require("triggerfield.base.ambient")
 
 local M = {}
 
@@ -48,79 +49,21 @@ INSERT INTO triggerfields VALUES (x,y,z,'triggerfield.spidernest_ambient');
 ]]
 
 
-spidernestList = {	add = function ( self, pos, direction, german, english, hours, months, chance )
-						if not self[pos.x] then
-							self[pos.x] = {};
-						end
-						if not self[pos.x][pos.y] then
-							self[pos.x][pos.y] = {};
-						end
-						if not self[pos.x][pos.y][pos.z] then
-							self[pos.x][pos.y][pos.z] = {};
-						end
-						local this = self[pos.x][pos.y][pos.z];
-						table.insert(this, {
-                            direction = direction or {0,1,2,3,4,5,6,7};
-							german = german or "";
-							english = english or "";
-							hours = hours or {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
-							months = months or {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-                            chance = chance or 100;
-							} );
-				end
-}
+local spidernestList = ambient_base()
 
 --spidernest triggers using Ambient.lua as a guide
-spidernestList:add( position(909,515,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
-spidernestList:add( position(909,516,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
-spidernestList:add( position(909,518,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
-spidernestList:add( position(909,519,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
-spidernestList:add( position(909,520,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
-spidernestList:add( position(909,521,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
+spidernestList.add( position(909,515,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
+spidernestList.add( position(909,516,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
+spidernestList.add( position(909,518,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
+spidernestList.add( position(909,519,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
+spidernestList.add( position(909,520,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
+spidernestList.add( position(909,521,-6),{5,6,7},"Zu deiner Rechten siehst du einen Totelschädel liegen, der dich mit glühend roten Augen zu mustern scheint.","Off to your right, you see a skull whose eyes glow a bright red, beckoning you over.");
 
 function M.MoveToField(Char)
-	local this = getspidernest(Char);
+	local this = spidernestList.get(Char)
 	if this then
-		common.InformNLS(Char,this.german,this.english);
+		common.InformNLS(Char, this.german, this.english)
 	end
-end
-
-function getspidernest(Char)
-	local pos = Char.pos;
-	local dir = Char:getFaceTo();
-	if spidernestList[pos.x] and spidernestList[pos.x][pos.y] and spidernestList[pos.x][pos.y][pos.z] then
-		local this = spidernestList[pos.x][pos.y][pos.z];
-		local list = {};
-		for _,spidernest in pairs(this) do
-			local timeOk, monthOk, dirOk = false, false, false;
-			for _,that in pairs(spidernest.hours) do
-				if that==world:getTime("hour") then
-					timeOk = true;
-					break;
-				end
-			end
-			for _,that in pairs(spidernest.months) do
-				if that==world:getTime("month") then
-					monthOk = true;
-					break;
-				end
-			end
-			for _,that in pairs(spidernest.direction) do
-				if that==dir then
-					dirOk = true;
-					break;
-				end
-			end
-			if timeOk and monthOk and dirOk and math.random(0,99)<spidernest.chance then --chance!
-				table.insert(list,spidernest);
-			end
-		end
-		if #list>0 then
-			return list[math.random(1,#list)];
-		end
-	end
-	return nil;
 end
 
 return M
-
