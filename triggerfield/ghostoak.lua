68d1d1e4b2bbfadbd3859dda84b40e27150196e3
caldarion <garinger@gmx.de>
2015-02-28 22:27:39 +0100

    dont let Bone ghost escape from quest area near Galmair

--- triggerfield/ghostoak.lua
+++ triggerfield/ghostoak.lua
@@ -14,22 +14,40 @@ details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
--- Quest: The Ghost Oak (112)
+
+-- INSERT INTO triggerfields VALUES (501,120,0,'triggerfield.ghostoak');
+
 local common = require("base.common")
 
 local M = {}
 
 function M.MoveToField(User)
-    if User:getType() ~= Character.player then
-        return
-    end
 
-   if (User:getQuestProgress(112) == 1) then
-      common.InformNLS(User,
-         "[Queststatus] Ein Schauer läuft dir den Rücken herunter, als du den Hain betrittst. Du hast die Geistereiche gefunden.",
-         "[Quest status] A shiver runs down your spine as you enter the grove. This must be the ghost oak.")
-      User:setQuestProgress(112, 2)
-   end
+    if User:getType() == Character.player and User:getQuestProgress(112) == 1 then
+        -- Quest: The Ghost Oak (112)
+        common.InformNLS(User,
+            "[Queststatus] Ein Schauer läuft dir den Rücken herunter, als du den Hain betrittst. Du hast die Geistereiche gefunden.",
+            "[Quest status] A shiver runs down your spine as you enter the grove. This must be the ghost oak.")
+        User:setQuestProgress(112, 2)
+        return
+    elseif User:getType() == Character.monster then
+        -- Dont let Bone Ghost escape from quest area
+        if User:getMosterType() ~= 743 then -- Bone Ghost
+            return
+        end
+
+        local Characters = world:getPlayersInRangeOf(User.pos, 5);
+        for _, Char in pairs(Characters) do
+            common.InformNLS(Char,
+                "Silbrige Fäden ziehen das Gerippe zur Geistereiche zurück.",
+                "Silver threads pull the Bones back to the Ghost Oak.")
+        end
+        world:gfx(41, User.pos)
+        world:makeSound(13, User.pos)
+        User:warp(position(501, 108, 0))
+        world:gfx(41, User.pos)
+        world:makeSound(13, User.pos)
+    end
 end
 
 return M
