10b520e1881884b0bf6b82850ea99eee7b6bc922
EviePryler <eviepryler@gmail.com>
2015-03-07 15:13:16 -0600

    Fountain script

---
+++ item/id_2079_fountain.lua
@@ -0,0 +1,47 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+-- UPDATE items SET itm_script='item.id_2079_fountain' WHERE itm_id IN (2079);
+
+local common = require("base.common")
+local lookat = require("base.lookat")
+
+local M = {}
+
+local lakeoflifeffountainPos = {position(660, 238, -9), position(744, 203, -9), position(777, 186, -9), position(705, 262, -9), position(725, 286, -9), position(759, 303, -9), position(777, 283, -9), position(763, 262, -9)}
+
+function M.UseItem(User, SourceItem, ltstate)
+
+    if SourceItem.pos == lakeoflifeffountainPos then
+       world:gfx(16, User.pos) -- nice GFX
+	   world:sfx(10, User.pos) -- nice SFX
+	   common.InformNLS(User, "Du wäschst dich.", "You start to cleanse.")
+    end
+end
+
+function M.LookAtItem(User, Item)
+
+  local lookAt = lookat.GenerateLookAt(User, Item)
+
+  if Item.pos == lakeoflifeffountainPos then
+    lookAt.name = common.GetNLS(User, "Badebrunnen", "Cleansing Fountain")
+  end
+
+  return lookAt
+end
+
+return M
