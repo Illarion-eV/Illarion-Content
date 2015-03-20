829e85a97c60c3ec43a2f4d9a9ae9d273d2e284d
EviePryler <eviepryler@gmail.com>
2015-03-20 15:17:55 -0500

    Remove dragon cave coffin events to right script

--- item/grave.lua
+++ item/grave.lua
@@ -26,6 +26,34 @@ local M = {}
 
 local graveItemNumbers = {337, 519, 520, 521}
 
+local dragonCaveCoffinPos = {
+    position(695, 612, -3),
+    position(696, 612, -3),
+    position(697, 612, -3),
+    position(698, 612, -3),
+    position(699, 612, -3),
+    position(700, 612, -3),
+    position(701, 612, -3),
+    position(702, 612, -3),
+    position(703, 612, -3),
+    position(704, 612, -3),
+	position(706, 621, -3),
+	position(706, 623, -3),
+	position(706, 625, -3),
+	position(707, 621, -3),
+	position(707, 623, -3),
+	position(707, 625, -3),
+	position(708, 621, -3),
+	position(708, 623, -3),
+	position(708, 625, -3),
+	position(709, 621, -3),
+	position(709, 623, -3),
+	position(709, 625, -3),
+	position(709, 623, -3),
+	position(809, 651, -3),
+	position(810, 651, -3),
+	}
+	
 function M.LookAtItem(User, Item)
 
 	if Item:getData("teachDogTransformationPotion") == "true" then
@@ -110,6 +138,53 @@ function M.UseItem(User, SourceItem)
 			end
 		end
 	end
-end
+	
+	if (User:getQuestProgress(510) == 21) and  SourceItem.pos == position(703, 612, -3) then --OK, the player does the quest at dragon cave
+        User:inform("", "Inside the coffin you find the remains of the dwarf from the vision.  You take the detached skull as proof for Obsidimine.")
+		User:setQuestProgress(510, 22)
+        end
+	
+	for i = 1, #dragonCaveCoffinPos do
+            if (SourceItem.pos == dragonCaveCoffinPos[i]) then
+                CoffinContents(User, SourceItem)
+                return
+            end
+        end	
+    end
+
+function CoffinContents(User, coffinItem)
+
+    -- skip if already tripped in the last 5 minutes
+    local serverTime = world:getTime("unix")
+    local trippingTime = coffinItem:getData("tripping_time")
+
+    if (trippingTime ~= "" and ((tonumber(trippingTime) + 300) > serverTime)) then
+        User:inform("Du findest nichts in diesem Sarg.","You find nothing inside this coffin.")
+        return
+    end
+    -- safe tripping time
+    coffinItem:setData("tripping_time", serverTime)
+    world:changeItem(coffinItem)
+        
+	local random_number = math.random(1,100)
+	if random_number >= 0 and random_number <= 35 then
+	    User:inform("", "The smell hits your nose before you can see inside, you drop the lid unable to open it any further.")
+	elseif random_number >= 36 and random_number <= 70 then
+	    User:inform("", "The remains in the coffin are ancient and mummified.  Who or what they once were is unknown.")
+	elseif random_number >= 71 and random_number <= 90 then
+        User:inform("","You quickly glance in the coffin and discover a simple dagger which you take for your own.")
+		local notCreated = User:createItem(27, 1, 333, nil) -- simple dagger
+        if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
+            world:createItemFromId(27, notCreated, User.pos, true, 333, nil)
+            common.HighInformNLS(User,"Du kannst nichts mehr halten.","You can't carry any more.")
+			end
+	elseif random_number >= 91 and random_number <=100 then
+		local monPos = common.getFreePos(coffinItem.pos, 2) -- radius 2 around vase
+            world:createMonster(754, monPos, -20)
+            world:gfx(41, monPos) -- swirly
+            User:inform("",
+                "You disturb the remains in the coffin and they attack!")
+	   end
+    end	
 
 return M
