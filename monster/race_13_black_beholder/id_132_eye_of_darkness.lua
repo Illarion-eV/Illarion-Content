f4de9a098291a9b453f06eb32787dcdd0dd5f9b6
EviePryler <eviepryler@gmail.com>
2015-04-08 16:22:43 -0500

    #10831 changes for monster magic testing - level 8s

--- monster/race_13_black_beholder/id_132_eye_of_darkness.lua
+++ monster/race_13_black_beholder/id_132_eye_of_darkness.lua
@@ -23,11 +23,11 @@ local monstermagic = require("monster.base.monstermagic")
 local magic = monstermagic()
 magic.addWarping{probability = 0.15, usage = magic.ONLY_NEAR_ENEMY}
 
-magic.addIceball{probability = 0.015, damage = {from = 1800, to = 2300}}
-magic.addIcestrike{probability = 0.009, damage = {from = 2300, to = 2800}}
+magic.addIceball{probability = 0.009, damage = {from = 1800, to = 2300}}
+magic.addIcestrike{probability = 0.001, damage = {from = 2300, to = 2800}}
 magic.addIceball{probability = 0.01, damage = {from = 875, to = 1200}, targetCount = 6}
 magic.addIceball{probability = 0.0001, damage = {from = 2500, to = 3000}}
-magic.addIcestrike{probability = 0.075,  damage = {from = 1750, to = 2000}}
+magic.addIcestrike{probability = 0.06,  damage = {from = 1750, to = 2000}}
 magic.addIcestrike{probability = 0.001, damage = {from = 1000, to = 2000}, targetCount = 4}
 
 local M = blackBeholders.generateCallbacks()
--- monster/race_26_transparent_spider/id_262_soulpain.lua
+++ monster/race_26_transparent_spider/id_262_soulpain.lua
@@ -21,11 +21,11 @@ local transparentSpiders = require("monster.race_26_transparent_spider.base")
 
 local magic = monstermagic()
 
-magic.addFlamestrike{probability = 0.015, damage = {from = 1800, to = 2300}}
-magic.addFireball{probability = 0.009, damage = {from = 2300, to = 2800}}
+magic.addFlamestrike{probability = 0.009, damage = {from = 1800, to = 2300}}
+magic.addFireball{probability = 0.001, damage = {from = 2300, to = 2800}}
 magic.addFireball{probability = 0.01, damage = {from = 875, to = 1200}, targetCount = 6}
 magic.addFlamestrike{probability = 0.0001, damage = {from = 2500, to = 3000}}
-magic.addFireball{probability = 0.075,  damage = {from = 1750, to = 2000}}
+magic.addFireball{probability = 0.06,  damage = {from = 1750, to = 2000}}
 magic.addFlamestrike{probability = 0.001, damage = {from = 1000, to = 2000}, targetCount = 4}
 
 local M = transparentSpiders.generateCallbacks()
