a4b70b9eee05d9fa15d8033903f2c9bb88af293d
merung <karten-spiel-haus@hotmail.de>
2015-05-09 11:55:00 +0200

    Use proper variable name

--- content/oldSlimeFeeding.lua
+++ content/oldSlimeFeeding.lua
@@ -203,7 +203,7 @@ function M.oldSlimeAbortRoute(oldSlime)
             local feeding = world:getItemOnField(acceptFeedingField)
             world:erase(feeding, feeding.number)
             local reward = rewardList[Random.uniform(1, #rewardList)]
-            world:createItemFromId(rewardList.itemId, rewardList.amount, rewardPosition, true, rewardList.quality, rewardList.data)
+            world:createItemFromId(reward.itemId, reward.amount, rewardPosition, true, reward.quality, reward.data)
             oldSlime.movepoints = oldSlime.movepoints -50
         end
         oldSlime.waypoints:addWaypoint(caveEntrance)
