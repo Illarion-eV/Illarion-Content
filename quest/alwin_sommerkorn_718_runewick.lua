8845a81231cf7ea51ae711742869007349710a8f
EviePryler <eviepryler@gmail.com>
2015-05-11 21:09:05 -0500

    #10962 Slighlty spell check of second npc

--- quest/alwin_sommerkorn_718_runewick.lua
+++ quest/alwin_sommerkorn_718_runewick.lua
@@ -26,7 +26,7 @@ local ENGLISH = Player.english
 -- Insert the quest title here, in both languages
 local Title = {}
 Title[GERMAN] = "Entspanntes Fischen in Runewick"
-Title[ENGLISH] = "Unstressed fishing in Runewick"
+Title[ENGLISH] = "Relaxed fishing in Runewick"
 
 -- Insert an extensive description of each status here, in both languages
 -- Make sure that the player knows exactly where to go and what to do
