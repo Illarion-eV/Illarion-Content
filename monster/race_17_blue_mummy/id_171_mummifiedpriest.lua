f188496f97829895f287d57ca68780b76faccf5a
EviePryler <eviepryler@gmail.com>
2015-05-18 00:40:20 -0500

    #10972 give magic to monsters whose name indicates they are magical

---
+++ monster/race_17_blue_mummy/id_171_mummifiedpriest.lua
@@ -0,0 +1,33 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+--ID 171, Mummified Priest, Level: 3
+
+local base = require("monster.base.base")
+local mageBehaviour = require("monster.base.behaviour.mage")
+local monstermagic = require("monster.base.monstermagic")
+local bluemummy = require("monster.race_17_blue_mummy.base")
+local M = bluemummy.generateCallbacks()
+
+
+local magic = monstermagic()
+magic.addWarping{probability = 0.15, usage = magic.ONLY_NEAR_ENEMY}
+
+magic.addHealing{probability = 0.05, damage = {from = 500, to = 1500}}
+magic.addHealing{probability = 0.005, damage = {from = 250, to = 750}, targetCount = 3}
+
+M = magic.addCallbacks(M)
+return mageBehaviour.addCallbacks(magic, M)
