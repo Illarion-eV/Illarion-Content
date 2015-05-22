0deb8007b8a96c45fada18d3dbdcdef4d54317c4
caldarion <garinger@gmx.de>
2015-05-22 13:52:07 +0200

    fix indent

--- triggerfield/dragon_cave_triggers.lua
+++ triggerfield/dragon_cave_triggers.lua
@@ -38,7 +38,7 @@ local common = require("base.common")
 
 local M = {}
 
-local firetrigger={} 
+local firetrigger = {}
 firetrigger[1] = position(697, 616, -3) -- Level -3
 firetrigger[2] = position(700, 613, -3)
 firetrigger[3] = position(702, 615, -3)
@@ -49,75 +49,83 @@ firetrigger[7] = position(699, 659, -6)
 firetrigger[8] = position(699, 660, -6)
 
 
-function M.MoveToField( User )
+function M.MoveToField(User)
+    if User:getType() ~= Character.player then
+        return
+    end
 
-	if User:getType() ~= Character.player then
-		return
-	end
-	
-     local theFiretrigger = 0
-        for i = 1, #firetrigger do
-            if User.pos == firetrigger[i] then
-                theFiretrigger = i
-                break
-            end
+    local theFiretrigger = 0
+    for i = 1, #firetrigger do
+        if User.pos == firetrigger[i] then
+            theFiretrigger = i
+            break
         end
-		
-        if Random.uniform(1, 3) == 1 and theFiretrigger >= 1 and theFiretrigger <=  4 then  -- trigger fire
-	        
-			common.InformNLS(User,"Flammen schießen neben dir in die Höhe. Die Hitze wird fast unerträglich.","Flames shoot up nearby, giving off an unexpected wall of heat.")
-			
-			world:gfx(38, position(700, 615, -3)) --a flame
-			world:makeSound(5, position(700, 615, -3)) --a flame
-				
-        elseif Random.uniform(1, 3) == 1 and theFiretrigger >= 5 and theFiretrigger <= 8 then -- trigger fire
-	        common.InformNLS(User,"Ein riesiges Feuer explodiert in der Nähe. Funken und Feuerbälle prasseln hernieder.","Giant flames explode nearby, shooting sparks and fireballs all over the area.")
-			
-			world:gfx(38, position(700, 654, -6)) --a flame
-			world:makeSound(5, position(700, 654, -6)) --a flame
-	
-        elseif Random.uniform(1, 4) == 1 and User.pos == position(739, 640, -3) then -- vision
-	        common.InformNLS(User,"Dir erscheint die Vision eines Bergtrolls, der einen prallen kleinen Halbling über einem lodernden Feuer brät.","You see a vision of a mountain troll roasting what appears to be a plump little halfling over a roaring fire.")
-            world:gfx(5, position(739, 642, -3)) 
-		    world:makeSound(13, position(739, 642, -3))
-			
-	    elseif Random.uniform(1, 4) == 1 and User.pos == position(725, 616, -3) then -- vision		
-		    common.InformNLS(User,"Dir erscheint die Vision von zwei Drachen im Kampf miteinander, die große Verwüstung an den Ländereien ringsherum anrichten.","You see a vision of two dragons in battle with each other, causing much destruction to the land around them.")
-            world:gfx(5, position(723, 618, -3))
-			world:makeSound(13, position(723, 618, -3))
-			
-        elseif Random.uniform(1, 4) == 1 and User.pos == position(725, 638, -3) then -- vision
-	        common.InformNLS(User,"Dir erscheint die Vision von einem Drachenbaby, das gerade die Schale durchbricht.","You see a vision of a baby dragon breaking through its shell.")
-            world:gfx(5, position(728, 638, -3))
-			world:makeSound(13, position(728, 638, -3))
-			
-        elseif Random.uniform(1, 4) == 1 and User.pos == position(728, 627, -3) then -- vision
-	        common.InformNLS(User,"Dir erscheint die Vision eines sehr alten Drachens, der friedlich zu einer Gruppe Menschen spricht.","You see a vision of a very old dragon, speaking peacefully in common to a group of humans.")
-            world:gfx(5, position(728, 631, -3))
-			world:makeSound(13, position(728, 631, -3)) 
-	    
-		elseif Random.uniform(1, 2) == 1 and User.pos == position(709, 649, -6) then -- skull trigger from feeding pen to dragon lair
-            common.InformNLS(User,"Als Du auf den Schädel trittst, hörst Du ein merkwürdiges Klicken und Du befindest Dich in der Drachenhöhle.","As you step on the skull, you hear an odd click and find yourself whisked away to the dragon's lair.")		
-            world:gfx(41, User.pos)
-	        world:makeSound(11, User.pos)
-            User:warp(position(703, 619, -6))
-            world:gfx(41, User.pos)
-			world:makeSound(11, User.pos)
-			
-   		elseif User:getQuestProgress(510) == 18 and User.pos == position(731, 616, -3) then -- vision for quest
-		    common.InformNLS(User,"Dir erscheint die Vision eines Zwerges, der von einem Drachen angegriffen wird. Der Zwerg ist in Flammen eingehüllt, ehe er von den Krallen des Drachens vom Boden gepflückt wird.","You see a vision of a dwarf being attacked by a dragon. The dwarf is engulfed in flames before he is plucked from the ground by the talons of the dragon.")
-			User:setQuestProgress(510, 19)
-            world:gfx(5, position(728, 616, -3))
-			world:makeSound(13, position(728, 616, -3))
-			
-		 elseif Random.uniform(1, 4) == 1 and User.pos == position(731, 616, -3) then -- vision
-	        common.InformNLS(User,"Dir erscheint die Vision eines Zwerges, der von einem Drachen angegriffen wird. Der Zwerg ist in Flammen eingehüllt, ehe er von den Krallen des Drachens vom Boden gepflückt wird.","You see a vision of a dwarf being attacked by a dragon. The dwarf is engulfed in flames before he is plucked from the ground by the talons of the dragon.")
-            world:gfx(5, position(728, 616, -3))
-			world:makeSound(13, position(728, 616, -3))
-			
-		elseif Random.uniform(1, 4) == 1 and User.pos == position(850, 666, 1) or User.pos == position(850, 665, 1) then -- Inform
-            common.InformNLS(User,"Der Anblick da vorne, mit dem kleinen Becken und dem Sandhügel, ist seltsam. Irgendwie scheinen sie miteinander verbunden, voneinander abhängig.","The visage ahead is curious with the small pool and mound of sand. They somehow seem to be tied together, dependant.")		
+    end
 
-        end
-	end
+    if Random.uniform(1, 3) == 1 and theFiretrigger >= 1 and theFiretrigger <=  4 then  -- trigger fire
+        common.InformNLS(User,
+            "Flammen schießen neben dir in die Höhe. Die Hitze wird fast unerträglich.",
+            "Flames shoot up nearby, giving off an unexpected wall of heat.")
+        world:gfx(38, position(700, 615, -3)) --a flame
+        world:makeSound(5, position(700, 615, -3)) --a flame
+
+    elseif Random.uniform(1, 3) == 1 and theFiretrigger >= 5 and theFiretrigger <= 8 then -- trigger fire
+        common.InformNLS(User,
+            "Ein riesiges Feuer explodiert in der Nähe. Funken und Feuerbälle prasseln hernieder.",
+            "Giant flames explode nearby, shooting sparks and fireballs all over the area.")
+        world:gfx(38, position(700, 654, -6)) --a flame
+        world:makeSound(5, position(700, 654, -6)) --a flame
+    elseif Random.uniform(1, 4) == 1 and User.pos == position(739, 640, -3) then -- vision
+        common.InformNLS(User,
+            "Dir erscheint die Vision eines Bergtrolls, der einen prallen kleinen Halbling über einem lodernden Feuer brät.",
+            "You see a vision of a mountain troll roasting what appears to be a plump little halfling over a roaring fire.")
+        world:gfx(5, position(739, 642, -3))
+        world:makeSound(13, position(739, 642, -3))
+    elseif Random.uniform(1, 4) == 1 and User.pos == position(725, 616, -3) then -- vision
+        common.InformNLS(User,
+            "Dir erscheint die Vision von zwei Drachen im Kampf miteinander, die große Verwüstung an den Ländereien ringsherum anrichten.",
+            "You see a vision of two dragons in battle with each other, causing much destruction to the land around them.")
+        world:gfx(5, position(723, 618, -3))
+        world:makeSound(13, position(723, 618, -3))
+    elseif Random.uniform(1, 4) == 1 and User.pos == position(725, 638, -3) then -- vision
+        common.InformNLS(User,
+            "Dir erscheint die Vision von einem Drachenbaby, das gerade die Schale durchbricht.",
+            "You see a vision of a baby dragon breaking through its shell.")
+        world:gfx(5, position(728, 638, -3))
+        world:makeSound(13, position(728, 638, -3))
+    elseif Random.uniform(1, 4) == 1 and User.pos == position(728, 627, -3) then -- vision
+        common.InformNLS(User,
+            "Dir erscheint die Vision eines sehr alten Drachens, der friedlich zu einer Gruppe Menschen spricht.",
+            "You see a vision of a very old dragon, speaking peacefully in common to a group of humans.")
+        world:gfx(5, position(728, 631, -3))
+        world:makeSound(13, position(728, 631, -3))
+    elseif Random.uniform(1, 2) == 1 and User.pos == position(709, 649, -6) then -- skull trigger from feeding pen to dragon lair
+        common.InformNLS(User,
+            "Als Du auf den Schädel trittst, hörst Du ein merkwürdiges Klicken und Du befindest Dich in der Drachenhöhle.",
+            "As you step on the skull, you hear an odd click and find yourself whisked away to the dragon's lair.")
+        world:gfx(41, User.pos)
+        world:makeSound(11, User.pos)
+        User:warp(position(703, 619, -6))
+        world:gfx(41, User.pos)
+        world:makeSound(11, User.pos)
+    elseif User:getQuestProgress(510) == 18 and User.pos == position(731, 616, -3) then -- vision for quest
+        common.InformNLS(User,
+            "Dir erscheint die Vision eines Zwerges, der von einem Drachen angegriffen wird. Der Zwerg ist in Flammen eingehüllt, ehe er von den Krallen des Drachens vom Boden gepflückt wird.",
+            "You see a vision of a dwarf being attacked by a dragon. The dwarf is engulfed in flames before he is plucked from the ground by the talons of the dragon.")
+        User:setQuestProgress(510, 19)
+        world:gfx(5, position(728, 616, -3))
+        world:makeSound(13, position(728, 616, -3))
+    elseif Random.uniform(1, 4) == 1 and User.pos == position(731, 616, -3) then -- vision
+        common.InformNLS(User,
+            "Dir erscheint die Vision eines Zwerges, der von einem Drachen angegriffen wird. Der Zwerg ist in Flammen eingehüllt, ehe er von den Krallen des Drachens vom Boden gepflückt wird.",
+            "You see a vision of a dwarf being attacked by a dragon. The dwarf is engulfed in flames before he is plucked from the ground by the talons of the dragon.")
+        world:gfx(5, position(728, 616, -3))
+        world:makeSound(13, position(728, 616, -3))
+    elseif Random.uniform(1, 4) == 1 and User.pos == position(850, 666, 1) or User.pos == position(850, 665, 1) then -- Inform
+        common.InformNLS(User,
+            "Der Anblick da vorne, mit dem kleinen Becken und dem Sandhügel, ist seltsam. Irgendwie scheinen sie miteinander verbunden, voneinander abhängig.",
+            "The visage ahead is curious with the small pool and mound of sand. They somehow seem to be tied together, dependant.")
+    end
+end
+
 return M
