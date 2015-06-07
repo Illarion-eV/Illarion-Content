0a06a06a1f5854b0e0b332570aa4c787a2011ecc
Martin Karing <nitram@illarion.org>
2015-06-07 20:59:22 +0200

    Replace \t to spaces

--- base/orders.lua
+++ base/orders.lua
@@ -106,7 +106,7 @@ end
 
 local function activateRetentionPeriod(user)
     local retentionperiod = LongTimeEffect(31,OrderRetentionPeriod * 600);
-	user.effects:addEffect(retentionperiod);
+    user.effects:addEffect(retentionperiod);
 end
 
 local function checkRetentionPeriod(usr)
@@ -343,7 +343,7 @@ function OrderNPC:receiveText(who,text)
             return;
         end
     end
-	for i, ttext in pairs(self.triggerSeeOrder) do
+    for i, ttext in pairs(self.triggerSeeOrder) do
         if ( string.find(text,ttext) ) then
             number = getNumberInString(text);
             local ordercount = #self.openOrders
@@ -451,15 +451,15 @@ function OrderNPC:payBoni(user,order)
     end
    -- give some rankpoints as reward
    if NpcLocation[thisNPC.id] == nil then
-   		factions.setLocation(thisNPC);
+           factions.setLocation(thisNPC);
    end
    local Factionvalues = factions.get(user);
    
-	Factionvalues[ DigitToIndex[NpcLocation[thisNPC.id]+RANKPOINTS_OFFSET] ]= 
-		     Factionvalues[ DigitToIndex[NpcLocation[thisNPC.id]+RANKPOINTS_OFFSET] ] +5;--add 5 rankpoints for this town
-	
-	factions.put(user,Factionvalues);
-	------------------
+    Factionvalues[ DigitToIndex[NpcLocation[thisNPC.id]+RANKPOINTS_OFFSET] ]= 
+             Factionvalues[ DigitToIndex[NpcLocation[thisNPC.id]+RANKPOINTS_OFFSET] ] +5;--add 5 rankpoints for this town
+    
+    factions.put(user,Factionvalues);
+    ------------------
     --user:inform("payboni end");
 end
 
@@ -487,7 +487,7 @@ function OrderNPC:checkOrder(user)
                 if ( self.lastOrderString ~= order:getDataString() ) then
                     --es ist noch nie versucht worden eine teillieferung zu veranlassen
                     order:partDelivery(user,orderstruct);
-					common.TalkNLS( self.npc, Character.say, self.textSomeItems.ger, self.textSomeItems.eng );
+                    common.TalkNLS( self.npc, Character.say, self.textSomeItems.ger, self.textSomeItems.eng );
                     self.lastOrderString = order:getDataString();
                     return true;
                 else
@@ -711,19 +711,19 @@ function Order:getInHandForNPC(Character,npc)
     end
     
     for i=12,17 do --checks the whole belt
-		local item = Character:getItemAt( i );
-	    if ( item.id == OrderItem ) then
-	        local data = item:getValue(1);
-	        if ( data:len() > 0 ) then
-	            local order = Order:new();
-	            order.orderitem = item;
-	            if ( order:get() ) then
-	                if ( order.npcname == npc.name) then
-	                    return order;
-	                end
-	            end
-	        end
-	    end
+        local item = Character:getItemAt( i );
+        if ( item.id == OrderItem ) then
+            local data = item:getValue(1);
+            if ( data:len() > 0 ) then
+                local order = Order:new();
+                order.orderitem = item;
+                if ( order:get() ) then
+                    if ( order.npcname == npc.name) then
+                        return order;
+                    end
+                end
+            end
+        end
     end
     return nil;
 end
@@ -732,7 +732,7 @@ end
 function Order:setTime()
     --stunden timestmap
     local curtime = world:getTime("illarion")/3600;
-	--offset aufaddieren
+    --offset aufaddieren
     curtime = curtime + self.reltime;
     local year,month,day,hour,minute,second = common.TimestampToDate(curtime*3600);
     self.time.day = day;
@@ -753,11 +753,11 @@ function Order:createOrderItem(Character)
         scriptitem = Character:getItemAt( Character.left_tool);
     end
     for i=12,17 do --checks the whole belt
-		if ( scriptitem.id ~= 0 ) then
-		    scriptitem = Character:getItemAt(i);
-		end
-	end
-	if ( scriptitem.id ~= 0 ) then 
+        if ( scriptitem.id ~= 0 ) then
+            scriptitem = Character:getItemAt(i);
+        end
+    end
+    if ( scriptitem.id ~= 0 ) then 
         common.InformNLS(Character,"Du brauchst eine freie Hand um einen Auftrag anzunehmen.","You need a free hand to get a contract.");
         return false;
     end
@@ -795,9 +795,9 @@ function Order:checkOrder(Character,npc)
     ret.someItems= false;
     for i,item in pairs(self.items) do    
         local itemlist = Character:getItemList(item.id);
-		--nach qualitï¿½t sortieren
+        --nach qualitï¿½t sortieren
         table.sort(itemlist, function(itema,itemb) return (itema.quality > itemb.quality); end);
-			
+            
         local curcount = 0;
         --durch alle items iterieren
         for y,charitem in pairs(itemlist) do
@@ -818,8 +818,8 @@ function Order:checkOrder(Character,npc)
             ret.allItems = false;
         end
         if Character:countItem(item.id) > 0 then
-        	ret.someItems=true;
-		end
+            ret.someItems=true;
+        end
         
     end
     averquali = math.floor((curquali / allcount) + 0.5);
@@ -914,8 +914,8 @@ function Order:partDelivery(character,orderstatestruct)
             end
         end
         if itemdeleted then
-			break; -- one item type collected, use npc again for the other items
-		end
+            break; -- one item type collected, use npc again for the other items
+        end
     end
     --lï¿½schen aller fertigen items
     for i,del in ipairs (toremove) do
@@ -955,7 +955,7 @@ function Order:doOrder(character,orderstatestruct)
         --Wert für schlecht erfüllten auftrag.
         M.setThrustWorthyness(character,ThrustworthynessChangeAfterNotSuccessOrder,GoodOrderChangeAfterNotSuccessOrder);
     end
-	common.InformNLS(character, "[Quest gelöst] Du erhältst "..price.." Kupferstücke.", "[Quest solved] You are awarded "..price.." copper coins.")
+    common.InformNLS(character, "[Quest gelöst] Du erhältst "..price.." Kupferstücke.", "[Quest solved] You are awarded "..price.." copper coins.")
 end
 
 
@@ -1090,37 +1090,37 @@ function Order:lookAt(Char)
     local enghour="";
     local ItemAmount;
     local RemainingHours;
-	gerhour,enghour = common.Hour_To_String(self.time.hour);
+    gerhour,enghour = common.Hour_To_String(self.time.hour);
     if (Char:getPlayerLanguage() == 0) then --deutsch
         text = "Auftrag von "..self.npcname.." ï¿½ber ";
         for key,item in pairs(self.items) do
             if item.id~=0 and item.count~=0 then --don't include the Item "nothing"
-				if item.count<10 then
-				    ItemAmount = getDigitName(item.count,0);
-				else
-					ItemAmount = item.count;
-				end
-				
-				text = text .. ItemAmount .. " " .. world:getItemName(item.id,0) .. ", ";
-			end
+                if item.count<10 then
+                    ItemAmount = getDigitName(item.count,0);
+                else
+                    ItemAmount = item.count;
+                end
+                
+                text = text .. ItemAmount .. " " .. world:getItemName(item.id,0) .. ", ";
+            end
         end
         if (self.quality+1)>6 then --ask for quality only if the NPC wants good wares or better
-			text = text .. " mit mindestens " .. QualityText[self.quality+1].ger.." Qualitï¿½t";
-		end
+            text = text .. " mit mindestens " .. QualityText[self.quality+1].ger.." Qualitï¿½t";
+        end
         
-		if ( self.orderitem ~= nil ) then
+        if ( self.orderitem ~= nil ) then
             RemainingHours = ConvertDateToHourOffset(self.time.year, self.time.month+1, self.time.day, self.time.hour);
-		    text = text .. " abzuliefern in " ..RemainingHours .. " Stunden";
-			--text = text .. ", abzuliefern am " .. self.time.day .. "." .. MonthNames[self.time.month+1] .. "." .. self.time.year .. " " .. gerhour;
-        else		
-		    text = text .. " abzuliefern in " ..self.reltime .. " Stunden";
+            text = text .. " abzuliefern in " ..RemainingHours .. " Stunden";
+            --text = text .. ", abzuliefern am " .. self.time.day .. "." .. MonthNames[self.time.month+1] .. "." .. self.time.year .. " " .. gerhour;
+        else        
+            text = text .. " abzuliefern in " ..self.reltime .. " Stunden";
         end
         g,s,c = CoinsToGSC(self.coins);
         if g == 0 then
-			text = text .. " für " .. s .. " Silber und " .. c .. " Kupfermünzen."; 
-		else
-			text = text .. " für " .. g .. " Gold " .. s .. " Silber und " .. c .. " Kupfermünzen.";
-		end  
+            text = text .. " für " .. s .. " Silber und " .. c .. " Kupfermünzen."; 
+        else
+            text = text .. " für " .. g .. " Gold " .. s .. " Silber und " .. c .. " Kupfermünzen.";
+        end  
         if ( Char:isAdmin() ) then
             text = text .. " Wertverlust Zeit: " .. self.timemodcoins.value .. " Kupfer in " ..self.timemodcoins.mod.." Stunden,"
             text = text .. " Wertverlust Qualität: "..self.qualitymodcoins.value .. " Kupfer pro "..self.qualitymodcoins.mod.." Qualität." 
@@ -1130,30 +1130,30 @@ function Order:lookAt(Char)
         text = "Contract from "..self.npcname.." over ";
         for key,item in pairs(self.items) do
             if item.id~=0 and item.count~=0 then --don't include the Item "nothing"
-            	if item.count<10 then
-				    ItemAmount = getDigitName(item.count,1);
-				else
-					ItemAmount = item.count;
-				end
-            	text = text .. ItemAmount .. " " .. world:getItemName(item.id,1) .. ",";
+                if item.count<10 then
+                    ItemAmount = getDigitName(item.count,1);
+                else
+                    ItemAmount = item.count;
+                end
+                text = text .. ItemAmount .. " " .. world:getItemName(item.id,1) .. ",";
             end
         end
         if (self.quality+1)>6 then --ask for quality only if the NPC wants good wares or better
-        	text = text .. " of at least " .. QualityText[self.quality+1].eng.." quality";
+            text = text .. " of at least " .. QualityText[self.quality+1].eng.." quality";
         end
         if ( self.orderitem ~= nil ) then
             RemainingHours = ConvertDateToHourOffset(self.time.year, self.time.month+1, self.time.day, self.time.hour);
-			text = text .. " to deliver in " ..RemainingHours.." hours";
-		--    text = text .. ", to deliver at " .. self.time.day .. "." .. MonthNames[self.time.month+1] .. "." .. self.time.year .. " " .. enghour;
+            text = text .. " to deliver in " ..RemainingHours.." hours";
+        --    text = text .. ", to deliver at " .. self.time.day .. "." .. MonthNames[self.time.month+1] .. "." .. self.time.year .. " " .. enghour;
         else
             text = text .. " to deliver in " ..self.reltime.." hours";
         end    
         g,s,c = CoinsToGSC(self.coins);
         if g == 0 then
-			text = text .. " for " .. s .. " silver and " .. c .. " copper coins."; 
-		else
-        	text = text .. " for " .. g .. " gold " .. s .. " silver and " .. c .. " copper coins."; 
-		end  
+            text = text .. " for " .. s .. " silver and " .. c .. " copper coins."; 
+        else
+            text = text .. " for " .. g .. " gold " .. s .. " silver and " .. c .. " copper coins."; 
+        end  
         if ( Char:isAdmin() ) then
             text = text .. " Valueloss time: " .. self.timemodcoins.value .. " copper in " ..self.timemodcoins.mod.." hours,"
             text = text .. " Valueloss quality: "..self.qualitymodcoins.value .. " copper per "..self.qualitymodcoins.mod.." quality." 
@@ -1383,16 +1383,16 @@ function OrderPool:generateOrder()
     for i,item in pairs(itemsfororder) do
         item.concretenumber = math.random(item.mincount,item.maxcount);
         
-		if item.concretenumber>5 and item.concretenumber<50 then --modification part of the item number to get only orders for 1,2,3,4,5,10,15,...
-			item.concretenumber = item.concretenumber/5;
-			item.concretenumber = 5* math.floor(item.concretenumber+0.5);
-		elseif item.concretenumber>50 and item.concretenumber<100 then
-			item.concretenumber = item.concretenumber/10;
-			item.concretenumber = 10* math.floor(item.concretenumber+0.5);
-		elseif item.concretenumber>100 then
-			item.concretenumber = item.concretenumber/50;
-			item.concretenumber = 50* math.floor(item.concretenumber+0.5);
-		end	
+        if item.concretenumber>5 and item.concretenumber<50 then --modification part of the item number to get only orders for 1,2,3,4,5,10,15,...
+            item.concretenumber = item.concretenumber/5;
+            item.concretenumber = 5* math.floor(item.concretenumber+0.5);
+        elseif item.concretenumber>50 and item.concretenumber<100 then
+            item.concretenumber = item.concretenumber/10;
+            item.concretenumber = 10* math.floor(item.concretenumber+0.5);
+        elseif item.concretenumber>100 then
+            item.concretenumber = item.concretenumber/50;
+            item.concretenumber = 50* math.floor(item.concretenumber+0.5);
+        end    
         
         ordertime = ordertime + (item.time * item.concretenumber);
         ordercoins = ordercoins + (item.price * item.concretenumber);
@@ -1424,28 +1424,28 @@ end
 function getDigitName(number,lang) --returns a digit from 0-9 as text
 
 if number<10 then
-	if not InitDigitNames then
-		InitDigitNames = true;
-		digit_de = {"eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun"};
-		digit_de[0] = "null";
-		digit_eng ={"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-		digit_eng[0] = "zero";
-	end
-	
-	if lang == 0 then
-		return digit_de[number];
-	else
-		return digit_eng[number];
-	end
+    if not InitDigitNames then
+        InitDigitNames = true;
+        digit_de = {"eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun"};
+        digit_de[0] = "null";
+        digit_eng ={"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        digit_eng[0] = "zero";
+    end
+    
+    if lang == 0 then
+        return digit_de[number];
+    else
+        return digit_eng[number];
+    end
 end
 
 end
 
 function ConvertDateToHourOffset(year, month, day, hour)
-	DeliverTimestamp = (year*365+(month-1)*24+day)*24+hour;
-	curHourTimestamp = (world:getTime("year")*365+(world:getTime("month")-1)*24+world:getTime("day"))*24+world:getTime("hour");
-	HoursTillInvalidOrder = DeliverTimestamp - curHourTimestamp;  
-	return HoursTillInvalidOrder;
-end		
-		
+    DeliverTimestamp = (year*365+(month-1)*24+day)*24+hour;
+    curHourTimestamp = (world:getTime("year")*365+(world:getTime("month")-1)*24+world:getTime("day"))*24+world:getTime("hour");
+    HoursTillInvalidOrder = DeliverTimestamp - curHourTimestamp;  
+    return HoursTillInvalidOrder;
+end        
+        
 return M
