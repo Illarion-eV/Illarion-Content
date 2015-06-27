b9bb4c308d5a98bea662c082aadd857ea8ca8201
merung <karten-spiel-haus@hotmail.de>
2015-06-27 18:47:35 +0200

    Add translation and correct function call

--- triggerfield/zelphiasStream.lua
+++ triggerfield/zelphiasStream.lua
@@ -27,7 +27,7 @@ function getCharacterLoad(user)
     local totalLoad = 0
     
     local backPack
-    if getItemAt(Character.backpack) then
+    if user:getItemAt(Character.backpack) then
         backPack = user:getBackPack()
     end
     
@@ -36,7 +36,7 @@ function getCharacterLoad(user)
     end
     
     for i = 1, 17 do
-        local currentItem = getItemAt(i)
+        local currentItem = user:getItemAt(i)
         if currentItem then
             totalLoad = totalLoad +  world:getItemStats(currentItem).Weight
         end
@@ -50,11 +50,11 @@ function M.MoveToField(user)
     if getCharacterLoad(user) >= getMaximumLoad(user)/5 then
         
         if user:getRace() == 5 then
-            User:inform("Durch das Gewicht deiner Ausrüstung, wirst du tief ins Wasser gezogen und von einer Strömung erfasst, doch durch das natürliche Schwimmvermögen deiner Rasse kannst du dich ihr entgegenstemmen.","")
+            User:inform("Durch das Gewicht deiner Ausrüstung, wirst du tief ins Wasser gezogen und von einer Strömung erfasst, doch durch das natürliche Schwimmvermögen deiner Rasse kannst du dich ihr entgegenstemmen.","The weighty equipment pulls you into the depths of the dark water. But the natural swimming abilities of your kind enable you to withstand the treacherous currents as you press on in spite of your burden.")
         else
-            user:inform("Durch das Gewicht deriner Ausrüstung, wirst du tief ins Wasser gezogen und von einer Strömung erfasst!", "")
-            user:warp(2, 22, 0)
-            user:talk(Character.say, "#me wird ans Ufer gespühlt.", "")
+            user:inform("Durch das Gewicht deriner Ausrüstung, wirst du tief ins Wasser gezogen und von einer Strömung erfasst!", "The weighty equipment pulls you into the depths of the dark water, treacherous currents carrying you away.")
+            user:warp(75, 564, 0)
+            user:talk(Character.say, "#me wird ans Ufer gespühlt.", "#me is washed ashore.")
         end
         
     end
