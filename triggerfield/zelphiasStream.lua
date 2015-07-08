0d324f58298b9f6cb7228f2513a95b3eeb0e6182
merung <karten-spiel-haus@hotmail.de>
2015-07-08 21:46:19 +0200

    Add missing position()

--- triggerfield/zelphiasStream.lua
+++ triggerfield/zelphiasStream.lua
@@ -53,7 +53,7 @@ function M.MoveToField(user)
             User:inform("Durch das Gewicht deiner Ausrüstung, wirst du tief ins Wasser gezogen und von einer Strömung erfasst, doch durch das natürliche Schwimmvermögen deiner Rasse kannst du dich ihr entgegenstemmen.","The weighty equipment pulls you into the depths of the dark water. But the natural swimming abilities of your kind enable you to withstand the treacherous currents as you press on in spite of your burden.")
         else
             user:inform("Durch das Gewicht deriner Ausrüstung, wirst du tief ins Wasser gezogen und von einer Strömung erfasst!", "The weighty equipment pulls you into the depths of the dark water, treacherous currents carrying you away.")
-            user:warp(75, 564, 0)
+            user:warp(position(75, 564, 0))
             user:talk(Character.say, "#me wird ans Ufer gespühlt.", "#me is washed ashore.")
         end
         
