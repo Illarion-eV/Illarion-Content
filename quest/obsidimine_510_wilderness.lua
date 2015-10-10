b6c90c2a5a5ad85e73d4f68d276720d7cfa600e8
EviePryler <eviepryler@gmail.com>
2015-10-09 22:25:44 -0500

    declare variable

--- quest/obsidimine_510_wilderness.lua
+++ quest/obsidimine_510_wilderness.lua
@@ -99,7 +99,7 @@ Description[GERMAN][32] = "Du hast alle Aufgaben für Obsidimine erledigt."
 Description[ENGLISH][32] = "You have completed all tasks for Obsidimine."
 
 -- Insert the position of the quest start here (probably the position of an NPC or item)
-Start = {809, 676, 0}
+local Start = {809, 676, 0}
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
 local QuestTarget = {}
