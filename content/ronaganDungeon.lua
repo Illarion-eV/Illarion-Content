4da7592fa9adf00f7b9126228dab0e005acf96d4
estralis <estralis@illarion.org>
2015-11-29 12:05:21 +0100

    #11182: Correct pos

--- content/ronaganDungeon.lua
+++ content/ronaganDungeon.lua
@@ -27,8 +27,7 @@ function M.questCheck(User, SourceItem)
 
     if SourceItem.pos == position(897, 577, -9) then -- your trying to steal the dagger
         User:inform("Als du versuchst, den Dolch herauszuziehen, verwandeln sich die Schatzwächter und untote Skelette erscheinen.", "As you try and pull out the dagger, the guardians of the treasure transform and undead skeletons emerge.")
-        guardians = {position(893,574,-9), position(896,574,-9), position(902,574,-9), position(893,574,-9)}
-        
+        guardians = {position(893,574,-9), position(896,574,-9), position(899,574,-9), position(902,574,-9)}
         for _, guardianpos in pairs (guardians) do
             world:createMonster(782, guardianpos, -20)
             world:gfx(41, guardianpos) -- swirly
