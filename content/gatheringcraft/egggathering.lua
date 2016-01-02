4d1f229e1038676ef2bd002a827f081a8391f1b7
EviePryler <eviepryler@gmail.com>
2016-01-01 21:37:15 -0600

    remove tabs

--- content/gatheringcraft/egggathering.lua
+++ content/gatheringcraft/egggathering.lua
@@ -27,99 +27,99 @@ local gathering = require("content.gathering")
 module("content.gatheringcraft.egggathering", package.seeall)
 
 function StartGathering(User, SourceItem, ltstate)
-	local EMPTY_NEST = 1172
-	local FILLED_NEST = 1171
-	local EGG = 1150
+    local EMPTY_NEST = 1172
+    local FILLED_NEST = 1171
+    local EGG = 1150
 
-	gathering.InitGathering();
-	local egggathering = gathering.egggathering;
+    gathering.InitGathering();
+    local egggathering = gathering.egggathering;
 
-	common.ResetInterruption( User, ltstate );
-	if ( ltstate == Action.abort ) then -- work interrupted
+    common.ResetInterruption( User, ltstate );
+    if ( ltstate == Action.abort ) then -- work interrupted
         User:talk(Character.say, "#me unterbricht "..common.GetGenderText(User, "seine", "ihre").." Arbeit.", "#me interrupts "..common.GetGenderText(User, "his", "her").." work.")
-		return
-	end
+        return
+    end
 
-	if not common.CheckItem( User, SourceItem ) then -- security check
-		return
-	end
+    if not common.CheckItem( User, SourceItem ) then -- security check
+        return
+    end
 
-	-- Disabled in order to give a hungry player a chance to strengthen.
-	-- if not common.FitForWork( User ) then -- check minimal food points
-		-- return
-	-- end
-	common.TurnTo( User, SourceItem.pos ); -- turn if necessary
+    -- Disabled in order to give a hungry player a chance to strengthen.
+    -- if not common.FitForWork( User ) then -- check minimal food points
+        -- return
+    -- end
+    common.TurnTo( User, SourceItem.pos ); -- turn if necessary
 
-	if SourceItem.id == EMPTY_NEST  then
+    if SourceItem.id == EMPTY_NEST  then
       User:inform("Hier befinden sich keine Eier.","There are no eggs here.",Player.highPriority)
-	  return;
-	end
-	-- check the amount
-	local amountStr = SourceItem:getData("amount");
-	local amount = 0;
-	if ( amountStr ~= "" ) then
-		amount = tonumber(amountStr);
-	else
-		-- first time that this item is harvested
-		amount = 10;
-		SourceItem:setData("amount","" .. amount);
-		world:changeItem(SourceItem);
+      return;
+    end
+    -- check the amount
+    local amountStr = SourceItem:getData("amount");
+    local amount = 0;
+    if ( amountStr ~= "" ) then
+        amount = tonumber(amountStr);
+    else
+        -- first time that this item is harvested
+        amount = 10;
+        SourceItem:setData("amount","" .. amount);
+        world:changeItem(SourceItem);
     User:changeSource(SourceItem);
-	end
-	if ( amount < 0 ) then
-		-- this should never happen...
-		User:inform("[ERROR] Negative amount " .. amount .. " for item id " .. SourceItem.id .. " at (" .. SourceItem.pos.x .. "," .. SourceItem.pos.y .. "," .. SourceItem.pos.z .. "). Please inform a developer.");
-		return;
-	end
-	if ( amount == 0 ) then
-		-- this should never happen...
-		User:inform("[ERROR] Zero amount for item id " .. SourceItem.id .. " at (" .. SourceItem.pos.x .. "," .. SourceItem.pos.y .. "," .. SourceItem.pos.z .. "). Please inform a developer.");
-		return;
-	end
+    end
+    if ( amount < 0 ) then
+        -- this should never happen...
+        User:inform("[ERROR] Negative amount " .. amount .. " for item id " .. SourceItem.id .. " at (" .. SourceItem.pos.x .. "," .. SourceItem.pos.y .. "," .. SourceItem.pos.z .. "). Please inform a developer.");
+        return;
+    end
+    if ( amount == 0 ) then
+        -- this should never happen...
+        User:inform("[ERROR] Zero amount for item id " .. SourceItem.id .. " at (" .. SourceItem.pos.x .. "," .. SourceItem.pos.y .. "," .. SourceItem.pos.z .. "). Please inform a developer.");
+        return;
+    end
 
-	-- since we're here, there is something we can harvest
+    -- since we're here, there is something we can harvest
 
-	if ( ltstate == Action.none ) then -- currently not working -> let's go
-		egggathering.SavedWorkTime[User.id] = egggathering:GenWorkTime(User,nil);
-		User:startAction( egggathering.SavedWorkTime[User.id], 0, 0, 0, 0);
-		User:talk(Character.say, "#me beginnt Eier zu sammeln.", "#me starts to gather eggs.")
-		return
-	end
+    if ( ltstate == Action.none ) then -- currently not working -> let's go
+        egggathering.SavedWorkTime[User.id] = egggathering:GenWorkTime(User,nil);
+        User:startAction( egggathering.SavedWorkTime[User.id], 0, 0, 0, 0);
+        User:talk(Character.say, "#me beginnt Eier zu sammeln.", "#me starts to gather eggs.")
+        return
+    end
 
-	-- since we're here, we're working
+    -- since we're here, we're working
 
-	if egggathering:FindRandomItem(User) then
-		return
-	end
+    if egggathering:FindRandomItem(User) then
+        return
+    end
 
-	User:learn( egggathering.LeadSkill, egggathering.SavedWorkTime[User.id], egggathering.LearnLimit);
-	amount = amount - 1;
-	local notCreated = User:createItem( EGG, 1, 333, nil ); -- create the new produced items
-	if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
-		world:createItemFromId( EGG, notCreated, User.pos, true, 333, nil );
-		common.HighInformNLS(User,
-		"Du kannst nichts mehr halten und der Rest fällt zu Boden.",
-		"You can't carry any more and the rest drops to the ground.");
-	else -- character can still carry something
-		if (amount>0) then  -- there are still fruits we can gather
-			egggathering.SavedWorkTime[User.id] = egggathering:GenWorkTime(User,nil);
-			User:startAction( egggathering.SavedWorkTime[User.id], 0, 0, 0, 0);
-		end
-	end
-	if (amount<=0) then
+    User:learn( egggathering.LeadSkill, egggathering.SavedWorkTime[User.id], egggathering.LearnLimit);
+    amount = amount - 1;
+    local notCreated = User:createItem( EGG, 1, 333, nil ); -- create the new produced items
+    if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
+        world:createItemFromId( EGG, notCreated, User.pos, true, 333, nil );
+        common.HighInformNLS(User,
+        "Du kannst nichts mehr halten und der Rest fällt zu Boden.",
+        "You can't carry any more and the rest drops to the ground.");
+    else -- character can still carry something
+        if (amount>0) then  -- there are still fruits we can gather
+            egggathering.SavedWorkTime[User.id] = egggathering:GenWorkTime(User,nil);
+            User:startAction( egggathering.SavedWorkTime[User.id], 0, 0, 0, 0);
+        end
+    end
+    if (amount<=0) then
       common.HighInformNLS(User,
       "Dieses Nest ist leer.",
       "This nest is empty." );
-	  -- reset amount
-		amount = 10;
-		SourceItem:setData("amount","" .. amount);
-		world:changeItem(SourceItem);
-			-- change item id
-		world:swap(SourceItem, EMPTY_NEST, 333);
-		return;
+      -- reset amount
+        amount = 10;
+        SourceItem:setData("amount","" .. amount);
+        world:changeItem(SourceItem);
+            -- change item id
+        world:swap(SourceItem, EMPTY_NEST, 333);
+        return;
     end
 
-	SourceItem:setData("amount","" .. amount);
-	world:changeItem(SourceItem);
-	User:changeSource(SourceItem);
+    SourceItem:setData("amount","" .. amount);
+    world:changeItem(SourceItem);
+    User:changeSource(SourceItem);
 end
