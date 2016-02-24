da8f22feab696f4f9c03b66762a3b5eae193fd89
EviePryler <eviepryler@gmail.com>
2016-02-23 23:52:03 -0600

    remove tabs

--- content/gatheringcraft/sanddigging.lua
+++ content/gatheringcraft/sanddigging.lua
@@ -26,106 +26,106 @@ module("content.gatheringcraft.sanddigging", package.seeall)
 
 function StartGathering(User, SourceItem, ltstate)
 
-	gathering.InitGathering();
-	local sanddigging = gathering.sanddigging;
+    gathering.InitGathering();
+    local sanddigging = gathering.sanddigging;
 
-	common.ResetInterruption( User, ltstate );
-	if ( ltstate == Action.abort ) then -- work interrupted
+    common.ResetInterruption( User, ltstate );
+    if ( ltstate == Action.abort ) then -- work interrupted
         User:talk(Character.say, "#me unterbricht "..common.GetGenderText(User, "seine", "ihre").." Arbeit.", "#me interrupts "..common.GetGenderText(User, "his", "her").." work.")
-		return
-	end
+        return
+    end
 
-	if not common.CheckItem( User, SourceItem ) then -- security check
-		return
-	end
+    if not common.CheckItem( User, SourceItem ) then -- security check
+        return
+    end
 
-	-- additional tool item is needed
-	if (User:countItemAt("all",24)==0) then
-		common.HighInformNLS( User,
-		"Du brauchst eine Schaufel um zu graben.",
-		"You need a shovel for digging." );
-		return
-	end
-	local toolItem = User:getItemAt(5);
-	if ( toolItem.id ~=24 ) then
-		toolItem = User:getItemAt(6);
-		if ( toolItem.id ~= 24 ) then
-			common.HighInformNLS( User,
-			"Du musst die Schaufel in der Hand haben!",
-			"You have to hold the shovel in your hand!" );
-			return
-		end
-	end
+    -- additional tool item is needed
+    if (User:countItemAt("all",24)==0) then
+        common.HighInformNLS( User,
+        "Du brauchst eine Schaufel um zu graben.",
+        "You need a shovel for digging." );
+        return
+    end
+    local toolItem = User:getItemAt(5);
+    if ( toolItem.id ~=24 ) then
+        toolItem = User:getItemAt(6);
+        if ( toolItem.id ~= 24 ) then
+            common.HighInformNLS( User,
+            "Du musst die Schaufel in der Hand haben!",
+            "You have to hold the shovel in your hand!" );
+            return
+        end
+    end
 
-	if not common.FitForWork( User ) then -- check minimal food points
-		return
-	end
+    if not common.FitForWork( User ) then -- check minimal food points
+        return
+    end
 
-	common.TurnTo( User, SourceItem.pos ); -- turn if necessary
+    common.TurnTo( User, SourceItem.pos ); -- turn if necessary
 
-	-- check the amount
-	local MaxAmount = 20
-	local changeItem = false;
-	local amountStr = SourceItem:getData("amount");
-	local amount = 0;
-	if ( amountStr ~= "" ) then
-		amount = tonumber(amountStr);
-	elseif ( SourceItem.wear == 255 ) then
-		amount = MaxAmount;
-	end
-	
-	
-	-- currently not working -> let's go
-	if ( ltstate == Action.none ) then
-		sanddigging.SavedWorkTime[User.id] = sanddigging:GenWorkTime(User,toolItem);
-		User:startAction( sanddigging.SavedWorkTime[User.id], 0, 0, 0, 0);
-		User:talk(Character.say, "#me beginnt nach Sand zu graben.", "#me starts to dig for sand.")
-		return
-	end
+    -- check the amount
+    local MaxAmount = 20
+    local changeItem = false;
+    local amountStr = SourceItem:getData("amount");
+    local amount = 0;
+    if ( amountStr ~= "" ) then
+        amount = tonumber(amountStr);
+    elseif ( SourceItem.wear == 255 ) then
+        amount = MaxAmount;
+    end
+    
+    
+    -- currently not working -> let's go
+    if ( ltstate == Action.none ) then
+        sanddigging.SavedWorkTime[User.id] = sanddigging:GenWorkTime(User,toolItem);
+        User:startAction( sanddigging.SavedWorkTime[User.id], 0, 0, 0, 0);
+        User:talk(Character.say, "#me beginnt nach Sand zu graben.", "#me starts to dig for sand.")
+        return
+    end
 
-	-- since we're here, we're working
+    -- since we're here, we're working
 
-	if sanddigging:FindRandomItem(User) then
-		if ( changeItem ) then
-			world:changeItem(SourceItem);
-		end
-		return
-	end
-	
-	User:learn( sanddigging.LeadSkill, sanddigging.SavedWorkTime[User.id], sanddigging.LearnLimit);
-	amount = amount - 1;
-	-- update the amount
-	SourceItem:setData("amount", "" .. amount);
-	world:changeItem(SourceItem)
-	
-	local notCreated = User:createItem( 726, 1, 333, nil ); -- create the new produced items
-	if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
-		world:createItemFromId( 726, notCreated, User.pos, true, 333, nil );
-		common.HighInformNLS(User,
-		"Du kannst nichts mehr halten.",
-		"You can't carry any more.");
-	else -- character can still carry something
-		if amount > 0 then
-		    sanddigging.SavedWorkTime[User.id] = sanddigging:GenWorkTime(User,toolItem)
-		    User:changeSource(SourceItem);
-		User:startAction( sanddigging.SavedWorkTime[User.id], 0, 0, 0, 0)
-		end
-	end
+    if sanddigging:FindRandomItem(User) then
+        if ( changeItem ) then
+            world:changeItem(SourceItem);
+        end
+        return
+    end
+    
+    User:learn( sanddigging.LeadSkill, sanddigging.SavedWorkTime[User.id], sanddigging.LearnLimit);
+    amount = amount - 1;
+    -- update the amount
+    SourceItem:setData("amount", "" .. amount);
+    world:changeItem(SourceItem)
+    
+    local notCreated = User:createItem( 726, 1, 333, nil ); -- create the new produced items
+    if ( notCreated > 0 ) then -- too many items -> character can't carry anymore
+        world:createItemFromId( 726, notCreated, User.pos, true, 333, nil );
+        common.HighInformNLS(User,
+        "Du kannst nichts mehr halten.",
+        "You can't carry any more.");
+    else -- character can still carry something
+        if amount > 0 then
+            sanddigging.SavedWorkTime[User.id] = sanddigging:GenWorkTime(User,toolItem)
+            User:changeSource(SourceItem);
+        User:startAction( sanddigging.SavedWorkTime[User.id], 0, 0, 0, 0)
+        end
+    end
 
-	if amount == 0 then
+    if amount == 0 then
         SourceItem:setData("amount","")
         SourceItem.id = 3632
         SourceItem.wear = 4
-		world:changeItem(SourceItem)
-	    User:inform( "An dieser Stelle gibt es nicht mehrs zu holen.", "There isn't anything left in this pit.", Character.highPriority);
-		return
-	end
-	
-	if common.GatheringToolBreaks( User, toolItem, sanddigging:GenWorkTime(User,toolItem) ) then -- damage and possibly break the tool
-		common.HighInformNLS(User,
-		"Deine alte Schaufel zerbricht.",
-		"Your old shovel breaks.");
-		return
-	end
+        world:changeItem(SourceItem)
+        User:inform( "An dieser Stelle gibt es nicht mehrs zu holen.", "There isn't anything left in this pit.", Character.highPriority);
+        return
+    end
+    
+    if common.GatheringToolBreaks( User, toolItem, sanddigging:GenWorkTime(User,toolItem) ) then -- damage and possibly break the tool
+        common.HighInformNLS(User,
+        "Deine alte Schaufel zerbricht.",
+        "Your old shovel breaks.");
+        return
+    end
 
 end
