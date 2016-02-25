b308a57c541b97c86c32ed6c86e0605fd562f98b
EviePryler <eviepryler@gmail.com>
2016-02-24 23:36:30 -0600

    #11278 make snow piles yield snowballs

---
+++ item/id_929_snowpatch.lua
@@ -0,0 +1,31 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+-- UPDATE items SET itm_script='item.id_929_snowpatch' WHERE itm_id=929;
+
+local common = require("base.common")
+local lookat = require("base.lookat")
+local snowballmaking = require("content.gatheringcraft.snowballmaking")
+
+local M = {}
+
+function M.UseItem(User, SourceItem, ltstate)
+    User:inform("",
+            "There is insufficient snow in this pile to make snowballs.",Character.highPriority)
+end
+
+return M
