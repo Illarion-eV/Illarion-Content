bd0cb7f59b5e875a70775bdaca51d7753d088b82
EviePryler <eviepryler@gmail.com>
2016-03-25 19:29:26 -0500

    fairy scripts

---
+++ monster/race_115_fairy/id_1153_mystical_fairy.lua
@@ -0,0 +1,34 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+--ID 1152, Blood fairy, Level: 1
+
+local base = require("monster.base.base")
+local fairies = require("monster.race_115_fairy.base")
+
+local M = fairies.generateCallbacks()
+
+local orgOnSpawn = M.onSpawn
+function M.onSpawn(monster)
+    if orgOnSpawn ~= nil then
+        orgOnSpawn(monster)
+    end
+
+    base.setColor{monster = monster, target = base.SKIN_COLOR, red = 69, green = 7, blue = 120}
+end
+
+return M
---
+++ monster/race_115_fairy/id_1154_elemental_fairy.lua
@@ -0,0 +1,34 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+--ID 1154, Elemental fairy, Level: 1
+
+local base = require("monster.base.base")
+local fairies = require("monster.race_115_fairy.base")
+
+local M = fairies.generateCallbacks()
+
+local orgOnSpawn = M.onSpawn
+function M.onSpawn(monster)
+    if orgOnSpawn ~= nil then
+        orgOnSpawn(monster)
+    end
+
+    base.setColor{monster = monster, target = base.SKIN_COLOR, red = 26, green = 102, blue = 45}
+end
+
+return M
