b0c5a00f17d9445b1eb18ae0e9e448dea2d9c2c6
estralis <estralis@illarion.org>
2016-04-09 10:49:36 +0200

    Change order in gemcutting

--- content/craft/new/final/gemcutting.lua
+++ content/craft/new/final/gemcutting.lua
@@ -28,18 +28,18 @@ local gemcutting = Craft {
     sfx = 16, sfxDuration = 1.7,
     
 ProductGroup {name = {english = "Gems", german = "Edelsteine"},
-Product {item = "ruby", Ingredient {item = "rawRuby", amount = 1}},
 Product {item = "amethyst", Ingredient {item = "rawAmethyst", amount = 1}},
-Product {item = "obsidian", Ingredient {item = "rawObsidian", amount = 1}},
+Product {item = "ruby", Ingredient {item = "rawRuby", amount = 1}},
 Product {item = "sapphire", Ingredient {item = "rawSapphire", amount = 1}},
+Product {item = "obsidian", Ingredient {item = "rawObsidian", amount = 1}},
 Product {item = "emerald", Ingredient {item = "rawEmerald", amount = 1}},
 Product {item = "topaz", Ingredient {item = "rawTopaz", amount = 1}},
 Product {item = "diamond", Ingredient {item = "rawDiamond", amount = 1}}
 },
 
 ProductGroup {name = {english = "Gem powder", german = "Edelsteinstaub"},
-Product {item = "rubyPowder", Ingredient {item = "ruby", amount = 2}},
 Product {item = "amethystPowder", Ingredient {item = "amethyst", amount = 2}},
+Product {item = "rubyPowder", Ingredient {item = "ruby", amount = 2}},
 Product {item = "sapphirePowder", Ingredient {item = "sapphire", amount = 2}},
 Product {item = "obsidianPowder", Ingredient {item = "obsidian", amount = 2}},
 Product {item = "emeraldPowder", Ingredient {item = "emerald", amount = 2}},
