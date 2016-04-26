694f0caaef51e55fccbda593eaffa2572c8c98b8
EviePryler <eviepryler@gmail.com>
2016-04-26 13:23:34 -0500

    continue standardizing

--- item/food.lua
+++ item/food.lua
@@ -78,22 +78,22 @@ FoodList = { add = function (self,id,Value,Leftover,BuffType,RacialFactor,UnEata
 FoodList:add(  15,VALUE_MEDIUM,0); -- apple
 FoodList:add(  80,VALUE_MEDIUM,0); -- banana
 FoodList:add(  81,VALUE_MEDIUM,0); -- berries
-FoodList:add( 142,VALUE_SMALL,0); -- sand berry
-FoodList:add( 143,VALUE_SMALL,0); -- red elder
-FoodList:add( 147,VALUE_SMALL,0); -- black berry
-FoodList:add( 151,VALUE_SMALL,0); -- strawberries
-FoodList:add( 160,VALUE_SMALL,0); -- redhead
-FoodList:add( 161,VALUE_SMALL,0); -- herders mushroom
-FoodList:add( 163,VALUE_SMALL,0); -- champignon
+FoodList:add( 142,VALUE_MEDIUM,0); -- sand berry
+FoodList:add( 143,VALUE_MEDIUM,0); -- red elder
+FoodList:add( 147,VALUE_MEDIUM,0); -- black berry
+FoodList:add( 151,VALUE_MEDIUM,0); -- strawberries
+FoodList:add( 160,VALUE_MEDIUM,0); -- redhead
+FoodList:add( 161,VALUE_MEDIUM,0); -- herders mushroom
+FoodList:add( 163,VALUE_MEDIUM,0); -- champignon
 FoodList:add( 199,VALUE_MEDIUM,0); -- tangerine
 FoodList:add( 200,VALUE_MEDIUM,0); -- tomato
 FoodList:add( 201,VALUE_MEDIUM,0); -- onion
 FoodList:add( 302,VALUE_MEDIUM,0); -- cherry
 FoodList:add( 388,VALUE_MEDIUM,0); -- grapes
-FoodList:add( 759,VALUE_SMALL,0); -- nuts
+FoodList:add( 759,VALUE_MEDIUM,0); -- nuts
 FoodList:add(2493,VALUE_MEDIUM,0); -- carrots
-FoodList:add(1149,VALUE_SMALL,0); -- brown egg
-FoodList:add(1150,VALUE_SMALL,0); -- white egg
+FoodList:add(1149,VALUE_MEDIUM,0); -- brown egg
+FoodList:add(1150,VALUE_MEDIUM,0); -- white egg
 FoodList:add(1207,VALUE_MEDIUM,0); -- orange
 FoodList:add(3567,VALUE_MEDIUM,0); -- potato
 FoodList:add(290,VALUE_MEDIUM,0); -- Cabbage
