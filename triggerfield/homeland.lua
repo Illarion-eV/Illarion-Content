004c8e65f6c1c4b2298a1e806b94d5c7d6566c55
EviePryler <eviepryler@gmail.com>
2016-05-05 14:24:40 -0500

    set up triggerfield new bridge leaving homeland

--- triggerfield/homeland.lua
+++ triggerfield/homeland.lua
@@ -26,6 +26,9 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- INSERT INTO triggerfields VALUES (784,690,0,'triggerfield.homeland');
 -- INSERT INTO triggerfields VALUES (785,690,0,'triggerfield.homeland');
 
+-- INSERT INTO triggerfields VALUES (161,748,0,'triggerfield.homeland');
+-- INSERT INTO triggerfields VALUES (162,748,0,'triggerfield.homeland');
+
 local common = require("base.common")
 
 local M = {}
@@ -39,7 +42,8 @@ function M.MoveToField(char)
     questvalue[1] = 166 --Cadomyr
     questvalue[2] = 167 --Galmair
     questvalue[3] = 168 --Runewick
-	questvalue[4] = 169 --Runewick second bridge set
+    questvalue[4] = 169 --Runewick second bridge set
+    questvalue[5] = 172 --Runewick second bridge set
 
     local location = {}
     location[1] = position(216, 647, 0) --Cadomyr
@@ -48,8 +52,10 @@ function M.MoveToField(char)
     location[4] = position(478, 250, 0) --Galmair
     location[5] = position(888, 646, 0) --Runewick
     location[6] = position(889, 646, 0) --Runewick
-	location[7] = position(784, 690, 0) --Runewick
+    location[7] = position(784, 690, 0) --Runewick
     location[8] = position(785, 690, 0) --Runewick
+    location[9] = position(161, 748, 0) --Cadomyr
+    location[10] = position(162, 748, 0) --Cadomyr
 
     local factionString
     if (char:getQuestProgress(questvalue[1]) == 0) and (char:getFaceTo() == 1 or char:getFaceTo() == 2 or char:getFaceTo() == 3) and (char.pos == location[1] or char.pos == location[2]) then --Didn't visit the triggerfield yet
@@ -64,6 +70,9 @@ function M.MoveToField(char)
     elseif (char:getQuestProgress(questvalue[4]) == 0) and (char:getFaceTo() == 7 or char:getFaceTo() == 0 or char:getFaceTo() == 1) and (char.pos == location[7] or char.pos == location[8]) then --Didn't visit the triggerfield yet
         char:setQuestProgress(questvalue[4], 1) --player was here
         factionString = "Runewick"
+    elseif (char:getQuestProgress(questvalue[5]) == 0) and (char:getFaceTo() == 3 or char:getFaceTo() == 4 or char:getFaceTo() == 5) and (char.pos == location[9] or char.pos == location[10]) then --Didn't visit the triggerfield yet
+        char:setQuestProgress(questvalue[5], 1) --player was here
+        factionString = "Cadomyr"
     else
         return --bailing out
     end
