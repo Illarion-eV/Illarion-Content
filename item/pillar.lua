11a5461ad254b742a96b86fd99972d778591cd27
EviePryler <eviepryler@gmail.com>
2016-07-29 22:24:57 -0500

    update galmair market statues for new chancellors

--- item/pillar.lua
+++ item/pillar.lua
@@ -196,8 +196,8 @@ function M.UseItem(User, SourceItem, ltstate)
         --Dialog
         local dialogTitle = common.GetNLS(User, "Liste der Galmarianischen Kanzler", "List of Galmairian Chancellors")
         local dialogText = common.GetNLS(User,
-            "Evie Pryler (Dranis) im Jahr 47\nEvie Pryler im Jahr 46\nArtimer Fault im Jahr 45\nArcia im Jahr 44\nEvie Pryler im Jahr 43\nOxiana im Jahr42\nOxiana im Jahr 41",
-            "Evie Pryler (Dranis) in the year 47\nEvie Pryler in the year 46\nArtimer Fault in the year 45\nArcia in the year 44\nEvie Pryler in the year 43\nOxiana in the year 42\nOxiana in the year 41")
+            "Unbesetzt im Jahr 48\nEvie Pryler (Dranis) im Jahr 47\nEvie Pryler im Jahr 46\nArtimer Fault im Jahr 45\nArcia im Jahr 44\nEvie Pryler im Jahr 43\nOxiana im Jahr42\nOxiana im Jahr 41",
+            "Vacant in the year 48\nEvie Pryler in the year 46\nArtimer Fault in the year 45\nArcia in the year 44\nEvie Pryler in the year 43\nOxiana in the year 42\nOxiana in the year 41")
         local callbackChancellor1 = function(nothing) end --empty callback
         local dialogChancellor1 = MessageDialog(dialogTitle, dialogText, callbackChancellor1)
 
@@ -208,8 +208,8 @@ function M.UseItem(User, SourceItem, ltstate)
         --Dialog
         local dialogTitle = common.GetNLS(User, "Liste der Galmarianischen Kanzler", "List of Galmairian Chancellors")
         local dialogText = common.GetNLS(User,
-            "Raina Narethil im Jahr 47\nKyre im Jahr 46\nKyre im Jahr 45\nKyre im Jahr 44\nVelisai im Jahr 43\nKyre im Jahr42\nKyre im Jahr 41",
-            "Raina Narethil in the year 47\nKyre in the year 45\nKyre in the year 44\nVelisai in the year 43\nKyre in the year 42\nKyre in the year 41")
+            "Bernie Bottoms im Jahr 48\nRaina Narethil im Jahr 47\nKyre im Jahr 46\nKyre im Jahr 45\nKyre im Jahr 44\nVelisai im Jahr 43\nKyre im Jahr42\nKyre im Jahr 41",
+            "Bernie Bottoms in the year 48\nRaina Narethil in the year 47\nKyre in the year 45\nKyre in the year 44\nVelisai in the year 43\nKyre in the year 42\nKyre in the year 41")
         local callbackChancellor2 = function(nothing) end --empty callback
         local dialogChancellor2 = MessageDialog(dialogTitle, dialogText, callbackChancellor2)
 
@@ -220,8 +220,8 @@ function M.UseItem(User, SourceItem, ltstate)
         --Dialog
         local dialogTitle = common.GetNLS(User, "Liste der Galmarianischen Kanzler", "List of Galmairian Chancellors")
         local dialogText = common.GetNLS(User,
-            "Artimer Fault im Jahr 47\nUlquiorra Dreadhart im Jahr 46\nTialdin im Jahr 45\nSarangerel (Ufedhin Copperhand) im Jahr 44\nSarangerel im Jahr 43\nUhuru Pryler im Jahr42\nPurple im Jahr 41",
-            "Artimer Fault in the year 47\nUlquiorra Dreadhart in the year 46\nTialdin in the year 45\nSarangerel (Ufedhin Copperhand) in the year 44\nSarangerel in the year 43\nUhuru Pryler in the year 42\nPurple in the year 41")
+            "Banduk Robberhauf el Luastelay im Jahr 48\nArtimer Fault im Jahr 47\nUlquiorra Dreadhart im Jahr 46\nTialdin im Jahr 45\nSarangerel (Ufedhin Copperhand) im Jahr 44\nSarangerel im Jahr 43\nUhuru Pryler im Jahr42\nPurple im Jahr 41",
+            "Banduk Robberhauf el Luastelay in the year 48\nUlquiorra Dreadhart in the year 46\nTialdin in the year 45\nSarangerel (Ufedhin Copperhand) in the year 44\nSarangerel in the year 43\nUhuru Pryler in the year 42\nPurple in the year 41")
         local callbackChancellor3 = function(nothing) end --empty callback
         local dialogChancellor3 = MessageDialog(dialogTitle, dialogText, callbackChancellor3)
 
