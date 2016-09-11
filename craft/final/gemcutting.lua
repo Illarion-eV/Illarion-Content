053faf153961fab061c4f2a013abba4965f0d5a9
estralis <estralis@illarion.org>
2016-09-11 11:34:33 +0200

    Roll out gemcutting

---
+++ craft/final/gemcutting.lua
@@ -0,0 +1,89 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>. 
+]]
+local crafts = require("craft.crafts")
+
+module("craft.final.gemcutting", package.seeall)
+
+gemcutting = crafts.Craft:new{
+                          craftEN = "Gemcutting",
+                          craftDE = "Edelsteinschleifen",
+                          handTool = 2140,
+                          leadSkill = Character.gemcutting,
+                        };
+
+gemcutting:addTool(270)  -- cutting stone
+
+local catId = gemcutting:addCategory("Gems", "Edelsteine")
+
+-- amethyst
+product = gemcutting:addProduct(catId, 197, 1)
+product:addIngredient(251, 1) -- raw amethyst
+
+-- ruby
+product = gemcutting:addProduct(catId, 46, 1)
+product:addIngredient(255, 1) -- raw ruby
+
+-- sapphire
+product = gemcutting:addProduct(catId, 284, 1)
+product:addIngredient(253, 1) -- raw sapphire
+
+-- obsidian
+product = gemcutting:addProduct(catId, 283, 1)
+product:addIngredient(252, 1) -- raw obsidian
+
+-- emerald
+product = gemcutting:addProduct(catId, 45, 1)
+product:addIngredient(256, 1) -- raw emerald
+
+-- topaz
+product = gemcutting:addProduct(catId, 198, 1)
+product:addIngredient(257, 1) -- raw topaz
+
+-- diamond
+product = gemcutting:addProduct(catId, 285, 1)
+product:addIngredient(254, 1) -- raw diamond
+
+local catId = gemcutting:addCategory("Gem powder", "Edelsteinstaub")
+
+-- amethyst powder
+product = gemcutting:addProduct(catId, 450, 1)
+product:addIngredient(197, 1) -- amethyst
+
+-- ruby powder
+product = gemcutting:addProduct(catId, 447, 1)
+product:addIngredient(46, 1) -- ruby
+
+-- sapphire powder
+product = gemcutting:addProduct(catId, 446, 1)
+product:addIngredient(284, 1) -- sapphire
+
+-- obsidian powder
+product = gemcutting:addProduct(catId, 449, 1)
+product:addIngredient(283, 1) -- obsidian
+
+-- emerald powder
+product = gemcutting:addProduct(catId, 448, 1)
+product:addIngredient(45, 1) -- emerald
+
+-- topaz powder
+product = gemcutting:addProduct(catId, 451, 1)
+product:addIngredient(198, 1) -- topaz
+
+-- diamond powder
+product = gemcutting:addProduct(catId, 452, 1)
+product:addIngredient(285, 1) -- diamond
+
