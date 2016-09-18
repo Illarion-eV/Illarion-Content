7de8c8e654a45b2193dd0b2b7e287fb2085d7ad1
estralis <estralis@illarion.org>
2016-09-18 12:56:04 +0200

    Activate sieving

--- item/id_312_woodenshovel.lua
+++ item/id_312_woodenshovel.lua
@@ -15,83 +15,12 @@ You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
--- UPDATE items SET itm_script='item.id_312_woodenshovel' WHERE itm_id IN (312);
-
-local licence = require("base.licence")
-local graingrinding = require("content.gatheringcraft.graingrinding")
-local sieving = require("content.gatheringcraft.sieving")
-local wood = require("item.general.wood")
-local common = require("base.common")
+local sieving = require("craft.intermediate.sieving")
 
 local M = {}
 
-M.LookAtItem = wood.LookAtItem
-
-function getMill(User)
-
-	local targetItem = common.GetFrontItem(User);
-	if (targetItem ~= nil and targetItem.id == 250) then
-		return targetItem;
-	end
-
-	local Radius = 1;
-	for x=-Radius,Radius do
-		for y=-Radius,Radius do
-			local targetPos = position(User.pos.x + x, User.pos.y + y, User.pos.z);
-			if (world:isItemOnField(targetPos)) then
-				local targetItem = world:getItemOnField(targetPos);
-				if (targetItem ~= nil and targetItem.id == 250) then
-					return targetItem;
-				end
-			end
-		end
-	end
-	return nil;
-end
-
-function getSieve(User)
-
-	local targetItem = common.GetFrontItem(User);
-	if (targetItem ~= nil and targetItem.id == 727) then
-		return targetItem;
-	end
-
-	local Radius = 1;
-	for x=-Radius,Radius do
-		for y=-Radius,Radius do
-			local targetPos = position(User.pos.x + x, User.pos.y + y, User.pos.z);
-			if (world:isItemOnField(targetPos)) then
-				local targetItem = world:getItemOnField(targetPos);
-				if (targetItem ~= nil and targetItem.id == 727) then
-					return targetItem;
-				end
-			end
-		end
-	end
-	return nil;
-end
-
 function M.UseItem(User, SourceItem, ltstate)
-	if licence.licence(User) then --checks if user is citizen or has a licence
-		return -- avoids crafting if user is neither citizen nor has a licence
-	end
-
-	local millItem = getMill(User);
-	if millItem then
-		graingrinding.StartGathering(User, millItem, ltstate);
-		return
-	end
-
-	local sieveItem = getSieve(User);
-	if sieveItem then
-		sieving.StartGathering(User, sieveItem, ltstate);
-		return
-	end
-
-	common.InformNLS(User,
-		"Du musst neben einem Mühlstein oder Sieb stehen um die Holzkelle zu benutzen.",
-		"You must stand next to a millstone or a sieve to use the wooden shovel.");
+    sieving.sieving:showDialog(User, SourceItem)
 end
 
 return M
-
