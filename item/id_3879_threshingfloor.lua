1ac38ee1e3cd279746bf01a3e10c95ce673d4893
estralis <estralis@illarion.org>
2016-09-18 13:28:53 +0200

    Activate threshing

---
+++ item/id_3879_threshingfloor.lua
@@ -0,0 +1,26 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+local threshing = require("craft.intermediate.threshing")
+
+local M = {}
+
+function M.UseItem(User, SourceItem, ltstate)
+    threshing.threshing:showDialog(User, SourceItem)
+end
+
+return M
