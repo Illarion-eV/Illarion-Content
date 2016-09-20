8507eedd3880c933a32aa846e1b25bfa8d2c69bd
estralis <estralis@illarion.org>
2016-09-20 22:12:19 +0200

    Correct crafting time

--- craft/crafts.lua
+++ craft/crafts.lua
@@ -395,7 +395,7 @@ function Product:getCraftingTime(skill)
         learnProgress = (skill - self.difficulty) / (self.learnLimit - self.difficulty) * 100
     end
     local theItem = world:getItemStatsFromId(self.item)
-    local minimum = math.max ((theItem.Worth * 0.0024),1) * self.quantity
+    local minimum = math.max ((self.quantity * theItem.Worth * 0.0024),1) 
     local craftingTime = common.Scale(minimum * 2, minimum, learnProgress)
     craftingTime = 5 * math.ceil(craftingTime)
     return craftingTime
