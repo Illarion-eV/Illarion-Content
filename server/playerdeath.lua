56e97e7b07c828c294cde12abe5994b72a425393
estralis <estralis@illarion.org>
2016-11-01 17:20:31 +0100

    #11435: Fix item damage upon death

--- server/playerdeath.lua
+++ server/playerdeath.lua
@@ -66,8 +66,7 @@ function M.playerDeath(deadPlayer)
         for i = Character.head, Character.coat do
             local item = deadPlayer:getItemAt(i)
             local commonItem = world:getItemStats(item)
-
-            if item.id > 0 and item.id ~= BLOCKED_ITEM and item.quality > 100 and commonItem.maxStack == 1 then
+            if item.id > 0 and item.id ~= BLOCKED_ITEM and item.quality > 100 and commonItem.MaxStack == 1 then
                 local durability = item.quality % 100
                 local newbieModficator = 1
                 if deadPlayer:isNewPlayer() then
