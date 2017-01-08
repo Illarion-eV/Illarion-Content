b9e55c7a3759d160c1769e19e5baca8b5138c3ae
merung <karten-spiel-haus@hotmail.de>
2017-01-08 15:16:49 +0100

    Increase balancing factor

--- magic/magicfighting.lua
+++ magic/magicfighting.lua
@@ -212,7 +212,7 @@ local function applyDamage(attackerStruct, defenderStruct)
     end
     attackerStruct.Char:inform("armourValue 3: "..armourValue)
     attackerStruct.Char:inform("defQualityBonus: "..defQualityBonus)
-    damage = damage - (damage * armourValue * defQualityBonus/140)
+    damage = damage - (damage * armourValue * defQualityBonus/200)
     damage = defSkillBonus * damage
     attackerStruct.Char:inform("DD 3: "..damage) -- DEBUG
     local resistance = math.max(1, math.floor(
