9a66a495a4d7dcf934f0a183a20d13b2f2f280d8
estralis <estralis@illarion.org>
2017-01-15 12:47:51 +0100

    Typo

--- magic/learnMagic.lua
+++ magic/learnMagic.lua
@@ -129,7 +129,7 @@ function M.useMagicWand(user, sourceItem)
                 
                 local messageDialog
                 if user:getPlayerLanguage() == Player.german then
-                    messageDialog = MessageDialog("Ein neuer Magier", "Du gibst dich der im Stab verborgenen arkanen Kraft hin und lässt sie durch deinen Körper fließen. Ja! Du kannst diese Macht beherrschen. Von nun an bist du in der Lage Magie zu werden. Der Stab wird dir gehorchen.", messageCallback)
+                    messageDialog = MessageDialog("Ein neuer Magier", "Du gibst dich der im Stab verborgenen arkanen Kraft hin und lässt sie durch deinen Körper fließen. Ja! Du kannst diese Macht beherrschen. Von nun an bist du in der Lage Magier zu werden. Der Stab wird dir gehorchen.", messageCallback)
                 else
                     messageDialog = MessageDialog("A new mage", "You induldge in the hidden arcane powers which you found in the wand. You let it run through your body. Yes! You are now able to control this force. From this day on, you are able to use magic. The wand will obey your commands.", messageCallback)
                 end
