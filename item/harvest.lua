a4149d60b6e24e136a6dbc8f3120a32b248e05d2
EviePryler <eviepryler@gmail.com>
2017-01-15 22:39:28 -0600

    #11411 add new blackberry bushes to scripts

--- item/harvest.lua
+++ item/harvest.lua
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
--- UPDATE items SET itm_script='item.harvest' WHERE itm_id IN (14,300,386,387,1195,11,299,386,1193,1194,3612,3613,3742,3743);
+-- UPDATE items SET itm_script='item.harvest' WHERE itm_id IN (14,300,386,387,1195,11,299,386,1193,1194,3612,3613,3742,3743,3782,3783);
 
 local common = require("base.common")
 local fruitgathering = require("content.gatheringcraft.fruitgathering")
@@ -25,9 +25,9 @@ local M = {}
 
 function M.UseItem(User, SourceItem, ltstate)
 
-    if SourceItem.id == 14 or SourceItem.id == 300 or SourceItem.id == 387 or SourceItem.id == 1195 or SourceItem.id == 3613 or SourceItem.id == 3743 or SourceItem.id ==3867 then
+    if SourceItem.id == 14 or SourceItem.id == 300 or SourceItem.id == 387 or SourceItem.id == 1195 or SourceItem.id == 3613 or SourceItem.id == 3743 or SourceItem.id ==3867 or SourceItem.id == 3892 then
         fruitgathering.StartGathering(User, SourceItem, ltstate);
-    elseif SourceItem.id == 386 or SourceItem.id == 3612 or SourceItem.id == 3742 or SourceItem.id == 3866 then
+    elseif SourceItem.id == 386 or SourceItem.id == 3612 or SourceItem.id == 3742 or SourceItem.id == 3866 or SourceItem.id == 3893 then
         common.HighInformNLS( User,"Diese Pflanze trägt keine Früchte.","This plant bears no fruits." );
     elseif SourceItem.id == 1193 or SourceItem.id == 1194 then
         common.HighInformNLS( User,"Dieser Baum trägt keine Früchte.","This tree bears no fruits." );
