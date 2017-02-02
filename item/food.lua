6df1c6f785f2db9d4af1d772d94c6bd1d6a8c9e1
merung <karten-spiel-haus@hotmail.de>
2017-02-02 23:18:01 +0100

    Fix broken quest

--- item/food.lua
+++ item/food.lua
@@ -401,6 +401,8 @@ function M.UseItem(User, sourceItem, ltstate)
         return
     elseif mapItem(User, sourceItem) then
         return
+    elseif thievesPoisonQuest(User, sourceItem)
+        return
     end
     
     fortuneCookie(sourceItem, User)
