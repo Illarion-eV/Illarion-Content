111a68ed240e5cecfdb9cdc57e120c2ed3e0f54c
estralis <estralis@illarion.org>
2017-02-11 23:31:37 +0100

    Text improvements

--- quest/thorgrem_silverbeard_708_galmair.lua
+++ quest/thorgrem_silverbeard_708_galmair.lua
@@ -25,8 +25,8 @@ local ENGLISH = Player.english
 
 -- Insert the quest title here, in both languages
 local Title = {}
-Title[GERMAN] = "Das Schmiedehandwerk"
-Title[ENGLISH] = "The smithing craft"
+Title[GERMAN] = "Die Schmiedegilde von Galmair"
+Title[ENGLISH] = "The blacksmithing guild of Galmair"
 
 -- Insert an extensive description of each status here, in both languages
 -- Make sure that the player knows exactly where to go and what to do
