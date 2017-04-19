2d2dd6e378dd56f4af7c00dbd8e3eb7682ae0b4b
estralis <estralis@illarion.org>
2017-04-19 19:09:37 +0200

    Move specific stuff from general script

--- item/general/wood.lua
+++ item/general/wood.lua
@@ -17,7 +17,6 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 local lookat = require("base.lookat")
 local checks = require("item.general.checks")
-local learnMagic = require("magic.learnMagic")
 
 local M = {}
 
@@ -30,47 +29,17 @@ local M = {}
 -- Weapons:
 -- UPDATE common SET com_script='item.general.wood' WHERE com_itemid IN ();
 
-local magicWands = {}
-    magicWands[323] = true
-    magicWands[2782] = true
-    magicWands[2783] = true
-    magicWands[2784] = true
-    magicWands[2785] = true
-    magicWands[3608] = true
-    
-
 function M.LookAtItem(user, item)
     return lookat.GenerateLookAt(user, item, lookat.WOOD)
 end
 
 function M.MoveItemBeforeMove(User, SourceItem, TargetItem)
-    local questProgress = User:getQuestProgress(38)
+
     if TargetItem:getType() == 4 then --inventory, not belt
-        if magicWands[SourceItem.id]then
-            if User:getMagicType() == 3 then 
-                User:inform("Alchemisten können die Stabmagie nicht erlernen.",
-                "Alchemist are unable to use wand magic.")
-            elseif User:increaseAttrib("willpower", 0) + User:increaseAttrib("essence", 0) + User:increaseAttrib("intelligence", 0) < 30 then
-                User:inform("Um Stabmagie zu benutzen muss die Summe der Attribute Intelligenz, Essenz und Willensstärke 30 ergeben. Attribute können bei den Trainer NPC's geändert werden.",
-                "To use wand magic, your combined attributes of intelligence, essence, and willpower must total at least 30. Attributes can be changed at the trainer NPC.")
-            elseif User:getMagicType() == 0 and (User:getQuestProgress(37) ~= 0 or User:getMagicFlags(0) > 0) then 
-            elseif bit32.extract(questProgress, 30) == 0 then
-                User:inform("Um das Handwerk der Stabmagie zu erlernen, musst du drei Bücher über magische Theorie lesen. Sieh dir die Liste der Bücher in den Bibliotheken der Städte.",
-                "To learn the craft of wand magic you must read three books of magical theory. Look for the list of books in your town's library.")
-            end
-        else
-            return checks.checkLevel(User, SourceItem)
-        end
+        return checks.checkLevel(User, SourceItem)
     end
 
     return true
-end
-
-function M.UseItem(user, sourceItem)
-
-    if magicWands[sourceItem.id] then
-        learnMagic.useMagicWand(user, sourceItem)
-    end
 end
 
 return M
