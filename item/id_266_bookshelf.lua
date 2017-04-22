5e2effa65415b936a464dc3be74cef70a8ffc169
Baylamon <banduk@illarion.org>
2017-04-22 21:42:18 +0200

    new book "The Land Illarion"

--- item/id_266_bookshelf.lua
+++ item/id_266_bookshelf.lua
@@ -205,6 +205,7 @@ M.bookList["magie_gift"] = {english = "The Old Path of Magic",german =  "Der alt
 M.bookList["firstaid"] = {english = "First Aid",german =  "Erste Hilfe", bookId = 412, bookGraphic = bookRedBooklet}
 M.bookList["origionofmagic"] = {english = "About the Origins of Magic",german =  "Von der Herkunft der Magie", bookId = 413, bookGraphic = bookYellowVeryHeavy}
 M.bookList["historysilverbrand"] = {english = "The History of Silverbrand",german =  "Die Geschichte Silberbrands", bookId = 414, bookGraphic = bookGreyLargeHeavy}
+M.bookList["landillarion"] = {english = "The Land Illarion",german =  "Das Land Illarion", bookId = 415, bookGraphic = bookBlueOrnament}
 M.bookList["cadomyr_order1"] = {english = "Order 04. Findos 38 AW",german =  "Befehl 04. Findos 38 n.VdH", bookId = 501, bookGraphic = pell}
 M.bookList["speech_rosaline_32_11_05"] = {english = "Speech 04. Findos 38 AW",german =  "Rede 05. Findos 38 n.VdH", bookId = 502, bookGraphic = pell}
 M.bookList["galmair_adjutant"] = {english = "Wanted! Adjutant",german =  "Gesucht! Adjutant", bookId = 506, bookGraphic = pell}
