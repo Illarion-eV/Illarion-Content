d780017a96d35094228b0ac4c27ab727a1eae1a4
merung <karten-spiel-haus@hotmail.de>
2017-05-31 19:25:09 +0200

    Remove outdated comment

--- alchemy/base/alchemy.lua
+++ alchemy/base/alchemy.lua
@@ -14,8 +14,6 @@ details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
--- ds_base_alchemy.lua
-
 local common = require("base.common")
 local licence = require("base.licence")
 local M = {}
