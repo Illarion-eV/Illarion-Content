faa5d861a823b28412a359d9f02ad58ec0f1aeda
EviePryler <eviepryler@gmail.com>
2017-06-16 00:26:09 -0500

    more spell checks thomas weste

--- quest/thomas_weste_716_runewick.lua
+++ quest/thomas_weste_716_runewick.lua
@@ -26,7 +26,7 @@ local ENGLISH = Player.english
 -- Insert the quest title here, in both languages
 local Title = {}
 Title[GERMAN] = "Adrons Braukunst Schule"
-Title[ENGLISH] = "Adrons Brewery School"
+Title[ENGLISH] = "Adron's Brewery School"
 
 -- Insert an extensive description of each status here, in both languages
 -- Make sure that the player knows exactly where to go and what to do
