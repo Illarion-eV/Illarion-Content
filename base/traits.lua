180c388b8f737952c7819e837437440397cef4d6
merung <karten-spiel-haus@hotmail.de>
2017-11-19 14:41:48 +0100

    Fix encoding

--- base/traits.lua
+++ base/traits.lua
@@ -38,5 +38,5 @@ local GENERAL = 1
 
 --Passive income--
 -- Beginning of each month, the character is granted some money by their secret sources.
-M.PASSIVE_INCOME = {questId = 720, bitPostion = 1, nameDe = "Passives Einkommen", nameEn = "Passive income", descriptionDe = "Jeden Monat erh√§lst du einen kleinen Geldbetrag aus deinen Quellen.", descriptionEn = "Every month, your sources grant you a small amount of money.", category = GENERAL)
+M.PASSIVE_INCOME = {questId = 720, bitPostion = 1, nameDe = "Passives Einkommen", nameEn = "Passive income", descriptionDe = "Jeden Monat erh‰lst du einen kleinen Geldbetrag aus deinen Quellen.", descriptionEn = "Every month, your sources grant you a small amount of money.", category = GENERAL)
 table.insert(allTraits, M.PASSIVE_INCOME)
