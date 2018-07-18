6db8b5a511c053549834797fa7f9b781da25c767
Baylamon <banduk@illarion.org>
2018-07-18 18:10:48 +0200

    Wand in Hand

--- item/wands.lua
+++ item/wands.lua
@@ -64,7 +64,7 @@ local function useWandSelection(user, item, ltstate)
         elseif actionIndex[index] == ACTION_FIND_GLYPH_FORGE then
             currentWandUse[user.id] = nil
             if not glyphmagic.findGlyphForge(user) then
-                common.InformNLS(user,"Du kannst in der weiteren Umgebung keinen Ritualplatz auchmachen.",
+                common.InformNLS(user,"Du kannst in der weiteren Umgebung keinen Ritualplatz ausmachen.",
                                       "You cannot detect any existing ritual place in that area.")
             end
         elseif actionIndex[index] == ACTION_COUNT_SHARDS then
@@ -130,20 +130,24 @@ function M.MoveItemAfterMove(User, SourceItem, TargetItem)
 end
 
 function M.UseItem(user, sourceItem, ltstate)
-    if ltstate == Action.none then
-        if magicWands[sourceItem.id] then
-            if user:getMagicType() == 0 and user:getQuestProgress(37) ~= 0 then
-                useWandSelection(user, sourceItem, ltstate)
-            else
-                learnMagic.useMagicWand(user, sourceItem)
+    if common.IsItemInHands(sourceItem) then
+        if ltstate == Action.none then
+            if magicWands[sourceItem.id] then
+                if user:getMagicType() == 0 and user:getQuestProgress(37) ~= 0 then
+                    useWandSelection(user, sourceItem, ltstate)
+                else
+                    learnMagic.useMagicWand(user, sourceItem)
+                end
+            end
+        else
+            if currentWandUse[user.id] == WAND_USE_GLYPH_FORGE_ERECT then
+                glyphmagic.placeGlyphForge(user, ltstate)
+            elseif currentWandUse[user.id] == WAND_USE_GLYPH_RITUAL_PREPARE then
+                glyphmagic.prepareGlyphRitual(user, ltstate)
             end
         end
     else
-        if currentWandUse[user.id] == WAND_USE_GLYPH_FORGE_ERECT then
-            glyphmagic.placeGlyphForge(user, ltstate)
-        elseif currentWandUse[user.id] == WAND_USE_GLYPH_RITUAL_PREPARE then
-            glyphmagic.prepareGlyphRitual(user, ltstate)
-        end
+        user:inform("Du solltest lieber den Zauberstab in die Hand nehmen, wenn du ihn benutzen willst.","To use the wand you should take it in your hands.")
     end
 end
 
