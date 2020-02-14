8c365b9ba56f0f462734d7486442f64d40af7aaa
cjk <chelsea.j.kay@gmail.com>
2020-02-14 14:23:03 -0500

    Make secondary fraction a constant as well.

--- npc/base/trade.lua
+++ npc/base/trade.lua
@@ -30,6 +30,7 @@ local baseNPC = require("npc.base.basic")
 local isFittingItem
 local tradeNPCItem
 local PRIMARY_PRICE = 0.15
+local SECONDARY_FRACTION = 2/3
 
 local tradeNPC = class(function(self, rootNPC)
     if rootNPC == nil or not rootNPC:is_a(baseNPC) then
@@ -241,7 +242,7 @@ tradeNPCItem = class(function(self, id, itemType, nameDe, nameEn, price, stack, 
         elseif (itemType == "buyPrimary") then
             self["_price"] = world:getItemStatsFromId(id).Worth * PRIMARY_PRICE
         elseif (itemType == "buySecondary") then
-            self["_price"] = world:getItemStatsFromId(id).Worth * PRIMARY_PRICE * 2/3
+            self["_price"] = world:getItemStatsFromId(id).Worth * PRIMARY_PRICE * SECONDARY_FRACTION
         end
     else
         self["_price"] = price
