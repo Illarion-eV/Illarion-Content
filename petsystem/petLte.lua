ba7ae1a95b017053970118b51bf403a359ac61a6
merung <karten-spiel-haus@hotmail.de>
2020-02-15 12:17:08 +0100

    Lower call time of pet lte for better performance

--- petsystem/petLte.lua
+++ petsystem/petLte.lua
@@ -100,7 +100,7 @@ function M.callEffect(petEffect, pet)
     local hp = pet:increaseAttrib("hitpoints", 0)
     base.savePetHitpoints(owner, hp)     
     
-    petEffect.nextCalled = 1
+    petEffect.nextCalled = 2
     return true
 end
 
