845c5c58f8d527b78542c9bf6f28e44089371436
Slightly <slightlyc@gmail.com>
2020-11-08 04:02:38 +0000

    Close #215 Adapt quest logs 604 - 606 to NPC change

--- quest/viola_baywillow_604.lua
+++ quest/viola_baywillow_604.lua
@@ -14,7 +14,7 @@ details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>. 
 ]]
--- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (605, 'quest.hummi_olaficht_605');
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (604, 'quest.viola_baywillow_604');
 
 local common = require("base.common")
 local M = {}
@@ -24,30 +24,30 @@ local ENGLISH = Player.english
 
 -- Insert the quest title here, in both languages
 local Title = {}
-Title[GERMAN] = "Finde Frizza in Cadomyr"
-Title[ENGLISH] = "Find Frizza in Cadomyr"
+Title[GERMAN] = "Finde Elesil Daelwon in Runewick"
+Title[ENGLISH] = "Find Elesil Daelwon in Runewick"
 
 -- Insert an extensive description of each status here, in both languages
 -- Make sure that the player knows exactly where to go and what to do
 local Description = {}
 Description[GERMAN] = {}
 Description[ENGLISH] = {}
-Description[GERMAN][1] = "Finde Frizza beim Marktplatz in Cadomyr and sprich mit ihr."
-Description[ENGLISH][1] = "Find Frizza at the market in Cadomyr and talk to her."
-Description[GERMAN][2] = "Da kannst nun mit Frizza sprechen. Frage nach 'Hilfe' wenn du nicht weißt, wonach du fragen sollst!\nDu kannst auch zurück zu Hummi gehen, um deine Belohnung abzuholen und später nochmals vorbei kommen. Frizza hat nämlich auch mindestens eine Aufgabe für dich."
-Description[ENGLISH][2] = "You can talk with Frizza now. Ask for 'help' if you do not know what to say!\nYou can also go back to Hummi to collect your reward and come back later. Frizza also has at least one task for you."
-Description[GERMAN][3] = "Hast du bereits nach den beiden anderen NPCs die Hummi erwähnt hat gefragt und sie auch besucht? Elesil and Iradona? Wenn nein, dann solltest du das jetzt tun. Wenn ja, dann hat Hummi einen zusätzlichen Auftrag für dich."
-Description[ENGLISH][3] = "Have you already asked for and visited the two other NPCs, Elesil and Iradona, that Hummi mentions? If not, you should do it now. If you have already visited them, Hummi has an additional task for you."
+Description[GERMAN][1] = "Finde Elesil Daelwon direkt hinter der BrÃ¼cke, die nach Runewick fÃ¼hrt, und rede mit ihr."
+Description[ENGLISH][1] = "Find Elesil Daelwon immediately as you cross the bridge into Runewick and talk to her."
+Description[GERMAN][2] = "Rede mit Elesil Daelwon, wenn du mehr Ã¼ber Runewick erfahren mÃ¶chtest. Bitte einfach um â€šHilfeâ€˜, wenn du nicht weiÃŸt, was du sagen sollst. \nDu kannst auch zu Viola Baywillow in Trollshaven zurÃ¼ckkehren, um deine Belohnung abzuholen."
+Description[ENGLISH][2] = "Talk to Elesil any time you would like to learn more about Runewick. Just ask for 'help' if you do not know what to say.\nYou can also return to Viola Baywillow in Troll's Haven to collect your reward."
+Description[GERMAN][3] = "Hast du schon Frizza und Iradona, die Informanten der anderen beiden Reiche besucht? Falls nicht, solltest du Viola Baywillow in Trollshaven fragen, wo sie zu finden sind, damit du sie aufsuchen kannst, um mehr Ã¼ber Cadomyr und Galmair zu erfahren."
+Description[ENGLISH][3] = "Have you already visited the other two Realm Informants, Frizza and Iradona? If not, you should find out where they are from Viola Baywillow in Troll's Haven so that you can visit them to learn all about Cadomyr and Galmair."
 
 
 -- Insert the position of the quest start here (probably the position of an NPC or item)
-local Start = {681, 311, 0}
-
+local Start = {693, 292, 0}
+ 
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
 local QuestTarget = {}
-QuestTarget[1] = {position(117, 599, 0)} -- Frizza
-QuestTarget[2] = {position(681, 311, 0)} -- Hummi
+QuestTarget[1] = {position(894, 817, 0)} -- Elesil
+QuestTarget[2] = {position(693, 292, 0)} -- Viola
 
 -- Insert the quest status which is reached at the end of the quest
 local FINAL_QUEST_STATUS = 3
--- quest/viola_baywillow_606.lua
+++ quest/viola_baywillow_606.lua
@@ -14,7 +14,7 @@ details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>. 
 ]]
--- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (604, 'quest.hummi_olaficht_604');
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (606, 'quest.viola_baywillow_606');
 
 local common = require("base.common")
 local M = {}
@@ -24,30 +24,30 @@ local ENGLISH = Player.english
 
 -- Insert the quest title here, in both languages
 local Title = {}
-Title[GERMAN] = "Finde Elesil Daelwon in Runewick"
-Title[ENGLISH] = "Find Elesil Daelwon in Runewick"
+Title[GERMAN] = "Finde Iradona Goldschein in Galmair"
+Title[ENGLISH] = "Find Iradona Goldschein in Galmair"
 
 -- Insert an extensive description of each status here, in both languages
 -- Make sure that the player knows exactly where to go and what to do
 local Description = {}
 Description[GERMAN] = {}
 Description[ENGLISH] = {}
-Description[GERMAN][1] = "Finde Elesil Daelwon an der Lunordbrück in Runewick and sprich mit ihr."
-Description[ENGLISH][1] = "Find Elesil Daelwon at the Lunord Bridge in Runewick and talk to her."
-Description[GERMAN][2] = "Da kannst nun mit Elesil sprechen. Frage nach 'Hilfe' wenn du nicht weißt, wonach du fragen sollst!\nDu kannst auch zurück zu Hummi gehen, um deine Belohnung abzuholen und später nochmals vorbei kommen. Elesil hat nämlich auch mindestens eine Aufgabe für dich."
-Description[ENGLISH][2] = "You can talk with Elesil now. Ask for 'help' if you do not know what to say!\nYou can also go back to Hummi to collect your reward and come back later. Elesil also has at least one task for you."
-Description[GERMAN][3] = "Hast du bereits nach den beiden anderen NPCs die Hummi erwähnt hat gefragt und sie auch besucht? Frizza and Iradona? Wenn nein, dann solltest du das jetzt tun. Wenn ja, dann hat Hummi einen zusätzlichen Auftrag für dich."
-Description[ENGLISH][3] = "Have you already asked for and visited the two other NPCs, Frizza and Iradona, that Hummi mentions? If not, you should do it now. If you have already visited them, Hummi has an additional task for you."
+Description[GERMAN][1] = "Finde Iradona Goldschein An den Toren der Krone, wenn du über den Markplatz schaust, und rede mit ihr."
+Description[ENGLISH][1] = "Find Iradona Goldschein at the gates to the Crest, looking over the market place in Galmair and talk to her."
+Description[GERMAN][2] = "Rede mit Iradona, wenn du mehr über Galmair erfahren möchtest. Bitte einfach um ‚Hilfe‘, wenn du nicht weißt, was du sagen sollst. \nDu kannst auch zu Viola Baywillow in Trollshaven zurückkehren, um deine Belohnung abzuholen."
+Description[ENGLISH][2] = "Talk to Iradona any time you would like to learn more about Galmair. Just ask for 'help' if you do not know what to say.\nYou can also return to Viola Baywillow in Troll's Haven to collect your reward."
+Description[GERMAN][3] = "Hast du schon Frizza und Elesil, die Informanten der anderen beiden Reiche besucht? Falls nicht, solltest du Viola Baywillow in Trollshaven fragen, wo sie zu finden sind, damit du sie aufsuchen kannst, um mehr über Cadomyr und Runewick zu erfahren."
+Description[ENGLISH][3] = "Have you already visited the other two Realm Informants, Frizza and Elesil? If not, you should find out where they are from Viola Baywillow in Troll's Haven so that you can visit them to learn all about Cadomyr and Runewick."
 
 
 -- Insert the position of the quest start here (probably the position of an NPC or item)
-local Start = {681, 311, 0}
+local Start = {693, 292, 0}
 
 
 -- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
 local QuestTarget = {}
-QuestTarget[1] = {position(894, 817, 0)} -- Elesil
-QuestTarget[2] = {position(681, 311, 0)} -- Hummi
+QuestTarget[1] = {position(369, 256, 0)} -- Iradona
+QuestTarget[2] = {position(693, 292, 0)} -- Viola
 
 -- Insert the quest status which is reached at the end of the quest
 local FINAL_QUEST_STATUS = 3
