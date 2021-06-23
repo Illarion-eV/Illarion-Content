8d0f4f4d93c7fba3dc39e89b79fa11b718cc2026
estralis <estralis@illarion.org>
2021-06-23 11:39:35 +0200

    Implement Nimbur's triggerfield for new tutorial

--- triggerfield/noobia_nimbur.lua
+++ triggerfield/noobia_nimbur.lua
@@ -14,10 +14,6 @@ details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
--- INSERT INTO triggerfields VALUES (41,74,100,'triggerfield.noobia_nimbur');
--- INSERT INTO triggerfields VALUES (40,74,100,'triggerfield.noobia_nimbur');
--- INSERT INTO triggerfields VALUES (39,74,100,'triggerfield.noobia_nimbur');
--- INSERT INTO triggerfields VALUES (38,74,100,'triggerfield.noobia_nimbur');
 
 local common = require("base.common")
 
@@ -25,20 +21,15 @@ local M = {}
 
 function M.MoveToField(Character)
 
-    -- for Noobia: the char has to walk to a field (this triggerfield); he gets a message and we change a queststatus so that we remember he was at the field
-    local find = Character.effects:find(13) --Noob effect
-    if find then --Is this even a noob?
-        local value = Character:getQuestProgress(313)
-        if (value == 0) then --Didn't visit the triggerfield yet
-            Character:setQuestProgress(313, 1) --player passed the triggerfield
-
-            local callbackNewbie = function() end --empty callback
-            local dialogText = common.GetNLS(Character,
-                "An dieser Station wird dir beigebracht, wie man einem Handwerk nachgeht. Nimbur Goldhand ist ein sehr begabter Artisan, der die nötigen Kenntnisse vermittelt, die du benötigst, um die Laufbahn des Handwerkers einzuschlagen. Handwerke erfordern ebenso wie das Sammeln von Ressourcen ein Handwerkzeug und einen Arbeitsplatz sowie die nötigen Rohstoffe.\n\nMassenproduktion wird dir nicht dabei helfen, ein Handwerk zu meistern, denn du wirst immer weniger lernen, je mehr Handlungen du ohne Rast durchführst. Du kannst deine Zeit also auch mit anderen Aktivitäten im Spiel verbringen, ohne davon einen Nachteil zu haben.",
-                "At this station, you will learn how to craft items. Nimbur Goldhand is a very skilled artisan who can teach you the necessary steps to get started with a crafting profession.\n\nCrafting requires a hand tool, just like gathering, and a static tool. You also need the necessary resources.\n\nYou don't need mass production to master a craft as you will learn less and less the more actions you do over time. It is just as beneficial to spend a greater proportion of your time involved in other game activities.")
-            local dialogNewbie = MessageDialog("Tutorial", dialogText, callbackNewbie)
-            Character:requestMessageDialog(dialogNewbie)
-        end
+    if Character:getQuestProgress(325) == 1 and Character:getQuestProgress(331) == 0 then --Accepted tutorial messages, didn't visit the triggerfield yet
+        Character:setQuestProgress(331, 1) --remember that the triggerfield was visited
+        Character:inform("Ein beleibter Zwerg mit einem Becher in der Hand probiert vergnügt sein neuestes Gebräu. Erfreut genug um einen kleinen Schluck zu teilen, können aber die Hände, welche nach seinem geheimen Rezepten trachten, einen ordentlichen Hieb auf die Knöchel mit dem oft gebrauchten Löffel erwarten.","A rotund dwarf with mug in hand gleefully samples his latest brew. Pleased enough to share a tipple, any hands straying to his secret recipes can expect a sharp rap across the knuckles from his well used spoon.")
+        local callbackNewbie = function() end --empty callback
+        local dialogText = common.GetNLS(Character,
+            "Illarion hat ein komplexes Handwerkssystem, das auf dem Sammeln und Veredeln von Rohstoffen basiert, welche bei der Herstellung zahlreicher Gegenstände verwendet werden, die in verschiedenen Skill basierten Handwerken zugeordnet sind. Jedes Reich ist auf verschiedene Handwerke ausgerichtet, was bedeutet, dass alle dafür notwendigen Werkzeuge zur Verfügung stehen, Ressourcen reichlich vorhanden sind und der Warenhandel zu höheren Preisen erfolgt.\n\nBefolge die Ratschläge von Nimbur Goldbrew, um zu lernen, wie du Ressourcen sammeln und Gegenstände herstellen kannst. Vielleicht findest du auch noch heraus, wie du dein erstes Monster tötest!",
+            "Illarion has a complex crafting system based on gathering and refining raw materials which are used in the production of numerous items distributed across distinct skill-based crafts. Each realm has their speciality crafts, meaning all necessary tools are available for use, resources are more abundant, and goods trade at higher prices.\n\nFollow Nimbur Goldbrew's advice to learn how to gather resources and craft items. You may even find out how to kill your first monster!")
+        local dialogNewbie = MessageDialog("Tutorial", dialogText, callbackNewbie)
+        Character:requestMessageDialog(dialogNewbie)
     end
 end
 
