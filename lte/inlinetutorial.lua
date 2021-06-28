0a707f29d8aaccb3922938a46f9218be176d64ef
estralis <estralis@illarion.org>
2021-06-28 17:09:51 +0200

    Close #266 Add LTE trigger "Orchard rat" for new tutorial

--- lte/inlinetutorial.lua
+++ lte/inlinetutorial.lua
@@ -58,12 +58,32 @@ function M.callEffect(inlineTutorial,Character)
             end
         end
 
+        --Orchard rat
+        if Character:getQuestProgress(335) == 0 and areas.PointInArea(Character.pos,"trollshaven") then
+            local monsters = world:getMonstersInRangeOf(Character.pos, 5)
+            if (#monsters > 0) then
+                for i, monster in pairs(monsters) do
+                    if monster:getMonsterType() == 1111 then
+                        Character:setQuestProgress(335, 1) --remember that the message was received
+                        local callbackOrchard = function(informOrchard)
+                            Character:inform("Eine kleine braune Ratte verlässt einen angenagten Apfel und springt vom Baum, beißt und kratzt dich.","A small brown rat abandons a partially gnawed apple and jumps from the tree, biting and clawing at you.")
+                        end --end callback
+                        local dialogText = common.GetNLS(Character,"Um anzugreifen, klickst du mit der rechten Maustaste auf das Monster oder drücke die Taste 'F', um das nächste Monster anzugreifen. Ein roter Ring markiert jedes Monster oder jeden Charakter, mit dem du dich im Kampf befindest. Sobald ein Monster getötet wurde, können fallengelassene Gegenstände gesammelt werden. Dazu kannst du die Taste 'P' drücken oder die Gegenstände mit der Maus in einen Inventarplatz ziehen. Deine Gesundheit wird durch den roten Balken in der unteren rechten Ecke des Fensters angezeigt. Wenn dein Charakter getötet wird, können drei Gegenstände aus deiner Tasche fallen, die Gegenstände die du an dir trägst oder am Gürtel sind aber nicht gefährdet.","To attack, right click on the monster or press 'F' to attack the nearest monster. A red ring highlights any monster or character you are engaged in battle with. Once a monster has been killed items dropped can be collected by pressing 'P' or dragging them into an inventory slot using the mouse. Health is indicated by the red bar in the bottom right corner of the window. If your character is killed, three items may drop from your bag, but those worn or on the belt are not at risk.")
+                        local dialogOrchard = MessageDialog("Tutorial", dialogText, callbackOrchard)
+                        Character:requestMessageDialog(dialogOrchard)
+                        break
+                    end
+                end
+            end
+        end
+
         --First encounter
         if Character:getQuestProgress(329) == 0 and not areas.PointInArea(Character.pos,"trollshaven") then --Not near the noob spawn to avoid spamming the player
             local dudes = world:getPlayersInRangeOf(Character.pos, 5)
             if (#dudes > 1) then
-                Character:inform("[Tutorial] Du bemerkst eine Gestalt in der Ferne, die ihren Geschäften nachgeht. Da du erst vor kurzem angekommen bist überrascht es nicht, dass du du die Person auch beim Näherkommen nicht erkennen wirst.","[Tutorial] You notice a figure in the distance going about their business. Having only recently arrive it can come as no surprise you do not recognise them as the two of you draw closer.")
-                local callbackDudes = function() end --empty callback
+                local callbackDudes = function(informDudes)
+                    Character:inform("[Tutorial] Du bemerkst eine Gestalt in der Ferne, die ihren Geschäften nachgeht. Da du erst vor kurzem angekommen bist überrascht es nicht, dass du du die Person auch beim Näherkommen nicht erkennen wirst.","[Tutorial] You notice a figure in the distance going about their business. Having only recently arrive it can come as no surprise you do not recognise them as the two of you draw closer.")
+                end --end callback
                 local dialogText = common.GetNLS(Character,"Vielleicht möchtest du dich einem anderen Charakter vorstellen oder anderen durch dein Rollenspiel eine Aktion vermitteln. Zusätzlich zu normalen Sprechen kann dein Charakter auch schreien oder flüstern indem du dem Text #s bzw. #w voranstellst. Mit #me rufst ein Emote auf, mit dem du das Erscheinungsbild deines Charakters vermitteln oder eine Handlung deines Charakters beschreiben kannst. Mit #o werden Nachrichten außerhalb deiner Charakterrolle verwendet. Diese sollten jedoch auf ein Minimum beschränkt bleiben, um die Spielatmosphäre nicht zu stören. Wenn sich dein Charakter innerhalb von zwei Feldern einem anderen vorgestellt hat, kannst du #i verwenden, damit der andere den Namen deines Charakters sich merken und dich in Zukunft wieder erkennen kann.","You may want to introduce yourself to another character, or perhaps convey some other action through your role play. In addition to talking normally, your character can shout by prefixing your text with #s or whisper (within two tiles) by prefixing with #w. #me invokes an emote, allowing you to convey the appearance or describe an action of your character. #o is used for out of character messages, but these should be kept to a minimum to avoid breaking the immersion. Once you have introduced your character to another within two tiles, #i  can be used to display your character's name so they will recognise you in future.")
                 local dialogTitle = common.GetNLS(Character,"Tutorial","Tutorial")
                 local dialogDudes = MessageDialog(dialogTitle, dialogText, callbackDudes)
