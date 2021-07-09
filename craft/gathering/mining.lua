3f54f94ab06d846a0c27c55021c6ca1c8bc46c5a
estralis <estralis@illarion.org>
2021-07-09 11:45:14 +0200

    Close #282 Remove Noobia mine

--- craft/gathering/mining.lua
+++ craft/gathering/mining.lua
@@ -209,40 +209,6 @@ SetResource( 6, 1276,  2536, 60); -- copper ore
 SetResource( 6, 1276,  256, 9); -- emerald
 SetResource( 6, 1276,  253, 3); -- sapphire
 
------------ Noobia Mine (increased coal yield) -----------
-AddArea( 7, position(59,49,100), 15 );
-AddStone( 7, 232 );
-SetResource( 7, 232, 2536, 40); -- copper
-SetResource( 7, 232, 234,  5); -- gold nuggets
-SetResource( 7, 232, 254,  3); -- diamonds
-SetResource( 7, 232, 251,  1); -- amethysts
-AddStone( 7, 914 );
-SetResource( 7, 914,  22, 60); -- iron ore
-SetResource( 7, 914,  255, 3); -- rubys
-SetResource( 7, 914,  257, 1); -- topas
-AddStone( 7, 1273 );
-SetResource( 7, 1273,  22, 60); -- iron ore
-SetResource( 7, 1273,  253, 3); -- sapphire
-SetResource( 7, 1273,  252, 1); -- obsidian
-AddStone( 7, 1245 );
-SetResource( 7, 1245,  21, 80); -- coal
-SetResource( 7, 1245,  252, 3); -- obsidian
-SetResource( 7, 1245,  255, 1); -- rubys
-AddStone( 7, 1246 );
-SetResource( 7, 1246, 21,  40); -- coal
-SetResource( 7, 1246, 251,  3); -- amethysts
-SetResource( 7, 1246,  256,  1); -- emerald
-AddStone( 7, 1276 );
-SetResource( 7, 1276,  2536, 40); -- copper ore
-SetResource( 7, 1276,  256, 3); -- emerald
-SetResource( 7, 1276,  253, 1); -- sapphire
-AddStone( 7, 1250 );
-SetResource( 7, 1250, 234,  10); -- gold nuggets
-SetResource( 7, 1250, 2534,  1); -- merinium ore
-SetResource( 7, 1250,  254,  3); -- diamonds
-SetResource( 7, 1250,  257,  1); -- topas
-------- Noobia Mine - FERTIG ------
-
 ----------- Prison Mine -----------
 AddArea( 8, position(-480,-480,-40), 30 );
 AddStone( 8, 232 );
