c2bfdbfa7f15ed4caffa80791058ac9824d189b2
brightrim <awildbrightrim@gmail.com>
2021-11-29 16:53:30 +0100

    Fix bug where estate items would sometimes not be listed for estates v2

--- craft/final/construction.lua
+++ craft/final/construction.lua
@@ -22,7 +22,7 @@ local M = {}
 local product
 local catId
 
-local function addIngredients(User, item, tile)
+local function addIngredients(user, item, tile)
     if item then
         for i = 1,4 do
             if item["ingredient"..i] then
@@ -41,50 +41,50 @@ local function addIngredients(User, item, tile)
     end
 end
 
-local function addProducts(craft, category, User, item, tile)
+local function addProducts(craft, category, user, item, tile)
     if item then
         if item.category == category.categoryEn and item.skill == craft then
             if item.typeOf == "Estate" then
-                if propertyList.checkIfEstate(User) then
+                if propertyList.checkIfEstate(user) then
                     product = M[tostring(category)..craft]:addProduct(catId, item.itemId, 1, {}, true)
-                    addIngredients(User, item, nil)
+                    addIngredients(user, item, nil)
                 end
             else
                 product = M[tostring(category)..craft]:addProduct(catId, item.itemId, 1, {}, true)
-                addIngredients(User, item, nil)
+                addIngredients(user, item, nil)
             end
         end
     elseif tile then
         if tile.category == category.categoryEn and tile.skill == craft then
             if tile.typeOf == "Estate" then
-                if propertyList.checkIfEstate(User) then
+                if propertyList.checkIfEstate(user) then
                     product = M[tostring(category)..craft]:addProduct(catId, tile.tileId, 1, {}, true, true)
-                    addIngredients(User, nil, tile)
+                    addIngredients(user, nil, tile)
                 end
             else
                 product = M[tostring(category)..craft]:addProduct(catId, tile.tileId, 1, {}, true, true)
-                addIngredients(User, nil, tile)
+                addIngredients(user, nil, tile)
             end
         end
     end
 end
 
-local function addItems(cat, craft, isTile, User)
+local function addItems(cat, craft, isTile, user)
     for _, category in ipairs(itemList.categories) do
         if category.categoryEn == cat and not isTile then
             catId = M[tostring(category)..craft]:addCategory(category.categoryEn, category.categoryDe)
             for _, item in ipairs(itemList.items) do
-                addProducts(craft, category, User, item, nil)
+                addProducts(craft, category, user, item, nil)
             end
         elseif category.categoryEn == cat then
             catId = M[tostring(category)..craft]:addCategory(category.categoryEn, category.categoryDe)
             for _, tile in ipairs(itemList.tiles) do
-                addProducts(craft, category, User, nil, tile)
+                addProducts(craft, category, user, nil, tile)
             end
         end
     end
 end
-local function createCategory(cat, craft, sfxNumber, sfxDur, isTile, User)
+local function createCategory(cat, craft, sfxNumber, sfxDur, isTile, user)
 local checkIfNoSkill = true
 local skill = 0
     if craft ~= "misc" then
@@ -104,13 +104,31 @@ local skill = 0
                             houseCraft = checkIfNoSkill,
                             housing = true
                             }
-            addItems(cat, craft, isTile, User)
+            addItems(cat, craft, isTile, user)
         end
     end
 end
-function M.makeAllCategories(User)
-product = nil --resets top variable for product. This fixes the bug where estate items would not show if you access a house builder menu first.
+
+local function clearPreviousBuild()
+    for _, item in ipairs(itemList.items) do
+        for _, category in ipairs(itemList.categories) do
+            if M[tostring(category)..item.skill] then
+                M[tostring(category)..item.skill] = nil
+            end
+        end
+    end
+    for _, tile in ipairs(itemList.tiles) do
+        for _, category in ipairs(itemList.categories) do
+            if M[tostring(category)..tile.skill] then
+                M[tostring(category)..tile.skill] = nil
+            end
+        end
+    end
+end
+
+function M.makeAllCategories(user)
 local sfx
+    clearPreviousBuild() --Fixes bug where estate items would not show if non estate build menu was accessed first.
     for _, item in ipairs(itemList.items) do
         for _, category in ipairs(itemList.categories) do
             if item.category == category.categoryEn then
@@ -120,7 +138,7 @@ local sfx
                             sfx = skill.sfx
                         end
                     end
-                    createCategory(item.category, item.skill, sfx, 25, false, User)
+                    createCategory(item.category, item.skill, sfx, 25, false, user)
                 end
             end
         end
@@ -134,7 +152,7 @@ local sfx
                             sfx = skill.sfx
                         end
                     end
-                    createCategory(tile.category, tile.skill, sfx, 25, true, User)
+                    createCategory(tile.category, tile.skill, sfx, 25, true, user)
                 end
             end
         end
