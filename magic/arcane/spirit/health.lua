787fe36f19cba0c60e4b0fe1d274e1979b64b593
brightrim <awildbrightrim@gmail.com>
2021-12-26 22:07:51 +0100

    PEN Sih

---
+++ magic/arcane/spirit/health.lua
@@ -0,0 +1,62 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+local runes = require("magic.arcane.runes")
+
+local M = {}
+
+M.healthTexts = {
+{english = "Target is perfectly healthy.", german = "", health = 10000},
+{english = "Target is slightly wounded.", german = "", health = 8000},
+{english = "Target is wounded.", german = "", health = 5000},
+{english = "Target is heavily wounded.", german = "", health = 2000},
+{english = "Target is near death.", german = "", health = 1},
+{english = "Target is dead.", german = "", health = 0},
+}
+
+function M.getHealthText(information, spell)
+    local Sih = runes.checkSpellForRuneByName("Sih", spell)
+
+    if not Sih then
+        return information
+    end
+
+    for _, target in pairs(information) do
+        local player = target.type == "player"
+        local creature = target.type == "creature"
+        if player or creature then
+            local health = target.target:increaseAttrib("hitpoints", 0)
+            local englishText
+            local germanText
+            for _, heal in ipairs(M.healthTexts) do
+                if health >= heal.health then
+                    germanText = heal.german
+                    englishText= heal.english
+                    break
+                end
+            end
+            target.HP = {}
+            target.HP.english = englishText
+            target.HP.german = germanText
+        end
+    end
+return information
+end
+
+
+
+return M
