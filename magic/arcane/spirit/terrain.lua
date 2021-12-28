99440004645cce4ae19d547db6a4eccf48eb90f2
brightrim <awildbrightrim@gmail.com>
2021-12-28 20:25:26 +0100

    fix faulty module pathing

--- magic/arcane/spirit/terrain.lua
+++ magic/arcane/spirit/terrain.lua
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local texts = require("magic.arcane.base.texts.lua")
+local texts = require("magic.arcane.base.texts")
 
 local M = {}
 
