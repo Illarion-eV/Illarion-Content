a4baadd558230721f48bfaef1052cb39747c5cde
brightrim <awildbrightrim@gmail.com>
2022-02-18 12:33:06 +0100

    Improved rent collection to survive server reloads

--- scheduled/rent.lua
+++ scheduled/rent.lua
@@ -18,25 +18,31 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 local notice = require("item.notice")
 local M = {}
 
-local collected = false
+local lastCollected = false
 
 function M.rent()
-    --Read date and time
+
     local month = world:getTime("month")
     local day = world:getTime("day")
-    local hour = world:getTime("hour")
+
     --Rent duration is reduced on the first day of each month except for Mas
     if (month == 16) or (day > 1) then
         return
-    -- Rent duration is reduced at 00:00
-    elseif (hour == 0) and not collected then
+    end
+
+    --the scriptVar gets saved as a top level variable so the database only has to get accessed once any time top level variables are reset
+    if not lastCollected then
+        local found, result = ScriptVars:find("lastMonthRentWasCalled")
+        if found then
+            lastCollected = result
+        end
+    end
+
+    if not lastCollected or lastCollected ~= month then
         notice.reduceRentTimer()
-        collected = true
-        return
-    -- To ensure that it only reduces once, it only goes back to being possible to collect rent at 01:00 meaning it won't happen again for a month
-    elseif (hour == 1) and collected then
-        notice.removeRentalIfDurationIsUp()
-        collected = false
+        lastCollected = month
+        ScriptVars:set("lastMonthRentWasCalled", month)
+        ScriptVars:save()
         return
     end
 end
