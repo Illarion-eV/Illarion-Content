7fe608f0d51242d38bbe713a3700b61dd5e48135
brightrim <awildbrightrim@gmail.com>
2022-09-05 13:10:52 +0200

    change messenger price to match bulletin

--- content/messenger.lua
+++ content/messenger.lua
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local price = 5000 -- fifty silver
+local price = 2000 -- twenty silver, same as bulletins
 
 local common = require("base.common")
 local money = require("base.money")
