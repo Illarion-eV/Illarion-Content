17e2926d263890d015e77ec9217557575b17c584
brightrim <awildbrightrim@gmail.com>
2023-03-06 19:51:49 +0100

    lowering of messenger cost to five silver as per approved discord proposal
    (cherry picked from commit 29488983c960ab8329a925d30fdcd6a729753a4c)
    (cherry picked from commit 033483c8b8b0dec69f92c1e544a9af05398ae04e)

--- content/messenger.lua
+++ content/messenger.lua
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local price = 2000 -- twenty silver, same as bulletins
+local price = 500 -- five silver
 
 local common = require("base.common")
 local money = require("base.money")
