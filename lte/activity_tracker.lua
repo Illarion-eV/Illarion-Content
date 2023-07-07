0a9b07671c1ebf8417dd486c59298ec8930f597b
brightrim <awildbrightrim@gmail.com>
2023-07-07 15:27:06 +0200

    Add missing number in comment
    (cherry picked from commit 85a3d0129c36860bcfa4ba1c7452724965416cdc)
    (cherry picked from commit 3c1ae09e783f898205b093848bf26f478921e838)

--- lte/activity_tracker.lua
+++ lte/activity_tracker.lua
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
--- Activity Tracker, effect id #
+-- Activity Tracker, effect id 84
 
 local common = require("base.common")
 
