b9981c4a9014f759161bc68c8b1dc51d99c512c9
brightrim <awildbrightrim@gmail.com>
2025-01-15 19:44:51 +0100

    removed findRandomItem from sowing as there are no random items attributed to it to find, leading to a bug in how the Nomizo glyph works and the script was doing nothing anyways with no items listed to find. It is likely it has none due to how it would be double dipping as harvesting what you sow does have them

--- craft/gathering/sowing.lua
+++ craft/gathering/sowing.lua
@@ -121,8 +121,6 @@ function M.StartGathering(User, SourceItem, ltstate)
 
     -- since we're here, we're working
 
-    sowing:FindRandomItem(User, nil)
-
     User:learn( sowing.LeadSkill, sowing.SavedWorkTime[User.id], sowing.LearnLimit);
     -- you always get at least one
   local amount = 1;
